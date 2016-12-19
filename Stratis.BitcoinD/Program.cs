69d68c7431bddea6f307dfd61fab533455bef424
NicolasDorier <nicolas.dorier@gmail.com>
2016-12-19 17:53:06 +0900

    parsing command line bitcoind

--- Stratis.BitcoinD/Program.cs
+++ Stratis.BitcoinD/Program.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging.Console;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Stratis.BitcoinD
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-        }
-    }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Logs.Configure(new FuncLoggerFactory(n => new ConsoleLogger(n, (a, b) => true, false)));
+			var dataDir = args.Where(a => a.StartsWith("-datadir=")).Select(a => a.Substring("-datadir=".Length).Replace("\"", "")).FirstOrDefault();
+			NodeArgs nodeArgs = NodeArgs.GetArgs(dataDir, args);
+		}
+	}
 }
