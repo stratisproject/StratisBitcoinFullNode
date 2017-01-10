f0bb4bc5d48b3f0458fd3c1098e9011f125af3ed
Dan Gershony <dan.gershony@gmail.com>
2017-01-10 13:41:15 +0000

    Moving BlockStore logic inside the behavior

--- Stratis.Bitcoin/BlockStore/BlockStoreBehaviour.cs
+++ Stratis.Bitcoin/BlockStore/BlockStoreBehaviour.cs
@@ -6,27 +6,30 @@ using System.Threading.Tasks;
 using NBitcoin;
 using NBitcoin.Protocol;
 using NBitcoin.Protocol.Behaviors;
+using Stratis.Bitcoin.Consensus;
 
 namespace Stratis.Bitcoin.BlockStore
 {
 	public class BlockStoreBehaviour : NodeBehavior
 	{
-		private readonly BlockStore blockStore;
+		private readonly ConcurrentChain concurrentChain;
+		private readonly BlockRepository blockRepository;
 
 		public bool CanRespondToGetBlocksPayload { get; set; }
 
 		public bool CanRespondeToGetDataPayload { get; set; }
 
-		public BlockStoreBehaviour(BlockStore blockStore)
+		public BlockStoreBehaviour(ConcurrentChain concurrentChain, BlockRepository blockRepository)
 		{
-			this.blockStore = blockStore;
-			
+			this.concurrentChain = concurrentChain;
+			this.blockRepository = blockRepository;
+
 			this.CanRespondToGetBlocksPayload = false;
 			this.CanRespondeToGetDataPayload = true;
 		}
 
 		protected override void AttachCore()
-		{
+		{	
 			this.AttachedNode.MessageReceived += AttachedNode_MessageReceived;
 		}
 
@@ -35,18 +38,55 @@ namespace Stratis.Bitcoin.BlockStore
 			this.AttachedNode.MessageReceived -= AttachedNode_MessageReceived;
 		}
 
-		private void AttachedNode_MessageReceived(Node node, IncomingMessage message)
+		private async void AttachedNode_MessageReceived(Node node, IncomingMessage message)
+		{
+			await this.AttachedNode_MessageReceivedAsync(node, message);
+		}
+
+		private async Task AttachedNode_MessageReceivedAsync(Node node, IncomingMessage message)
 		{
 			if (this.CanRespondeToGetDataPayload)
-				message.Message.IfPayloadIs<GetDataPayload>(data => this.blockStore.ProcessGetDataAsync(node, data));
+				await message.Message.IfPayloadIsAsync<GetDataPayload>(async data => await this.ProcessGetDataAsync(node, data));
 
 			if (this.CanRespondToGetBlocksPayload)
-				message.Message.IfPayloadIs<GetBlocksPayload>(data => this.blockStore.ProcessGetBlocksAsync(node, data));
+				await message.Message.IfPayloadIsAsync<GetBlocksPayload>(async data => await this.ProcessGetBlocksAsync(node, data));
+		}
+
+		private async Task ProcessGetDataAsync(Node node, GetDataPayload getDataPayload)
+		{
+			foreach (var item in getDataPayload.Inventory.Where(inv => inv.Type == InventoryType.MSG_BLOCK))
+			{
+				var block = await this.blockRepository.GetAsync(item.Hash);
+
+				if (block != null)
+					await node.SendMessageAsync(new BlockPayload(block));
+			}
+		}
+
+		private async Task ProcessGetBlocksAsync(Node node, GetBlocksPayload getBlocksPayload)
+		{
+			ChainedBlock chainedBlock = this.concurrentChain.FindFork(getBlocksPayload.BlockLocators);
+
+			if (chainedBlock != null)
+			{
+				var inv = new InvPayload();
+				for (var limit = 0; limit < 500; limit++)
+				{
+					chainedBlock = this.concurrentChain.GetBlock(chainedBlock.Height + 1);
+					if (chainedBlock.HashBlock == getBlocksPayload.HashStop)
+						break;
+
+					inv.Inventory.Add(new InventoryVector(InventoryType.MSG_BLOCK, chainedBlock.HashBlock));
+				}
+
+				if (inv.Inventory.Any())
+					await node.SendMessageAsync(inv);
+			}
 		}
 
 		public override object Clone()
 		{
-			return new BlockStoreBehaviour(this.blockStore)
+			return new BlockStoreBehaviour(this.concurrentChain, this.blockRepository)
 			{
 				CanRespondToGetBlocksPayload = this.CanRespondToGetBlocksPayload,
 				CanRespondeToGetDataPayload = this.CanRespondeToGetDataPayload
--- Stratis.Bitcoin/Consensus/Extensions.cs
+++ Stratis.Bitcoin/Consensus/Extensions.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NBitcoin.Protocol;
 
 namespace Stratis.Bitcoin.Consensus
 {
@@ -23,6 +24,14 @@ namespace Stratis.Bitcoin.Consensus
 				highest = highest.Previous;
 			}
 			return highest;
+		}
+
+		public static async Task<bool> IfPayloadIsAsync<TPayload>(this Message message, Func<TPayload, Task> action) where TPayload : Payload
+		{
+			TPayload payload = message.Payload as TPayload;
+			if (payload == null) return await Task.FromResult(false);
+			await action(payload);
+			return await Task.FromResult(true);
 		}
 	}
 }
