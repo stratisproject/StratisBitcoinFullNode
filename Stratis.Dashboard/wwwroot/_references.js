56326d39483ccec23eea9f83a0f027064620822d
MithrilMan <red.angel@libero.it>
2017-03-03 15:26:06 +0100

    removed typescript references and files

--- Stratis.Dashboard/wwwroot/_references.js
+++ Stratis.Dashboard/wwwroot/_references.js
@@ -1,5 +1,4 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="app/dist/app.js" />
 /// <reference path="js/custom.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/angular/angular.js" />
