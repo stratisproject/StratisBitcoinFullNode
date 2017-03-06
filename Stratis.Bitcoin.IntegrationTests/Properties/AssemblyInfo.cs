c2f84dfdb9003eaa8e574b2bac0e92843f0030ec
Neurosploit <hoekstraman@gmail.com>
2017-03-06 19:16:32 +0100

    Rename Tests project to IntegrationTests
    (cherry picked from commit db27697b6c0bece4a8b2916da0475a9257ce16d6)

--- Stratis.Bitcoin.IntegrationTests/Properties/AssemblyInfo.cs
+++ Stratis.Bitcoin.IntegrationTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Stratis.Bitcoin.Tests")]
+[assembly: AssemblyTitle("Stratis.Bitcoin.IntegrationTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Stratis.Bitcoin.Tests")]
+[assembly: AssemblyProduct("Stratis.Bitcoin.IntegrationTests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
