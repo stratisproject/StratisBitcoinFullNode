23667a20bb7274bdd221d3ebe347facba650d0d8
Neurosploit <hoekstraman@gmail.com>
2017-03-07 19:54:28 +0100

    Unit tests for the signaler, signalobserver and signals classes.

---
+++ Stratis.Bitcoin.Tests/SignalObserverTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Internal;
+using Moq;
+using NBitcoin;
+using Stratis.Bitcoin;
+using Stratis.Bitcoin.Logging;
+using Xunit;
+
+namespace Stratis.Bitcoin.Tests
+{
+    public class SignalObserverTest
+    {
+        SignalObserver<Block> observer;
+        Mock<ILoggerFactory> loggerFactory;
+        Mock<Microsoft.Extensions.Logging.ILogger> fullNodeLogger;
+
+        public SignalObserverTest()
+        {
+            fullNodeLogger = new Mock<ILogger>();
+            loggerFactory = new Mock<ILoggerFactory>();
+            loggerFactory.Setup(l => l.CreateLogger(It.IsAny<string>()))
+               .Returns(new Mock<ILogger>().Object);
+            loggerFactory.Setup(l=> l.CreateLogger("FullNode"))
+               .Returns(fullNodeLogger.Object)
+               .Verifiable();
+            Logs.Configure(loggerFactory.Object);
+
+            observer = new TestBlockSignalObserver();
+        }
+
+        [Fact]
+        public void SignalObserverLogsSignalOnError()
+        {
+            var exception = new InvalidOperationException("This should not have occurred!");
+
+            observer.OnError(exception);
+
+            AssertLog(fullNodeLogger, LogLevel.Error, exception.ToString());
+        }
+       
+        private void AssertLog(Mock<ILogger> logger, LogLevel logLevel, string message)
+        {
+            logger.Verify(f => f.Log<Object>(logLevel,
+                It.IsAny<EventId>(),
+                It.Is<Object>(l => ((FormattedLogValues)l)[0].Value.ToString().EndsWith(message)),
+                null,
+                It.IsAny<Func<Object, Exception, string>>()));
+        }
+
+        private class TestBlockSignalObserver : SignalObserver<Block>
+        {
+            public TestBlockSignalObserver()
+            {
+            }
+
+            protected override void OnNextCore(Block value)
+            {
+
+            }
+        }
+    }
+}
