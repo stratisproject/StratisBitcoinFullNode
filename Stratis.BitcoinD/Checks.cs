498c82c4dad04b08b4550c3a8b350c782243bf10
Richard Anthony Hein <richard.hein@gmail.com>
2017-03-28 17:06:37 -0400

    Moved access checks into a separate class.

---
+++ Stratis.BitcoinD/Checks.cs
@@ -0,0 +1,26 @@
+using System;
+using Stratis.Bitcoin.Configuration;
+
+namespace Stratis.BitcoinD
+{
+    public class Checks
+    {
+        public static void VerifyAccess(NodeSettings nodeSettings) 
+        {
+            // Verify folders are accessible by attempting to create sub directories,
+            // and removing them after the test.  Need to test write access.
+            var dataFolder = new DataFolder(nodeSettings);
+            foreach(var path in new []{ dataFolder.BlockPath, dataFolder.ChainPath, dataFolder.CoinViewPath }) {
+                try 
+                {
+                    var subDir = new System.IO.DirectoryInfo(path).CreateSubdirectory(Guid.NewGuid().ToString());
+                    subDir.Delete();
+                }
+                catch(UnauthorizedAccessException)
+                {
+                    throw new UnauthorizedAccessException(string.Format("Access to the path '{0}' was denied.", path));
+                }
+            }
+        }
+    }
+}
--- Stratis.BitcoinD/Program.cs
+++ Stratis.BitcoinD/Program.cs
@@ -23,21 +23,8 @@ namespace Stratis.BitcoinD
 
 			try	
 			{
-				// Verify folders are accessible by attempting to create sub directories,
-				// and removing them after the test.
-				var dataFolder = new DataFolder(nodeSettings);
-				foreach(var path in new []{ dataFolder.BlockPath, dataFolder.ChainPath, dataFolder.CoinViewPath }) {
-					try 
-					{
-						var subDir = new System.IO.DirectoryInfo(path).CreateSubdirectory(Guid.NewGuid().ToString());
-						subDir.Delete();
-					}
-					catch(UnauthorizedAccessException)
-					{
-						throw new UnauthorizedAccessException(string.Format("Access to the path '{0}' was denied.", path));
-					}
-				}
-				
+				Checks.VerifyAccess(nodeSettings);
+							
 				var node = (FullNode) new FullNodeBuilder()
 					.UseNodeSettings(nodeSettings)
 					.UseBlockStore()
