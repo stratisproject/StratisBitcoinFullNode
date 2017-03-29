9f29140985a58f4dc375bd9e1b206d3388bfdcc3
Richard Anthony Hein <richard.hein@gmail.com>
2017-03-29 16:13:03 -0400

    Fix for issue #76: Daemon crashes if first ran as root, then as regular user on Linux (#88)
    * Create Checks.cs
    
    * Update Program.cs
    
    Added Checks.VerifyAccess to check for permissions on the data directories.

--- Stratis.BitcoinD/Program.cs
+++ Stratis.BitcoinD/Program.cs
@@ -23,30 +23,39 @@ namespace Stratis.BitcoinD
 		{
 			Logs.Configure(new LoggerFactory().AddConsole(LogLevel.Trace, false));
 			NodeSettings nodeSettings = NodeSettings.FromArguments(args);
-
-			var node = (FullNode)new FullNodeBuilder()
-				.UseNodeSettings(nodeSettings)
-				.UseConsensus()
-				.UseBlockStore()
-				.UseMempool()
-				.AddMining(args.Any(a => a.Contains("mine")))
-				.AddRPC()
-				.Build();
-
-			// == shut down thread ==
-			new Thread(() =>
+			
+			try 
 			{
-				Console.WriteLine("Press one key to stop");
-				Console.ReadLine();
+				Checks.VerifyAccess(nodeSettings);
+
+				var node = (FullNode)new FullNodeBuilder()
+					.UseNodeSettings(nodeSettings)
+					.UseConsensus()
+					.UseBlockStore()
+					.UseMempool()
+					.AddMining(args.Any(a => a.Contains("mine")))
+					.AddRPC()
+					.Build();
+
+				// == shut down thread ==
+				new Thread(() =>
+				{
+					Console.WriteLine("Press one key to stop");
+					Console.ReadLine();
+					node.Dispose();
+				})
+				{
+					IsBackground = true //so the process terminates
+				}.Start();
+
+				node.Start();
+				node.WaitDisposed();
 				node.Dispose();
-			})
+			}
+			catch(UnauthorizedAccessException ex) 
 			{
-				IsBackground = true //so the process terminates
-			}.Start();
-
-			node.Start();
-			node.WaitDisposed();
-			node.Dispose();
+				Logs.Configuration.LogCritical(ex.Message);
+			}
 		}
 	}
 }
