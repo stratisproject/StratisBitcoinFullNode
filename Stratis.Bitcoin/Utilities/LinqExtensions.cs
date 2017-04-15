82053bef2e9fc12d6afe945317d8f467a171a68b
Derrick- <derrick@alienseed.com>
2017-04-15 10:50:44 -0400

    Add LinqExtension to calculate median

---
+++ Stratis.Bitcoin/Utilities/LinqExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Linq
+{
+    public static class LinqExtensions
+    {
+        public static long Median(this IEnumerable<long> source)
+        {
+            long count = source.LongCount();
+            if (count == 0)
+                return 0;
+            int midpoint = source.Count() / 2;
+            IOrderedEnumerable<long> ordered = source.OrderBy(n => n);
+            if ((count % 2) == 0)
+                return (long)Math.Round(ordered.Skip(midpoint-1).Take(2).Average());
+            else
+                return ordered.ElementAt(midpoint);
+        }
+    }
+}
