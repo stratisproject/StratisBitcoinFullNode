46fe2787c0f094abb1d5cc3e872039c2cc76957f
Dan Gershony <dan.gershony@gmail.com>
2017-05-04 23:16:05 +0200

    Adding the stratis testnet parameters (#133)

--- Stratis.StratisD/Program.cs
+++ Stratis.StratisD/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -13,6 +14,7 @@ using Stratis.Bitcoin.Logging;
 using Stratis.Bitcoin.MemoryPool;
 using Stratis.Bitcoin.Miner;
 using Stratis.Bitcoin.RPC;
+using Stratis.Bitcoin.Utilities;
 
 namespace Stratis.StratisD
 {
@@ -27,7 +29,8 @@ namespace Stratis.StratisD
 			if (NodeSettings.PrintHelp(args, Network.StratisMain))
 				return;
 
-			NodeSettings nodeSettings = NodeSettings.FromArguments(args, "stratis", Network.StratisMain, ProtocolVersion.ALT_PROTOCOL_VERSION);
+			var network = args.Contains("-testnet") ? InitStratisTest() : Network.StratisMain;
+			NodeSettings nodeSettings = NodeSettings.FromArguments(args, "stratis", network, ProtocolVersion.ALT_PROTOCOL_VERSION);
 
 			// NOTES: running BTC and STRAT side by side is not possible yet as the flags for serialization are static
 
@@ -44,5 +47,50 @@ namespace Stratis.StratisD
 			Console.ReadLine();
 			node.Dispose();
 		}
+
+		private static Network InitStratisTest()
+		{
+			Block.BlockSignature = true;
+			Transaction.TimeStamp = true;
+			
+			var consensus = Network.StratisMain.Consensus.Clone();
+			consensus.PowLimit = new Target(uint256.Parse("0000ffff00000000000000000000000000000000000000000000000000000000"));
+			
+			// The message start string is designed to be unlikely to occur in normal data.
+			// The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+			// a large 4-byte int at any alignment.
+			var pchMessageStart = new byte[4];
+			pchMessageStart[0] = 0x71;
+			pchMessageStart[1] = 0x31;
+			pchMessageStart[2] = 0x21;
+			pchMessageStart[3] = 0x11;
+			var magic = BitConverter.ToUInt32(pchMessageStart, 0); //0x5223570; 
+
+			var genesis = Network.StratisMain.GetGenesis().Clone();
+			genesis.Header.Time = 1493909211;
+			genesis.Header.Nonce = 2433759;
+			genesis.Header.Bits = consensus.PowLimit;
+			consensus.HashGenesisBlock = genesis.GetHash();
+
+			Guard.Assert(consensus.HashGenesisBlock == uint256.Parse("0x00000e246d7b73b88c9ab55f2e5e94d9e22d471def3df5ea448f5576b1d156b9"));
+
+			var builder = new NetworkBuilder()
+				.SetName("StratisTest")
+				.SetConsensus(consensus)
+				.SetMagic(magic)
+				.SetGenesis(genesis)
+				.SetPort(26178)
+				.SetRPCPort(26174)
+				.SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, new byte[] {(65)})
+				.SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, new byte[] {(196)})
+				.SetBase58Bytes(Base58Type.SECRET_KEY, new byte[] {(65 + 128)})
+				.SetBase58Bytes(Base58Type.ENCRYPTED_SECRET_KEY_NO_EC, new byte[] {0x01, 0x42})
+				.SetBase58Bytes(Base58Type.ENCRYPTED_SECRET_KEY_EC, new byte[] {0x01, 0x43})
+				.SetBase58Bytes(Base58Type.EXT_PUBLIC_KEY, new byte[] {(0x04), (0x88), (0xB2), (0x1E)})
+				.SetBase58Bytes(Base58Type.EXT_SECRET_KEY, new byte[] {(0x04), (0x88), (0xAD), (0xE4)});
+
+			return builder.BuildAndRegister();
+		}
+
 	}
 }
