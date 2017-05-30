ca7ee88c62ef115b62bc292bff7aa5320f5ec37c
Jeremy Bokobza <bokobza@gmail.com>
2017-05-30 10:49:28 +0100

    Added base classes for integration with tumblebit. NStratis and NBitcoin are not playing nicely together so as a temporary measure I've brought all the NTumbleBit code as part of our solution and I've replaced the reference to NBitcoin to NStratis. A solution possibly involving extern aliases should be integrated at some point.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using Breeze.Api;
+using Breeze.TumbleBit;
 using Microsoft.Extensions.Logging;
 using Stratis.Bitcoin;
 using Stratis.Bitcoin.Builder;
@@ -11,27 +13,36 @@ using Stratis.Bitcoin.Notifications;
 
 namespace Breeze.Daemon
 {
-	public class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-			// configure Full Node
-			Logs.Configure(new LoggerFactory().AddConsole(LogLevel.Trace, false));
-			NodeSettings nodeSettings = NodeSettings.FromArguments(args);
+            // configure Full Node
+            Logs.Configure(new LoggerFactory().AddConsole(LogLevel.Trace, false));
+            NodeSettings nodeSettings = NodeSettings.FromArguments(args);
             
-            var node = (FullNode)new FullNodeBuilder()
-				.UseNodeSettings(nodeSettings)
-				.UseWallet()				
-				.UseBlockNotification()
+            var fullNodeBuilder = new FullNodeBuilder()
+                .UseNodeSettings(nodeSettings)
+                .UseWallet()
+                .UseBlockNotification()
                 .UseTransactionNotification()
-				.UseApi()
-				.Build();
+                .UseApi();
 
-			// start Full Node - this will also start the API
-			node.Start();
-			Console.WriteLine("Press any key to stop");
-			Console.ReadLine();
-			node.Dispose();
-		}
+            // add the tumbler's settings
+            var tumblerAddress = args.SingleOrDefault(arg => arg.StartsWith("-tumbler-uri="));
+            if (!string.IsNullOrEmpty(tumblerAddress))
+            {
+                tumblerAddress = tumblerAddress.Replace("-tumbler-uri=", string.Empty);
+                fullNodeBuilder.UseTumbleBit(new Uri(tumblerAddress));
+            }
+            
+            var node = (FullNode)fullNodeBuilder.Build();
+            
+            // start Full Node - this will also start the API
+            node.Start();
+            Console.WriteLine("Press any key to stop");
+            Console.ReadLine();
+            node.Dispose();
+        }
     }
 }
