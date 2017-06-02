83bef09d313d46a49ccb2df78bb716e4cde185db
Jeremy Bokobza <bokobza@gmail.com>
2017-06-02 20:05:04 +0100

    Tidying of Breeze's sync manager

--- Program.cs
+++ Program.cs
@@ -25,125 +25,126 @@ namespace Breeze.Daemon
     {
         public static void Main(string[] args)
         {
-	        IFullNodeBuilder fullNodeBuilder = null;
+            IFullNodeBuilder fullNodeBuilder = null;
 
-			if (args.Contains("stratis"))
-	        {
-				// configure Full Node
-		        Logs.Configure(Logs.GetLoggerFactory(args));
-		        if (NodeSettings.PrintHelp(args, Network.StratisMain))
-			        return;
+            // configure logging
+            Logs.Configure(Logs.GetLoggerFactory(args));
 
-		        var network = args.Contains("-testnet") ? InitStratisTest() : Network.StratisMain;
-		        var nodeSettings = NodeSettings.FromArguments(args, "stratis", network, ProtocolVersion.ALT_PROTOCOL_VERSION);
+            if (args.Contains("stratis"))
+            {
+                if (NodeSettings.PrintHelp(args, Network.StratisMain))
+                    return;
 
-		        if (args.Contains("light"))
-		        {
-					fullNodeBuilder = new FullNodeBuilder()
-						.UseNodeSettings(nodeSettings)
-						.UseLightWallet()
-						.UseBlockNotification()
-						.UseTransactionNotification()
-						.UseApi();
-				}
-		        else
-		        {
-			        fullNodeBuilder = new FullNodeBuilder()
-				        .UseNodeSettings(nodeSettings)
-				        .UseStratisConsensus()
-				        .UseBlockStore()
-				        .UseMempool()
-				        .UseWallet()
-				        .AddPowPosMining()
-				        .UseApi();
-		        }
-			}
-			else
-			{
-				NodeSettings nodeSettings = NodeSettings.FromArguments(args);
+                var network = args.Contains("-testnet") ? InitStratisTest() : Network.StratisMain;
+                var nodeSettings = NodeSettings.FromArguments(args, "stratis", network, ProtocolVersion.ALT_PROTOCOL_VERSION);
 
-				if (args.Contains("light"))
-				{
-					fullNodeBuilder = new FullNodeBuilder()
-						.UseNodeSettings(nodeSettings)
-						.UseLightWallet()
-						.UseBlockNotification()
-						.UseTransactionNotification()
-						.UseApi();
-				}
-				else
-				{
-					fullNodeBuilder = new FullNodeBuilder()
-						.UseNodeSettings(nodeSettings)
-						.UseConsensus()
-						.UseBlockStore()
-						.UseMempool()
-						.UseWallet()
-						.UseApi();
-				}
-			}
+                if (args.Contains("light"))
+                {
+                    fullNodeBuilder = new FullNodeBuilder()
+                        .UseNodeSettings(nodeSettings)
+                        .UseLightWallet()
+                        .UseBlockNotification()
+                        .UseTransactionNotification()
+                        .UseApi();
+                }
+                else
+                {
+                    fullNodeBuilder = new FullNodeBuilder()
+                        .UseNodeSettings(nodeSettings)
+                        .UseStratisConsensus()
+                        .UseBlockStore()
+                        .UseMempool()
+                        .UseWallet()
+                        .AddPowPosMining()
+                        .UseApi();
+                }
+            }
+            else
+            {
+                NodeSettings nodeSettings = NodeSettings.FromArguments(args);
 
-			// add the tumbler's settings
-			var tumblerAddress = args.SingleOrDefault(arg => arg.StartsWith("-tumbler-uri="));
-			if (!string.IsNullOrEmpty(tumblerAddress))
-			{
-				tumblerAddress = tumblerAddress.Replace("-tumbler-uri=", string.Empty);
-				fullNodeBuilder.UseTumbleBit(new Uri(tumblerAddress));
-			}
+                if (args.Contains("light"))
+                {
+                    fullNodeBuilder = new FullNodeBuilder()
+                        .UseNodeSettings(nodeSettings)
+                        .UseLightWallet()
+                        .UseBlockNotification()
+                        .UseTransactionNotification()
+                        .UseApi();
+                }
+                else
+                {
+                    fullNodeBuilder = new FullNodeBuilder()
+                        .UseNodeSettings(nodeSettings)
+                        .UseConsensus()
+                        .UseBlockStore()
+                        .UseMempool()
+                        .UseWallet()
+                        .UseApi();
+                }
+            }
 
-			var node = fullNodeBuilder.Build();
+            // add the tumbler's settings
+            var tumblerAddress = args.SingleOrDefault(arg => arg.StartsWith("-tumbler-uri="));
+            if (!string.IsNullOrEmpty(tumblerAddress))
+            {
+                tumblerAddress = tumblerAddress.Replace("-tumbler-uri=", string.Empty);
+                fullNodeBuilder.UseTumbleBit(new Uri(tumblerAddress));
+            }
 
-			//start Full Node - this will also start the API
-			node.Run();
+            var node = fullNodeBuilder.Build();
+
+            //start Full Node - this will also start the API
+            node.Run();
         }
 
-	    private static Network InitStratisTest()
-	    {
-		    Block.BlockSignature = true;
-		    Transaction.TimeStamp = true;
+        private static Network InitStratisTest()
+        {
+            Block.BlockSignature = true;
+            Transaction.TimeStamp = true;
 
-		    var consensus = Network.StratisMain.Consensus.Clone();
-		    consensus.PowLimit = new Target(uint256.Parse("0000ffff00000000000000000000000000000000000000000000000000000000"));
+            var consensus = Network.StratisMain.Consensus.Clone();
+            consensus.PowLimit = new Target(uint256.Parse("0000ffff00000000000000000000000000000000000000000000000000000000"));
 
-		    // The message start string is designed to be unlikely to occur in normal data.
-		    // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
-		    // a large 4-byte int at any alignment.
-		    var pchMessageStart = new byte[4];
-		    pchMessageStart[0] = 0x71;
-		    pchMessageStart[1] = 0x31;
-		    pchMessageStart[2] = 0x21;
-		    pchMessageStart[3] = 0x11;
-		    var magic = BitConverter.ToUInt32(pchMessageStart, 0); //0x5223570; 
+            // The message start string is designed to be unlikely to occur in normal data.
+            // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+            // a large 4-byte int at any alignment.
+            var pchMessageStart = new byte[4];
+            pchMessageStart[0] = 0x71;
+            pchMessageStart[1] = 0x31;
+            pchMessageStart[2] = 0x21;
+            pchMessageStart[3] = 0x11;
+            var magic = BitConverter.ToUInt32(pchMessageStart, 0); //0x5223570; 
 
-		    var genesis = Network.StratisMain.GetGenesis().Clone();
-		    genesis.Header.Time = 1493909211;
-		    genesis.Header.Nonce = 2433759;
-		    genesis.Header.Bits = consensus.PowLimit;
-		    consensus.HashGenesisBlock = genesis.GetHash();
+            var genesis = Network.StratisMain.GetGenesis().Clone();
+            genesis.Header.Time = 1493909211;
+            genesis.Header.Nonce = 2433759;
+            genesis.Header.Bits = consensus.PowLimit;
+            consensus.HashGenesisBlock = genesis.GetHash();
 
-		    Guard.Assert(consensus.HashGenesisBlock == uint256.Parse("0x00000e246d7b73b88c9ab55f2e5e94d9e22d471def3df5ea448f5576b1d156b9"));
+            Guard.Assert(consensus.HashGenesisBlock == uint256.Parse("0x00000e246d7b73b88c9ab55f2e5e94d9e22d471def3df5ea448f5576b1d156b9"));
 
-		    var builder = new NetworkBuilder()
-			    .SetName("StratisTest")
-			    .SetConsensus(consensus)
-			    .SetMagic(magic)
-			    .SetGenesis(genesis)
-			    .SetPort(26178)
-			    .SetRPCPort(26174)
-			    .SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, new byte[] { (65) })
-			    .SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, new byte[] { (196) })
-			    .SetBase58Bytes(Base58Type.SECRET_KEY, new byte[] { (65 + 128) })
-			    .SetBase58Bytes(Base58Type.ENCRYPTED_SECRET_KEY_NO_EC, new byte[] { 0x01, 0x42 })
-			    .SetBase58Bytes(Base58Type.ENCRYPTED_SECRET_KEY_EC, new byte[] { 0x01, 0x43 })
-			    .SetBase58Bytes(Base58Type.EXT_PUBLIC_KEY, new byte[] { (0x04), (0x88), (0xB2), (0x1E) })
-			    .SetBase58Bytes(Base58Type.EXT_SECRET_KEY, new byte[] { (0x04), (0x88), (0xAD), (0xE4) })
-			    .AddDNSSeeds(new[]
-			    {
-				    new DNSSeedData("stratisplatform.com", "testnode1.stratisplatform.com"),
-			    });
+            var builder = new NetworkBuilder()
+                .SetName("StratisTest")
+                .SetConsensus(consensus)
+                .SetMagic(magic)
+                .SetGenesis(genesis)
+                .SetPort(26178)
+                .SetRPCPort(26174)
+                .SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, new byte[] { (65) })
+                .SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, new byte[] { (196) })
+                .SetBase58Bytes(Base58Type.SECRET_KEY, new byte[] { (65 + 128) })
+                .SetBase58Bytes(Base58Type.ENCRYPTED_SECRET_KEY_NO_EC, new byte[] { 0x01, 0x42 })
+                .SetBase58Bytes(Base58Type.ENCRYPTED_SECRET_KEY_EC, new byte[] { 0x01, 0x43 })
+                .SetBase58Bytes(Base58Type.EXT_PUBLIC_KEY, new byte[] { (0x04), (0x88), (0xB2), (0x1E) })
+                .SetBase58Bytes(Base58Type.EXT_SECRET_KEY, new byte[] { (0x04), (0x88), (0xAD), (0xE4) })
+                .AddDNSSeeds(new[]
+                {
+                    new DNSSeedData("stratisplatform.com", "testnode1.stratisplatform.com"),
+                });
 
-		    return builder.BuildAndRegister();
-	    }
+            return builder.BuildAndRegister();
+        }
 
-	}
+    }
 }
