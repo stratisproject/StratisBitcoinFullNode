743ffc62fb467aeafcad0e4c039f1d4e705d46a3
Jeremy Bokobza <bokobza@gmail.com>
2017-06-19 17:34:41 +0100

    Added 'watch only' wallet

---
+++ Stratis.Bitcoin/WatchOnlyWallet/Notifications/BlockObserver.cs
@@ -0,0 +1,26 @@
+﻿using NBitcoin;
+
+namespace Stratis.Bitcoin.WatchOnlyWallet
+{
+    /// <summary>
+    /// Observer that receives notifications about the arrival of new <see cref="Block"/>s.
+    /// </summary>
+	public class BlockObserver : SignalObserver<Block>
+    {        
+        private readonly IWatchOnlyWalletManager walletManager;
+
+        public BlockObserver(IWatchOnlyWalletManager walletManager)
+        {
+            this.walletManager = walletManager;
+        }
+
+        /// <summary>
+        /// Manages what happens when a new block is received.
+        /// </summary>
+        /// <param name="block">The new block</param>
+        protected override void OnNextCore(Block block)
+        {
+            this.walletManager.ProcessBlock(block);
+        }
+    }
+}
