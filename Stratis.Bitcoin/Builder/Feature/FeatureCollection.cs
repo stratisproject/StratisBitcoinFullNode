fc05018fa792c5e095ac9ad02fffcee174e3de73
Aprogiena <Aprogiena@users.noreply.github.com>
2017-07-11 14:37:32 +0100

    Adding and fixing comments to have proper XML doc
    May also contain code style fixes

--- Stratis.Bitcoin/Builder/Feature/FeatureCollection.cs
+++ Stratis.Bitcoin/Builder/Feature/FeatureCollection.cs
@@ -6,39 +6,55 @@ using System.Globalization;
 
 namespace Stratis.Bitcoin.Builder.Feature
 {
-	public interface IFeatureCollection
-	{
-		List<IFeatureRegistration> FeatureRegistrations { get; }
+    /// <summary>
+    /// Defines methods for collection of features of the FullNode.
+    /// </summary>
+    public interface IFeatureCollection
+    {
+        /// <summary>Obtains the list of features already registered in the collection.</summary>
+        List<IFeatureRegistration> FeatureRegistrations { get; }
 
-		IFeatureRegistration AddFeature<TImplementation>() where TImplementation : class, IFullNodeFeature;
-	}
+        /// <summary>Adds a new feature to the collection provided that the feature of the same type has not been added already.</summary>
+        /// <typeparam name="TImplementation">Type of the feature to be added to the collection.</typeparam>
+        /// <returns>Representation of the registered feature.</returns>
+        IFeatureRegistration AddFeature<TImplementation>() where TImplementation : class, IFullNodeFeature;
+    }
 
-	public class FeatureCollection : IFeatureCollection
-	{
-		private readonly List<IFeatureRegistration> featureRegistrations;
+    /// <summary>
+    /// Collection of features available to and/or used by the FullNode.
+    /// </summary>
+    public class FeatureCollection : IFeatureCollection
+    {
+        /// <summary>List of features already registered in the collection.</summary>
+        private readonly List<IFeatureRegistration> featureRegistrations;
 
-		public FeatureCollection()
-		{
-			this.featureRegistrations = new List<IFeatureRegistration>();
-		}
+        /// <summary>Initializes the object instance.</summary>
+        public FeatureCollection()
+        {
+            this.featureRegistrations = new List<IFeatureRegistration>();
+        }
 
-		public List<IFeatureRegistration> FeatureRegistrations
-		{
-			get
-			{
-				return this.featureRegistrations;
-			}			
-		}
+        /// <summary>Obtains the list of features already registered in the collection.</summary>
+        public List<IFeatureRegistration> FeatureRegistrations
+        {
+            get
+            {
+                return this.featureRegistrations;
+            }
+        }
 
-		public IFeatureRegistration AddFeature<TImplementation>() where TImplementation : class, IFullNodeFeature
-		{
-			if (this.featureRegistrations.Any(f => f.FeatureType == typeof(TImplementation)))
-				throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, "Feature of type {0} has already been registered.", typeof(TImplementation).FullName));			
+        /// <summary>Adds a new feature to the collection provided that the feature of the same type has not been added already.</summary>
+        /// <typeparam name="TImplementation">Type of the feature to be added to the collection.</typeparam>
+        /// <returns>Representation of the registered feature.</returns>
+        public IFeatureRegistration AddFeature<TImplementation>() where TImplementation : class, IFullNodeFeature
+        {
+            if (this.featureRegistrations.Any(f => f.FeatureType == typeof(TImplementation)))
+                throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, "Feature of type {0} has already been registered.", typeof(TImplementation).FullName));
 
-			var featureRegistration = new FeatureRegistration<TImplementation>();
-			this.featureRegistrations.Add(featureRegistration);
+            var featureRegistration = new FeatureRegistration<TImplementation>();
+            this.featureRegistrations.Add(featureRegistration);
 
-			return featureRegistration;
-		}
-	}
+            return featureRegistration;
+        }
+    }
 }
