342fab05b7389584585519f27ea2b01085e02370
Aprogiena <Aprogiena@users.noreply.github.com>
2017-07-11 14:37:05 +0100

    Adding and fixing comments to have proper XML doc
    May also contain code style fixes

--- Stratis.Bitcoin/Builder/Feature/FullNodeFeature.cs
+++ Stratis.Bitcoin/Builder/Feature/FullNodeFeature.cs
@@ -1,38 +1,45 @@
 ﻿namespace Stratis.Bitcoin.Builder.Feature
 {
-	/// <summary>
-	/// Defines methods for features that are managed by the FullNode.
-	/// </summary>
-	public interface IFullNodeFeature
-	{
-		/// <summary>
-		/// Triggered when the FullNode host has fully started
-		/// </summary>
-		void Start();
+    /// <summary>
+    /// Defines methods for features that are managed by the FullNode.
+    /// </summary>
+    public interface IFullNodeFeature
+    {
+        /// <summary>
+        /// Triggered when the FullNode host has fully started.
+        /// </summary>
+        void Start();
 
-		/// <summary>
-		/// Triggered when the FullNode is performing a graceful shutdown.
-		/// Requests may still be in flight. Shutdown will block until this event completes.
-		/// </summary>
-		void Stop();
-	}
+        /// <summary>
+        /// Triggered when the FullNode is performing a graceful shutdown.
+        /// Requests may still be in flight. Shutdown will block until this event completes.
+        /// </summary>
+        void Stop();
+    }
 
-	/// <summary>
-	/// A feature is used to extend functionality into the full node
-	/// It can can manage its life time or use the full node disposable resources.
+    /// <summary>
+    /// A feature is used to extend functionality into the full node.
+    /// It can manage its life time or use the full node disposable resources.
+    /// <para>
+    /// If a feature adds an option of a certain functionality to be available to be used by the node
+    /// (it may be disabled/enabled by the configuration) the naming convention is
+    /// <c>Add[Feature]()</c>. Conversely, when a feature is inclined to be used if included,
+    /// the naming convention should be <c>Use[Feature]()</c>.
+    /// </para>
+    /// </summary>
+    public abstract class FullNodeFeature : IFullNodeFeature
+    {
+        /// <summary>
+        /// Triggered when the FullNode host has fully started.
+        /// </summary>
+        public abstract void Start();
 
-	/// If a feature is added functionality available to use by the node
-	/// (but may be disabled/enabled by the configuration) the naming convention is
-	/// Add[Feature](). Conversely, when a features is inclined to be used if included,
-	/// the naming convention should be Use[Feature]()
-
-	/// </summary>
-	public abstract class FullNodeFeature : IFullNodeFeature
-	{
-		public abstract void Start();
-
-		public virtual void Stop()
-		{
-		}
-	}
+        /// <summary>
+        /// Triggered when the FullNode is performing a graceful shutdown.
+        /// Requests may still be in flight. Shutdown will block until this event completes.
+        /// </summary>
+        public virtual void Stop()
+        {
+        }
+    }
 }
