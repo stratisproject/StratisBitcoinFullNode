78dde29bdeff4c579788a0133d93cf700b15d146
Aprogiena <aprogiena@users.noreply.github.com>
2017-07-22 18:27:46 +0100

    Adding and fixing comments to have proper XML doc
    Also contains code style fixes

--- Stratis.Bitcoin/Utilities/ProtocolExtensions.cs
+++ Stratis.Bitcoin/Utilities/ProtocolExtensions.cs
@@ -4,8 +4,19 @@ using NBitcoin.Protocol;
 
 namespace Stratis.Bitcoin.Utilities
 {
+    /// <summary>
+    /// Extension methods related to the network protocol.
+    /// </summary>
     public static class ProtocolExtensions
     {
+        /// <summary>
+        /// Calculates a median of time offsets of the node's connected peers.
+        /// <para>
+        /// The peers' time offsets are differences in seconds between the node's clock and the peer's clock.
+        /// </para>
+        /// </summary>
+        /// <param name="source">Collection of connected peer nodes.</param>
+        /// <returns>Median time offset among the given nodes.</returns>
         public static long GetMedianTimeOffset(this IEnumerable<Node> source)
         {
             return source
