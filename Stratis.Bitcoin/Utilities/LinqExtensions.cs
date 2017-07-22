debd79fc59aa9edf305aedd18c82829560ba1ba7
Aprogiena <aprogiena@users.noreply.github.com>
2017-07-22 18:18:53 +0100

    Adding and fixing comments to have proper XML doc
    Also contains code style fixes

--- Stratis.Bitcoin/Utilities/LinqExtensions.cs
+++ Stratis.Bitcoin/Utilities/LinqExtensions.cs
@@ -4,13 +4,23 @@ using System.Linq;
 
 namespace Stratis.Bitcoin.Utilities
 {
+    /// <summary>
+    /// Extension methods for collections.
+    /// </summary>
+    /// <remarks>TODO: Why is this called LinqExtensions? Wouldn't CollectionsExtensions be better?</remarks>
     public static class LinqExtensions
     {
+        /// <summary>
+        /// Calculates a median value of a collection of integers.
+        /// </summary>
+        /// <param name="source">Collection of numbers to count median of.</param>
+        /// <returns>Median value, or 0 if the collection is empty.</returns>
         public static long Median(this IEnumerable<long> source)
         {
             long count = source.LongCount();
             if (count == 0)
                 return 0;
+
             int midpoint = source.Count() / 2;
             IOrderedEnumerable<long> ordered = source.OrderBy(n => n);
             if ((count % 2) == 0)
