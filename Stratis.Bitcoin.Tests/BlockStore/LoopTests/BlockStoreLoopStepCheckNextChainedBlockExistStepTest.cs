c0a5acc9c3651fed189979c7ab2f83b7ac36e4a0
Francois de la Rouviere <fassadlr@gmail.com>
2017-07-26 21:43:30 +0200

    Split BlockStoreLoop into 4 logical areas and add unit tests
    Add Steps
    
    CheckExists
    Reorganise
    TryPending
    DownloadBlocks

---
+++ Stratis.Bitcoin.Tests/BlockStore/LoopTests/BlockStoreLoopStepCheckNextChainedBlockExistStepTest.cs
@@ -0,0 +1,49 @@
+﻿using NBitcoin;
+using Stratis.Bitcoin.Features.BlockStore;
+using Stratis.Bitcoin.Features.BlockStore.LoopSteps;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace Stratis.Bitcoin.Tests.BlockStore.LoopTests
+{
+    public sealed class BlockStoreLoopStepCheckNextChainedBlockExistStepTest : BlockStoreLoopStepBaseTest
+    {
+        [Fact]
+        public void CanExecute_CheckNextChainedBlockExistStep()
+        {
+            List<Block> blocks = CreateBlocks(5);
+
+            // The BlockRepository has 5 blocks stored
+            using (var blockRepository = new BlockRepository(Network.Main, TestBase.AssureEmptyDirAsDataFolder(@"BlockStore\LoopTest_CheckExists")))
+            {
+                blockRepository.PutAsync(blocks.Last().GetHash(), blocks).GetAwaiter().GetResult();
+
+                // The BlockRepository has 4 blocks appended
+                var chain = new ConcurrentChain(Network.Main);
+                AppendBlock(chain, blocks[0]);
+                AppendBlock(chain, blocks[1]);
+                AppendBlock(chain, blocks[2]);
+                AppendBlock(chain, blocks[3]);
+
+                // Create the last chained block without appending to the chain
+                ChainedBlock block03 = chain.GetBlock(blocks[3].GetHash());
+                var block04 = new ChainedBlock(blocks[4].Header, blocks[4].Header.GetHash(), block03);
+
+                BlockStoreLoop blockStoreLoop = CreateBlockStoreLoop(chain, blockRepository);
+
+                Assert.Null(blockStoreLoop.StoredBlock);
+
+                ChainedBlock nextChainedBlock = block04;
+                var checkExistsStep = new CheckNextChainedBlockExistStep(blockStoreLoop);
+                checkExistsStep.Execute(nextChainedBlock, new CancellationToken(), false).GetAwaiter().GetResult();
+
+                Assert.Equal(blockStoreLoop.StoredBlock.Header.GetHash(), block04.Header.GetHash());
+                Assert.Equal(blockStoreLoop.BlockRepository.BlockHash, block04.Header.GetHash());
+
+                chain = null;
+            }
+        }
+    }
+}
---
+++ Stratis.Bitcoin.Tests/BlockStore/LoopTests/BlockStoreLoopStepTryPendingTest.cs
@@ -0,0 +1,60 @@
+﻿using NBitcoin;
+using Stratis.Bitcoin.Features.BlockStore;
+using Stratis.Bitcoin.Features.BlockStore.LoopSteps;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace Stratis.Bitcoin.Tests.BlockStore.LoopTests
+{
+    public sealed class BlockStoreLoopStepTryPendingTest : BlockStoreLoopStepBaseTest
+    {
+        [Fact]
+        public void CanExecute_TryPending()
+        {
+            // Create 15 blocks
+            List<Block> blocks = CreateBlocks(15);
+
+            // The repository has 5 blocks stored
+            using (var blockRepository = new BlockRepository(Network.Main, TestBase.AssureEmptyDirAsDataFolder(@"BlockStore\LoopTest_TryPending")))
+            {
+                blockRepository.PutAsync(blocks.Take(5).Last().GetHash(), blocks.Take(5).ToList()).GetAwaiter().GetResult();
+
+                // The chain has 10 blocks appended
+                var chain = new ConcurrentChain(Network.Main);
+                AppendBlock(chain, blocks[0]);
+                AppendBlock(chain, blocks[1]);
+                AppendBlock(chain, blocks[2]);
+                AppendBlock(chain, blocks[3]);
+                AppendBlock(chain, blocks[4]);
+                AppendBlock(chain, blocks[5]);
+                AppendBlock(chain, blocks[6]);
+                AppendBlock(chain, blocks[7]);
+                AppendBlock(chain, blocks[8]);
+                AppendBlock(chain, blocks[9]);
+
+                // Create block store loop
+                BlockStoreLoop blockStoreLoop = CreateBlockStoreLoop(chain, blockRepository);
+
+                // Add chained blocks 5 - 9 to PendingStorage
+                AddToPendingStorage(blockStoreLoop, blocks[5]);
+                AddToPendingStorage(blockStoreLoop, blocks[6]);
+                AddToPendingStorage(blockStoreLoop, blocks[7]);
+                AddToPendingStorage(blockStoreLoop, blocks[8]);
+                AddToPendingStorage(blockStoreLoop, blocks[9]);
+
+                //Start processing pending blocks from block 5
+                ChainedBlock nextChainedBlock = blockStoreLoop.Chain.GetBlock(blocks[5].GetHash());
+
+                var processPendingStorageStep = new ProcessPendingStorageStep(blockStoreLoop);
+                processPendingStorageStep.Execute(nextChainedBlock, new CancellationToken(), false).GetAwaiter().GetResult();
+
+                Assert.Equal(blocks[9].GetHash(), blockStoreLoop.BlockRepository.BlockHash);
+                Assert.Equal(blocks[9].GetHash(), blockStoreLoop.StoredBlock.HashBlock);
+
+                chain = null;
+            }
+        }
+    }
+}
