4e7cd506e9f1418c2d353fc91456b894d5dc26b8
Mike Dennis <strykitenterprises@gmail.com>
2017-08-03 11:10:54 -0600

    RPC getpeerinfo
    Initial implementation of RPC method getpeerinfo and associated unit test.

--- Stratis.Bitcoin/Features/RPC/Controllers/ConnectionManagerController.cs
+++ Stratis.Bitcoin/Features/RPC/Controllers/ConnectionManagerController.cs
@@ -1,9 +1,13 @@
-﻿using System;
-using System.Net;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using NBitcoin.Protocol;
+using Stratis.Bitcoin.Base;
 using Stratis.Bitcoin.Configuration;
 using Stratis.Bitcoin.Connection;
+using Stratis.Bitcoin.Features.RPC.Models;
 using Stratis.Bitcoin.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Net;
 
 namespace Stratis.Bitcoin.Features.RPC.Controllers
 {
@@ -35,5 +39,60 @@ namespace Stratis.Bitcoin.Features.RPC.Controllers
             }
             return true;
         }
+
+        /// <summary>
+        /// RPC implementation of getpeerinfo
+        /// </summary>
+        /// <returns>List of RPC peer nodes</returns>
+        [ActionName("getpeerinfo")]
+        public List<PeerNodeModel> GetPeerInfo()
+        {
+            List<PeerNodeModel> peerList = new List<PeerNodeModel>();
+            int index = 0;
+            foreach (Node node in this.ConnectionManager.ConnectedNodes)
+            {
+                if (node != null && node.RemoteSocketAddress != null)
+                {
+                    PeerNodeModel rpcNode = new PeerNodeModel
+                    {
+                        Id = index,
+                        Address = node.RemoteSocketEndpoint.ToString(),
+                    };
+
+                    if (node.MyVersion != null)
+                    {
+                        rpcNode.LocalAddress = node.MyVersion.AddressReceiver?.ToString();
+                        rpcNode.Services = ((ulong)node.MyVersion.Services).ToString("X");
+                        rpcNode.Version = (uint)node.MyVersion.Version;
+                        rpcNode.SubVersion = node.MyVersion.UserAgent;
+                        rpcNode.StartingHeight = node.MyVersion.StartHeight;
+                    }
+
+                    ConnectionManagerBehavior connectionManagerBehavior = node.Behavior<ConnectionManagerBehavior>();
+                    if (connectionManagerBehavior != null)
+                    {
+                        rpcNode.Inbound = connectionManagerBehavior.Inbound;
+                        rpcNode.IsWhiteListed = connectionManagerBehavior.Whitelisted;
+                    }
+
+                    ChainHeadersBehavior chainHeadersBehavior = node.Behavior<ChainHeadersBehavior>();
+                    if (chainHeadersBehavior != null)
+                    {
+                        rpcNode.Blocks = chainHeadersBehavior.PendingTip.Height;
+                    }
+
+                    if (node.TimeOffset != null)
+                    {
+                        rpcNode.TimeOffset = node.TimeOffset.Value.Seconds;
+                    }
+
+                    peerList.Add(rpcNode);
+                }
+                index++;
+            }
+
+            return peerList;
+        }
+
     }
 }
