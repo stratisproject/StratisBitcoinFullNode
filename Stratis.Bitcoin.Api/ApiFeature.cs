c53a6c719bc4453da78cb241a41714e8f505573c
Jeremy Bokobza <bokobza@gmail.com>
2017-08-03 13:31:21 +0100

    Brought Breeze.Api from the Breeze project

---
+++ Stratis.Bitcoin.Api/ApiFeature.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading.Tasks;
+using Breeze.Api.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Stratis.Bitcoin;
+using Stratis.Bitcoin.Builder;
+using Stratis.Bitcoin.Builder.Feature;
+using Stratis.Bitcoin.Utilities;
+
+namespace Breeze.Api
+{
+    /// <summary>
+    /// Provides an Api to the full node
+    /// </summary>
+    public class ApiFeature : FullNodeFeature
+    {		
+        private readonly IFullNodeBuilder fullNodeBuilder;
+        private readonly FullNode fullNode;
+        private readonly ApiFeatureOptions apiFeatureOptions;
+        private readonly IAsyncLoopFactory asyncLoopFactory;
+        private readonly ILogger logger;
+
+        public ApiFeature(
+            IFullNodeBuilder fullNodeBuilder, 
+            FullNode fullNode, 
+            ApiFeatureOptions apiFeatureOptions, 
+            IAsyncLoopFactory asyncLoopFactory,
+            ILoggerFactory loggerFactory)
+        {
+            this.fullNodeBuilder = fullNodeBuilder;
+            this.fullNode = fullNode;
+            this.apiFeatureOptions = apiFeatureOptions;
+            this.asyncLoopFactory = asyncLoopFactory;
+            this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
+        }
+
+        public override void Start()
+        {
+            this.logger.LogInformation($"Api starting on url {this.fullNode.Settings.ApiUri}");
+            Program.Initialize(this.fullNodeBuilder.Services, this.fullNode);
+
+            this.TryStartKeepaliveMonitor();
+        }
+
+        /// <summary>
+        /// A KeepaliveMonitor when enabled will shutdown the
+        /// node if no one is calling the keepalive endpoint 
+        /// during a certain trashold window
+        /// </summary>
+        public void TryStartKeepaliveMonitor()
+        {
+            if (this.apiFeatureOptions.KeepaliveMonitor?.KeepaliveInterval.TotalSeconds > 0)
+            {
+                this.asyncLoopFactory.Run("ApiFeature.KeepaliveMonitor", token =>
+                    {
+                        // shortened for redability
+                        var monitor = this.apiFeatureOptions.KeepaliveMonitor;
+
+                        // check the trashold to trigger a shutdown
+                        if (monitor.LastBeat.Add(monitor.KeepaliveInterval) < DateTime.UtcNow)
+                            this.fullNode.Stop();
+
+                        return Task.CompletedTask;
+                    },
+                    this.fullNode.NodeLifetime.ApplicationStopping,
+                    repeatEvery: this.apiFeatureOptions.KeepaliveMonitor?.KeepaliveInterval,
+                    startAfter: TimeSpans.Minute);
+            }
+        }
+    }
+
+    public class ApiFeatureOptions
+    {
+        public KeepaliveMonitor KeepaliveMonitor { get; set; }
+
+        public void Keepalive(TimeSpan timeSpan)
+        {
+            this.KeepaliveMonitor = new KeepaliveMonitor {KeepaliveInterval = timeSpan};
+        }
+    }
+
+    public static class ApiFeatureExtension
+    {
+        public static IFullNodeBuilder UseApi(this IFullNodeBuilder fullNodeBuilder, Action<ApiFeatureOptions> optionsAction = null)
+        {
+            // TODO: move the options in to the feature builder
+            var options = new ApiFeatureOptions();
+            optionsAction?.Invoke(options);
+
+            fullNodeBuilder.ConfigureFeature(features =>
+            {
+                features
+                .AddFeature<ApiFeature>()
+                .FeatureServices(services =>
+                    {
+                        services.AddSingleton(fullNodeBuilder);
+                        services.AddSingleton(options);
+                    });
+            });
+
+            return fullNodeBuilder;
+        }
+    }	
+}
---
+++ Stratis.Bitcoin.Api/Models/KeepaliveMonitor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Breeze.Api.Models
+{
+    public class KeepaliveMonitor
+    {
+        public DateTime LastBeat { get; set; }
+        public TimeSpan KeepaliveInterval { get; set; }
+    }
+}
