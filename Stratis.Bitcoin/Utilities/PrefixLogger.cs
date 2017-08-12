7904e3ebd4909a5bb9f0fdd76da9305804445ee6
Aprogiena <aprogiena@users.noreply.github.com>
2017-08-12 10:20:55 +0100

    stylecop

--- Stratis.Bitcoin/Utilities/PrefixLogger.cs
+++ Stratis.Bitcoin/Utilities/PrefixLogger.cs
@@ -49,7 +49,7 @@ namespace Stratis.Bitcoin.Utilities
         /// <inheritdoc />
         public bool IsEnabled(LogLevel logLevel)
         {
-            return IsEnabled(logLevel.ToNLogLevel());
+            return this.IsEnabled(logLevel.ToNLogLevel());
         }
 
         /// <summary>
@@ -66,7 +66,7 @@ namespace Stratis.Bitcoin.Utilities
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
             NLog.LogLevel nLogLevel = logLevel.ToNLogLevel();
-            if (!IsEnabled(nLogLevel))
+            if (!this.IsEnabled(nLogLevel))
                 return;
 
             if (formatter == null)
