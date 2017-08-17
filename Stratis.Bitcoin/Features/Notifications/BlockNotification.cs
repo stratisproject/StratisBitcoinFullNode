480fa7642a4bc9d5a07dba3611c75321621ff50f
Jeremy Bokobza <bokobza@gmail.com>
2017-08-17 17:54:22 +0100

    Fixed tabs

--- Stratis.Bitcoin/Features/Notifications/BlockNotification.cs
+++ Stratis.Bitcoin/Features/Notifications/BlockNotification.cs
@@ -10,91 +10,91 @@ namespace Stratis.Bitcoin.Features.Notifications
     /// Class used to broadcast about new blocks.
     /// </summary>
     public class BlockNotification
-	{
-		private readonly ISignals signals;
+    {
+        private readonly ISignals signals;
         private readonly IAsyncLoopFactory asyncLoopFactory;
-	    private readonly INodeLifetime nodeLifetime;
+        private readonly INodeLifetime nodeLifetime;
 
-	    private ChainedBlock tip;
+        private ChainedBlock tip;
 
-        public BlockNotification(ConcurrentChain chain, ILookaheadBlockPuller puller, ISignals signals, 
+        public BlockNotification(ConcurrentChain chain, ILookaheadBlockPuller puller, ISignals signals,
             IAsyncLoopFactory asyncLoopFactory, INodeLifetime nodeLifetime)
-		{
-			Guard.NotNull(chain, nameof(chain));
-			Guard.NotNull(puller, nameof(puller));
-			Guard.NotNull(signals, nameof(signals));
+        {
+            Guard.NotNull(chain, nameof(chain));
+            Guard.NotNull(puller, nameof(puller));
+            Guard.NotNull(signals, nameof(signals));
             Guard.NotNull(asyncLoopFactory, nameof(asyncLoopFactory));
 
-			this.Chain = chain;
-			this.Puller = puller;
-			this.signals = signals;
+            this.Chain = chain;
+            this.Puller = puller;
+            this.signals = signals;
             this.asyncLoopFactory = asyncLoopFactory;
-		    this.nodeLifetime = nodeLifetime;
-		}
+            this.nodeLifetime = nodeLifetime;
+        }
 
-		public ILookaheadBlockPuller Puller { get; }
+        public ILookaheadBlockPuller Puller { get; }
 
-		public ConcurrentChain Chain { get; }
+        public ConcurrentChain Chain { get; }
 
-		public uint256 StartHash { get; private set; }
+        public uint256 StartHash { get; private set; }
 
-		private bool reSync;
+        private bool reSync;
 
-		public virtual void SyncFrom(uint256 startHash)
-		{
-			if (this.StartHash != null)
-			{
-				this.reSync = true;
-			    ChainedBlock startBlock = this.Chain.GetBlock(startHash);
-			    if (startBlock != null)
-			    {
-			        // sets the location of the puller to the latest hash that was broadcasted
-			        this.Puller.SetLocation(startBlock);
+        public virtual void SyncFrom(uint256 startHash)
+        {
+            if (this.StartHash != null)
+            {
+                this.reSync = true;
+                ChainedBlock startBlock = this.Chain.GetBlock(startHash);
+                if (startBlock != null)
+                {
+                    // sets the location of the puller to the latest hash that was broadcasted
+                    this.Puller.SetLocation(startBlock);
                     this.tip = startBlock;
                 }
-                
+
             }
 
-			this.StartHash = startHash;
-		}
-		
-		/// <summary>
-		/// Notifies about blocks, starting from block with hash passed as parameter.
-		/// </summary>
-		public virtual Task Notify()
-		{
-			return this.asyncLoopFactory.Run("block notifier", token =>
-			{
-				// if the StartHash hasn't been set yet
-				if (this.StartHash == null)
-				{
-					return Task.CompletedTask;
-				}
+            this.StartHash = startHash;
+        }
 
-				// make sure the chain has been downloaded
-				ChainedBlock startBlock = this.Chain.GetBlock(this.StartHash);
-				if (startBlock == null)
-				{
-					return Task.CompletedTask;
-				}
+        /// <summary>
+        /// Notifies about blocks, starting from block with hash passed as parameter.
+        /// </summary>
+        public virtual Task Notify()
+        {
+            return this.asyncLoopFactory.Run("block notifier", token =>
+            {
+                // if the StartHash hasn't been set yet
+                if (this.StartHash == null)
+                {
+                    return Task.CompletedTask;
+                }
 
-				// sets the location of the puller to the latest hash that was broadcasted
-				this.Puller.SetLocation(startBlock);
+                // make sure the chain has been downloaded
+                ChainedBlock startBlock = this.Chain.GetBlock(this.StartHash);
+                if (startBlock == null)
+                {
+                    return Task.CompletedTask;
+                }
+
+                // sets the location of the puller to the latest hash that was broadcasted
+                this.Puller.SetLocation(startBlock);
                 this.tip = startBlock;
 
-				// send notifications for all the following blocks
-				while (!this.reSync)
-				{					
-					var block = this.Puller.NextBlock(token);
+                // send notifications for all the following blocks
+                while (!this.reSync)
+                {
+                    var block = this.Puller.NextBlock(token);
 
-					if (block != null)
-					{
-						// broadcast the block to the registered observers
-						this.signals.SignalBlock(block);
+                    if (block != null)
+                    {
+                        // broadcast the block to the registered observers
+                        this.signals.SignalBlock(block);
                         this.tip = this.Chain.GetBlock(block.GetHash());
-					}
-					else
-					{
+                    }
+                    else
+                    {
                         // in reorg we reset the puller to the fork
                         // when a reorg happens the puller is pushed
                         // back and continues from the current fork
@@ -105,13 +105,13 @@ namespace Stratis.Bitcoin.Features.Notifications
 
                         // set the puller to the fork location
                         this.Puller.SetLocation(this.tip);
-					}
-				}
-				
-				this.reSync = false;
+                    }
+                }
 
-				return Task.CompletedTask;
-			}, this.nodeLifetime.ApplicationStopping);
-		}		
-	}
+                this.reSync = false;
+
+                return Task.CompletedTask;
+            }, this.nodeLifetime.ApplicationStopping);
+        }
+    }
 }
--- Stratis.Bitcoin/Features/Notifications/TransactionNotification.cs
+++ Stratis.Bitcoin/Features/Notifications/TransactionNotification.cs
@@ -8,27 +8,27 @@ namespace Stratis.Bitcoin.Features.Notifications
     /// Class used to broadcast about new transactions.
     /// </summary>
     public class TransactionNotification
-	{
-		private readonly ISignals signals;
+    {
+        private readonly ISignals signals;
 
-		public TransactionNotification(ISignals signals)
-		{
-			Guard.NotNull(signals, nameof(signals));
+        public TransactionNotification(ISignals signals)
+        {
+            Guard.NotNull(signals, nameof(signals));
 
-			this.signals = signals;			
-		}
+            this.signals = signals;
+        }
 
-	    /// <summary>
-	    /// Broadcast new transactions to subscribers.
-	    /// </summary>
-	    /// <param name="transaction">The transaction to braodcast.</param>
-	    public virtual void Notify(Transaction transaction)
-		{
+        /// <summary>
+        /// Broadcast new transactions to subscribers.
+        /// </summary>
+        /// <param name="transaction">The transaction to braodcast.</param>
+        public virtual void Notify(Transaction transaction)
+        {
             if (transaction != null)
             {
                 // broadcast the transaction to the registered observers
                 this.signals.SignalTransaction(transaction);
             }
-        }		
-	}
+        }
+    }
 }
