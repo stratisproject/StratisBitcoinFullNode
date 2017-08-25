30a2275f3b333332df8127abf9c5e3768a63e7a7
Aprogiena <aprogiena@users.noreply.github.com>
2017-08-25 12:56:30 +0100

    Adding and fixing comments to have proper XML doc

--- Stratis.Bitcoin/Features/Consensus/CoinViews/RewindData.cs
+++ Stratis.Bitcoin/Features/Consensus/CoinViews/RewindData.cs
@@ -3,9 +3,15 @@ using NBitcoin;
 
 namespace Stratis.Bitcoin.Features.Consensus.CoinViews
 {
+    /// <summary>
+    /// Information about a previous state of the coinview that contains all information 
+    /// needed to rewind the coinview from the current state to the previous state.
+    /// </summary>
     public class RewindData : IBitcoinSerializable
     {
+        /// <summary>Hash of the block header of the tip of the previous state of the coinview.</summary>
         private uint256 previousBlockHash;
+        /// <summary>Hash of the block header of the tip of the previous state of the coinview.</summary>
         public uint256 PreviousBlockHash
         {
             get
@@ -18,7 +24,9 @@ namespace Stratis.Bitcoin.Features.Consensus.CoinViews
             }
         }
 
-        List<uint256> transactionsToRemove = new List<uint256>();
+        /// <summary>List of transaction IDs that needs to be removed when rewinding to the previous state as they haven't existed in the previous state.</summary>
+        private List<uint256> transactionsToRemove = new List<uint256>();
+        /// <summary>List of transaction IDs that needs to be removed when rewinding to the previous state as they haven't existed in the previous state.</summary>
         public List<uint256> TransactionsToRemove
         {
             get
@@ -31,7 +39,9 @@ namespace Stratis.Bitcoin.Features.Consensus.CoinViews
             }
         }
 
-        List<UnspentOutputs> outputsToRestore = new List<UnspentOutputs>();
+        /// <summary>List of unspent output transaction information that needs to be restored when rewinding to the previous state as they were fully spent in the current view.</summary>
+        private List<UnspentOutputs> outputsToRestore = new List<UnspentOutputs>();
+        /// <summary>List of unspent output transaction information that needs to be restored when rewinding to the previous state as they were fully spent in the current view.</summary>
         public List<UnspentOutputs> OutputsToRestore
         {
             get
@@ -44,15 +54,23 @@ namespace Stratis.Bitcoin.Features.Consensus.CoinViews
             }
         }
 
+        /// <summary>
+        /// Creates uninitialized instance of the object.
+        /// </summary>
         public RewindData()
         {
         }
 
+        /// <summary>
+        /// Initializes instance of the object with coinview's tip hash.
+        /// </summary>
+        /// <param name="previousBlockHash">Hash of the block header of the tip of the previous state of the coinview.</param>
         public RewindData(uint256 previousBlockHash)
         {
             this.previousBlockHash = previousBlockHash;
         }
 
+        /// <inheritdoc />
         public void ReadWrite(BitcoinStream stream)
         {
             stream.ReadWrite(ref this.previousBlockHash);
