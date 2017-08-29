90b6506d39d697c14cd1f44e3e2062082d2db8b8
SomeGuy <someguy.fromafrica@gmail.com>
2017-08-30 09:28:59 +1000

    Remove GetWorkRequired from ConsensusManager

--- Stratis.Bitcoin/Features/Consensus/ConsensusManager.cs
+++ Stratis.Bitcoin/Features/Consensus/ConsensusManager.cs
@@ -53,69 +53,9 @@ namespace Stratis.Bitcoin.Features.Consensus
         public Target GetNetworkDifficulty()
         {
             if (this.ConsensusValidator?.ConsensusParams != null && this.ChainState?.HighestValidatedPoW != null)
-                return GetWorkRequired(this.ConsensusValidator.ConsensusParams, this.ChainState?.HighestValidatedPoW);
+                return this.ChainState?.HighestValidatedPoW?.GetWorkRequired(this.ConsensusValidator.ConsensusParams);
             else
                 return null;
-        }
-
-        public static Target GetWorkRequired(NBitcoin.Consensus consensus, ChainedBlock chainedBlock)
-        {
-            // Genesis block
-            if (chainedBlock.Height == 0)
-                return consensus.PowLimit;
-            var nProofOfWorkLimit = consensus.PowLimit;
-            var pindexLast = chainedBlock.Previous;
-            var height = chainedBlock.Height;
-
-            if (pindexLast == null)
-                return nProofOfWorkLimit;
-
-            // Only change once per interval
-            if ((height) % consensus.DifficultyAdjustmentInterval != 0)
-            {
-                if (consensus.PowAllowMinDifficultyBlocks)
-                {
-                    // Special difficulty rule for testnet:
-                    // If the new block's timestamp is more than 2* 10 minutes
-                    // then allow mining of a min-difficulty block.
-                    if (chainedBlock.Header.BlockTime > pindexLast.Header.BlockTime + TimeSpan.FromTicks(consensus.PowTargetSpacing.Ticks * 2))
-                        return nProofOfWorkLimit;
-                    else
-                    {
-                        // Return the last non-special-min-difficulty-rules-block
-                        ChainedBlock pindex = pindexLast;
-                        while (pindex.Previous != null && (pindex.Height % consensus.DifficultyAdjustmentInterval) != 0 && pindex.Header.Bits == nProofOfWorkLimit)
-                            pindex = pindex.Previous;
-                        return pindex.Header.Bits;
-                    }
-                }
-                return pindexLast.Header.Bits;
-            }
-
-            // Go back by what we want to be 14 days worth of blocks
-            var pastHeight = pindexLast.Height - (consensus.DifficultyAdjustmentInterval - 1);
-            ChainedBlock pindexFirst = chainedBlock.EnumerateToGenesis().FirstOrDefault(o => o.Height == pastHeight);
-            Guard.Assert(pindexFirst != null);
-
-            if (consensus.PowNoRetargeting)
-                return pindexLast.Header.Bits;
-
-            // Limit adjustment step
-            var nActualTimespan = pindexLast.Header.BlockTime - pindexFirst.Header.BlockTime;
-            if (nActualTimespan < TimeSpan.FromTicks(consensus.PowTargetTimespan.Ticks / 4))
-                nActualTimespan = TimeSpan.FromTicks(consensus.PowTargetTimespan.Ticks / 4);
-            if (nActualTimespan > TimeSpan.FromTicks(consensus.PowTargetTimespan.Ticks * 4))
-                nActualTimespan = TimeSpan.FromTicks(consensus.PowTargetTimespan.Ticks * 4);
-
-            // Retarget
-            var bnNew = pindexLast.Header.Bits.ToBigInteger();
-            bnNew = bnNew.Multiply(BigInteger.ValueOf((long)nActualTimespan.TotalSeconds));
-            bnNew = bnNew.Divide(BigInteger.ValueOf((long)consensus.PowTargetTimespan.TotalSeconds));
-            var newTarget = new Target(bnNew);
-            if (newTarget > nProofOfWorkLimit)
-                newTarget = nProofOfWorkLimit;
-
-            return newTarget;
         }
     }
 }
