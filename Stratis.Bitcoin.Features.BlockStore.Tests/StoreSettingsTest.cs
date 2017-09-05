50721db9aa88794d650250ed448abefb5a930a33
SomeGuy <someguy.fromafrica@gmail.com>
2017-09-05 18:31:40 +1000

    Add unit test for StoreSettings

---
+++ Stratis.Bitcoin.Features.BlockStore.Tests/StoreSettingsTest.cs
@@ -0,0 +1,40 @@
+﻿using Stratis.Bitcoin.Builder;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Tests;
+using Xunit;
+
+namespace Stratis.Bitcoin.Features.BlockStore.Tests
+{
+    public class StoreSettingsTest : TestBase
+    {
+        [Fact]
+        public void CanSpecifyStoreSettings()
+        {
+            var dir = AssureEmptyDir("TestData/StoreSettingsTest/CanSpecifyStoreSettings");
+
+            NodeSettings nodeSettings = NodeSettings.FromArguments(new string[] { $"-datadir={dir}" });
+
+            var node1 = new FullNodeBuilder()
+                .UseNodeSettings(nodeSettings)
+                .UseBlockStore()
+                .Build();
+
+            var settings1 = node1.NodeService<StoreSettings>();
+
+            settings1.Load(nodeSettings);
+
+            Assert.False(settings1.TxIndex);
+
+            var node2 = new FullNodeBuilder()
+                .UseNodeSettings(nodeSettings)
+                .UseBlockStore(x => x.TxIndex = true)
+                .Build();
+
+            var settings2 = node2.NodeService<StoreSettings>();
+
+            settings2.Load(nodeSettings);
+
+            Assert.True(settings2.TxIndex);
+        }
+    }
+}
