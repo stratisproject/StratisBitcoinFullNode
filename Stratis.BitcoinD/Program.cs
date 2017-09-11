931a7baa274ae4a2b61379e44d37c74a97f4fc7a
Dan Gershony <dan.gershony@gmail.com>
2017-09-12 00:09:25 +0100

    Move minig config to settings class and start miners in the feature

--- Stratis.BitcoinD/Program.cs
+++ Stratis.BitcoinD/Program.cs
@@ -1,25 +1,19 @@
 ﻿using Stratis.Bitcoin.Builder;
 using Stratis.Bitcoin.Configuration;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 using NBitcoin;
-using Stratis.Bitcoin;
-using Stratis.Bitcoin.Configuration.Settings;
 using Stratis.Bitcoin.Features.BlockStore;
 using Stratis.Bitcoin.Features.Consensus;
 using Stratis.Bitcoin.Features.MemoryPool;
 using Stratis.Bitcoin.Features.Miner;
 using Stratis.Bitcoin.Features.RPC;
 using Stratis.Bitcoin.Utilities;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Stratis.BitcoinD
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
             if (NodeSettings.PrintHelp(args, Network.Main))
             {
                 RpcSettings.PrintHelp(Network.Main);
@@ -28,33 +22,16 @@ namespace Stratis.BitcoinD
 
             NodeSettings nodeSettings = NodeSettings.FromArguments(args);
 
-			var node = new FullNodeBuilder()
-				.UseNodeSettings(nodeSettings)
-				.UseConsensus()
-				.UseBlockStore()
-				.UseMempool()
-				.AddMining()
-				.AddRPC()
-				.Build();
+            var node = new FullNodeBuilder()
+                .UseNodeSettings(nodeSettings)
+                .UseConsensus()
+                .UseBlockStore()
+                .UseMempool()
+                .AddMining()
+                .AddRPC()
+                .Build();
 
-			// start the miner (this is temporary a miner should be started using RPC.
-			Task.Delay(TimeSpan.FromMinutes(1)).ContinueWith((t) => { TryStartMiner(args, node); });
-
-			node.Run();
-		}
-
-		private static void TryStartMiner(string[] args, IFullNode node)
-		{
-			// mining can be called from either RPC or on start
-			// to manage the on strat we need to get an address to the mining code
-			var mine = args.FirstOrDefault(a => a.Contains("mine="));
-			if (mine != null)
-			{
-				// get the address to mine to
-				var addres = mine.Replace("mine=", string.Empty);
-				var pubkey = BitcoinAddress.Create(addres, node.Network);
-				node.Services.ServiceProvider.GetService<PowMining>().Mine(pubkey.ScriptPubKey);
-			}
-		}
-	}
+            node.Run();
+        }
+    }
 }
