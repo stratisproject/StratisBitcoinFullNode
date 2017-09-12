6487e45f0ab67e8b636f30c51c3f38ae86e61b0f
Dan Gershony <dan.gershony@gmail.com>
2017-09-12 18:54:43 +0100

    Cli daemon  (#448)
    * Move minig config to settings class and start miners in the feature
    
    * Add a cli project
    
    * Review changes

--- Stratis.Bitcoin.Features.Miner/MiningFeature.cs
+++ Stratis.Bitcoin.Features.Miner/MiningFeature.cs
@@ -1,26 +1,82 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NBitcoin;
 using Stratis.Bitcoin.Builder;
 using Stratis.Bitcoin.Builder.Feature;
 using Stratis.Bitcoin.Configuration.Logging;
 using Stratis.Bitcoin.Features.Wallet;
+using Stratis.Bitcoin.Utilities;
 
 namespace Stratis.Bitcoin.Features.Miner
 {
     public class MiningFeature : FullNodeFeature
     {
-        public MiningFeature()
-        {
-        }
+	    private readonly Network network;
+	    private readonly MinerSettings minerSettings;
+	    private readonly PowMining powMining;
+	    private readonly PosMinting posMinting;
+	    private readonly WalletManager walletManager;
+	    private readonly ILogger logger;
 
-        ///<inheritdoc />
-        public override void Start()
+		private IAsyncLoop posLoop;
+	    private IAsyncLoop powLoop;
+
+		public MiningFeature(
+			Network network, 
+			MinerSettings minerSettings, 
+			ILoggerFactory loggerFactory, 
+			PowMining powMining, 
+			PosMinting posMinting = null, 
+			WalletManager walletManager = null)
+	    {
+		    this.network = network;
+		    this.minerSettings = minerSettings;
+		    this.powMining = powMining;
+		    this.posMinting = posMinting;
+		    this.walletManager = walletManager;
+		    this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
+		}
+
+		///<inheritdoc />
+		public override void Start()
         {
+	        if (this.minerSettings.Mine)
+	        {
+		        var minto = this.minerSettings.MineAddress;
+		        if (string.IsNullOrEmpty(minto)) ;
+				//	TODO: get an address from the wallet.
+
+		        if (!string.IsNullOrEmpty(minto))
+		        {
+			        this.powLoop = this.powMining.Mine(BitcoinAddress.Create(minto, this.network).ScriptPubKey);
+		        }
+			}
+
+	        if (this.minerSettings.Stake)
+	        {
+		        if (!string.IsNullOrEmpty(this.minerSettings.WalletName)
+		            && !string.IsNullOrEmpty(this.minerSettings.WalletPassword))
+		        {
+			        this.posLoop = this.posMinting.Mine(new PosMinting.WalletSecret()
+			        {
+				        WalletPassword = this.minerSettings.WalletPassword,
+				        WalletName = this.minerSettings.WalletName
+			        });
+		        }
+		        else
+		        {
+					this.logger.LogWarning("Staking not started, wallet name or password where not provided.");
+				}
+			}
         }
 
         ///<inheritdoc />
         public override void Stop()
         {
-        }
+			this.powLoop?.Dispose();
+	        this.posLoop?.Dispose();
+		}
 
         ///<inheritdoc />
         public override void ValidateDependencies(IFullNodeServiceProvider services)
@@ -37,7 +93,7 @@ namespace Stratis.Bitcoin.Features.Miner
     /// </summary>
     public static partial class IFullNodeBuilderExtensions
     {
-        public static IFullNodeBuilder AddMining(this IFullNodeBuilder fullNodeBuilder)
+        public static IFullNodeBuilder AddMining(this IFullNodeBuilder fullNodeBuilder, Action<MinerSettings> setup = null)
         {
             LoggingConfiguration.RegisterFeatureNamespace<MiningFeature>("mining");
 
@@ -50,13 +106,15 @@ namespace Stratis.Bitcoin.Features.Miner
                         services.AddSingleton<PowMining>();
                         services.AddSingleton<AssemblerFactory, PowAssemblerFactory>();
                         services.AddSingleton<MiningRPCController>();
-                    });
+	                    services.AddSingleton<MinerSettings>(new MinerSettings(setup));
+
+					});
             });
             
             return fullNodeBuilder;
         }
 
-        public static IFullNodeBuilder AddPowPosMining(this IFullNodeBuilder fullNodeBuilder)
+        public static IFullNodeBuilder AddPowPosMining(this IFullNodeBuilder fullNodeBuilder, Action<MinerSettings> setup = null)
         {
             LoggingConfiguration.RegisterFeatureNamespace<MiningFeature>("mining");
 
@@ -70,7 +128,8 @@ namespace Stratis.Bitcoin.Features.Miner
                         services.AddSingleton<PosMinting>();
                         services.AddSingleton<AssemblerFactory, PosAssemblerFactory>();
                         services.AddSingleton<MiningRPCController>();
-                    });
+	                    services.AddSingleton<MinerSettings>(new MinerSettings(setup));
+					});
             });
 
             return fullNodeBuilder;
