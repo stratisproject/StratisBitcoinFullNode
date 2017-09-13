dc0ac81562299f6692a3440517f6c7e3ba5800e9
SomeGuy <someguy.fromafrica@gmail.com>
2017-09-14 03:19:11 +1000

    Allow required indexes to be specified via IndexStore settings.

--- Stratis.Bitcoin.Features.IndexStore/IndexSetings.cs
+++ Stratis.Bitcoin.Features.IndexStore/IndexSetings.cs
@@ -1,6 +1,7 @@
 ﻿using Stratis.Bitcoin.Configuration;
 using Stratis.Bitcoin.Features.BlockStore;
 using System;
+using System.Collections.Generic;
 
 namespace Stratis.Bitcoin.Features.IndexStore
 {
@@ -10,16 +11,23 @@ namespace Stratis.Bitcoin.Features.IndexStore
     public class IndexSettings:StoreSettings
     {
         private Action<IndexSettings> callback = null;
+        public Dictionary<string, IndexExpression> indexes { get; private set; }
 
         public IndexSettings()
             :base()
         {
+            this.indexes = new Dictionary<string, IndexExpression>();
         }
 
         public IndexSettings(Action<IndexSettings> callback)
             :this()
         {
             this.callback = callback;
+        }
+
+        public void RegisterIndex(string name, string builder, bool multiValue, string[] dependencies = null)
+        {
+            this.indexes[name] = new IndexExpression(multiValue, builder, dependencies);
         }
 
         /// <summary>
