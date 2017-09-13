305e0617ab31d9a1880bfb113d5734169761c12d
quantumagi <someguy.fromafrica@gmail.com>
2017-09-13 19:34:49 +1000

    Move remaining Consensus specific files to Consensus project (#451)
    * Move StakeItem class to separate file
    
    * Move UnspentOutputsCompararer class to separate file
    
    * Make Stratis.Bitcoin.Utilities.ReaderWriterLock class accessible
    
    * Move Consensus related files to Consensus project
    
    * Fix RpcSettings class and add test case
    
    * Add default RPC port
    
    * Refactor RpcSettings class based on review

---
+++ Stratis.Bitcoin.Features.RPC.Tests/RPCSettingsTest.cs
@@ -0,0 +1,36 @@
+﻿namespace Stratis.Bitcoin.Features.RPC.Tests
+{
+    using Stratis.Bitcoin.Builder;
+    using Stratis.Bitcoin.Configuration;
+    using Stratis.Bitcoin.Features.RPC;
+    using Stratis.Bitcoin.Tests;
+    using Xunit;
+
+    public class RPCSettingsTest : TestBase
+    {
+        [Fact]
+        public void CanSpecifyRPCSettings()
+        {
+            var dir = AssureEmptyDir("TestData/StoreSettingsTest/CanSpecifyRPCSettings");
+
+            NodeSettings nodeSettings = NodeSettings.FromArguments(new string[] { $"-datadir={dir}" });
+
+            var node = new FullNodeBuilder()
+                .UseNodeSettings(nodeSettings)
+                .AddRPC(x => {
+                    x.RpcUser = "abc";
+                    x.RpcPassword = "def";
+                    x.RPCPort = 91;
+                    })
+                .Build();
+
+            var settings = node.NodeService<RpcSettings>();
+
+            settings.Load(nodeSettings);
+
+            Assert.Equal("abc", settings.RpcUser);
+            Assert.Equal("def", settings.RpcPassword);
+            Assert.Equal(91, settings.RPCPort);
+        }
+    }
+}
