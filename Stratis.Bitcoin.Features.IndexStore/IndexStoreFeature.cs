98e8432ea85ab35f3c6aa9c37ec5b96f8a54f4bf
quantumagi <someguy.fromafrica@gmail.com>
2017-09-16 01:23:50 +1000

    Allow required indexes to be specified using IndexStore settings (#458)
    * Allow required indexes to be specified via IndexStore settings.
    
    * Update the IndexStore classes to use IIndexRepository and IIndexStoreCache  where the base classes use IBlockRepository and IBlockStoreCache.
    
    * Improve configurability of the StratisBitcoinRunner class
    
    * Remove unused reference from NodeBuilder.cs
    
    * Add a test case for adding indexes via the IndexStore settings.
    
    * Re-introduce RPC tests for IndexStore

--- Stratis.Bitcoin.Features.IndexStore/IndexStoreFeature.cs
+++ Stratis.Bitcoin.Features.IndexStore/IndexStoreFeature.cs
@@ -14,8 +14,8 @@ namespace Stratis.Bitcoin.Features.IndexStore
 {
     public class IndexStoreFeature : BlockStoreFeature
     {
-        public IndexStoreFeature(ConcurrentChain chain, IConnectionManager connectionManager, Signals.Signals signals, IndexRepository indexRepository,
-            IndexStoreCache indexStoreCache, IndexBlockPuller blockPuller, IndexStoreLoop indexStoreLoop, IndexStoreManager indexStoreManager,
+        public IndexStoreFeature(ConcurrentChain chain, IConnectionManager connectionManager, Signals.Signals signals, IIndexRepository indexRepository,
+            IIndexStoreCache indexStoreCache, IndexBlockPuller blockPuller, IndexStoreLoop indexStoreLoop, IndexStoreManager indexStoreManager,
             IndexStoreSignaled indexStoreSignaled, INodeLifetime nodeLifetime, NodeSettings nodeSettings, ILoggerFactory loggerFactory, IndexSettings indexSettings) :
             base(chain, connectionManager, signals, indexRepository, indexStoreCache, blockPuller, indexStoreLoop, indexStoreManager,
                 indexStoreSignaled, nodeLifetime, nodeSettings, loggerFactory, indexSettings, name: "IndexStore")
@@ -24,7 +24,7 @@ namespace Stratis.Bitcoin.Features.IndexStore
 
         public override BlockStoreBehavior BlockStoreBehaviorFactory()
         {
-            return new IndexStoreBehavior(this.chain, this.blockRepository as IndexRepository, this.blockStoreCache as IndexStoreCache, this.loggerFactory);
+            return new IndexStoreBehavior(this.chain, this.blockRepository as IIndexRepository, this.blockStoreCache as IIndexStoreCache, this.loggerFactory);
         }
     }
 
@@ -38,8 +38,8 @@ namespace Stratis.Bitcoin.Features.IndexStore
                 .AddFeature<IndexStoreFeature>()
                 .FeatureServices(services =>
                 {
-                    services.AddSingleton<IndexRepository>();
-                    services.AddSingleton<IndexStoreCache>();
+                    services.AddSingleton<IIndexRepository, IndexRepository>();
+                    services.AddSingleton<IIndexStoreCache, IndexStoreCache>();
                     services.AddSingleton<IndexBlockPuller>();
                     services.AddSingleton<IndexStoreLoop>();
                     services.AddSingleton<IndexStoreManager>();
