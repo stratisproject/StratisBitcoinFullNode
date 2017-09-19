b98d73db87656607cbaf9c9a9ad3a3203336c4d4
nopara73 <adam.ficsor73@gmail.com>
2017-09-19 11:53:51 +0100

    Bulletproof dynamic fee estimation

--- Stratis.Bitcoin.Features.LightWallet/LightWalletFeePolicy.cs
+++ Stratis.Bitcoin.Features.LightWallet/LightWalletFeePolicy.cs
@@ -15,10 +15,11 @@ namespace Stratis.Bitcoin.Features.LightWallet
     public class LightWalletFeePolicy : IWalletFeePolicy
     {
         private readonly Money maxTxFee;
-        private readonly HttpClient httpClient;
+        private static readonly HttpClient HttpClient = new HttpClient();
         private readonly IAsyncLoopFactory asyncLoopFactory;
         private readonly INodeLifetime nodeLifetime;
         private readonly ILogger logger;
+        private bool initializedOnce;
 
         private FeeRate highTxFeePerKb;
         private FeeRate mediumTxFeePerKb;
@@ -29,29 +30,63 @@ namespace Stratis.Bitcoin.Features.LightWallet
             this.highTxFeePerKb = null;
             this.mediumTxFeePerKb = null;
             this.lowTxFeePerKb = null;
-            this.httpClient = new HttpClient();
             this.asyncLoopFactory = asyncLoopFactory;
             this.nodeLifetime = nodeLifetime;
             this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
             this.maxTxFee = new Money(0.1M, MoneyUnit.BTC);
+            this.initializedOnce = false;
         }
 
         public Task Initialize()
         {
             return this.asyncLoopFactory.Run(nameof(LightWalletFeePolicy), async token =>
             {
-                HttpResponseMessage response =
-                    await this.httpClient.GetAsync(@"http://api.blockcypher.com/v1/btc/main", HttpCompletionOption.ResponseContentRead, token)
-                    .ConfigureAwait(false);
+                // This will run evry 3 to 10 minutes randomly
+                // So the API provider is not able to identify our transaction with a timing attack
+                var waitMinutes = new Random().Next(3, 10);
+
+                HttpResponseMessage response = null;
+                try
+                {
+                    // TestNet fee estimation is useless, because the miners don't select transactions, like mainnet miners
+                    // Test results on RegTest and TestNet are more illustrative with mainnet fees
+                    response =
+                        await HttpClient.GetAsync(@"http://api.blockcypher.com/v1/btc/main", HttpCompletionOption.ResponseContentRead, token)
+                        .ConfigureAwait(false);
+                }
+                catch (OperationCanceledException)
+                {
+                    return;
+                }
+                catch (Exception ex) when (ex is HttpRequestException || ex is TimeoutException)
+                {
+                    response = null;
+                }
+                if (response == null || !response.IsSuccessStatusCode)
+                {
+                    // If it's already been initialized once just keep using the feerate we already have
+                    if (this.initializedOnce)
+                    {
+                        await Task.Delay(TimeSpan.FromMinutes(waitMinutes), token).ContinueWith(t => { }).ConfigureAwait(false);
+                        return;
+                    }
+                    else // Try again 3 seconds later, first time fee query is critical
+                    {
+                        await Task.Delay(TimeSpan.FromSeconds(3), token).ContinueWith(t => { }).ConfigureAwait(false);
+                        return;
+                    }
+                }
+                if (token.IsCancellationRequested) return;
 
                 var json = JObject.Parse(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
                 this.lowTxFeePerKb = new FeeRate(new Money((int)(json.Value<decimal>("low_fee_per_kb")), MoneyUnit.Satoshi));
                 this.mediumTxFeePerKb = new FeeRate(new Money((int)(json.Value<decimal>("medium_fee_per_kb")), MoneyUnit.Satoshi));
                 this.highTxFeePerKb = new FeeRate(new Money((int)(json.Value<decimal>("high_fee_per_kb")), MoneyUnit.Satoshi));
 
+                this.initializedOnce = true;
+
                 if (token.IsCancellationRequested) return;
 
-                var waitMinutes = new Random().Next(3, 10);
                 await Task.Delay(TimeSpan.FromMinutes(waitMinutes), token).ContinueWith(t => { }).ConfigureAwait(false);
             },
                 this.nodeLifetime.ApplicationStopping,
