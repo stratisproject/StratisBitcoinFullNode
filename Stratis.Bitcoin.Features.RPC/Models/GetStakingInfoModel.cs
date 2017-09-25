db460726013e3532939c63a8423cfe392aa79c2f
Aprogiena <aprogiena@users.noreply.github.com>
2017-09-25 17:51:07 +0100

    getstakinginfo model

---
+++ Stratis.Bitcoin.Features.RPC/Models/GetStakingInfoModel.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+
+namespace Stratis.Bitcoin.Features.RPC.Models
+{
+    /// <summary>
+    /// Data structure returned by RPC command "getstakinginfo".
+    /// </summary>
+    public class GetStakingInfoModel
+    {
+        /// <summary><c>true</c> if the staking is enabled.</summary>
+        [JsonProperty(PropertyName = "enabled")]
+        public bool Enabled { get; set; }
+
+        /// <summary><c>true</c> if the node is currently staking.</summary>
+        [JsonProperty(PropertyName = "staking")]
+        public bool Staking { get; set; }
+
+        /// <summary>Last recorded warning message related to staking.</summary>
+        [JsonProperty(PropertyName = "errors")]
+        public string Errors { get; set; }
+
+        /// <summary>Size of the next block the node wants to mine in bytes.</summary>
+        [JsonProperty(PropertyName = "currentblocksize")]
+        public long CurrentBlockSize { get; set; }
+
+        /// <summary>Number of transactions the node wants to put in the next block.</summary>
+        [JsonProperty(PropertyName = "currentblocktx")]
+        public long CurrentBlockTx { get; set; }
+
+        /// <summary>Number of transactions in the memory pool.</summary>
+        [JsonProperty(PropertyName = "pooledtx")]
+        public long PooledTx { get; set; }
+
+        /// <summary>Target difficulty that the next block must meet.</summary>
+        [JsonProperty(PropertyName = "difficulty")]
+        public double Difficulty { get; set; }
+
+        /// <summary>Length of the last staking search interval in seconds.</summary>
+        [JsonProperty(PropertyName = "search-interval")]
+        public int SearchInterval { get; set; }
+
+        /// <summary>Staking weight of the node.</summary>
+        [JsonProperty(PropertyName = "weight")]
+        public long Weight { get; set; }
+
+        /// <summary>Estimation of the total staking weight of all nodes on the network.</summary>
+        [JsonProperty(PropertyName = "netstakeweight")]
+        public long NetStakeWeight { get; set; }
+
+        /// <summary>Expected time of the node to find new block in seconds.</summary>
+        [JsonProperty(PropertyName = "expectedtime")]
+        public long ExpectedTime { get; set; }
+    }
+}
