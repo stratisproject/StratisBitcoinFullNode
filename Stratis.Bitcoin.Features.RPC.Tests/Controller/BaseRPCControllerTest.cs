db6ef89e62968caee9de905a4fd68d656254f957
Aprogiena <aprogiena@users.noreply.github.com>
2017-09-27 09:50:40 +0100

    add comments and staking node

--- Stratis.Bitcoin.Features.RPC.Tests/Controller/BaseRPCControllerTest.cs
+++ Stratis.Bitcoin.Features.RPC.Tests/Controller/BaseRPCControllerTest.cs
@@ -3,20 +3,51 @@ using Stratis.Bitcoin.Configuration;
 using Stratis.Bitcoin.Features.BlockStore;
 using Stratis.Bitcoin.Features.Consensus;
 using Stratis.Bitcoin.Features.MemoryPool;
+using Stratis.Bitcoin.Features.Miner;
+using Stratis.Bitcoin.Features.Wallet;
 using Stratis.Bitcoin.Tests;
 
 namespace Stratis.Bitcoin.Features.RPC.Tests.Controller
 {
+    /// <summary>
+    /// Base class for RPC tests.
+    /// </summary>
     public abstract class BaseRPCControllerTest : TestBase
     {
+        /// <summary>
+        /// Builds a node with basic services and RPC enabled.
+        /// </summary>
+        /// <param name="dir">Data directory that the node should use.</param>
+        /// <returns>Interface to the newly built node.</returns>
         public IFullNode BuildServicedNode(string dir)
         {
-            var nodeSettings = NodeSettings.FromArguments(new string[] { $"-datadir={dir}" });
+            NodeSettings nodeSettings = NodeSettings.FromArguments(new string[] { $"-datadir={dir}" });
             var fullNodeBuilder = new FullNodeBuilder(nodeSettings);
             IFullNode fullNode = fullNodeBuilder
                 .UseConsensus()
                 .UseBlockStore()
                 .UseMempool()
+                .AddRPC()
+                .Build();
+
+            return fullNode;
+        }
+
+        /// <summary>
+        /// Builds a node with POS miner and RPC enabled.
+        /// </summary>
+        /// <param name="dir">Data directory that the node should use.</param>
+        /// <returns>Interface to the newly built node.</returns>
+        public IFullNode BuildStakingNode(string dir)
+        {
+            NodeSettings nodeSettings = NodeSettings.FromArguments(new string[] { $"-datadir={dir}", "-stake=1" });
+            var fullNodeBuilder = new FullNodeBuilder(nodeSettings);
+            IFullNode fullNode = fullNodeBuilder
+                .UseStratisConsensus()
+                .UseBlockStore()
+                .UseMempool()
+                .UseWallet()
+                .AddPowPosMining()
                 .AddRPC()
                 .Build();
 
