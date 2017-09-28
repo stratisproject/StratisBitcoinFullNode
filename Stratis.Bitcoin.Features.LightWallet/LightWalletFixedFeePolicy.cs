64310eef9a367d39a626547e5aea7053b79059fc
mikedennis <strykitenterprises@gmail.com>
2017-09-28 02:51:11 -0600

    Add stratis fee policy to light wallet (#478)
    * Add stratis fee policy to light wallet.
    
    * Make min and fallback transaction fees configurable.  Added some extension methods to network class.
    
    * Add some logging of tx fee configuration.
    
    * Fix logging of tx fee configuration. Fix issue with network detection.
    
    * Use min tx fee from node settings.
    
    * Unit tests for the light wallet's fixed fee policy.
    
    * Update to .net core 2.0
    
    * Fix appveyor build issue

---
+++ Stratis.Bitcoin.Features.LightWallet/LightWalletFixedFeePolicy.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Logging;
+using NBitcoin;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Features.Wallet;
+using System.Threading.Tasks;
+
+namespace Stratis.Bitcoin.Features.LightWallet
+{
+    /// <summary>
+    /// Light wallet fixed fee policy used for stratis network.
+    /// </summary>
+    /// <seealso cref="https://github.com/stratisproject/stratisX/blob/master/src/wallet.cpp#L1437">StratisX fee calculation.</seealso>
+    public class LightWalletFixedFeePolicy : IWalletFeePolicy
+    {
+        /// <summary>Logger instance for this class.</summary>
+        private readonly ILogger logger;
+
+        /// <summary>Fixed fee rate to use for this policy.</summary>
+        public FeeRate TxFeeRate { get; set; }
+
+        /// <summary>Minimum fee rate to use for this policy.</summary>
+        public FeeRate MinTxFee { get; set; }
+
+        /// <summary>
+        /// Constructor for the light wallet fixed fee policy.
+        /// </summary>
+        /// <param name="loggerFactory">The factory for building logger instances.</param>
+        /// <param name="settings">The node settings.</param>
+        public LightWalletFixedFeePolicy(ILoggerFactory loggerFactory, NodeSettings settings)
+        {
+            this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
+
+            this.MinTxFee = settings.MinTxFee;
+            this.TxFeeRate = this.MinTxFee;
+        }
+
+        /// <inheritdoc />
+        public FeeRate GetFeeRate(int confirmTarget)
+        {
+            return this.TxFeeRate;
+        }
+
+        /// <inheritdoc />
+        public Money GetMinimumFee(int txBytes, int confirmTarget)
+        {
+            return this.GetMinimumFee(txBytes, confirmTarget, Money.Zero);
+        }
+
+        /// <inheritdoc />
+        public Money GetMinimumFee(int txBytes, int confirmTarget, Money targetFee)
+        {
+            return this.MinTxFee.GetFee(txBytes);
+        }
+
+        /// <inheritdoc />
+        public Money GetRequiredFee(int txBytes)
+        {
+            return this.TxFeeRate.GetFee(txBytes);
+        }
+
+        /// <inheritdoc />
+        public Task Initialize()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- Stratis.Bitcoin/Utilities/NetworkExtensions.cs
+++ Stratis.Bitcoin/Utilities/NetworkExtensions.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using NBitcoin;
+﻿using NBitcoin;
 
 namespace Stratis.Bitcoin.Utilities
 {
@@ -13,6 +10,18 @@ namespace Stratis.Bitcoin.Utilities
         /// <summary>Fake height value used in Coins to signify they are only in the memory pool (since 0.8).</summary>
         public const int MempoolHeight = 0x7FFFFFFF;
 
+        /// <summary>Default minimum transaction fee for bitcoin network.</summary>
+        private const long DefaultBitcoinMinTxFee = 1000;
+
+        /// <summary>Default fallback fee for bitcoin network.</summary>
+        private const long DefaultBitcoinFallbackTxFee = 20000;
+
+        /// <summary>Default minimum transaction fee for stratis network.</summary>
+        private const long DefaultStratisMinTxFee = 10000;
+
+        /// <summary>Default fallback fee for stratis network.</summary>
+        private const long DefaultStratisFallbackTxFee = 40000;
+
         /// <summary>
         /// Determines whether this network is a test network.
         /// </summary>
@@ -21,6 +30,36 @@ namespace Stratis.Bitcoin.Utilities
         public static bool IsTest(this Network network)
         {
             return network.Name.ToLowerInvariant().Contains("test");
+        }
+
+        /// <summary>
+        /// Determines whether this network is a bitcoin network.
+        /// </summary>
+        /// <param name="network">The network.</param>
+        /// <returns><c>true</c> if the specified network is bitcoin, <c>false</c> otherwise.</returns>
+        public static bool IsBitcoin(this Network network)
+        {
+           return !network.Name.ToLowerInvariant().Contains("stratis");
+        }
+
+        /// <summary>
+        /// The default minimum transaction fee for the network.
+        /// </summary>
+        /// <param name="network">The network.</param>
+        /// <returns>The default minimum transaction fee.</returns>
+        public static long DefaultMinTxFee(this Network network)
+        {
+            return network.IsBitcoin() ? DefaultBitcoinMinTxFee : DefaultStratisMinTxFee;
+        }
+
+        /// <summary>
+        /// The default fall back transaction fee for the network.
+        /// </summary>
+        /// <param name="network">The network.</param>
+        /// <returns>The default fallback transaction fee.</returns>
+        public static long DefaultFallbackTxFee(this Network network)
+        {
+            return network.IsBitcoin() ? DefaultBitcoinFallbackTxFee : DefaultStratisFallbackTxFee;
         }
     }
 }
