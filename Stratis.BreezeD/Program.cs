3192476f0a7fa848de25937f529d52d5f38dd9b0
Jeremy Bokobza <bokobza@gmail.com>
2017-09-29 11:24:43 +0100

    Made the BreezeD console app sexy and light again. Removed the need to add light to start the daemons as BreezeD is light by default.

--- Stratis.BreezeD/Program.cs
+++ Stratis.BreezeD/Program.cs
@@ -5,12 +5,8 @@ using Stratis.Bitcoin.Configuration;
 using NBitcoin;
 using NBitcoin.Protocol;
 using Stratis.Bitcoin.Api;
-using Stratis.Bitcoin.Features.BlockStore;
-using Stratis.Bitcoin.Features.Consensus;
 using Stratis.Bitcoin.Features.LightWallet;
-using Stratis.Bitcoin.Features.MemoryPool;
 using Stratis.Bitcoin.Features.Notifications;
-using Stratis.Bitcoin.Features.Wallet;
 using Stratis.Bitcoin.Utilities;
 
 namespace Stratis.BreezeD
@@ -24,119 +20,41 @@ namespace Stratis.BreezeD
         {
             IFullNodeBuilder fullNodeBuilder = null;
 
-            // get the api uri 
+            // Get the API uri. 
             var apiUri = args.GetValueOf("apiuri");
+            var isTestNet = args.Contains("-testnet");
+            var isStratis = args.Contains("stratis");
 
-            if (args.Contains("stratis"))
+            NodeSettings nodeSettings;
+            if (isStratis)
             {
                 if (NodeSettings.PrintHelp(args, Network.StratisMain))
                     return;
 
-                var network = args.Contains("-testnet") ? InitStratisTest() : Network.StratisMain;
-                if (args.Contains("-testnet"))
+                var network = isTestNet ? Network.StratisTest : Network.StratisMain;
+                if (isTestNet)
                     args = args.Append("-addnode=13.64.76.48").ToArray(); // TODO: fix this temp hack 
-                var nodeSettings = NodeSettings.FromArguments(args, "stratis", network, ProtocolVersion.ALT_PROTOCOL_VERSION);                
-                nodeSettings.ApiUri = new Uri(string.IsNullOrEmpty(apiUri) ? DefaultStratisUri : apiUri);
 
-                if (args.Contains("light"))
-                {
-                    fullNodeBuilder = new FullNodeBuilder()
-                        .UseNodeSettings(nodeSettings)
-                        .UseLightWallet()
-                        .UseBlockNotification()
-                        .UseTransactionNotification()
-                        .UseApi();
-                }
-                else
-                {
-                    fullNodeBuilder = new FullNodeBuilder()
-                        .UseNodeSettings(nodeSettings)
-                        .UseStratisConsensus()
-                        .UseBlockStore()
-                        .UseMempool()
-                        .UseWallet()
-                        .UseApi();
-                }
+                nodeSettings = NodeSettings.FromArguments(args, "stratis", network, ProtocolVersion.ALT_PROTOCOL_VERSION);
+                nodeSettings.ApiUri = new Uri(string.IsNullOrEmpty(apiUri) ? DefaultStratisUri : apiUri);
             }
             else
             {
-                NodeSettings nodeSettings = NodeSettings.FromArguments(args);
+                nodeSettings = NodeSettings.FromArguments(args);
                 nodeSettings.ApiUri = new Uri(string.IsNullOrEmpty(apiUri) ? DefaultBitcoinUri : apiUri);
-
-                if (args.Contains("light"))
-                {
-                    fullNodeBuilder = new FullNodeBuilder()
-                        .UseNodeSettings(nodeSettings)
-                        .UseLightWallet()
-                        .UseBlockNotification()
-                        .UseTransactionNotification()
-                        .UseApi();
-                }
-                else
-                {
-                    fullNodeBuilder = new FullNodeBuilder()
-                        .UseNodeSettings(nodeSettings)
-                        .UseConsensus()
-                        .UseBlockStore()
-                        .UseMempool()
-                        .UseWallet()
-                        .UseApi();
-                }
             }
-            
+
+            fullNodeBuilder = new FullNodeBuilder()
+                .UseNodeSettings(nodeSettings)
+                .UseLightWallet()
+                .UseBlockNotification()
+                .UseTransactionNotification()
+                .UseApi();
+
             var node = fullNodeBuilder.Build();
 
-            //start Full Node - this will also start the API
+            // Start Full Node - this will also start the API.
             node.Run();
         }
-
-        private static Network InitStratisTest()
-        {
-            Block.BlockSignature = true;
-            Transaction.TimeStamp = true;
-
-            var consensus = Network.StratisMain.Consensus.Clone();
-            consensus.PowLimit = new Target(uint256.Parse("0000ffff00000000000000000000000000000000000000000000000000000000"));
-
-            // The message start string is designed to be unlikely to occur in normal data.
-            // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
-            // a large 4-byte int at any alignment.
-            var pchMessageStart = new byte[4];
-            pchMessageStart[0] = 0x71;
-            pchMessageStart[1] = 0x31;
-            pchMessageStart[2] = 0x21;
-            pchMessageStart[3] = 0x11;
-            var magic = BitConverter.ToUInt32(pchMessageStart, 0); //0x5223570; 
-
-            var genesis = Network.StratisMain.GetGenesis().Clone();
-            genesis.Header.Time = 1493909211;
-            genesis.Header.Nonce = 2433759;
-            genesis.Header.Bits = consensus.PowLimit;
-            consensus.HashGenesisBlock = genesis.GetHash();
-
-            Guard.Assert(consensus.HashGenesisBlock == uint256.Parse("0x00000e246d7b73b88c9ab55f2e5e94d9e22d471def3df5ea448f5576b1d156b9"));
-
-            var builder = new NetworkBuilder()
-                .SetName("StratisTest")
-                .SetConsensus(consensus)
-                .SetMagic(magic)
-                .SetGenesis(genesis)
-                .SetPort(26178)
-                .SetRPCPort(26174)
-                .SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, new byte[] { (65) })
-                .SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, new byte[] { (196) })
-                .SetBase58Bytes(Base58Type.SECRET_KEY, new byte[] { (65 + 128) })
-                .SetBase58Bytes(Base58Type.ENCRYPTED_SECRET_KEY_NO_EC, new byte[] { 0x01, 0x42 })
-                .SetBase58Bytes(Base58Type.ENCRYPTED_SECRET_KEY_EC, new byte[] { 0x01, 0x43 })
-                .SetBase58Bytes(Base58Type.EXT_PUBLIC_KEY, new byte[] { (0x04), (0x88), (0xB2), (0x1E) })
-                .SetBase58Bytes(Base58Type.EXT_SECRET_KEY, new byte[] { (0x04), (0x88), (0xAD), (0xE4) })
-                .AddDNSSeeds(new[]
-                {
-                    new DNSSeedData("stratisplatform.com", "testnode1.stratisplatform.com"),
-                });
-
-            return builder.BuildAndRegister();
-        }
-
     }
 }
