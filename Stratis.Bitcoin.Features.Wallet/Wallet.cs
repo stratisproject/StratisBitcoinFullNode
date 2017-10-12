a6475390af42bdd739b361642164311548070835
Dan Gershony <dan.gershony@gmail.com>
2017-10-12 11:18:12 +0100

    Wallet balance correctly calculated  (#571)
    * Add wallet stats to node and correctly calculate confirmations
    
    * Remove new line

--- Stratis.Bitcoin.Features.Wallet/Wallet.cs
+++ Stratis.Bitcoin.Features.Wallet/Wallet.cs
@@ -610,8 +610,12 @@ namespace Stratis.Bitcoin.Features.Wallet
             List<UnspentOutputReference> unspentOutputs = new List<UnspentOutputReference>();
             foreach (var address in this.GetCombinedAddresses())
             {
+                // A block that is at the tip has 1 confirmation.
+                // When calculating the confirmations the tip must be advanced by one.
+
+                var countFrom = currentChainHeight + 1;
                 var unspentTransactions = address.UnspentTransactions()
-                    .Where(a => currentChainHeight - (a.BlockHeight ?? currentChainHeight) >= confirmations).ToList();
+                    .Where(a => countFrom - (a.BlockHeight ?? countFrom) >= confirmations).ToList();
 
                 foreach (var transactionData in unspentTransactions)
                 {
