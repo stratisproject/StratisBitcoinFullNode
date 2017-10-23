ce7248b687ddf9e69cc31c825a01352c66a8fb4f
Aprogiena <aprogiena@users.noreply.github.com>
2017-10-23 16:31:53 +0100

    fixing the asynclock + tests
    introduction of cancellation token make it more complex. we need to throw operation cancelled exception in case the cancellation token triggers, otherwise we would enter the critical section without having the lock and then we would release the lock that we don't own

--- Stratis.Bitcoin/Utilities/AsyncLock.cs
+++ Stratis.Bitcoin/Utilities/AsyncLock.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -47,7 +45,7 @@ namespace Stratis.Bitcoin.Utilities
         /// </summary>
         private sealed class Releaser : IDisposable
         {
-            /// <summary>Parent lock to be released when this releaser is disposed.</summary>
+            /// <summary>Parent lock to be released when this releaser is disposed, or <c>null</c> if no action should be taken on disposing it.</summary>
             private readonly AsyncLock toRelease;
 
             /// <summary>
@@ -61,14 +59,17 @@ namespace Stratis.Bitcoin.Utilities
             /// <inheritdoc />
             public void Dispose()
             {
-                this.toRelease.semaphore.Release();
+                this.toRelease?.semaphore.Release();
             }
         }
 
         /// <summary>Internal synchronization primitive used as a mutex to only allow one thread to occupy the critical section.</summary>
         private readonly SemaphoreSlim semaphore;
 
-        /// <summary>Helper object that allows implementation of disposable lock for convenient use with <c>using</c> statement.</summary>
+        /// <summary>
+        /// Helper object that allows implementation of disposable lock for convenient use with <c>using</c> statement.
+        /// <para>This releaser is used when the lock has been acquired and disposing it will release the lock.</para>
+        /// </summary>
         /// <remarks>We use the disposable interfaced in a task here to avoid allocations on acquiring the lock when it is free.</remarks>
         private readonly Task<IDisposable> releaser;
 
@@ -86,15 +87,31 @@ namespace Stratis.Bitcoin.Utilities
         /// </summary>
         /// <param name="cancel">Cancellation token that can be used to abort waiting for the lock.</param>
         /// <returns>Disposable interface to enable using construct. Disposing it releases the lock.</returns>
+        /// <exception cref="OperationCanceledException">Thrown when the <paramref name="cancel"/> is triggered and the lock is not acquired.</exception>
         public Task<IDisposable> LockAsync(CancellationToken cancel = default(CancellationToken))
         {
             Task wait = this.semaphore.WaitAsync(cancel);
-
+            
+            // If the lock is available, quickly return.
             if (wait.IsCompleted)
-                return this.releaser;
+            {
+                // We only hold the lock if the task was completed successfully.
+                // If the task was cancelled, we don't hold the lock and we need to throw.
+                if (wait.IsCanceled) throw new OperationCanceledException();
 
-            return wait.ContinueWith((task, state) => (IDisposable)state, this.releaser.Result, 
-                cancel, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+                return this.releaser;
+            }
+
+            // If the lock is not available, we wait until it is available 
+            // or the wait is cancelled.
+            return wait.ContinueWith((task, state) => 
+            {
+                // We only hold the lock if the task was completed successfully.
+                // If the task was cancelled, we don't hold the lock and we need to throw.
+                if (task.IsCanceled) throw new OperationCanceledException();
+
+                return (IDisposable)state;
+            }, this.releaser.Result, cancel, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
         }
 
         /// <summary>
@@ -102,9 +119,12 @@ namespace Stratis.Bitcoin.Utilities
         /// </summary>
         /// <param name="cancel">Cancellation token that can be used to abort waiting for the lock.</param>
         /// <returns>Disposable interface to enable using construct. Disposing it releases the lock.</returns>
+        /// <exception cref="OperationCanceledException">Thrown when the <paramref name="cancel"/> is triggered and the lock is not acquired.</exception>
         public IDisposable Lock(CancellationToken cancel = default(CancellationToken))
         {
             this.semaphore.Wait(cancel);
+
+            // We are holding the lock here, so we will want unlocking.
             return this.releaser.Result;
         }
     }
