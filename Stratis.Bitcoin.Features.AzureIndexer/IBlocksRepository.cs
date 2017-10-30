63a35b3bf7ceae6e1b3d40cc5b3b210b27b16268
SomeGuy FromAfrica <someguy.fromafrica@gmail.com>
2017-10-30 21:41:06 +1100

    Add block fetcher to read blocks from full node

--- Stratis.Bitcoin.Features.AzureIndexer/IBlocksRepository.cs
+++ Stratis.Bitcoin.Features.AzureIndexer/IBlocksRepository.cs
@@ -1,12 +1,9 @@
-﻿using NBitcoin.Protocol;
-using System;
+﻿using NBitcoin;
+using NBitcoin.Protocol;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
-namespace NBitcoin.Indexer.IndexTasks
+namespace Stratis.Bitcoin.Features.AzureIndexer.IndexTasks
 {
     public interface IBlocksRepository
     {
@@ -25,6 +22,34 @@ namespace NBitcoin.Indexer.IndexTasks
         public IEnumerable<Block> GetBlocks(IEnumerable<uint256> hashes, CancellationToken cancellationToken)
         {
             return _Node.GetBlocks(hashes, cancellationToken);
+        }
+
+        #endregion
+    }
+
+    public class FullNodeBlocksRepository : IBlocksRepository
+    {
+        FullNode _Node;
+        BlockStore.BlockRepository _Repo;
+
+        public FullNodeBlocksRepository(FullNode node)
+        {
+            _Node = node;
+            _Repo = node.NodeService<BlockStore.IBlockRepository>() as BlockStore.BlockRepository;
+        }
+
+        #region IBlocksRepository Members
+
+        public IEnumerable<Block> GetBlocks(IEnumerable<uint256> hashes, CancellationToken cancellationToken)
+        {
+            foreach (var hash in hashes)
+            {
+                if (cancellationToken.IsCancellationRequested) break;
+                if (hash == _Node.Network.GenesisHash)
+                    yield return _Node.Network.GetGenesis();
+                else
+                    yield return _Repo.GetAsync(hash).GetAwaiter().GetResult();
+            }
         }
 
         #endregion
