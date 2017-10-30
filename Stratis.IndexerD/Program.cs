ff3953e857c75f8e3f16f9da30780753bc5efbc4
SomeGuy FromAfrica <someguy.fromafrica@gmail.com>
2017-10-30 21:51:44 +1100

    Add new Stratis.IndexerD project

---
+++ Stratis.IndexerD/Program.cs
@@ -0,0 +1,39 @@
+﻿using NBitcoin;
+using NBitcoin.Protocol;
+using Stratis.Bitcoin.Builder;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Features.BlockStore;
+using Stratis.Bitcoin.Features.Consensus;
+using Stratis.Bitcoin.Features.AzureIndexer;
+using Stratis.Bitcoin.Utilities;
+using System.Linq;
+
+namespace Stratis.Bitcoin.Indexer.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Network network = args.Contains("-testnet") ? Network.StratisTest : Network.StratisMain;
+
+            if (NodeSettings.PrintHelp(args, network))
+            {
+                AzureIndexerSettings.PrintHelp(network);
+                return;
+            }
+
+            NodeSettings nodeSettings = NodeSettings.FromArguments(args, "stratis", network, ProtocolVersion.ALT_PROTOCOL_VERSION);
+
+            // NOTES: running BTC and STRAT side by side is not possible yet as the flags for serialization are static
+
+            var node = new FullNodeBuilder()
+                .UseNodeSettings(nodeSettings)
+                .UseStratisConsensus()
+                .UseBlockStore()
+                .UseAzureIndexer()
+                .Build();
+
+            node.Run();
+        }
+    }
+}
