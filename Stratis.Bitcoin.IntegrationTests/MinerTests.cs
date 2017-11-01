c37d4d86a1f1678a880f7fe9bad1d12d90203e7a
Aprogiena <aprogiena@users.noreply.github.com>
2017-11-01 17:59:29 +0000

    conflict resolution

--- Stratis.Bitcoin.IntegrationTests/MinerTests.cs
+++ Stratis.Bitcoin.IntegrationTests/MinerTests.cs
@@ -136,9 +136,9 @@ namespace Stratis.Bitcoin.IntegrationTests
                 var nodeSettings = NodeSettings.Default();
 
                 ConnectionManager connectionManager = new ConnectionManager(this.network, new NodeConnectionParameters(), nodeSettings, loggerFactory, new NodeLifetime());
-                LookaheadBlockPuller blockPuller = new LookaheadBlockPuller(chain, connectionManager, new LoggerFactory());
+                LookaheadBlockPuller blockPuller = new LookaheadBlockPuller(this.chain, connectionManager, new LoggerFactory());
 
-                this.consensus = new ConsensusLoop(new AsyncLoopFactory(loggerFactory), consensusValidator, new NodeLifetime(), this.chain, this.cachedCoinView, blockPuller, new NodeDeployments(this.network, chain), loggerFactory, new ChainState(new FullNode()), connectionManager, new Signals.Signals(), new Checkpoints(this.network));
+                this.consensus = new ConsensusLoop(new AsyncLoopFactory(loggerFactory), consensusValidator, new NodeLifetime(), this.chain, this.cachedCoinView, blockPuller, new NodeDeployments(this.network, this.chain), loggerFactory, new ChainState(new FullNode()), connectionManager, dateTimeProvider, new Signals.Signals(), new Checkpoints(this.network));
                 this.consensus.Start();
 
                 this.entry.Fee(11);
