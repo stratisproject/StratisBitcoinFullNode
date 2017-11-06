d4aa66c333b3f8efc5f424c37754a139d1b9f366
Jeremy Bokobza <bokobza@users.noreply.github.com>
2017-11-06 21:19:07 +0000

    Code styling improvements for Stratis.Bitcoin.Configuration (#709)
    * Added launchSettings file for testnet launch
    
    * Prevent the console apps from crashing  if an exception is thrown while initializing the comman dline arguments.
    
    * Improved code styling in Stratis.Bitcoin\Configuration
    
    * move the node building inside the try-catch of the console app.

--- src/Stratis.Bitcoin/Configuration/Settings/ConnectionManagerSettings.cs
+++ src/Stratis.Bitcoin/Configuration/Settings/ConnectionManagerSettings.cs
@@ -13,31 +13,21 @@ namespace Stratis.Bitcoin.Configuration.Settings
         /// </summary>
         public ConnectionManagerSettings()
         {
+            this.Connect = new List<IPEndPoint>();
+            this.AddNode = new List<IPEndPoint>();
+            this.Listen = new List<NodeServerEndpoint>();
         }
 
         /// <summary>List of exclusive end points that the node should be connected to.</summary>
-        public List<IPEndPoint> Connect
-        {
-            get; set;
-        } = new List<IPEndPoint>();
+        public List<IPEndPoint> Connect { get; set; } 
 
         /// <summary>List of end points that the node should try to connect to.</summary>
-        public List<IPEndPoint> AddNode
-        {
-            get; set;
-        } = new List<IPEndPoint>();
+        public List<IPEndPoint> AddNode { get; set; }
 
         /// <summary>List of network interfaces on which the node should listen on.</summary>
-        public List<NodeServerEndpoint> Listen
-        {
-            get; set;
-        } = new List<NodeServerEndpoint>();
+        public List<NodeServerEndpoint> Listen { get; set; }
 
         /// <summary>External (or public) IP address of the node.</summary>
-        public IPEndPoint ExternalEndpoint
-        {
-            get;
-            internal set;
-        }
+        public IPEndPoint ExternalEndpoint { get; internal set; }
     }
 }
