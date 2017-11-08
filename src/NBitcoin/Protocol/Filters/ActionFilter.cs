7e3277156f87900f50171d91f5ee7f34fe3c1459
Aprogiena <Aprogiena@users.noreply.github.com>
2017-11-08 09:19:10 +0000

    code style (#717)

--- src/NBitcoin/Protocol/Filters/ActionFilter.cs
+++ src/NBitcoin/Protocol/Filters/ActionFilter.cs
@@ -1,34 +1,26 @@
-﻿#if !NOSOCKET
+﻿using NBitcoin.Protocol;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NBitcoin.Protocol.Filters
 {
-	public class ActionFilter : INodeFilter
-	{
-		Action<IncomingMessage, Action> _OnIncoming;
-		Action<Node, Payload, Action> _OnSending;
-		public ActionFilter(Action<IncomingMessage, Action> onIncoming = null, Action<Node, Payload, Action> onSending = null)
-		{
-			_OnIncoming = onIncoming ?? new Action<IncomingMessage, Action>((m, n) => n());
-			_OnSending = onSending ?? new Action<Node, Payload, Action>((m, p, n) => n());
-		}
-		#region INodeFilter Members
+    public class ActionFilter : INodeFilter
+    {
+        private readonly Action<IncomingMessage, Action> onIncoming;
+        private readonly Action<Node, Payload, Action> onSending;
+        public ActionFilter(Action<IncomingMessage, Action> onIncoming = null, Action<Node, Payload, Action> onSending = null)
+        {
+            this.onIncoming = onIncoming ?? new Action<IncomingMessage, Action>((m, n) => n());
+            this.onSending = onSending ?? new Action<Node, Payload, Action>((m, p, n) => n());
+        }
 
-		public void OnReceivingMessage(IncomingMessage message, Action next)
-		{
-			_OnIncoming(message, next);
-		}
+        public void OnReceivingMessage(IncomingMessage message, Action next)
+        {
+            this.onIncoming(message, next);
+        }
 
-		public void OnSendingMessage(Node node, Payload payload, Action next)
-		{
-			_OnSending(node, payload, next);
-		}
-
-		#endregion
-	}
+        public void OnSendingMessage(Node node, Payload payload, Action next)
+        {
+            this.onSending(node, payload, next);
+        }
+    }
 }
-#endif
