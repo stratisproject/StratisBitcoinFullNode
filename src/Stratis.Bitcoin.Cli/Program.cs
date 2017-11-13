8b4578274c1d3f3d935a79875e9532cebf67d1ad
SomeGuy FromAfrica <someguy.fromafrica@gmail.com>
2017-11-13 15:38:42 +1100

    Changes based on feedback

--- src/Stratis.Bitcoin.Cli/Program.cs
+++ src/Stratis.Bitcoin.Cli/Program.cs
@@ -23,12 +23,12 @@ namespace Stratis.Bitcoin.Cli
         {
             try
             {
+                // Display help if required.
                 if (args.Length == 0 || args.Contains("-help") || args.Contains("--help"))
                 {
                     var builder = new StringBuilder();
                     builder.AppendLine("Usage:");
-                    // TODO: Shouldn't this be dotnet run instead of dotnet exec?
-                    builder.AppendLine(" dotnet exec <Stratis.Bitcoin.Cli/Stratis.Bitcoin.Cli.dll> [network-name] [command] [arguments]");
+                    builder.AppendLine(" dotnet run <Stratis.Bitcoin.Cli/Stratis.Bitcoin.Cli.dll> [network-name] [command] [arguments]");
                     builder.AppendLine();
                     builder.AppendLine("Command line arguments:");
                     builder.AppendLine();
@@ -39,23 +39,26 @@ namespace Stratis.Bitcoin.Cli
                     return;
                 }
             
-                // hack until static flags are removed
+                // Determine API port.
                 string blockchain = "bitcoin";
+                int apiPort = 37220;
                 if (args.Any(a => a.Contains("stratis")))
                 {
                     blockchain = "stratis";
+                    apiPort = 37221;
+                    // hack until static flags are removed.
                     var s = Network.StratisMain;
                     var st = Network.StratisTest;
                 }
 
-                // The first argument is the network name
+                // The first argument is the network name.
                 var network = Network.GetNetwork(args.First());
 
                 // API calls require both the contoller name and the method name separated by "/".
                 // If this is not an API call then assume it is an RPC call.
                 if (!args.ElementAt(1).Contains("/"))
                 {
-                    // Process RPC call
+                    // Process RPC call.
                     try
                     {
                         NodeSettings nodeSettings = NodeSettings.FromArguments(args, blockchain, network);
@@ -65,56 +68,80 @@ namespace Stratis.Bitcoin.Cli
                         // Find the binding to 127.0.0.1 or the first available. The logic in RPC settings ensures there will be at least 1.
                         System.Net.IPEndPoint nodeEndPoint = rpcSettings.Bind.FirstOrDefault(b => b.Address.ToString() == "127.0.0.1") ?? rpcSettings.Bind[0];
 
-                        // Initilize the RPC client with the configured or passed userid, password and endpoint
+                        // Initilize the RPC client with the configured or passed userid, password and endpoint.
                         RPCClient rpc = new RPCClient($"{rpcSettings.RpcUser}:{rpcSettings.RpcPassword}", new Uri($"http://{nodeEndPoint}"));
 
                         // Execute the RPC command
                         RPCResponse response = rpc.SendCommand(args.ElementAt(1), args.Skip(2).ToArray());
 
-                        // Return the result as a string to the console
+                        // Return the result as a string to the console.
                         Console.WriteLine(response.ResultString);
                     }
                     catch (Exception err)
                     {
-                        Console.WriteLine(err.Message);
+                        Console.WriteLine(ExceptionToString(err));
                     }
                 }
                 else
                 {
-                    // Process API call
-
-                    var ApiURI = "http://localhost:37220/";
-
+                    // Process API call.
                     using (var client = new HttpClient())
                     {
                         client.DefaultRequestHeaders.Accept.Clear();
                         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                        var url = $"{ApiURI}api/{args.ElementAt(1)}?{string.Join("&", args.Skip(2))}";
+                        var url = $"http://localhost:{apiPort}/api/{args.ElementAt(1)}?{string.Join("&", args.Skip(2))}";
                         try
                         {
+                            // Get the response.
                             var response = client.GetStringAsync(url).GetAwaiter().GetResult();
-                            // Attempt to format the response
-                            try
-                            {
-                                response = JsonConvert.SerializeObject(JsonConvert.DeserializeObject<object>(response), Formatting.Indented);
-                            }
-                            finally { }
-
-                            // Return the result as a string to the console
-                            Console.WriteLine(response);
+                            
+                            // Format and return the result as a string to the console.
+                            Console.WriteLine(JsonConvert.SerializeObject(JsonConvert.DeserializeObject<object>(response), Formatting.Indented));
                         }
                         catch (Exception err)
                         {
-                            Console.WriteLine(err.Message);
+                            Console.WriteLine(ExceptionToString(err));
                         }
                     }
                 }
             }
             catch (Exception err)
             {
-                // Report any errors to the console
-                Console.WriteLine(err.ToString());
+                // Report any errors to the console.
+                Console.WriteLine(ExceptionToString(err));
             }
+        }
+
+        /// <summary>
+        /// Determine both the exception message and any inner exception messages.
+        /// </summary>
+        /// <param name="exception">The exception object.</param>
+        /// <returns>Returns the exception message plus any inner exceptions.</returns>
+        public static string ExceptionToString(Exception exception)
+        {
+            bool isDebugMode = false;
+#if DEBUG
+            isDebugMode = true;
+#endif
+            Exception ex = exception;
+            StringBuilder stringBuilder = new StringBuilder(128);
+            while (ex != null)
+            {
+                if (isDebugMode)
+                {
+                    stringBuilder.Append(ex.GetType().Name);
+                    stringBuilder.Append(": ");
+                }
+                stringBuilder.Append(ex.Message);
+                if (isDebugMode)
+                    stringBuilder.AppendLine(ex.StackTrace);
+                ex = ex.InnerException;
+                if (ex != null)
+                {
+                    stringBuilder.Append(" ---> ");
+                }
+            }
+            return stringBuilder.ToString();
         }
     }
 }
