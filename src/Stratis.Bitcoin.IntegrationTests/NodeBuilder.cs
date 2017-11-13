34275331375894d40ba4f2dfe4b7c9439d5a36fa
Jeremy Bokobza <bokobza@users.noreply.github.com>
2017-11-13 21:24:23 +0000

    Fixed RPC unit test (#737)
    * Fixed RPC unit test
    
    * Moved Staking integration tests to the integration tests project.
    
    * Added some logs that helps with the failing test in appveyor.

--- src/Stratis.Bitcoin.IntegrationTests/NodeBuilder.cs
+++ src/Stratis.Bitcoin.IntegrationTests/NodeBuilder.cs
@@ -200,6 +200,28 @@ namespace Stratis.Bitcoin.IntegrationTests
             return builder.BuildAndRegister();
         }
 
+        /// <summary>
+        /// Builds a node with POS miner and RPC enabled.
+        /// </summary>
+        /// <param name="dir">Data directory that the node should use.</param>
+        /// <returns>Interface to the newly built node.</returns>
+        /// <remarks>Currently the node built here does not actually stake as it has no coins in the wallet,
+        /// but all the features required for it are enabled.</remarks>
+        public static IFullNode BuildStakingNode(string dir, bool staking = true)
+        {
+            NodeSettings nodeSettings = NodeSettings.FromArguments(new string[] { $"-datadir={dir}", $"-stake={(staking ? 1 : 0)}", "-walletname=dummy", "-walletpassword=dummy" });
+            var fullNodeBuilder = new FullNodeBuilder(nodeSettings);
+            IFullNode fullNode = fullNodeBuilder
+                .UseStratisConsensus()
+                .UseBlockStore()
+                .UseMempool()
+                .UseWallet()
+                .AddPowPosMining()
+                .AddRPC()
+                .Build();
+
+            return fullNode;
+        }
     }
 
     public class StratisBitcoinPowRunner : INodeRunner
