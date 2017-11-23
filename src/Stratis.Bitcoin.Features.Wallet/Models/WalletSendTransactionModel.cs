ef5222cf70cfbfd2bf58b4892e85d5cdbd8383b5
Jeremy Bokobza <bokobza@users.noreply.github.com>
2017-11-23 22:12:46 +0000

    Added some extra data to a successfully sent transaction. (#781) (#790)
    (cherry picked from commit b237616409e9784f1bee5058176913a877d777b3)

---
+++ src/Stratis.Bitcoin.Features.Wallet/Models/WalletSendTransactionModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using NBitcoin;
+using Newtonsoft.Json;
+
+namespace Stratis.Bitcoin.Features.Wallet.Models
+{
+    /// <summary>
+    /// A model class to be returned when the user sends a transaction successfully.
+    /// </summary>
+    public class WalletSendTransactionModel
+    {
+        /// <summary>
+        /// The transaction id.
+        /// </summary>
+        [JsonProperty(PropertyName = "transactionId")]
+        public uint256 TransactionId { get; set; }
+
+        /// <summary>
+        /// The list of outputs in this transaction.
+        /// </summary>
+        [JsonProperty(PropertyName = "outputs")]
+        public ICollection<TransactionOutputModel> Outputs { get; set; }
+    }
+
+    /// <summary>
+    /// A simple transaction output.
+    /// </summary>
+    public class TransactionOutputModel
+    {
+        /// <summary>
+        /// The output address in Base58.
+        /// </summary>
+        [JsonProperty(PropertyName = "address")]
+        public string Address { get; set; }
+
+        /// <summary>
+        /// The amount associated with the output.
+        /// </summary>
+        [JsonProperty(PropertyName = "amount")]
+        public Money Amount { get; set; }
+    }
+}
