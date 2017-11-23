228ee896086f74a91cd0d9b7fa0c2ec79114c315
SomeGuy FromAfrica <someguy.fromafrica@gmail.com>
2017-11-23 19:05:37 +1100

    Fix CanGetNetworkName not to test unsupported feature

--- src/NBitcoin.Tests/NetworkTests.cs
+++ src/NBitcoin.Tests/NetworkTests.cs
@@ -25,7 +25,6 @@ namespace NBitcoin.Tests
 			Assert.Equal(Network.GetNetwork("reg"), Network.RegTest);
 			Assert.Equal(Network.GetNetwork("regtest"), Network.RegTest);
 			Assert.Equal(Network.GetNetwork("testnet"), Network.TestNet);
-			Assert.Equal(Network.GetNetwork("testnet3"), Network.TestNet);
 			Assert.Null(Network.GetNetwork("invalid"));
 		}
 
