fc5d23182e4d365f8a820d754fb7d42cc9062a7a
noescape0 <noescape0@ya.ru>
2017-12-04 17:04:48 +0300

    InvPayload- removed unused methods and inheritage of IBitcoinSerializable because it already inherited from Payload which inherits IBitcoinSerializable.

--- src/Stratis.Bitcoin/P2P/Protocol/Payloads/InvPayload.cs
+++ src/Stratis.Bitcoin/P2P/Protocol/Payloads/InvPayload.cs
@@ -8,16 +8,12 @@ namespace Stratis.Bitcoin.P2P.Protocol.Payloads
     /// Announce the hash of a transaction or block.
     /// </summary>
     [Payload("inv")]
-    public class InvPayload : Payload, IBitcoinSerializable, IEnumerable<InventoryVector>
+    public class InvPayload : Payload, IEnumerable<InventoryVector>
     {
         public const int MaxInventorySize = 50000;
 
         private List<InventoryVector> inventory = new List<InventoryVector>();
         public List<InventoryVector> Inventory { get { return this.inventory; } }
-
-        public InvPayload()
-        {
-        }
 
         public InvPayload(params Transaction[] transactions)
             : this(transactions.Select(tx => new InventoryVector(InventoryType.MSG_TX, tx.GetHash())).ToArray())
@@ -26,11 +22,6 @@ namespace Stratis.Bitcoin.P2P.Protocol.Payloads
 
         public InvPayload(params Block[] blocks)
             : this(blocks.Select(b => new InventoryVector(InventoryType.MSG_BLOCK, b.GetHash())).ToArray())
-        {
-        }
-
-        public InvPayload(InventoryType type, params uint256[] hashes)
-            : this(hashes.Select(h => new InventoryVector(type, h)).ToArray())
         {
         }
 
@@ -49,7 +40,7 @@ namespace Stratis.Bitcoin.P2P.Protocol.Payloads
 
         public override string ToString()
         {
-            return "Count: " + this.Inventory.Count.ToString();
+            return "Count: " + this.Inventory.Count;
         }
 
         public IEnumerator<InventoryVector> GetEnumerator()
