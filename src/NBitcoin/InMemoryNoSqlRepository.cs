1c35b83b8ab9a51090953a7a3ef34916ca534e49
SomeGuy FromAfrica <someguy.fromafrica@gmail.com>
2017-12-05 01:45:08 +1100

    Fix style issues

--- src/NBitcoin/InMemoryNoSqlRepository.cs
+++ src/NBitcoin/InMemoryNoSqlRepository.cs
@@ -6,31 +6,31 @@ namespace NBitcoin
 {
     public class InMemoryNoSqlRepository : NoSqlRepository
     {
-        Dictionary<string, byte[]> _Table = new Dictionary<string, byte[]>();
+        Dictionary<string, byte[]> table = new Dictionary<string, byte[]>();
 
         public InMemoryNoSqlRepository(NetworkOptions options = null)
             :base(options)
         {
         }
 
-		protected override Task PutBytesBatch(IEnumerable<Tuple<string, byte[]>> enumerable)
-		{
-			foreach(var data in enumerable)
-			{
-				if(data.Item2 == null)
-				{
-					_Table.Remove(data.Item1);
-				}
-				else
-					_Table.AddOrReplace(data.Item1, data.Item2);
-			}
-			return Task.FromResult(true);
-		}
+        protected override Task PutBytesBatch(IEnumerable<Tuple<string, byte[]>> enumerable)
+        {
+            foreach(var data in enumerable)
+            {
+                if(data.Item2 == null)
+                {
+                    this.table.Remove(data.Item1);
+                }
+                else
+                    this.table.AddOrReplace(data.Item1, data.Item2);
+            }
+            return Task.FromResult(true);
+        }
 
         protected override Task<byte[]> GetBytes(string key)
         {
             byte[] result = null;
-            _Table.TryGetValue(key, out result);
+            this.table.TryGetValue(key, out result);
             return Task.FromResult(result);
         }
     }
--- src/NBitcoin/NoSqlTransactionRepository.cs
+++ src/NBitcoin/NoSqlTransactionRepository.cs
@@ -5,31 +5,31 @@ namespace NBitcoin
 {
     public class NoSqlTransactionRepository : ITransactionRepository
     {
-        private readonly NoSqlRepository _Repository;
+        private readonly NoSqlRepository repository;
         public NoSqlRepository Repository
         {
             get
             {
-                return _Repository;
+                return repository;
             }
         }
 
-		public NoSqlTransactionRepository(NetworkOptions options = null)
+        public NoSqlTransactionRepository(NetworkOptions options = null)
             :this(new InMemoryNoSqlRepository(options))
-		{
+        {
 
         }
         public NoSqlTransactionRepository(NoSqlRepository repository)
         {
             if(repository == null)
                 throw new ArgumentNullException("repository");
-            _Repository = repository;
+            this.repository = repository;
         }
         #region ITransactionRepository Members
 
         public Task<Transaction> GetAsync(uint256 txId)
         {
-            return _Repository.GetAsync<Transaction>(GetId(txId));
+            return repository.GetAsync<Transaction>(GetId(txId));
         }
 
         private string GetId(uint256 txId)
@@ -39,7 +39,7 @@ namespace NBitcoin
 
         public Task PutAsync(uint256 txId, Transaction tx)
         {
-            return _Repository.PutAsync(GetId(txId), tx);
+            return repository.PutAsync(GetId(txId), tx);
         }
 
         #endregion
