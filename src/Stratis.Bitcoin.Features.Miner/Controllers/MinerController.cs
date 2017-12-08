823711748d7bbe0b5062a3943a33e7a01e1178f2
Neurosploit <hoekstraman@gmail.com>
2017-12-08 13:33:37 +0100

    MinerController tests. Moved PosMinting Interface to separate folder. Minor refactorings in the MinerController.

--- src/Stratis.Bitcoin.Features.Miner/Controllers/MinerController.cs
+++ src/Stratis.Bitcoin.Features.Miner/Controllers/MinerController.cs
@@ -5,6 +5,7 @@ using System.Security;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NBitcoin;
+using Stratis.Bitcoin.Features.Miner.Interfaces;
 using Stratis.Bitcoin.Features.Miner.Models;
 using Stratis.Bitcoin.Features.Wallet.Interfaces;
 using Stratis.Bitcoin.Utilities;
--- src/Stratis.Bitcoin.Features.Miner/PosMinting.cs
+++ src/Stratis.Bitcoin.Features.Miner/PosMinting.cs
@@ -12,88 +12,13 @@ using Stratis.Bitcoin.Connection;
 using Stratis.Bitcoin.Features.Consensus;
 using Stratis.Bitcoin.Features.Consensus.CoinViews;
 using Stratis.Bitcoin.Features.MemoryPool;
+using Stratis.Bitcoin.Features.Miner.Interfaces;
 using Stratis.Bitcoin.Features.Wallet;
 using Stratis.Bitcoin.Features.Wallet.Interfaces;
 using Stratis.Bitcoin.Utilities;
 
 namespace Stratis.Bitcoin.Features.Miner
-{
-    /// <summary>
-    /// <see cref="PosMinting"/> is used in order to generate new blocks. It involves a sort of lottery, similar to proof-of-work,
-    /// but the chances of winning this lottery is proportional to how many coins you are staking, not on hashing power.
-    /// </summary>
-    public interface IPosMinting
-    {
-        /// <summary>
-        /// Creates a coinstake transaction with kernel that satisfies POS staking target.
-        /// </summary>
-        /// <param name="utxoStakeDescriptions">List of UTXOs that are available in the wallet for staking.</param>
-        /// <param name="block">Template of the block that we are trying to mine.</param>
-        /// <param name="chainTip">Tip of the best chain.</param>
-        /// <param name="searchInterval">Length of an unexplored block time space in seconds. It only makes sense to look for a solution within this interval.</param>
-        /// <param name="fees">Transaction fees from the transactions included in the block if we mine it.</param>
-        /// <param name="coinstakeContext">Information about coinstake transaction and its private key that is to be filled when the kernel is found.</param>
-        /// <returns><c>true</c> if the function succeeds, <c>false</c> otherwise.</returns>
-        Task<bool> CreateCoinstakeAsync(List<PosMinting.UtxoStakeDescription> utxoStakeDescriptions, Block block, ChainedBlock chainTip, long searchInterval, long fees, PosMinting.CoinstakeContext coinstakeContext);
-
-        /// <summary>
-        /// Attempts to stake new blocks in a loop.
-        /// <para>
-        /// Staking is attempted only if the node is fully synchronized and connected to the network.
-        /// </para>
-        /// </summary>
-        /// <param name="walletSecret">Credentials to the wallet with which will be used for staking.</param>
-        Task GenerateBlocksAsync(PosMinting.WalletSecret walletSecret);
-
-        /// <summary>
-        /// Calculates staking difficulty for a specific block.
-        /// </summary>
-        /// <param name="block">Block at which to calculate the difficulty.</param>
-        /// <returns>Staking difficulty.</returns>
-        /// <remarks>
-        /// The actual idea behind the calculation is a mystery. It was simply ported from
-        /// <see cref="https://github.com/stratisproject/stratisX/blob/47851b7337f528f52ec20e86dca7dcead8191cf5/src/rpcblockchain.cpp#L16"/>.
-        /// </remarks>
-        double GetDifficulty(ChainedBlock block);
-
-        /// <summary>
-        /// Constructs model for RPC "getstakinginfo" call.
-        /// </summary>
-        /// <returns>Staking information RPC response.</returns>
-        Models.GetStakingInfoModel GetGetStakingInfoModel();
-
-        /// <summary>
-        /// Calculates the total balance from all UTXOs in the wallet that are mature.
-        /// </summary>
-        /// <param name="utxoStakeDescriptions">Description of coins in the wallet that will be used for staking.</param>
-        /// <returns>Total balance from all UTXOs in the wallet that are mature.</returns>
-        Money GetMatureBalance(List<PosMinting.UtxoStakeDescription> utxoStakeDescriptions);
-
-        /// <summary>
-        /// Estimates the total staking weight of the network.
-        /// </summary>
-        /// <returns>Estimated amount of money that is used by all stakers on the network.</returns>
-        /// <remarks>
-        /// The idea behind estimating the network staking weight is very similar to estimating
-        /// the total hash power of PoW network. The difficulty retarget algorithm tries to make
-        /// sure of certain distribution of the blocks over a period of time. Base on real distribution
-        /// and using the actual difficulty targets, one is able to compute how much stake was
-        /// presented on the network to generate each block.
-        /// <para>
-        /// The method was ported from
-        /// <see cref="https://github.com/stratisproject/stratisX/blob/47851b7337f528f52ec20e86dca7dcead8191cf5/src/rpcblockchain.cpp#L74"/>.
-        /// </para>
-        /// </remarks>
-        double GetNetworkWeight();
-
-        /// <summary>
-        /// Starts the main POS staking loop.
-        /// </summary>
-        /// <param name="walletSecret">Credentials to the wallet with which will be used for staking.</param>
-        /// <returns>Interface to started loop, so it can be stopped during shutdown.</returns>
-        IAsyncLoop Stake(PosMinting.WalletSecret walletSecret);
-    }
-
+{  
     /// <summary>
     /// <see cref="PosMinting"/> is used in order to generate new blocks. It involves a sort of lottery, similar to proof-of-work,
     /// but the chances of winning this lottery is proportional to how many coins you are staking, not on hashing power.
