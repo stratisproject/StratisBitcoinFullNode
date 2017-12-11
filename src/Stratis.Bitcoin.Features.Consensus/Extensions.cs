aa00ec376534463f5f0d2df0a27490435200e51e
Mike Dennis <strykitenterprises@gmail.com>
2017-12-11 13:45:39 -0700

    Remove extension findfork extension method. This is duplicate of ChainedBlock.FindFork and not used anywhere.

--- src/Stratis.Bitcoin.Features.Consensus/Extensions.cs
+++ src/Stratis.Bitcoin.Features.Consensus/Extensions.cs
@@ -8,22 +8,6 @@ namespace Stratis.Bitcoin.Features.Consensus
 {
     public static class Extensions
     {
-        public static ChainedBlock FindFork(this ChainedBlock newTip, ChainedBlock tip)
-        {
-            ChainedBlock highest = newTip.Height > tip.Height ? newTip : tip;
-            ChainedBlock lowest = highest == newTip ? tip : newTip;
-
-            highest = highest.GetAncestor(lowest.Height);
-
-            while (lowest.HashBlock != highest.HashBlock)
-            {
-                lowest = lowest.Previous;
-                highest = highest.Previous;
-            }
-
-            return highest;
-        }
-
         public static async Task<bool> IfPayloadIsAsync<TPayload>(this Message message, Func<TPayload, Task> action) where TPayload : Payload
         {
             TPayload payload = message.Payload as TPayload;
