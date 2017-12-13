44e6bf665a44d9dcb9ed76bf653c6dd368400826
345James <james@345.systems>
2017-12-13 11:49:45 +0000

    Initial submission of skeleton structure for dns crawler

---
+++ src/Stratis.Bitcoin.Features.Dns.Tests/GivenADnsFeature.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Stratis.Bitcoin.P2P;
+using Xunit;
+
+namespace Stratis.Bitcoin.Features.Dns.Tests
+{
+    /// <summary>
+    /// Tests for the <see cref="DnsFeature"/> class.
+    /// </summary>
+    public class GivenADnsFeature
+    {
+        [Fact]
+        [Trait("UnitTest", "UnitTest")]
+        public void WhenConstructorCalledAndPeerAddressManagerIsNullThenArgumentNullExceptionIsThrown()
+        {
+            // Arrange.
+            Action a = () => { new DnsFeature(null, null); };
+
+            // Act and Assert.
+            a.ShouldThrow<ArgumentNullException>().Which.Message.Should().Contain("peerAddressManager");
+        }
+
+        [Fact]
+        [Trait("UnitTest", "UnitTest")]
+        public void WhenConstructorCalledAndLoggerFactoryIsNullThenArgumentNullExceptionIsThrown()
+        {
+            // Arrange.
+            IPeerAddressManager peerAddressManager = new Mock<IPeerAddressManager>().Object;
+            Action a = () => { new DnsFeature(peerAddressManager, null); };
+
+            // Act and Assert.
+            a.ShouldThrow<ArgumentNullException>().Which.Message.Should().Contain("loggerFactory");
+        }
+
+        [Fact]
+        [Trait("UnitTest", "UnitTest")]
+        public void WhenConstructorCalledAndAllParametersValidThenTypeCreated()
+        {
+            // Arrange.
+            IPeerAddressManager peerAddressManager = new Mock<IPeerAddressManager>().Object;
+            ILoggerFactory loggerFactory = new Mock<ILoggerFactory>().Object;
+
+            // Act.
+            DnsFeature feature = new DnsFeature(peerAddressManager, loggerFactory);
+
+            // Assert.
+            feature.Should().NotBeNull();
+        }
+    }
+}
---
+++ src/Stratis.Bitcoin.Features.Dns/DnsFeature.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using Stratis.Bitcoin.Builder.Feature;
+using Stratis.Bitcoin.P2P;
+using Stratis.Bitcoin.Utilities;
+
+namespace Stratis.Bitcoin.Features.Dns
+{
+    /// <summary>
+    /// Responsible for managing the Dns feature.
+    /// </summary>
+    public class DnsFeature : FullNodeFeature
+    {
+        private readonly IPeerAddressManager peerAddressManager;
+
+        private readonly ILogger logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DnsFeature"/> class.
+        /// </summary>
+        /// <param name="peerAddressManager">The peer address manager.</param>
+        /// <param name="loggerFactory">The factory to create the logger.</param>
+        public DnsFeature(IPeerAddressManager peerAddressManager, ILoggerFactory loggerFactory)
+        {
+            Guard.NotNull(peerAddressManager, nameof(peerAddressManager));
+            Guard.NotNull(loggerFactory, nameof(loggerFactory));
+
+            this.peerAddressManager = peerAddressManager;
+            this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
+        }
+
+        /// <inheritdoc />
+        public override void Initialize()
+        {
+            this.logger.LogInformation("Starting DNS");
+            // todo: add implementation.            
+        }
+    }
+}
