4d69a4ac2572814965dee2318bb2861106d88116
Mike Dennis <strykitenterprises@gmail.com>
2017-12-16 11:21:46 -0700

    Move  wallet dependency check to miner feature instead of API

--- src/Stratis.Bitcoin.Features.Api/ApiFeature.cs
+++ src/Stratis.Bitcoin.Features.Api/ApiFeature.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Logging;
 using Stratis.Bitcoin.Builder;
 using Stratis.Bitcoin.Builder.Feature;
 using Stratis.Bitcoin.Features.Api.Models;
-using Stratis.Bitcoin.Features.Wallet;
 using Stratis.Bitcoin.Utilities;
 
 namespace Stratis.Bitcoin.Features.Api
@@ -120,7 +119,6 @@ namespace Stratis.Bitcoin.Features.Api
             {
                 features
                 .AddFeature<ApiFeature>()
-                .DependOn<WalletFeature>()
                 .FeatureServices(services =>
                     {
                         services.AddSingleton(fullNodeBuilder);
