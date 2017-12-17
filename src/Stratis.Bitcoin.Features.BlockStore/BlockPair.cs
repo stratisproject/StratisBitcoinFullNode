a17f6f83c693cc7971c6deae0867ee43d88abb0c
noescape0 <noescape0@ya.ru>
2017-12-17 13:26:20 +0300

    Added assert and some comments.

--- src/Stratis.Bitcoin.Features.BlockStore/BlockPair.cs
+++ src/Stratis.Bitcoin.Features.BlockStore/BlockPair.cs
@@ -3,15 +3,19 @@ using Stratis.Bitcoin.Utilities;
 
 namespace Stratis.Bitcoin.Features.BlockStore
 {
+    /// <summary>
+    /// BlockPair is a structure made of a block and a ChainedBlock that represents the block.
+    /// </summary>
     public sealed class BlockPair
     {
         /// <summary>
-        /// Construct BlockPair with a Block and ChainedBlock instance to ensure that its valid
+        /// Creates instance of <see cref="BlockPair"/>.
         /// </summary>
         public BlockPair(Block block, ChainedBlock chainedBlock)
         {
             Guard.NotNull(block, nameof(block));
             Guard.NotNull(chainedBlock, nameof(chainedBlock));
+            Guard.Assert(block.GetHash() == chainedBlock.HashBlock);
 
             this.Block = block;
             this.ChainedBlock = chainedBlock;
