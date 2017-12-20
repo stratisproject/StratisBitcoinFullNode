e5b11b1532b586a42c9f9958787aed376936e59d
noescape0 <noescape0@ya.ru>
2017-12-20 19:33:26 +0300

    Added unit tests for IBD

---
+++ src/Stratis.Bitcoin.Features.Consensus.Tests/IBDTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Logging;
+using NBitcoin;
+using Stratis.Bitcoin.Base;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Utilities;
+using Xunit;
+
+namespace Stratis.Bitcoin.Features.Consensus.Tests
+{
+    public class IBDTest
+    {
+        private readonly ConsensusSettings consensusSettings;
+        private readonly Checkpoints checkpoints;
+        private readonly NodeSettings nodeSettings;
+        private readonly ChainState chainState;
+
+        public IBDTest()
+        {
+            this.consensusSettings = new ConsensusSettings(NodeSettings.Default(), new LoggerFactory());
+            this.checkpoints = new Checkpoints(Network.Main, this.consensusSettings);
+            this.nodeSettings = new NodeSettings("stratis", Network.Main);
+            this.chainState = new ChainState(new InvalidBlockHashStore(new DateTimeProvider()));
+        }
+
+        [Fact]
+        public void NotInIBDIfChainStateIsNull()
+        {
+            var blockDownloadState = new BlockDownloadState(null, Network.Main, this.nodeSettings, this.checkpoints);
+            Assert.False(blockDownloadState.IsInitialBlockDownload());
+        }
+
+        [Fact]
+        public void InIBDIfChainTipIsNull()
+        {
+            this.chainState.ConsensusTip = null;
+            var blockDownloadState = new BlockDownloadState(this.chainState, Network.Main, this.nodeSettings, this.checkpoints);
+            Assert.True(blockDownloadState.IsInitialBlockDownload());
+        }
+
+        [Fact]
+        public void InIBDIfBehindCheckpoint()
+        {
+            this.chainState.ConsensusTip = new ChainedBlock(new BlockHeader(), uint256.Zero, 1000);
+            var blockDownloadState = new BlockDownloadState(this.chainState, Network.Main, this.nodeSettings, this.checkpoints);
+            Assert.True(blockDownloadState.IsInitialBlockDownload());
+        }
+
+        [Fact]
+        public void InIBDIfChainWorkIsLessThanMinimum()
+        {
+            this.chainState.ConsensusTip = new ChainedBlock(new BlockHeader(), uint256.Zero, this.checkpoints.GetLastCheckpointHeight() + 1);
+            var blockDownloadState = new BlockDownloadState(this.chainState, Network.Main, this.nodeSettings, this.checkpoints);
+            Assert.True(blockDownloadState.IsInitialBlockDownload());
+        }
+    }
+}
