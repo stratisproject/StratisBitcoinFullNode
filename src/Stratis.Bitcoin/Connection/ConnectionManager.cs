7b506ff1c76e70546507ff46d81979b164f5edc2
noescape0 <noescape0@ya.ru>
2017-12-25 00:24:24 +0300

    Removed assumption that if pending tip is null the peer is at our height

--- src/Stratis.Bitcoin/Connection/ConnectionManager.cs
+++ src/Stratis.Bitcoin/Connection/ConnectionManager.cs
@@ -291,7 +291,7 @@ namespace Stratis.Bitcoin.Connection
                     "Peer:" + (node.RemoteInfo() + ", ").PadRight(LoggingConfiguration.ColumnLength + 15) +
                     (" connected" + " (" + (connectionManagerBehavior.Inbound ? "inbound" : "outbound") + "),").PadRight(LoggingConfiguration.ColumnLength + 7) +
                     (" agent " + agent + ", ").PadRight(LoggingConfiguration.ColumnLength + 2) +
-                    " height=" + (chainHeadersBehavior.PendingTip != null ? chainHeadersBehavior.PendingTip.Height : chainHeadersBehavior.Chain.Tip.Height));
+                    " height=" + (chainHeadersBehavior.PendingTip != null ? chainHeadersBehavior.PendingTip.Height.ToString() : "unknown"));
             }
 
             return builder.ToString();
