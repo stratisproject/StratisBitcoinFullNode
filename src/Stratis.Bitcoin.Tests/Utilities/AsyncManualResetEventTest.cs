a13f828bc0a314d4f2622eaa75e61a122e98c2a6
noescape0 <noescape0@ya.ru>
2017-12-28 21:03:02 +0300

    Improved existing tests and added new one.

--- src/Stratis.Bitcoin.Tests/Utilities/AsyncManualResetEventTest.cs
+++ src/Stratis.Bitcoin.Tests/Utilities/AsyncManualResetEventTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,24 +11,44 @@ namespace Stratis.Bitcoin.Tests.Utilities
 {
     public class AsyncManualResetEventTest
     {
+        /// <summary>
+        /// Tests that:
+        /// <list type="bullet">
+        /// <item><see cref="AsyncManualResetEvent"/> does not crush.</item>
+        /// <item>WaitAsync eventually completes.</item>
+        /// <item>WaitAsync completes when the event is set.</item>
+        /// </list>
+        /// </summary>
         [Fact]
         public async void AsyncManualResetEvent_WaitAsync()
         {
             AsyncManualResetEvent manualResetEvent = new AsyncManualResetEvent(false);
 
-            Task task = Task.Run(async delegate
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            Task task = Task.Run(async () =>
             {
                 await Task.Delay(500);
                 manualResetEvent.Set();
             });
 
-            await manualResetEvent.WaitAsync();
+            Task mreAwaitingTask = manualResetEvent.WaitAsync();
+            await mreAwaitingTask;
+
+            stopwatch.Stop();
+
+            Assert.True(stopwatch.ElapsedMilliseconds >= 500 && stopwatch.ElapsedMilliseconds < 600);
+            Assert.True(mreAwaitingTask.Status == TaskStatus.RanToCompletion);
         }
 
         [Fact]
-        public void AsyncManualResetEvent_CanReset()
+        public void AsyncManualResetEvent_CanSetAndReset()
         {
-            AsyncManualResetEvent manualResetEvent = new AsyncManualResetEvent(true);
+            AsyncManualResetEvent manualResetEvent = new AsyncManualResetEvent(false);
+
+            Assert.False(manualResetEvent.IsSet);
+
+            manualResetEvent.Set();
 
             Assert.True(manualResetEvent.IsSet);
 
@@ -93,6 +114,32 @@ namespace Stratis.Bitcoin.Tests.Utilities
             await Task.Delay(200);
 
             Assert.False(task.IsCompleted);
+        }
+
+        [Fact]
+        public async void AsyncManualResetEvent_CanBeCancelled()
+        {
+            AsyncManualResetEvent manualResetEvent = new AsyncManualResetEvent(false);
+
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            Task task = Task.Run(async () =>
+            {
+                await Task.Delay(500);
+                tokenSource.Cancel();
+            });
+
+            Task mreAwaitingTask = manualResetEvent.WaitAsync(tokenSource.Token);
+
+            try
+            {
+                await mreAwaitingTask;
+            }
+            catch (TaskCanceledException)
+            {
+            }
+
+            Assert.True(mreAwaitingTask.Status == TaskStatus.Canceled);
         }
     }
 }
