7063ab6f06f9db0e40240d9eec04daf5a587b5d6
Jandrews <jordandrews@hotmail.com>
2017-12-28 21:48:07 +1100

    test

--- src/Stratis.Bitcoin.Features.SmartContracts/SmartContractTransaction.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/SmartContractTransaction.cs
@@ -7,6 +7,20 @@ namespace Stratis.Bitcoin.Features.SmartContracts
 {
     public class SmartContractTransaction : IBitcoinSerializable
     {
+        // SHA-256 value of encoded transaction
+        public byte[] Hash { get; set; }
+
+        public ulong Nonce { get; set; }
+
+        public ulong Value { get; set; }
+
+        public uint160 ReceiveAddress { get; set; }
+
+        public uint160 SendAddress { get; set; }
+
+
+        // Other values:  gas limit + value, contract params, data etc.
+
 
 
         public void ReadWrite(BitcoinStream stream)
---
+++ src/Stratis.Bitcoin.IntegrationTests/SmartContractTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Stratis.Bitcoin.Features.SmartContracts;
+using Xunit;
+
+namespace Stratis.Bitcoin.IntegrationTests
+{
+    public class SmartContractTests
+    {
+
+
+
+        [Fact]
+        public async Task TestSmartContractConsensusLoop()
+        {
+            // Should be like the constructor of TestContext in 'MinerTests.cs'
+
+            var conensus = new SmartContractConsensusLoop();
+            await conensus.StartAsync();
+
+            // await conensus.ValidateAndExecuteBlockAsync();
+        }
+    }
+}
