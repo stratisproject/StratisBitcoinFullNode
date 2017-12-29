d554cc1d5ae52bf9c5b0e124e6525993e6039666
SomeGuy FromAfrica <someguy.fromafrica@gmail.com>
2017-12-29 15:32:34 +1100

    Changes based on feedback

--- src/Stratis.Bitcoin.Features.Api/ApiSettings.cs
+++ src/Stratis.Bitcoin.Features.Api/ApiSettings.cs
@@ -1,28 +1,43 @@
 ﻿using System;
 using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Utilities;
 
 namespace Stratis.Bitcoin.Features.Api
 {
     /// <summary>
-    /// Configuration related to storage of transactions.
+    /// Configuration related to the API interface.
     /// </summary>
     public class ApiSettings
     {
         /// <summary>URI to node's API interface.</summary>
-        public Uri ApiUri { get; set; }       
+        public Uri ApiUri { get; set; }
 
+        /// <summary>The callback used to override/constrain/extend the settings provided by the Load method.</summary>
         private Action<ApiSettings> callback = null;
 
+        /// <summary>
+        /// Constructs this object.
+        /// </summary>
         public ApiSettings()
         {
         }
 
+        /// <summary>
+        /// Constructs this object whilst providing a callback to override/constrain/extend 
+        /// the settings provided by the Load method.
+        /// </summary>
+        /// <param name="callback">The callback used to override/constrain/extend the settings provided by the Load method.</param>
         public ApiSettings(Action<ApiSettings> callback)
             : this()
         {
             this.callback = callback;
         }
 
+        /// <summary>
+        /// Constructs this object from the NodeSettings and the provided callback.
+        /// </summary>
+        /// <param name="nodeSettings">The NodeSettings object.</param>
+        /// <param name="callback">The callback used to override the node settings.</param>
         public ApiSettings(NodeSettings nodeSettings, Action<ApiSettings> callback = null)
             : this(callback)
         {
@@ -30,15 +45,14 @@ namespace Stratis.Bitcoin.Features.Api
         }
 
         /// <summary>
-        /// Loads the storage related settings from the application configuration.
+        /// Loads the API related settings from the application configuration.
         /// </summary>
         /// <param name="nodeSettings">Application configuration.</param>
-        public virtual void Load(NodeSettings nodeSettings)
+        public void Load(NodeSettings nodeSettings)
         {
-            var config = nodeSettings.ConfigReader;
+            TextFileConfiguration config = nodeSettings.ConfigReader;
 
-            this.ApiUri = config.GetOrDefault("apiuri", new Uri(
-                $"http://localhost:{ (nodeSettings.Network.ToString().StartsWith("Stratis") ? 37221 : 37220) }"));
+            this.ApiUri = config.GetOrDefault("apiuri", new Uri($"http://localhost:{ (nodeSettings.Network.IsBitcoin() ? 37220 : 37221) }"));
 
             this.callback?.Invoke(this);
         }
