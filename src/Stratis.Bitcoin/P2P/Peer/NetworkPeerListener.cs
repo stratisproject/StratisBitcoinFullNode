7498ab6381525bb99c203cbb68d963e8c0160a98
Aprogiena <aprogiena@users.noreply.github.com>
2018-01-06 15:51:32 +0000

    clear using section

--- src/Stratis.Bitcoin/P2P/Peer/NetworkPeerListener.cs
+++ src/Stratis.Bitcoin/P2P/Peer/NetworkPeerListener.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using NBitcoin.Protocol;
 using Stratis.Bitcoin.P2P.Protocol;
 using Stratis.Bitcoin.P2P.Protocol.Payloads;
 
