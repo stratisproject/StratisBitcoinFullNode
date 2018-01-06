af67d05081a7ed1ce00e3ea17eb942a5116ed4f6
Aprogiena <aprogiena@users.noreply.github.com>
2018-01-06 15:44:49 +0000

    Part 20 starts here
    separation of IMessageListener

--- src/Stratis.Bitcoin/P2P/Protocol/MessageListener.cs
+++ src/Stratis.Bitcoin/P2P/Protocol/MessageListener.cs
@@ -3,24 +3,9 @@ using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 using Stratis.Bitcoin.Utilities;
-using NBitcoin.Protocol;
 
 namespace Stratis.Bitcoin.P2P.Protocol
 {
-    /// <summary>
-    /// Contract for a recipient part of a consumer logic that handles incoming messages.
-    /// </summary>
-    /// <typeparam name="T">Type of the messages that are being handled.</typeparam>
-    /// <seealso cref="MessageProducer{T}"/>
-    public interface IMessageListener<in T>
-    {
-        /// <summary>
-        /// Handles a newly received message.
-        /// </summary>
-        /// <param name="message">Message to handle.</param>
-        void PushMessage(T message);
-    }
-
     /// <summary>
     /// Represents a callback rountine to be called when a new message arrives to the listener.
     /// <para>
