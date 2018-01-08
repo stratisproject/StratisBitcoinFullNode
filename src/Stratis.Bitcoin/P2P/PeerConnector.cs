c4bd717243c5840297359258b5a73b1d5f761604
noescape0 <NoEscape0@ya.ru>
2018-01-08 09:55:31 +0300

    Fix: slow node shutdown if many -connect or -addNode options were specified (#1032)
    * OnConnectAsync cancellation token check
    
    * Improved logging and disconnection reason
    
    * fix based on the review

--- src/Stratis.Bitcoin/P2P/PeerConnector.cs
+++ src/Stratis.Bitcoin/P2P/PeerConnector.cs
@@ -220,8 +220,16 @@ namespace Stratis.Bitcoin.P2P
             }
             catch (OperationCanceledException timeout)
             {
-                this.logger.LogDebug("Peer {0} connection timeout.", peerAddress.NetworkAddress.Endpoint);
-                peer?.DisconnectWithException("Timeout", timeout);
+                if (this.nodeLifetime.ApplicationStopping.IsCancellationRequested)
+                {
+                    this.logger.LogDebug("Peer {0} connection canceled because application is stopping.", peerAddress.NetworkAddress.Endpoint);
+                    peer?.DisconnectWithException("Application stopping");
+                }
+                else
+                {
+                    this.logger.LogDebug("Peer {0} connection timeout.", peerAddress.NetworkAddress.Endpoint);
+                    peer?.DisconnectWithException("Timeout", timeout);
+                }
             }
             catch (Exception exception)
             {
