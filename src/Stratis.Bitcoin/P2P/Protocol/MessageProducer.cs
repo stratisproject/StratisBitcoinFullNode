92ab6f8bbf05145445803d2fc6b09a44eeed87d4
aprogiena <aprogiena@users.noreply.github.com>
2018-01-11 14:15:08 +0000

    refactoring of and comments for message producer

--- src/Stratis.Bitcoin/P2P/Protocol/MessageProducer.cs
+++ src/Stratis.Bitcoin/P2P/Protocol/MessageProducer.cs
@@ -1,75 +1,93 @@
 ﻿using System;
-using System.Collections.Generic;
-using NBitcoin;
+using ConcurrentCollections;
 using Stratis.Bitcoin.Utilities;
 
 namespace Stratis.Bitcoin.P2P.Protocol
 {
+    /// <summary>
+    /// Binding between <see cref="MessageProducer{T}"/> and <see cref="IMessageListener{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">Type of the messages that are being handled.</typeparam>
+    public class MessageProducerRegistration<T> : IDisposable
+    {
+        /// <summary>Producer of messages.</summary>
+        private readonly MessageProducer<T> producer;
+
+        /// <summary>Consumer of messages.</summary>
+        private readonly IMessageListener<T> listener;
+
+        /// <summary>
+        /// Initializes an instance of the object.
+        /// </summary>
+        /// <param name="listener">Consumer of messages.</param>
+        /// <param name="producer">Producer of messages.</param>
+        public MessageProducerRegistration(IMessageListener<T> listener, MessageProducer<T> producer)
+        {
+            this.listener = listener;
+            this.producer = producer;
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this.producer.RemoveMessageListener(this.listener);
+        }
+    }
+
+    /// <summary>
+    /// Distributor of messages to attached consumers.
+    /// </summary>
+    /// <typeparam name="T">Type of the messages that are being handled.</typeparam>
     public class MessageProducer<T>
     {
-        private List<IMessageListener<T>> listeners = new List<IMessageListener<T>>();
+        /// <summary>List of attached consumers of this producer's messages.</summary>
+        private readonly ConcurrentHashSet<IMessageListener<T>> listeners;
 
-        public IDisposable AddMessageListener(IMessageListener<T> listener)
+        /// <summary>
+        /// Initializes an instance of the object.
+        /// </summary>
+        public MessageProducer()
+        {
+            this.listeners = new ConcurrentHashSet<IMessageListener<T>>();
+        }
+
+        /// <summary>
+        /// Add a new consumer to distribute messages to.
+        /// </summary>
+        /// <param name="listener">New consumer to distribute messages to.</param>
+        /// <returns>Disposable binding between the producer and the consumer.</returns>
+        public MessageProducerRegistration<T> AddMessageListener(IMessageListener<T> listener)
         {
             Guard.NotNull(listener, nameof(listener));
 
-            lock (this.listeners)
-            {
-                return new Scope(() =>
-                {
-                    this.listeners.Add(listener);
-                }, () =>
-                {
-                    lock (this.listeners)
-                    {
-                        this.listeners.Remove(listener);
-                    }
-                });
-            }
+            this.listeners.Add(listener);
+
+            var registration = new MessageProducerRegistration<T>(listener, this);
+            return registration;
         }
 
+        /// <summary>
+        /// Stops distribution of message to a registered consumer.
+        /// </summary>
+        /// <param name="listener">Registered consumer to stop distributing messages to.</param>
         public void RemoveMessageListener(IMessageListener<T> listener)
         {
-            if (listener == null)
-                throw new ArgumentNullException("listener");
+            Guard.NotNull(listener, nameof(listener));
 
-            lock (this.listeners)
-            {
-                this.listeners.Remove(listener);
-            }
+            this.listeners.TryRemove(listener);
         }
 
+        /// <summary>
+        /// Distributes a message among all attached consumers.
+        /// </summary>
+        /// <param name="message">Message to distribute.</param>
         public void PushMessage(T message)
         {
-            if (message == null)
-                throw new ArgumentNullException("message");
+            Guard.NotNull(message, nameof(message));
 
-            lock (this.listeners)
+            foreach (IMessageListener<T> listener in this.listeners)
             {
-                foreach (IMessageListener<T> listener in this.listeners)
-                {
-                    listener.PushMessage(message);
-                }
-            }
-        }
-
-        public void PushMessages(IEnumerable<T> messages)
-        {
-            if (messages == null)
-                throw new ArgumentNullException("messages");
-
-            lock (this.listeners)
-            {
-                foreach (T message in messages)
-                {
-                    if (message == null)
-                        throw new ArgumentNullException("message");
-
-                    foreach (IMessageListener<T> listener in this.listeners)
-                    {
-                        listener.PushMessage(message);
-                    }
-                }
+                listener.PushMessage(message);
             }
         }
     }
