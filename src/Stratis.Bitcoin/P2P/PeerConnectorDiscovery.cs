db65eecde83707c432d2a38decbb29fa140ee626
Francois de la Rouviere <fassadlr@gmail.com>
2018-01-15 17:58:20 +0200

    [Refactor] Remove NetworkAddress from PeerAddress (#1053)
    * Remove NetworkAddress from PeerAddress
    
    * Fix logging when seed node cannot be connected to.

--- src/Stratis.Bitcoin/P2P/PeerConnectorDiscovery.cs
+++ src/Stratis.Bitcoin/P2P/PeerConnectorDiscovery.cs
@@ -83,37 +83,37 @@ namespace Stratis.Bitcoin.P2P
                     continue;
                 }
 
-                if (!peer.NetworkAddress.Endpoint.Address.IsValid())
+                if (!peer.EndPoint.Address.IsValid())
                 {
-                    this.logger.LogTrace("Peer selection failed, peer endpoint is not valid '{0}'.", peer.NetworkAddress.Endpoint);
+                    this.logger.LogTrace("Peer selection failed, peer endpoint is not valid '{0}'.", peer.EndPoint);
                     peerSelectionFailed++;
                     continue;
                 }
 
                 // If the peer is already connected just continue.
-                if (this.IsPeerConnected(peer.NetworkAddress.Endpoint))
+                if (this.IsPeerConnected(peer.EndPoint))
                 {
-                    this.logger.LogTrace("Peer selection failed, peer is already connected '{0}'.", peer.NetworkAddress.Endpoint);
+                    this.logger.LogTrace("Peer selection failed, peer is already connected '{0}'.", peer.EndPoint);
                     peerSelectionFailed++;
                     continue;
                 }
 
                 // If the peer exists in the -addnode collection don't
                 // try and connect to it.
-                var peerExistsInAddNode = this.NodeSettings.ConnectionManager.AddNode.Any(p => p.MapToIpv6().Match(peer.NetworkAddress.Endpoint));
+                var peerExistsInAddNode = this.NodeSettings.ConnectionManager.AddNode.Any(p => p.MapToIpv6().Match(peer.EndPoint));
                 if (peerExistsInAddNode)
                 {
-                    this.logger.LogTrace("Peer selection failed, peer exists in -addnode args '{0}'.", peer.NetworkAddress.Endpoint);
+                    this.logger.LogTrace("Peer selection failed, peer exists in -addnode args '{0}'.", peer.EndPoint);
                     peerSelectionFailed++;
                     continue;
                 }
 
                 // If the peer exists in the -connect collection don't
                 // try and connect to it.
-                var peerExistsInConnectNode = this.NodeSettings.ConnectionManager.Connect.Any(p => p.MapToIpv6().Match(peer.NetworkAddress.Endpoint));
+                var peerExistsInConnectNode = this.NodeSettings.ConnectionManager.Connect.Any(p => p.MapToIpv6().Match(peer.EndPoint));
                 if (peerExistsInConnectNode)
                 {
-                    this.logger.LogTrace("Peer selection failed, peer exists in -connect args '{0}'.", peer.NetworkAddress.Endpoint);
+                    this.logger.LogTrace("Peer selection failed, peer exists in -connect args '{0}'.", peer.EndPoint);
                     peerSelectionFailed++;
                     continue;
                 }
@@ -121,7 +121,7 @@ namespace Stratis.Bitcoin.P2P
                 break;
             }
 
-            this.logger.LogTrace("Peer selected: '{0}'", peer?.NetworkAddress.Endpoint);
+            this.logger.LogTrace("Peer selected: '{0}'", peer?.EndPoint);
 
             if (peer != null)
                 await ConnectAsync(peer).ConfigureAwait(false);
---
+++ src/Stratis.Bitcoin/Utilities/JsonConverters/IPEndPointConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Stratis.Bitcoin.Utilities.JsonConverters
+{
+    /// <summary>
+    /// Converter used to convert <see cref="IPEndPoint"/> to and from JSON.
+    /// </summary>
+    /// <seealso cref="JsonConverter" />
+    public sealed class IPEndPointConverter : JsonConverter
+    {
+        /// <inheritdoc />
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(IPEndPoint);
+        }
+
+        /// <inheritdoc />
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var json = JToken.Load(reader).ToString();
+            if (string.IsNullOrWhiteSpace(json))
+                return null;
+
+            var endPointComponents = json.Split('|');
+            return new IPEndPoint(IPAddress.Parse(endPointComponents[0]), Convert.ToInt32(endPointComponents[1]));
+        }
+
+        /// <inheritdoc />
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is IPEndPoint ipEndPoint)
+            {
+                if (ipEndPoint.Address != null || ipEndPoint.Port != 0)
+                {
+                    JToken.FromObject(string.Format("{0}|{1}", ipEndPoint.Address, ipEndPoint.Port)).WriteTo(writer);
+                    return;
+                }
+            }
+
+            writer.WriteNull();
+        }
+    }
+}
