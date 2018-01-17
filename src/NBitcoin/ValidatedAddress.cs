a05c56207bfac00f9c23828c58c50e579afa8e0c
Daniel Gould <dcsled@gmail.com>
2018-01-17 13:26:17 -0500

    make ValidatedAddress return type

---
+++ src/NBitcoin/ValidatedAddress.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBitcoin
+{
+    public class ValidatedAddress
+    {
+        public bool IsValid { get; set; }
+    }
+}
