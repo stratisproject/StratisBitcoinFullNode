d50e30cf6c7d21d32d02521f21b072ae1975e59b
Dan Gershony <dan.gershony@gmail.com>
2018-01-18 17:01:22 +0000

    Check that segwit activates on a c# node

--- src/Stratis.Bitcoin.IntegrationTests/SegWitTests.cs
+++ src/Stratis.Bitcoin.IntegrationTests/SegWitTests.cs
@@ -1,4 +1,5 @@
-﻿using NBitcoin;
+﻿using System;
+using NBitcoin;
 using NBitcoin.RPC;
 using Stratis.Bitcoin.Base.Deployments;
 using Stratis.Bitcoin.Features.Api;
@@ -12,15 +13,15 @@ using Stratis.Bitcoin.Features.RPC;
 using Stratis.Bitcoin.Features.Wallet;
 using Stratis.Bitcoin.Features.Wallet.Interfaces;
 using Stratis.Bitcoin.IntegrationTests.EnvironmentMockUpHelpers;
+using Stratis.Bitcoin.Utilities.Extensions;
 using Xunit;
-using static NBitcoin.Consensus;
 
 namespace Stratis.Bitcoin.IntegrationTests
 {
     public class SegWitTests
     {
         [Fact]
-        public void TestSegwitActivation()
+        public void TestSegwit_MinedOnCore_ActivatedOn_Stratisnode()
         {
             using (NodeBuilder builder = NodeBuilder.Create(version: "0.15.1"))
             {
@@ -30,7 +31,7 @@ namespace Stratis.Bitcoin.IntegrationTests
                 coreNode.ConfigParameters.AddOrReplace("printtoconsole", "0");
                 coreNode.Start();
 
-                CoreNode node1 = builder.CreateStratisPowNode(true, fullNodeBuilder =>
+                CoreNode stratisNode = builder.CreateStratisPowNode(true, fullNodeBuilder =>
                 {
                     fullNodeBuilder
                         .UseConsensus()
@@ -39,35 +40,63 @@ namespace Stratis.Bitcoin.IntegrationTests
                         .UseBlockNotification()
                         .UseTransactionNotification()
                         .AddMining()
-                        .UseWallet()                        
+                        .UseWallet()
                         .UseApi()
                         .AddRPC();
                 });
 
-                WalletManager wm1 = node1.FullNode.NodeService<IWalletManager>() as WalletManager;
-                wm1.CreateWallet("Test1", "alice1");
+                WalletManager stratisNodeWallet = stratisNode.FullNode.NodeService<IWalletManager>() as WalletManager;
+                stratisNodeWallet.CreateWallet("Test1", "alice1");
 
-                RPCClient rpc1 = node1.CreateRPCClient();
+                RPCClient stratisNodeRpc = stratisNode.CreateRPCClient();
                 RPCClient coreRpc = coreNode.CreateRPCClient();
 
-                coreRpc.AddNode(node1.Endpoint, false);
-                rpc1.AddNode(coreNode.Endpoint, false);
+                coreRpc.AddNode(stratisNode.Endpoint, false);
+                stratisNodeRpc.AddNode(coreNode.Endpoint, false);
 
-                coreRpc.Generate(450);
+                // core does not mine segwit blocks by default so we disable the segwit auto activation on regtest
+                var prevSegwitDeployment = Network.RegTest.Consensus.BIP9Deployments[BIP9Deployments.Segwit];
+                Network.RegTest.Consensus.BIP9Deployments[BIP9Deployments.Segwit] = new BIP9DeploymentsParameters(1, 0, DateTime.Now.AddDays(50).ToUnixTimestamp());
 
-                BIP9DeploymentsArray bip9Constants = node1.FullNode.Network.Consensus.BIP9Deployments;
-                IConsensusLoop consensusLoop = node1.FullNode.NodeService<IConsensusLoop>();
-                ThresholdState[] bip9State = consensusLoop.NodeDeployments.BIP9.GetStates(node1.FullNode.Chain.Tip.Previous);
+                try
+                {
+                    // generate 450 blocks, block432 is a segwit block
+                    coreRpc.Generate(450);
 
-                Money amount = new Money(5.0m, MoneyUnit.BTC);
-                HdAddress destination1 = wm1.GetUnusedAddress(new WalletAccountReference("alice1", "account 0"));
+                    TestHelper.WaitLoop(() => stratisNode.CreateRPCClient().GetBestBlockHash() == coreNode.CreateRPCClient().GetBestBlockHash());
 
-                coreRpc.SendToAddress(BitcoinAddress.Create(destination1.Address, Network.RegTest), amount);
+                    // segwit activation on Bitcoin regtest
+                    // - On regtest deployment state changes every 144 block, the threshold for activating a rule is 108 blocks.
+                    // segwit should be:
+                    // - Defined up to block 143
+                    // - Started at block 144 to block 288 
+                    // - LockedIn 288 (as segwit blocks should already be signaled)
+                    // - Active at block 432 
 
-                coreRpc.Generate(1);
+                    IConsensusLoop consensusLoop = stratisNode.FullNode.NodeService<IConsensusLoop>();
+                    ThresholdState[] segwitDefinedState = consensusLoop.NodeDeployments.BIP9.GetStates(stratisNode.FullNode.Chain.GetBlock(142));
+                    ThresholdState[] segwitStartedState = consensusLoop.NodeDeployments.BIP9.GetStates(stratisNode.FullNode.Chain.GetBlock(143));
+                    ThresholdState[] segwitLockedInState = consensusLoop.NodeDeployments.BIP9.GetStates(stratisNode.FullNode.Chain.GetBlock(287));
+                    ThresholdState[] segwitActiveState = consensusLoop.NodeDeployments.BIP9.GetStates(stratisNode.FullNode.Chain.GetBlock(431));
 
-                Assert.Equal(ThresholdState.Active, bip9State.GetValue((int)BIP9Deployments.Segwit));
+                    // check that segwit is got activated at block 432
+                    Assert.Equal(ThresholdState.Defined, segwitDefinedState.GetValue((int)BIP9Deployments.Segwit));
+                    Assert.Equal(ThresholdState.Started, segwitStartedState.GetValue((int)BIP9Deployments.Segwit));
+                    Assert.Equal(ThresholdState.LockedIn, segwitLockedInState.GetValue((int)BIP9Deployments.Segwit));
+                    Assert.Equal(ThresholdState.Active, segwitActiveState.GetValue((int)BIP9Deployments.Segwit));
+                }
+                finally
+                {
+                    Network.RegTest.Consensus.BIP9Deployments[BIP9Deployments.Segwit] = prevSegwitDeployment;
+                }
             }
+        }
+
+        public void TestSegwit_MinedOnStratisNode_ActivatedOn_Corenode()
+        {
+            // TODO: mine segwit onh a stratis node on the bitcoin network
+            // write a tests that mines segwit blocks on the stratis node 
+            // and signals them to a core not, then segwit will get activated on core
         }
     }
 }
