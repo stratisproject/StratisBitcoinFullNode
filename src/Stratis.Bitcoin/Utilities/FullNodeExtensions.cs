42440fcba76c7c8042c8177e88e96a744c7f67d7
Jeremy Bokobza <bokobza@gmail.com>
2018-01-22 12:06:07 +0000

    Changed catch to finally in the FullNodeExtensions

--- src/Stratis.Bitcoin/Utilities/FullNodeExtensions.cs
+++ src/Stratis.Bitcoin/Utilities/FullNodeExtensions.cs
@@ -52,12 +52,10 @@ namespace Stratis.Bitcoin.Utilities
                 {
                     await node.RunAsync(cts.Token, "Application started. Press Ctrl+C to shut down.", "Application stopped.").ConfigureAwait(false);
                 }
-                catch (Exception e)
+                finally
                 {
-                    Console.WriteLine(e);
+                    done.Set();
                 }
-
-                done.Set();
             }
         }
 
