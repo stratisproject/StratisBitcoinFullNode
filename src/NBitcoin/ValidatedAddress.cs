1bee7176f9814a1c541c9721bb440e2a16c7d49e
Daniel Gould <dcsled@gmail.com>
2018-02-02 13:27:51 -0500

    define ValidatedAddress JsonProperty isvalid

--- src/NBitcoin/ValidatedAddress.cs
+++ src/NBitcoin/ValidatedAddress.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
+
 
 namespace NBitcoin
 {
     public class ValidatedAddress
     {
+        [JsonProperty(PropertyName = "isvalid")]
         public bool IsValid { get; set; }
     }
 }
