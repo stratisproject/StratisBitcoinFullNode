865b054e0f0a41b48866eee9f4dd0510bf1f0628
Jeremy Bokobza <bokobza@users.noreply.github.com>
2018-02-05 13:54:21 +0000

    Keepalive Timer (#1129)
    * wip
    
    Replace the keepalive monitor by a timer
    
    * Remove unused objects and tidied up coding style
    
    * Disposing of the timer
    
    * Made the keepalive interval format more explicit
    
    * Moved keepalive from options to settings so it doesn't hurt Dan's eyes.

--- src/Stratis.Bitcoin.Features.Api/ApiFeature.cs
+++ src/Stratis.Bitcoin.Features.Api/ApiFeature.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Timers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Stratis.Bitcoin.Builder;
 using Stratis.Bitcoin.Builder.Feature;
-using Stratis.Bitcoin.Features.Api.Models;
 using Stratis.Bitcoin.Utilities;
 
 namespace Stratis.Bitcoin.Features.Api
@@ -15,14 +15,8 @@ namespace Stratis.Bitcoin.Features.Api
     /// </summary>
     public sealed class ApiFeature : FullNodeFeature
     {
-        /// <summary>The async loop we need to wait upon before we can shut down this feature.</summary>
-        private IAsyncLoop asyncLoop;
-
-        /// <summary>Factory for creating background async loop tasks.</summary>
-        private readonly IAsyncLoopFactory asyncLoopFactory;
-
         /// <summary>How long we are willing to wait for the API to stop.</summary>
-        private const int APIStopTimeoutSeconds = 10;
+        private const int ApiStopTimeoutSeconds = 10;
 
         private readonly IFullNodeBuilder fullNodeBuilder;
 
@@ -34,20 +28,18 @@ namespace Stratis.Bitcoin.Features.Api
 
         private readonly ILogger logger;
 
-        private IWebHost webHost = null;
+        private IWebHost webHost;
 
         public ApiFeature(
             IFullNodeBuilder fullNodeBuilder,
             FullNode fullNode,
             ApiFeatureOptions apiFeatureOptions,
-            IAsyncLoopFactory asyncLoopFactory,
             ApiSettings apiSettings,
             ILoggerFactory loggerFactory)
         {
             this.fullNodeBuilder = fullNodeBuilder;
             this.fullNode = fullNode;
             this.apiFeatureOptions = apiFeatureOptions;
-            this.asyncLoopFactory = asyncLoopFactory;
             apiSettings.Load(fullNode.Settings);
             this.apiSettings = apiSettings;
             this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
@@ -58,60 +50,51 @@ namespace Stratis.Bitcoin.Features.Api
             this.logger.LogInformation("API starting on URL '{0}'.", this.apiSettings.ApiUri);
             this.webHost = Program.Initialize(this.fullNodeBuilder.Services, this.fullNode, this.apiSettings);
 
-            this.TryStartKeepaliveMonitor();
+            // Start the keepalive timer, if set.
+            // If the timer expires, the node will shut down.
+            if (this.apiSettings.KeepaliveTimer != null)
+            {
+                this.apiSettings.KeepaliveTimer.Elapsed += (sender, args) =>
+                {
+                    this.logger.LogInformation($"The application will shut down because the keepalive timer has elapsed.");
+
+                    this.apiSettings.KeepaliveTimer.Stop();
+                    this.apiSettings.KeepaliveTimer.Enabled = false;
+                    this.fullNode.NodeLifetime.StopApplication();
+                };
+
+                this.apiSettings.KeepaliveTimer.Start();
+            }
         }
 
         /// <inheritdoc />
         public override void Dispose()
         {
-            this.asyncLoop?.Dispose();
+            // Make sure the timer is stopped and disposed.
+            if (this.apiSettings.KeepaliveTimer != null)
+            {
+                this.apiSettings.KeepaliveTimer.Stop();
+                this.apiSettings.KeepaliveTimer.Enabled = false;
+                this.apiSettings.KeepaliveTimer.Dispose();
+            }
 
             // Make sure we are releasing the listening ip address / port.
             if (this.webHost != null)
             {
                 this.logger.LogInformation("API stopping on URL '{0}'.", this.apiSettings.ApiUri);
-                this.webHost.StopAsync(TimeSpan.FromSeconds(APIStopTimeoutSeconds)).Wait();
+                this.webHost.StopAsync(TimeSpan.FromSeconds(ApiStopTimeoutSeconds)).Wait();
                 this.webHost = null;
-            }
-        }
-
-        /// <summary>
-        /// A KeepaliveMonitor when enabled will shutdown the
-        /// node if no one is calling the keepalive endpoint
-        /// during a certain trashold window
-        /// </summary>
-        public void TryStartKeepaliveMonitor()
-        {
-            if (this.apiFeatureOptions.KeepaliveMonitor?.KeepaliveInterval.TotalSeconds > 0)
-            {
-                this.asyncLoop = this.asyncLoopFactory.Run("ApiFeature.KeepaliveMonitor", token =>
-                {
-                    // shortened for redability
-                    KeepaliveMonitor monitor = this.apiFeatureOptions.KeepaliveMonitor;
-
-                    // check the trashold to trigger a shutdown
-                    if (monitor.LastBeat.Add(monitor.KeepaliveInterval) < this.fullNode.DateTimeProvider.GetUtcNow())
-                        this.fullNode.Dispose();
-
-                    return Task.CompletedTask;
-                },
-                this.fullNode.NodeLifetime.ApplicationStopping,
-                repeatEvery: this.apiFeatureOptions.KeepaliveMonitor?.KeepaliveInterval,
-                startAfter: TimeSpans.Minute);
             }
         }
     }
 
     public sealed class ApiFeatureOptions
     {
-        public KeepaliveMonitor KeepaliveMonitor { get; private set; }
-
-        public void Keepalive(TimeSpan timeSpan)
-        {
-            this.KeepaliveMonitor = new KeepaliveMonitor { KeepaliveInterval = timeSpan };
-        }
     }
 
+    /// <summary>
+    /// A class providing extension methods for <see cref="IFullNodeBuilder"/>.
+    /// </summary>
     public static class ApiFeatureExtension
     {
         public static IFullNodeBuilder UseApi(this IFullNodeBuilder fullNodeBuilder, Action<ApiSettings> setup = null, Action<ApiFeatureOptions> optionsAction = null)
