0c252f22bab2395b7d2b5fffdb5b7aaca05c5768
Dan Gershony <dan.gershony@gmail.com>
2018-02-13 17:14:06 +0000

    Remove witness validation code from validator

--- src/Stratis.Bitcoin.Features.Consensus/PowConsensusValidator.cs
+++ src/Stratis.Bitcoin.Features.Consensus/PowConsensusValidator.cs
@@ -65,60 +65,6 @@ namespace Stratis.Bitcoin.Features.Consensus
             this.logger.LogTrace("()");
 
             Block block = context.BlockValidationContext.Block;
-            DeploymentFlags deploymentFlags = context.Flags;
-
-            // Validation for witness commitments.
-            // * We compute the witness hash (which is the hash including witnesses) of all the block's transactions, except the
-            //   coinbase (where 0x0000....0000 is used instead).
-            // * The coinbase scriptWitness is a stack of a single 32-byte vector, containing a witness nonce (unconstrained).
-            // * We build a merkle tree with all those witness hashes as leaves (similar to the hashMerkleRoot in the block header).
-            // * There must be at least one output whose scriptPubKey is a single 36-byte push, the first 4 bytes of which are
-            //   {0xaa, 0x21, 0xa9, 0xed}, and the following 32 bytes are SHA256^2(witness root, witness nonce). In case there are
-            //   multiple, the last one is used.
-            bool haveWitness = false;
-            if (deploymentFlags.ScriptFlags.HasFlag(ScriptVerify.Witness))
-            {
-                int commitpos = this.GetWitnessCommitmentIndex(block);
-                if (commitpos != -1)
-                {
-                    uint256 hashWitness = this.BlockWitnessMerkleRoot(block, out bool unused);
-
-                    // The malleation check is ignored; as the transaction tree itself
-                    // already does not permit it, it is impossible to trigger in the
-                    // witness tree.
-                    WitScript witness = block.Transactions[0].Inputs[0].WitScript;
-                    if ((witness.PushCount != 1) || (witness.Pushes.First().Length != 32))
-                    {
-                        this.logger.LogTrace("(-)[BAD_WITNESS_NONCE_SIZE]");
-                        ConsensusErrors.BadWitnessNonceSize.Throw();
-                    }
-
-                    var hashed = new byte[64];
-                    Buffer.BlockCopy(hashWitness.ToBytes(), 0, hashed, 0, 32);
-                    Buffer.BlockCopy(witness.Pushes.First(), 0, hashed, 32, 32);
-                    hashWitness = Hashes.Hash256(hashed);
-
-                    if (!this.EqualsArray(hashWitness.ToBytes(), block.Transactions[0].Outputs[commitpos].ScriptPubKey.ToBytes(true).Skip(6).ToArray(), 32))
-                    {
-                        this.logger.LogTrace("(-)[WITNESS_MERKLE_MISMATCH]");
-                        ConsensusErrors.BadWitnessMerkleMatch.Throw();
-                    }
-
-                    haveWitness = true;
-                }
-            }
-
-            if (!haveWitness)
-            {
-                for (int i = 0; i < block.Transactions.Count; i++)
-                {
-                    if (block.Transactions[i].HasWitness)
-                    {
-                        this.logger.LogTrace("(-)[UNEXPECTED_WITNESS]");
-                        ConsensusErrors.UnexpectedWitness.Throw();
-                    }
-                }
-            }
 
             // After the coinbase witness nonce and commitment are verified,
             // we can check if the block weight passes (before we've checked the
