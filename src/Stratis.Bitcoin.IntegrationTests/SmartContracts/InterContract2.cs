d6287dd44a32ff0f61b94b708f6821ab15f625d5
Jandrews <jordandrews@hotmail.com>
2018-02-23 16:38:54 +1100

    Extra unit test

--- src/Stratis.Bitcoin.IntegrationTests/SmartContracts/InterContract2.cs
+++ src/Stratis.Bitcoin.IntegrationTests/SmartContracts/InterContract2.cs
@@ -14,4 +14,15 @@ public class InterContract2 : SmartContract
 
         return (int) result.ReturnValue;
     }
+
+    public bool ContractTransferWithFail(string addressString)
+    {
+        TransferResult result = Transfer(new Address(addressString), 100, new TransactionDetails
+        {
+            ContractMethodName = "ThrowException",
+            ContractTypeName = "InterContract1"
+        });
+
+        return result.Success;
+    }
 }
