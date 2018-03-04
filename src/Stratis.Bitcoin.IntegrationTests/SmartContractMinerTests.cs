d86efbf085123b01f4c366c839ccc439b64a0748
Jandrews <jordandrews@hotmail.com>
2018-03-04 20:43:41 +1100

    Test network

--- src/Stratis.Bitcoin.IntegrationTests/SmartContractMinerTests.cs
+++ src/Stratis.Bitcoin.IntegrationTests/SmartContractMinerTests.cs
@@ -152,13 +152,7 @@ namespace Stratis.Bitcoin.IntegrationTests
                     this.blockinfo.Add(new Blockinfo { extranonce = (int)lst[i], nonce = (uint)lst[i + 1] });
 
                 // Note that by default, these tests run with size accounting enabled.
-                this.network = Network.SmartContractsRegTest;
-                //this.networkOptions = new NetworkOptions
-                //{
-                //    IsProofOfStake = false,
-                //    IsWitness = false,
-                //    IsSmartContracts = true
-                //};
+                this.network = Network.SmartContractsTest;
                 var hex = Encoders.Hex.DecodeData("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f");
                 this.scriptPubKey = new Script(new[] { Op.GetPushOp(hex), OpcodeType.OP_CHECKSIG });
                 this.coinbaseAddress = new uint160(new Script(hex).Hash.ToBytes(), false);
