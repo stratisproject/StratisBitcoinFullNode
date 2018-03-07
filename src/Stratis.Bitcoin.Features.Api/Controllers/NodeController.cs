d4498955fcb557e7425118288ecb75e8f8ad98aa
bokobza <bokobza@gmail.com>
2018-03-07 14:11:41 +0000

    Removed keepalive from the NodeController as it's unused and superfluous

--- src/Stratis.Bitcoin.Features.Api/Controllers/NodeController.cs
+++ src/Stratis.Bitcoin.Features.Api/Controllers/NodeController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Stratis.Bitcoin.Utilities;
 
 namespace Stratis.Bitcoin.Features.Api.Controllers
 {
@@ -8,12 +7,9 @@ namespace Stratis.Bitcoin.Features.Api.Controllers
     {
         private readonly IFullNode fullNode;
 
-        private readonly ApiSettings apiSettings;
-
-        public NodeController(IFullNode fullNode, ApiSettings apiSettings)
+        public NodeController(IFullNode fullNode)
         {
             this.fullNode = fullNode;
-            this.apiSettings = apiSettings;
         }
 
         /// <summary>
@@ -38,21 +34,6 @@ namespace Stratis.Bitcoin.Features.Api.Controllers
             // start the node shutdown process
             this.fullNode.Dispose();
 
-            return this.Ok();
-        }
-
-        /// <summary>
-        /// Set the keepalive flag.
-        /// </summary>
-        /// <returns>An HTTP status code indicating success (200).</returns>
-        [HttpPost]
-        [Route("keepalive")]
-        public IActionResult Keepalive()
-        {
-            if (this.apiSettings.KeepaliveTimer == null)
-                return new ObjectResult("Keepalive Disabled") { StatusCode = 405 }; // (405) Method Not Allowed
-
-            this.apiSettings.KeepaliveTimer.Reset();
             return this.Ok();
         }
     }
