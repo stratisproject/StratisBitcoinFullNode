db40b386c87706fc4e387813a0daa580d6c3b9a2
Dan Gershony <dan.gershony@gmail.com>
2018-03-14 19:49:53 +0000

    Add the correct attribute to the rule

--- src/Stratis.Bitcoin.Features.Consensus/Rules/CommonRules/TransactionDuplicationActivationRule.cs
+++ src/Stratis.Bitcoin.Features.Consensus/Rules/CommonRules/TransactionDuplicationActivationRule.cs
@@ -12,6 +12,7 @@ namespace Stratis.Bitcoin.Features.Consensus.Rules.CommonRules
     /// <remarks>
     /// More info here https://github.com/bitcoin/bips/blob/master/bip-0030.mediawiki
     /// </remarks>
+    [ExecutionRule]
     public class TransactionDuplicationActivationRule : UtxoStoreConsensusRule
     {
         /// <inheritdoc />>
