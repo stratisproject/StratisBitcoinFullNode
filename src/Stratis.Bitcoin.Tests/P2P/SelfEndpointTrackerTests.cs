46073cce16a573c7d126a5d8cb407e415f035a18
majikandy <andy@tenkaisolutions.com>
2018-03-14 16:56:14 +0000

    self endpoint tracker added with expiry

---
+++ src/Stratis.Bitcoin.Tests/P2P/SelfEndpointTrackerTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using Stratis.Bitcoin.P2P;
+using Xunit;
+
+namespace Stratis.Bitcoin.Tests.P2P
+{
+    public class SelfEndpointTrackerTests : IClassFixture<SelfEndpointTracker>
+    {
+        [Fact]
+        public void Add_OneIpEndpoint_GetsAdded()
+        {
+            ISelfEndpointTracker selfEndpointTrackerTracker = new SelfEndpointTracker();
+            selfEndpointTrackerTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
+            Assert.True(selfEndpointTrackerTracker.IsSelf(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234)));
+        }
+
+        [Fact]
+        public void Add_TwoIpEndpointsTheSame_GetsAddedOnce()
+        {
+            ISelfEndpointTracker selfEndpointTrackerTracker = new SelfEndpointTracker();
+            selfEndpointTrackerTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
+            selfEndpointTrackerTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
+            Assert.True(selfEndpointTrackerTracker.IsSelf(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234)));
+        }
+
+        [Fact]
+        public void Add_TwoDifferentIpEndpoints_BothAreSelf()
+        {
+            ISelfEndpointTracker selfEndpointTrackerTracker = new SelfEndpointTracker();
+            selfEndpointTrackerTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
+            selfEndpointTrackerTracker.Add(new IPEndPoint(IPAddress.Parse("5.6.7.8"), 1234));
+            Assert.True(selfEndpointTrackerTracker.IsSelf(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234)));
+            Assert.True(selfEndpointTrackerTracker.IsSelf(new IPEndPoint(IPAddress.Parse("5.6.7.8"), 1234)));
+        }
+
+        [Fact]
+        public void IsSelf_ForDifferentEndpoint_IsFalse()
+        {
+            ISelfEndpointTracker selfEndpointTrackerTracker = new SelfEndpointTracker();
+            selfEndpointTrackerTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
+            Assert.False(selfEndpointTrackerTracker.IsSelf(new IPEndPoint(IPAddress.Parse("5.6.7.8"), 1234)));
+        }
+
+        [Fact]
+        public void IsSelf_ForSameEndpointButExpired_IsFalse()
+        {
+            SelfEndpointTracker selfEndpointTrackerTracker = new SelfEndpointTracker();
+
+            selfEndpointTrackerTracker.Now = () => DateTime.UtcNow.AddHours(-SelfEndpointTracker.ExpiryInHours);
+            selfEndpointTrackerTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
+            selfEndpointTrackerTracker.Now = () => DateTime.UtcNow;
+
+            Assert.False(selfEndpointTrackerTracker.IsSelf(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234)));
+        }
+    }
+}
---
+++ src/Stratis.Bitcoin/P2P/ISelfEndpointTracker.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Stratis.Bitcoin.P2P
+{
+    public interface ISelfEndpointTracker
+    {
+        void Add(IPEndPoint ipEndPoint);
+        bool IsSelf(IPEndPoint ipEndPoint);
+    }
+}
