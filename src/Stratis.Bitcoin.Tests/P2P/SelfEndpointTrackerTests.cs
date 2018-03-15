edabd48a5d8fe2a314f4520ea58776f9e05c3c30
majikandy <andy@tenkaisolutions.com>
2018-03-15 09:57:12 +0000

    concurrent dictionary

--- src/Stratis.Bitcoin.Tests/P2P/SelfEndpointTrackerTests.cs
+++ src/Stratis.Bitcoin.Tests/P2P/SelfEndpointTrackerTests.cs
@@ -1,59 +1,68 @@
 ﻿using System;
 using System.Net;
+using Moq;
 using Stratis.Bitcoin.P2P;
+using Stratis.Bitcoin.Utilities;
 using Xunit;
 
 namespace Stratis.Bitcoin.Tests.P2P
 {
-    public class SelfEndpointTrackerTests : IClassFixture<SelfEndpointTracker>
+    public class SelfEndpointTrackerTests
     {
+        private readonly Mock<IDateTimeProvider> datetimeProvider;
+        private readonly ISelfEndpointTracker selfEndpointTracker;
+
+        public SelfEndpointTrackerTests()
+        {
+            this.datetimeProvider = new Mock<IDateTimeProvider>();
+            this.selfEndpointTracker = new SelfEndpointTracker(this.datetimeProvider.Object);
+        }
+
         [Fact]
         public void Add_OneIpEndpoint_GetsAdded()
         {
-            ISelfEndpointTracker selfEndpointTrackerTracker = new SelfEndpointTracker();
-            selfEndpointTrackerTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
-            Assert.True(selfEndpointTrackerTracker.IsSelf(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234)));
+            this.selfEndpointTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
+            Assert.True(this.selfEndpointTracker.IsSelf(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234)));
         }
 
         [Fact]
         public void Add_TwoIpEndpointsTheSame_GetsAddedOnce()
         {
-            ISelfEndpointTracker selfEndpointTrackerTracker = new SelfEndpointTracker();
-            selfEndpointTrackerTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
-            selfEndpointTrackerTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
-            Assert.True(selfEndpointTrackerTracker.IsSelf(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234)));
+            this.selfEndpointTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
+            this.selfEndpointTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
+            Assert.True(this.selfEndpointTracker.IsSelf(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234)));
         }
 
         [Fact]
         public void Add_TwoDifferentIpEndpoints_BothAreSelf()
         {
-            ISelfEndpointTracker selfEndpointTrackerTracker = new SelfEndpointTracker();
-            selfEndpointTrackerTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
-            selfEndpointTrackerTracker.Add(new IPEndPoint(IPAddress.Parse("5.6.7.8"), 1234));
-            Assert.True(selfEndpointTrackerTracker.IsSelf(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234)));
-            Assert.True(selfEndpointTrackerTracker.IsSelf(new IPEndPoint(IPAddress.Parse("5.6.7.8"), 1234)));
+            this.selfEndpointTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
+            this.selfEndpointTracker.Add(new IPEndPoint(IPAddress.Parse("5.6.7.8"), 1234));
+            Assert.True(this.selfEndpointTracker.IsSelf(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234)));
+            Assert.True(this.selfEndpointTracker.IsSelf(new IPEndPoint(IPAddress.Parse("5.6.7.8"), 1234)));
         }
 
         [Fact]
         public void IsSelf_ForDifferentEndpoint_IsFalse()
         {
-            ISelfEndpointTracker selfEndpointTrackerTracker = new SelfEndpointTracker();
-            selfEndpointTrackerTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
-            Assert.False(selfEndpointTrackerTracker.IsSelf(new IPEndPoint(IPAddress.Parse("5.6.7.8"), 1234)));
+            this.selfEndpointTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
+            Assert.False(this.selfEndpointTracker.IsSelf(new IPEndPoint(IPAddress.Parse("5.6.7.8"), 1234)));
         }
 
         [Fact]
         public void IsSelf_ForSameEndpointButExpired_IsFalse()
         {
-            SelfEndpointTracker selfEndpointTrackerTracker = new SelfEndpointTracker();
-            selfEndpointTrackerTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
-            this.SimulateMovingForwardInTimeByExpiryHours(selfEndpointTrackerTracker);
-            Assert.False(selfEndpointTrackerTracker.IsSelf(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234)));
+            selfEndpointTracker.Add(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234));
+
+            this.SimulateMovingForwardInTimeByExpiryHours();
+
+            Assert.False(selfEndpointTracker.IsSelf(new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234)));
         }
 
-        private void SimulateMovingForwardInTimeByExpiryHours(SelfEndpointTracker selfEndpointTrackerTracker)
+        private void SimulateMovingForwardInTimeByExpiryHours()
         {
-            selfEndpointTrackerTracker.Now = () => DateTime.UtcNow.AddHours(SelfEndpointTracker.ExpiryInHours);
+            this.datetimeProvider.Setup(x => x.GetUtcNow())
+                .Returns(DateTime.UtcNow.AddHours(SelfEndpointTracker.ExpiryInHours));
         }
     }
 }
