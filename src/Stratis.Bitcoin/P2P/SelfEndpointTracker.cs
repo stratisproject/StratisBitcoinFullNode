a9ee2f32d1816a7a028986c16f8d06466f32ff16
majikandy <andy@tenkaisolutions.com>
2018-03-15 09:57:12 +0000

    concurrent dictionary

--- src/Stratis.Bitcoin/P2P/SelfEndpointTracker.cs
+++ src/Stratis.Bitcoin/P2P/SelfEndpointTracker.cs
@@ -1,25 +1,31 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using Stratis.Bitcoin.Utilities;
 
 namespace Stratis.Bitcoin.P2P
 {
     public class SelfEndpointTracker : ISelfEndpointTracker
     {
         public const int ExpiryInHours = 12;
-        public Func<DateTime> Now = () => DateTime.UtcNow;
 
-        private Dictionary<IPEndPoint, DateTime> currentKnownEndpointsAndExpiryDate = new Dictionary<IPEndPoint, DateTime>();
+        private readonly IDateTimeProvider dateTimeProvider;
+
+        public SelfEndpointTracker(IDateTimeProvider dateTimeProvider)
+        {
+            this.dateTimeProvider = dateTimeProvider;
+        }
+
+        private ConcurrentDictionary<IPEndPoint, DateTime> currentKnownEndpointsAndExpiryDate = new ConcurrentDictionary<IPEndPoint, DateTime>();
 
         public void Add(IPEndPoint ipEndPoint)
         {
             this.Prune();
 
-            if (this.currentKnownEndpointsAndExpiryDate.ContainsKey(ipEndPoint))
-                this.currentKnownEndpointsAndExpiryDate.Remove(ipEndPoint);
-
-            this.currentKnownEndpointsAndExpiryDate.Add(ipEndPoint, this.Now().AddHours(ExpiryInHours));
+            DateTime newExpiryDate = this.dateTimeProvider.GetUtcNow().AddHours(ExpiryInHours);
+            this.currentKnownEndpointsAndExpiryDate.AddOrUpdate(ipEndPoint, newExpiryDate, (key, value) => newExpiryDate);
         }
 
         public bool IsSelf(IPEndPoint ipEndPoint)
@@ -30,10 +36,12 @@ namespace Stratis.Bitcoin.P2P
 
         private void Prune()
         {
-            this.currentKnownEndpointsAndExpiryDate = 
+            Dictionary<IPEndPoint, DateTime> dictionary = 
                 this.currentKnownEndpointsAndExpiryDate
-                    .Where(x => x.Value > this.Now())
+                    .Where(x => x.Value > this.dateTimeProvider.GetUtcNow())
                     .ToDictionary(x => x.Key, x => x.Value);
+
+            this.currentKnownEndpointsAndExpiryDate = new ConcurrentDictionary<IPEndPoint, DateTime>(dictionary);
         }
     }
 }
