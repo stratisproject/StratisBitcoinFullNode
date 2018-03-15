ef4a664a2f30fd1722920cd942c6ed18031a778a
majikandy <andy@tenkaisolutions.com>
2018-03-15 16:59:51 +0000

    Added required documentation

--- src/Stratis.Bitcoin/P2P/ISelfEndpointTracker.cs
+++ src/Stratis.Bitcoin/P2P/ISelfEndpointTracker.cs
@@ -2,9 +2,18 @@
 
 namespace Stratis.Bitcoin.P2P
 {
+    /// <summary>
+    /// Tracker for endpoints known to be self.
+    /// </summary>
     public interface ISelfEndpointTracker
     {
+        /// <summary>Adds an endpoint to the currently known list.</summary>
+        /// <param name="ipEndPoint">The endpoint to add.</param>
         void Add(IPEndPoint ipEndPoint);
+
+        /// <summary>Checks if endpoint is known to be itself against the pruned dictionary.</summary>
+        /// <param name="ipEndPoint"></param>
+        /// <returns><c>true</c> if self, <c>false</c> if unknown.</returns>
         bool IsSelf(IPEndPoint ipEndPoint);
     }
 }
--- src/Stratis.Bitcoin/P2P/SelfEndpointTracker.cs
+++ src/Stratis.Bitcoin/P2P/SelfEndpointTracker.cs
@@ -7,41 +7,54 @@ using Stratis.Bitcoin.Utilities;
 
 namespace Stratis.Bitcoin.P2P
 {
+    /// <summary>
+    /// Tracker for endpoints known to be self.
+    /// </summary>
     public class SelfEndpointTracker : ISelfEndpointTracker
     {
+        /// <summary>Stored endpoints expire after this many hours and need to be readded.
+        /// <para>So that changes in Endpoint Address don't prevent a connection to another node
+        /// in the event they have the Endpoint Address that used to belong to this node</para>
+        /// </summary>
         public const int ExpiryInHours = 12;
 
+        /// <summary>Provides the datetime in controllable way.</summary>
         private readonly IDateTimeProvider dateTimeProvider;
 
+        /// <summary>Dictionary to hold the endpoints currently known to be itself and their expiry date.</summary>
+        private ConcurrentDictionary<IPEndPoint, DateTime> knownSelfEndpointsAndExpiryDate = new ConcurrentDictionary<IPEndPoint, DateTime>();
+
+        /// <summary>Constructor</summary>
+        /// <param name="dateTimeProvider">Datetime provider injected so it can be controlled in tests.</param>
         public SelfEndpointTracker(IDateTimeProvider dateTimeProvider)
         {
             this.dateTimeProvider = dateTimeProvider;
         }
 
-        private ConcurrentDictionary<IPEndPoint, DateTime> currentKnownEndpointsAndExpiryDate = new ConcurrentDictionary<IPEndPoint, DateTime>();
-
+        /// <inheritdoc/>
         public void Add(IPEndPoint ipEndPoint)
         {
-            this.Prune();
+            this.PruneExpired();
 
             DateTime newExpiryDate = this.dateTimeProvider.GetUtcNow().AddHours(ExpiryInHours);
-            this.currentKnownEndpointsAndExpiryDate.AddOrUpdate(ipEndPoint, newExpiryDate, (key, value) => newExpiryDate);
+            this.knownSelfEndpointsAndExpiryDate.AddOrUpdate(ipEndPoint, newExpiryDate, (key, value) => newExpiryDate);
         }
-
+        /// <inheritdoc/>
         public bool IsSelf(IPEndPoint ipEndPoint)
         {
-            this.Prune();
-            return this.currentKnownEndpointsAndExpiryDate.ContainsKey(ipEndPoint);
+            this.PruneExpired();
+            return this.knownSelfEndpointsAndExpiryDate.ContainsKey(ipEndPoint);
         }
 
-        private void Prune()
+        /// <summary>Removes the expired items from the dictionary.</summary>
+        private void PruneExpired()
         {
             Dictionary<IPEndPoint, DateTime> unexpiredItems = 
-                this.currentKnownEndpointsAndExpiryDate
+                this.knownSelfEndpointsAndExpiryDate
                     .Where(x => x.Value > this.dateTimeProvider.GetUtcNow())
                     .ToDictionary(x => x.Key, x => x.Value);
 
-            this.currentKnownEndpointsAndExpiryDate = new ConcurrentDictionary<IPEndPoint, DateTime>(unexpiredItems);
+            this.knownSelfEndpointsAndExpiryDate = new ConcurrentDictionary<IPEndPoint, DateTime>(unexpiredItems);
         }
     }
 }
