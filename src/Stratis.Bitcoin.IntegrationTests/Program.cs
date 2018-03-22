cf424c96dfe36e828a52c58e556378e9bed49e53
Dan Gershony <dan.gershony@gmail.com>
2018-03-22 15:30:53 +0000

    Update test packages version

--- src/Stratis.Bitcoin.IntegrationTests/Program.cs
+++ src/Stratis.Bitcoin.IntegrationTests/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            new WalletTests().CanMineAndSendToAddress();
+            new NodeSyncTests().CanStratisSyncFromStratis();
         }
     }
 }
