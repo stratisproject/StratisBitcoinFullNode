d9164f69662e3178518c796d47c7db5bf7e6dbfb
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-03-26 16:33:24 +1100

    Add MSTest and basic unit test

--- src/Stratis.SmartContracts.Template/Tests/SmartContractTests.cs
+++ src/Stratis.SmartContracts.Template/Tests/SmartContractTests.cs
@@ -1,6 +1,72 @@
-﻿namespace $safeprojectname$
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stratis.SmartContracts;
+
+namespace $safeprojectname$
 {
-	public class SmartContractTests
+    [TestClass]
+    public class SmartContractTests
     {
+        private TestSmartContractState smartContractState;
+        private const ulong Balance = 10000;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var block = new Block(0, Address.Zero, 0);
+            var message = new Message(Address.Zero, Address.Zero, 0, (Gas)0);
+            var getBalance = new Func<ulong>(() => Balance);
+
+            this.smartContractState = new TestSmartContractState(
+                block,
+                message,
+                null,
+                null,
+                null,
+                getBalance
+            );
+        }
+
+        [TestMethod]
+        public void TestMethod()
+        {
+            var contract = new Contract(smartContractState);
+
+            Assert.AreEqual("Test", contract.Test());
+        }
+
+        [TestMethod]
+        public void TestBalance()
+        {
+            var contract = new Contract(smartContractState);
+
+            Assert.AreEqual(Balance, contract.Balance);
+        }
+    }
+
+    public class TestSmartContractState : ISmartContractState
+    {
+        public TestSmartContractState(
+            Block block,
+            Message message,
+            IPersistentState persistentState,
+            IGasMeter gasMeter,
+            IInternalTransactionExecutor transactionExecutor,
+            Func<ulong> getBalance)
+        {
+            this.Block = block;
+            this.Message = message;
+            this.PersistentState = persistentState;
+            this.GasMeter = gasMeter;
+            this.InternalTransactionExecutor = transactionExecutor;
+            this.GetBalance = getBalance;
+        }
+
+        public Block Block { get; }
+        public Message Message { get; }
+        public IPersistentState PersistentState { get; }
+        public IGasMeter GasMeter { get; }
+        public IInternalTransactionExecutor InternalTransactionExecutor { get; }
+        public Func<ulong> GetBalance { get; }
     }
 }
