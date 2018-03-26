fc3223d4c43cf088fab6c277ff4a6658ca75b19a
Andy Davis <github@andy-davis.co.uk>
2018-03-26 16:20:45 +0100

    Spend coin straight after mining should fail (#1238)
    * v1
    
    * added fluent assertions to complement bdd
    
    * pow test for immature coin spend fail pre bdded
    
    * rest of test specced out unimplemented
    
    * moved into bdd actions and steps separated
    
    * typo fix
    
    * spec to specification
    
    * spec to specification
    
    * spec to specification
    
    * spec to specification
    
    * Mempool checked for transaction sent and mining 1 block followed by the two scenarios (just before maturity and just after)
    
    * Added optional outputting of the test step names
    
    * Moved logging constructor to steps file
    
    * Methods move up/down
    
    * Move BDD framework into folder
    
    * Added timecode to step trace

---
+++ src/Stratis.Bitcoin.IntegrationTests/BlockStore/ProofOfWorkSpendingSpecification.cs
@@ -0,0 +1,28 @@
+﻿using Stratis.Bitcoin.IntegrationTests.TestFramework;
+using Xunit;
+
+namespace Stratis.Bitcoin.IntegrationTests.BlockStore
+{
+    public partial class ProofOfWorkSpendingSpecification : BddSpecification
+    {
+        [Fact]
+        public void Attempt_to_spend_coin_earned_through_proof_of_work_BEFORE_coin_maturity_will_fail()
+        {
+            Given(a_sending_and_receiving_stratis_bitcoin_node_and_wallet);
+            And(a_block_is_mined_creating_spendable_coins);
+            And(more_blocks_mined_to_just_BEFORE_maturity_of_original_block);
+            When(spending_the_coins_from_original_block);
+            Then(the_transaction_is_rejected_from_the_mempool);
+        }
+
+        [Fact]
+        public void Attempt_to_spend_coin_earned_through_proof_of_work_AFTER_maturity_will_succeed()
+        {
+            Given(a_sending_and_receiving_stratis_bitcoin_node_and_wallet);
+            And(a_block_is_mined_creating_spendable_coins);
+            And(more_blocks_mined_to_just_AFTER_maturity_of_original_block);
+            When(spending_the_coins_from_original_block);
+            Then(the_transaction_is_put_in_the_mempool);
+        }
+    }
+}
