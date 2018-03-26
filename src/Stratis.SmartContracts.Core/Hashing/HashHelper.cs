8dbffc395e779744a356de1a4b716233adb2fe73
Jandrews <jordandrews@hotmail.com>
2018-03-26 12:29:46 +1100

    Fixing master

--- src/Stratis.SmartContracts.Core/Hashing/HashHelper.cs
+++ src/Stratis.SmartContracts.Core/Hashing/HashHelper.cs
@@ -8,11 +8,11 @@ namespace Stratis.SmartContracts.Core.Hashing
     /// </summary>
     public static class HashHelper
     {
+        private static readonly IHash Keccak = HashFactory.Crypto.SHA3.CreateKeccak256();
         public static readonly byte[] EmptyByteArray = new byte[0];
         public static readonly byte[] EmptyDataHash = Keccak256(EmptyByteArray);
         public static readonly byte[] EmptyElementRlp = RLP.EncodeElement(EmptyByteArray);
         public static readonly byte[] EmptyTrieHash = Keccak256(EmptyElementRlp);
-        private static readonly IHash Keccak = HashFactory.Crypto.SHA3.CreateKeccak256();
 
         /// <summary>
         /// Returns a 32-byte Keccak256 hash of the given bytes.
