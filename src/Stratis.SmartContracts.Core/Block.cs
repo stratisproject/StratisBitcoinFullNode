60f03c2126c2981e1304d0c3c46d6e1986b2518c
Jandrews <jordandrews@hotmail.com>
2018-03-31 00:49:40 +1100

    Post Review

--- src/Stratis.SmartContracts.Core/Block.cs
+++ src/Stratis.SmartContracts.Core/Block.cs
@@ -3,16 +3,9 @@
     public struct Block : IBlock
     {
         /// <inheritdoc/>
-        /// <summary>
-        /// The coinbase address of the current block. 
-        /// The address that will receive the mining award for this block.
-        /// </summary>
         public Address Coinbase { get; }
 
         /// <inheritdoc/>
-        /// <summary>
-        /// The height of the current block.
-        /// </summary>
         public ulong Number { get; }
 
         public Block(ulong number, Address coinbase)
--- src/Stratis.SmartContracts.Core/Message.cs
+++ src/Stratis.SmartContracts.Core/Message.cs
@@ -3,27 +3,15 @@
     public sealed class Message : IMessage
     {
         /// <inheritdoc/>
-        /// <summary>
-        /// The address of the contract currently being executed.
-        /// </summary>
         public Address ContractAddress { get; }
 
         /// <inheritdoc/>
-        /// <summary>
-        /// The address that called this contract.
-        /// </summary>
         public Address Sender { get; }
 
         /// <inheritdoc/>
-        /// <summary>
-        /// The total gas allocated allowed to be spend during contract execution.
-        /// </summary>
         public Gas GasLimit { get; }
 
         /// <inheritdoc/>
-        /// <summary>
-        /// The amount of STRAT sent in this call. 
-        /// </summary>
         public ulong Value { get; }
 
         public Message(Address contractAddress, Address sender, ulong value, Gas gasLimit)
