563dcf5dbc1ac3cbc23db86769eb7cef27bd8a6c
Carlton Pringle <carlton.pringle@stratisplatform.com>
2018-04-04 14:57:04 +0000

    Created FedKeyPairGen console app.
    Imported from Changeset 124

---
+++ FedKeyPairGenManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Stratis.FederatedPeg;
+
+namespace FedKeyPairGen
+{
+    // The FedKeyPairGenManager handles console output and input.
+    internal static class FedKeyPairGenManager
+    {
+        // Standard information header.
+        public static void OutputHeader()
+        {
+            Console.WriteLine($"Stratis Federation KeyPair Generator v{Assembly.GetEntryAssembly().GetName().Version.ToString()} - Generates cryptographic key pairs for Sidechain Federation Members.");
+            Console.WriteLine("Copyright (c) 2018 Stratis Group Limited");
+            Console.WriteLine();
+        }
+
+        // A standard usage message with examples.  This is output on -h command and also in some cases if validation fails.
+        public static void OutputUsage()
+        {
+            Console.WriteLine("usage: fedkeypairgen [-name=<name>] [-folder=<output_folder>] [-pass=<passPhrase>] [-h]");
+            Console.WriteLine(" -name     Your full name as recognised by your Federation Administrator.");
+            Console.WriteLine(" -folder   The output folder where files will be written (default is current folder).");
+            Console.WriteLine(" -pass     The pass word or phrase used to encrypt your private key file.");
+            Console.WriteLine(" -h        This help message.");
+            Console.WriteLine();
+            Console.WriteLine("Example:  fedkeypairgen -name=\"John Smith\" -folder=\"C:\\KeyPairs\" -pass=\"secret\"");
+            Console.WriteLine("          fedkeypairgen -name=\"John Smith\" -pass=\"secret\"                       (uses current directory)");
+            Console.WriteLine("          fedkeypairgen                                                         (interactive)");
+            Console.WriteLine();
+        }
+
+        // Output completion message and secret warning.
+        public static void OutputSuccess()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Four key files were successfully output. Two of the files are PRIVATE keys that you must keep secret. Do not distribute these private keys.");
+        }
+
+        // On error we output in red. 
+        public static void OutputErrorLine(string message)
+        {
+            var colorSaved = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = colorSaved;
+        }
+
+        // Ask and confirm a password/phrase for interactive mode.
+        public static bool AskForPassPhrase(out string passPhrase)
+        {
+            Console.WriteLine("Please enter a pass phrase.  We will use this pass phrase to encrypt your private key.");
+            Console.WriteLine("Keep this pass phrase safe.");
+            Console.WriteLine();
+
+            passPhrase = Console.ReadLine();
+            Console.WriteLine("Please reenter your pass phrase.");
+
+            string passwordReenter = Console.ReadLine();
+            if (passPhrase != passwordReenter)
+            {
+                OutputErrorLine("The pass phrases must match.");
+                return false;
+            }
+            return true;
+        }
+
+        // Ask and confirm a name for interactive mode.
+        public static bool AskForName(out string name)
+        {
+            Console.WriteLine("Please enter a valid name.  Your name is used to identify you to your Sidechain Administrator.");
+
+            name = Console.ReadLine();
+            if (FederationMemberPrivate.IsValidName(name) == false)
+            {
+                OutputErrorLine("Invalid name. Please enter at least 3 characters and do not include underscore characters.");
+                return false;
+            }
+
+            Console.WriteLine($"Thank you, {name}.");
+            Console.WriteLine();
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using Stratis.FederatedPeg;
+
+namespace FedKeyPairGen
+{
+    /*
+        Stratis Federation KeyPair Generator v1.0.0.0 - Generates cryptographic key pairs for Sidechain Federation Members.
+        Copyright(c) 2018 Stratis Group Limited
+
+        usage:  fedkeypairgen [-name=<name>] [-folder=<output_folder>] [-pass=<passPhrase>] [-h]
+         -name     Your full name as recognised by your Federation Administrator.
+         -folder   The output folder where files will be written (default is current folder).
+         -pass     The pass word or phrase used to encrypt your private key file.
+         -h        This help message.
+
+        Example:  fedkeypairgen -name="John Smith" -folder="C:\KeyPairs" -pass="secret"
+                  fedkeypairgen -name="John Smith" -pass="secret"                           (uses current directory)
+                  fedkeypairgen                                                             (interactive)
+    */
+
+    // The Stratis Federation KeyPair Generator is a console app that can be sent to Federation Members
+    // in order to generate their Private (and Public) keys without a need to run a Node at this stage.
+    // See the "Use Case - Generate Federation Member Key Pairs" located in the Requirements folder in the
+    // project repo.
+
+    // Todo: The password is referred to as a PassPhrase although it isn't really a passphrase.
+    // Todo: The password/phrase has no format constraints such as length special characters.
+    // Todo: Native .Net AES256 Rijndael encryption is used.  Consider know full node alternative.
+    // Todo: Consider leveraging the FullNode Wallet mnemonic instead of the way it is handled here.
+    // Todo: We might consider outputting a config snippet for the full node also. 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Start with the banner.
+                FedKeyPairGenManager.OutputHeader();
+
+                // Validate arguments.  After this step we must have valid input.
+                bool @continue = ArgumentValidator.ProcessArgs(args, out bool _, out string name, out string folder, out string passPhrase);
+                if (!@continue) return;
+
+                // Create the private version of the Federation Member. 
+                var federationMemberPrivate = FederationMemberPrivate.CreateNew(name, passPhrase);
+
+                // File integrity check.  Do not continue if any clashing files exist in the destination folder.
+                var memberFolderManager = new MemberFolderManager(folder);
+                if (memberFolderManager.CountKeyFilesForMember(name) > 0)
+                    throw new InvalidOperationException(
+                        $"FedKeyPairGen cannot continue. The specified folder already contains keys for {name}.");
+
+                // Output a public and a private key for each chain (mainchain and sidechain).
+                memberFolderManager.OutputKeys(federationMemberPrivate);
+
+                // Write success message including warnings to keep secret private keys safe.
+                FedKeyPairGenManager.OutputSuccess();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                FedKeyPairGenManager.OutputErrorLine("The folder must exist.");
+                Console.WriteLine();
+                FedKeyPairGenManager.OutputUsage();
+            }
+            catch (Exception ex)
+            {
+                FedKeyPairGenManager.OutputErrorLine($"An error occurred: {ex.Message}");
+                Console.WriteLine();
+                FedKeyPairGenManager.OutputUsage();
+            }
+        }
+    }
+}
