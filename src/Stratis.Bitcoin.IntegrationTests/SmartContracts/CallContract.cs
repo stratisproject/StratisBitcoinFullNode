c7eb5ba6f1b71868b1ca83e3ce38644e79e947e7
Jandrews <jordandrews@hotmail.com>
2018-04-05 12:49:52 +1000

    Adding InvokeContract unit test

---
+++ src/Stratis.Bitcoin.IntegrationTests/SmartContracts/CallContract.cs
@@ -0,0 +1,65 @@
+﻿using Stratis.SmartContracts;
+
+public class CallContract : SmartContract
+{
+    public CallContract(ISmartContractState state) : base(state)
+    {
+        Test = "Initial";
+    }
+
+    public string Test
+    {
+        get
+        {
+            return PersistentState.GetObject<string>("Test");
+        }
+        set
+        {
+            PersistentState.SetObject<string>("Test", value);
+        }
+    }
+
+    public bool CallOther(string addressString)
+    {
+
+        ITransferResult result = TransferFunds(new Address(addressString), 100, new TransferFundsToContract
+        {
+            ContractMethodName = "IncrementCount",
+            ContractTypeName = "CountContract"
+        });
+
+        return result.Success;
+    }
+
+    public bool GetOtherCountValueAndUpdateOurs(string addressString)
+    {
+        ITransferResult result = TransferFunds(new Address(addressString), 100, new TransferFundsToContract
+        {
+            ContractMethodName = "get_Count",
+            ContractTypeName = "CountContract"
+        });
+
+        if (result.Success)
+            this.NewCount = (int)result.ReturnValue;
+
+        return result.Success;
+    }
+
+    public bool Tester(string addressString)
+    {
+        Test = "Not Initial!";
+        ITransferResult result = TransferFunds(new Address(addressString), 0, new TransferFundsToContract
+        {
+            ContractMethodName = "Callback",
+            ContractTypeName = "CountContract"
+        });
+        return (bool)result.ReturnValue;
+    }
+
+    public bool Asserter()
+    {
+        return Test == "Not Initial!";
+    }
+
+    public int NewCount { get; set; }
+}
---
+++ src/Stratis.Bitcoin.IntegrationTests/SmartContracts/CountContract.cs
@@ -0,0 +1,45 @@
+﻿using Stratis.SmartContracts;
+
+public class CountContract : SmartContract
+{
+    public CountContract(ISmartContractState state) : base(state) { }
+
+    public int Count
+    {
+        get
+        {
+            return PersistentState.GetObject<int>("Count");
+        }
+        set
+        {
+            PersistentState.SetObject<int>("Count", value);
+        }
+    }
+
+    public bool SaveWorked {
+        get
+        {
+            return PersistentState.GetObject<bool>("SaveWorked");
+        }
+        set
+        {
+            PersistentState.SetObject("SaveWorked", value);
+        }
+    }
+
+    public void IncrementCount()
+    {
+        this.Count++;
+    }
+
+    public bool Callback()
+    {
+        ITransferResult result = TransferFunds(new Address(Message.Sender), 0, new TransferFundsToContract
+        {
+            ContractMethodName = "Asserter",
+            ContractTypeName = "CallContract"
+        });
+        SaveWorked = (bool)result.ReturnValue;
+        return (bool)result.ReturnValue;
+    }
+}
