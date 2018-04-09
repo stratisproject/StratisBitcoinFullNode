3738eeb4281c6f24fa71f8450dcb2352ab15c355
Neurosploit <hoekstraman@gmail.com>
2018-04-09 13:17:57 +0200

    Consensus project tests part 1 (#1252)
    * Coinview folder tests
    
    * Move rule tests to commonrules folder
    
    * Add tests for consensusrule(s)
    
    * ConsensusRulesExtension tests
    
    * AssumeValidRule tests
    
    * Solve code review comments

---
+++ src/Stratis.Bitcoin.Features.Consensus.Tests/CoinViews/FetchCoinsResponseTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NBitcoin;
+using Stratis.Bitcoin.Features.Consensus.CoinViews;
+using Stratis.Bitcoin.Utilities;
+using Xunit;
+
+namespace Stratis.Bitcoin.Features.Consensus.Tests.CoinViews
+{
+    public class FetchCoinsResponseTest
+    {
+        [Fact]
+        public void Constructor_InitializesClass()
+        {
+            var blockHash = new uint256(124);
+            var unspentOutputs = new UnspentOutputs[] {
+                new UnspentOutputs(1, new Transaction()),
+                new UnspentOutputs(2, new Transaction())
+            };
+
+            var fetchCoinsResponse = new FetchCoinsResponse(unspentOutputs, blockHash);
+
+            Assert.Equal(2, fetchCoinsResponse.UnspentOutputs.Length);
+            Assert.Equal((uint)1, fetchCoinsResponse.UnspentOutputs[0].Height);
+            Assert.Equal((uint)2, fetchCoinsResponse.UnspentOutputs[1].Height);
+            Assert.Equal(blockHash, fetchCoinsResponse.BlockHash);
+        }
+    }
+}
