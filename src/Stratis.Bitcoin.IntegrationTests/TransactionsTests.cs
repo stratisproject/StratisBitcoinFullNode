608d523fc4a2e8eee8ebc15e0589b1c7e5fd51ed
monsieurleberre <monsieur.leberre@gmail.com>
2018-04-11 13:35:09 +0100

    little progress

--- src/Stratis.Bitcoin.IntegrationTests/TransactionsTests.cs
+++ src/Stratis.Bitcoin.IntegrationTests/TransactionsTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NBitcoin;
+using Stratis.Bitcoin.Features.Wallet;
+using Stratis.Bitcoin.IntegrationTests.EnvironmentMockUpHelpers;
 using Stratis.Bitcoin.IntegrationTests.TestFramework;
 using Xunit;
 using Xunit.Abstractions;
@@ -9,41 +12,67 @@ namespace Stratis.Bitcoin.IntegrationTests
 {
     public class TransactionsTests : BddSpecification
     {
+        private NodeBuilder builder;
+        private CoreNode node;
+        private Wallet wallet;
+
         protected TransactionsTests(ITestOutputHelper output) : base(output)
         {
         }
         protected override void BeforeTest()
         {
-            // Start a fullnode
+            this.builder = NodeBuilder.Create();
+        }
+
+        protected override void AfterTest()
+        {
+            this.builder.Dispose();
         }
 
         [Fact]
         public void A_nulldata_transaction_is_sent_to_the_network()
         {
-            Given(a_node_running);
+            Given(a_proof_of_stake_node_running);
+            And(a_funded_wallet);
             And(a_nulldata_transaction);
             When(the_transaction_is_broadcasted);
+            And(the_block_is_mined);
             Then(no_error_should_happen);
             And(the_transaction_should_get_confirmed);
             And(the_transaction_should_appear_in_the_blockchain);
         }
 
         #region steps
-        private void a_node_running()
+        private void a_proof_of_stake_node_running()
         {
-            throw new NotImplementedException();
+            this.node = this.builder.CreateStratisPosNode();
+            this.node.Start();
+            this.node.NotInIBD();
+        }
+
+        private void a_funded_wallet()
+        {
+            this.node.FullNode.WalletManager().CreateWallet("123456", "mywallet");
+
+            HdAddress addr = this.node.FullNode.WalletManager().GetUnusedAddress(new WalletAccountReference("mywallet", "account 0"));
+            this.wallet = this.node.FullNode.WalletManager().GetWalletByName("mywallet");
+            //Key key = wallet.GetExtendedPrivateKeyForAddress("123456", addr).PrivateKey;
         }
 
         private void a_nulldata_transaction()
         {
-            throw new NotImplementedException();
+            wallet.
+            node.FullNode.WalletTransactionHandler().BuildTransaction()
         }
 
         private void the_transaction_is_broadcasted()
         {
             throw new NotImplementedException();
         }
-
+        private void the_block_is_mined()
+        {
+            throw new NotImplementedException();
+        }
         private void no_error_should_happen()
         {
             throw new NotImplementedException();
@@ -60,9 +89,5 @@ namespace Stratis.Bitcoin.IntegrationTests
         } 
         #endregion
 
-        protected override void AfterTest()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
