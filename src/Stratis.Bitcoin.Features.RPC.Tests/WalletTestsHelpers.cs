c1be412d7f4236a0c535fda32cb97bb660ed9695
majikandy <andy@tenkaisolutions.com>
2018-04-12 16:24:32 +0100

    removed reference between RPC and WalletTests

---
+++ src/Stratis.Bitcoin.Features.RPC.Tests/WalletTestsHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NBitcoin;
+
+namespace Stratis.Bitcoin.Features.RPC.Tests
+{
+    public class WalletTestsHelpers
+    {
+        public static ConcurrentChain GenerateChainWithHeight(int blockAmount, Network network)
+        {
+            var chain = new ConcurrentChain(network);
+            var nonce = RandomUtils.GetUInt32();
+            var prevBlockHash = chain.Genesis.HashBlock;
+            for (var i = 0; i < blockAmount; i++)
+            {
+                var block = new Block();
+                block.AddTransaction(new Transaction());
+                block.UpdateMerkleRoot();
+                block.Header.BlockTime = new DateTimeOffset(new DateTime(2017, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddDays(i));
+                block.Header.HashPrevBlock = prevBlockHash;
+                block.Header.Nonce = nonce;
+                chain.SetTip(block.Header);
+                prevBlockHash = block.GetHash();
+            }
+
+            return chain;
+        }
+
+    }
+}
