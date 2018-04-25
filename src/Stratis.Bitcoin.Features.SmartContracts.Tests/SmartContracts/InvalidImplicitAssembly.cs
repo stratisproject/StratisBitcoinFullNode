d54719d4dca7e3ab422dbb4d3b76d260e41828a3
Jandrews <jordandrews@hotmail.com>
2018-04-25 17:50:30 +1000

    Unit Test

---
+++ src/Stratis.Bitcoin.Features.SmartContracts.Tests/SmartContracts/InvalidImplicitAssembly.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using Stratis.SmartContracts;
+
+public class InvalidImplicitAssembly
+{
+    public InvalidImplicitAssembly(ISmartContractState state)
+    {
+    }
+
+    public void Test()
+    {
+        new string[] { }.ToList().Sort();
+    }
+}
