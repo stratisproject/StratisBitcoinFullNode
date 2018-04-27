9bb24cf2e48e25ef4daaf9126768b674291a2461
Jandrews <jordandrews@hotmail.com>
2018-04-27 12:34:30 +1000

    Hard Gas Limit

--- src/Stratis.Bitcoin.Features.SmartContracts/Consensus/Rules/GasBudgetRule.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/Consensus/Rules/GasBudgetRule.cs
@@ -14,6 +14,8 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Consensus.Rules
     [ValidationRule(CanSkipValidation = false)]
     public class GasBudgetRule : ConsensusRule, ISmartContractMempoolRule
     {
+        public const ulong HardGasLimit = 1_000_000;
+
         public override Task RunAsync(RuleContext context)
         {
             Block block = context.BlockValidationContext.Block;
@@ -40,14 +42,27 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Consensus.Rules
 
             var carrier = SmartContractCarrier.Deserialize(transaction, smartContractOutput);
 
+            if (carrier.GasLimit > HardGasLimit)
+            {
+                // Supplied gas limit is too high - at a certain point we deem that a contract is taking up too much time. 
+                this.ThrowGasGreaterThanHardLimit();
+            }
+
+
             if (suppliedBudget < new Money(carrier.GasCostBudget))
             {
                 // Supplied satoshis are less than the budget we said we had for the contract execution
-                this.Throw();
+                this.ThrowGasGreaterThanFee();
             }
         }
 
-        private void Throw()
+        private void ThrowGasGreaterThanHardLimit()
+        {
+            // TODO make nicer
+            new ConsensusError("total-gas-value-greater-than-hard-limit", "total supplied gas value was greater than our hard limit of " + HardGasLimit).Throw();
+        }
+
+        private void ThrowGasGreaterThanFee()
         {
             // TODO make nicer
             new ConsensusError("total-gas-value-greater-than-total-fee", "total supplied gas value was greater than total supplied fee value").Throw();
--- src/Stratis.Bitcoin.IntegrationTests/SmartContractMemoryPoolTests.cs
+++ src/Stratis.Bitcoin.IntegrationTests/SmartContractMemoryPoolTests.cs
@@ -2,5 +2,33 @@
 {
     public class SmartContractMemoryPoolTests
     {
+        [Fact]
+        public void AddToMempool()
+        {
+            using (NodeBuilder builder = NodeBuilder.Create())
+            {
+                var stratisNodeSync = builder.CreateStratisPowNode();
+                builder.StartAll();
+
+                stratisNodeSync.SetDummyMinerSecret(new BitcoinSecret(new Key(), stratisNodeSync.FullNode.Network));
+                stratisNodeSync.GenerateStratis(105); // coinbase maturity = 100
+                TestHelper.WaitLoop(() => stratisNodeSync.FullNode.ConsensusLoop().Tip.HashBlock == stratisNodeSync.FullNode.Chain.Tip.HashBlock);
+                TestHelper.WaitLoop(() => stratisNodeSync.FullNode.HighestPersistedBlock().HashBlock == stratisNodeSync.FullNode.Chain.Tip.HashBlock);
+
+                var block = stratisNodeSync.FullNode.BlockStoreManager().BlockRepository.GetAsync(stratisNodeSync.FullNode.Chain.GetBlock(4).HashBlock).Result;
+                var prevTrx = block.Transactions.First();
+                var dest = new BitcoinSecret(new Key(), stratisNodeSync.FullNode.Network);
+
+                Transaction tx = new Transaction();
+                tx.AddInput(new TxIn(new OutPoint(prevTrx.GetHash(), 0), PayToPubkeyHashTemplate.Instance.GenerateScriptPubKey(stratisNodeSync.MinerSecret.PubKey)));
+                tx.AddOutput(new TxOut("25", dest.PubKey.Hash));
+                tx.AddOutput(new TxOut("24", new Key().PubKey.Hash)); // 1 btc fee
+                tx.Sign(stratisNodeSync.MinerSecret, false);
+
+                stratisNodeSync.Broadcast(tx);
+
+                TestHelper.WaitLoop(() => stratisNodeSync.CreateRPCClient().GetRawMempool().Length == 1);
+            }
+        }
     }
 }
