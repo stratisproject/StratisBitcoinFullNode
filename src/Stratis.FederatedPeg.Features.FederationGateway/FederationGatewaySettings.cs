28995c9a170f9eb50239b3212d9688e6ad161a95
Carlton Pringle <carlton.pringle@stratisplatform.com>
2018-04-29 14:49:10 +0000

    Added FederationGatewaySettings class Imported from Changeset 136

---
+++ src/Stratis.FederatedPeg.Features.FederationGateway/FederationGatewaySettings.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Utilities;
+
+namespace Stratis.FederatedPeg.Features.FederationGateway
+{
+    /// <summary>
+    /// Configuration settings used to initialize a FederationGateway.
+    /// </summary>
+    public sealed class FederationGatewaySettings
+    {
+        /// <summary>
+        /// The MemberName is used to distiguish between federation gateways in the debug logs.
+        /// </summary>
+        public string MemberName { get; set; }
+
+        /// <summary>
+        /// A string representation of the PublicKey used for determining turns in the round robin.
+        /// </summary>
+        public string PublicKey { get; set; }
+
+        /// <summary>
+        /// Path to the public keys of the federation members.
+        /// </summary>
+        public string FederationFolder { get; set; }
+
+        /// <summary>
+        /// Path to the folder containing the private key this node uses.  Used for signing multi-sig transactions.
+        /// </summary>
+        public string MemberPrivateFolder { get; set; }
+
+        /// <summary>
+        /// The API port of the counterchain.  <example>The federation members are required to run full nodes for both the
+        /// sidechain and the mainchain.  If this is the mainchain then the CounterChainApiPort is the api port of the sidechain node.</example>
+        /// </summary>
+        public int CounterChainApiPort { get; set; }
+
+        /// <summary>
+        /// A callback allow changing the settings after they have been read from the nodeSettings.
+        /// </summary>
+        private readonly Action<FederationGatewaySettings> callback;
+
+        /// <summary>
+        /// Initialize the settings with an optional callback which can be used to change the settings after they have been read from the config.
+        /// </summary>
+        /// <param name="callback">This optional callback is called after the settings are read from the config allowing the settings to be changed in code.</param>
+        public FederationGatewaySettings(Action<FederationGatewaySettings> callback = null)
+        {
+            this.callback = callback;
+        }
+
+        /// <summary>
+        /// Loads our feature specific settings from the nodeSettings' ConfigReader. 
+        /// </summary>
+        /// <param name="nodeSettings"></param>
+        public void Load(NodeSettings nodeSettings)
+        {
+            Guard.NotNull(nodeSettings, nameof(nodeSettings));
+
+            TextFileConfiguration config = nodeSettings.ConfigReader;
+            this.MemberName = config.GetOrDefault("membername", "unspecified");
+            this.PublicKey = config.GetOrDefault<string>("publickey", null);
+            this.FederationFolder = config.GetOrDefault<string>("federationfolder", null);
+            this.MemberPrivateFolder = config.GetOrDefault<string>("memberprivatefolder", null);
+            this.CounterChainApiPort = config.GetOrDefault("counterchainapiport", 0);
+            this.callback?.Invoke(this);
+        }
+    }
+}
