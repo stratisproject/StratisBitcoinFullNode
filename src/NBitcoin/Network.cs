cff3a378774ce3d119ddc3709b085bcf9953de2e
Neurosploit <hoekstraman@gmail.com>
2018-04-30 17:21:33 +0200

    Move checkpointinfo class to a seperate file and remove setters.

--- src/NBitcoin/Network.cs
+++ src/NBitcoin/Network.cs
@@ -83,31 +83,7 @@ namespace NBitcoin
         /// Strict DER signature
         /// </summary>
         BIP66
-    }
-
-    /// <summary>
-    /// Description of checkpointed block.
-    /// </summary>
-    public class CheckpointInfo
-    {
-        /// <summary>Hash of the checkpointed block header.</summary>
-        public uint256 Hash { get; set; }
-
-        /// <summary>Stake modifier V2 value of the checkpointed block.</summary>
-        /// <remarks>Stratis only.</remarks>
-        public uint256 StakeModifierV2 { get; set; }
-
-        /// <summary>
-        /// Initializes a new instance of the object.
-        /// </summary>
-        /// <param name="hash">Hash of the checkpointed block header.</param>
-        /// <param name="stakeModifierV2">Stake modifier V2 value of the checkpointed block. Stratis network only.</param>
-        public CheckpointInfo(uint256 hash, uint256 stakeModifierV2 = null)
-        {
-            this.Hash = hash;
-            this.StakeModifierV2 = stakeModifierV2;
-        }
-    }
+    }    
 
     public class Consensus
     {
