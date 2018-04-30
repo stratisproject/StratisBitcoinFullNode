a8fb52de8f80beed77e50fe1695e9182231c93f1
Jandrews <jordandrews@hotmail.com>
2018-04-30 12:49:06 +1000

    Mempool Op Spend self review

--- src/Stratis.Bitcoin.Features.SmartContracts/Consensus/Rules/MempoolOpSpendRule.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/Consensus/Rules/MempoolOpSpendRule.cs
@@ -1,23 +1,31 @@
-﻿using Stratis.Bitcoin.Features.Consensus;
+﻿using System.Linq;
+using NBitcoin;
+using Stratis.Bitcoin.Features.Consensus;
 using Stratis.Bitcoin.Features.MemoryPool;
 
 namespace Stratis.Bitcoin.Features.SmartContracts.Consensus.Rules
 {
     /// <summary>
-    /// Checks that transactsions sent to the mempool don't include the OP_SPEND opcode.
+    /// Checks that transactions sent to the mempool don't include the OP_SPEND opcode.
     /// </summary>
     public class MempoolOpSpendRule : ISmartContractMempoolRule
     {
         public void CheckTransaction(MempoolValidationContext context)
         {
-            if (context.Transaction.IsSmartContractSpendTransaction())
+            if (context.Transaction.Inputs.Any(x=>ContainsOpSpend(x.ScriptSig))
+                || context.Transaction.Outputs.Any(x=>ContainsOpSpend(x.ScriptPubKey)))
                 Throw();
+        }
+
+        private static bool ContainsOpSpend(Script script)
+        {
+            return script.ToOps().Any(x => x.Code == OpcodeType.OP_SPEND);
         }
 
         private void Throw()
         {
             // TODO make nicer
-            new ConsensusError("opspend-in-mempool", "opspend shouldn't be in mempool").Throw();
+            new ConsensusError("opspend-in-mempool", "opspend shouldn't be in transactions created by users").Throw();
         }
     }
 }
