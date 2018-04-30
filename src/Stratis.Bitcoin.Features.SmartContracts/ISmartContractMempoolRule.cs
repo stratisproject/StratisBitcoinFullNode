4ebd4ba3ebe681a288839215fba6a90000c9dd47
Jandrews <jordandrews@hotmail.com>
2018-04-30 12:57:20 +1000

    Docs - self review

--- src/Stratis.Bitcoin.Features.SmartContracts/ISmartContractMempoolRule.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/ISmartContractMempoolRule.cs
@@ -2,8 +2,15 @@
 
 namespace Stratis.Bitcoin.Features.SmartContracts
 {
+    /// <summary>
+    /// Smart-contract related rule to be checked before transactions are added to the mempool.
+    /// </summary>
     public interface ISmartContractMempoolRule
     {
+        /// <summary>
+        /// Check that the transaction meets certain criteria before being added to mempool. If it doesn't, a ConsensusErrorException
+        /// will be thrown.
+        /// </summary>
         void CheckTransaction(MempoolValidationContext context);
     }
 }
