f8ca925b8585bd10bea0eaac9b2a01264fe8f251
Francois de la Rouviere <fassadlr@gmail.com>
2018-05-01 16:07:18 +0100

    Fix null check (#1367)

--- src/Stratis.Bitcoin.Features.Miner/PowMining.cs
+++ src/Stratis.Bitcoin.Features.Miner/PowMining.cs
@@ -155,7 +155,7 @@ namespace Stratis.Bitcoin.Features.Miner
         public void StopMining()
         {
             this.miningCancellationTokenSource.Cancel();
-            this.miningLoop.Dispose();
+            this.miningLoop?.Dispose();
             this.miningLoop = null;
             this.miningCancellationTokenSource.Dispose();
             this.miningCancellationTokenSource = null;
