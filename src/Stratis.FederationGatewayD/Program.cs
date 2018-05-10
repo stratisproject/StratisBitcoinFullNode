dc7b3f45364358ff6b6a11e58828ca0396dbd874
Carlton Pringle <carlton.pringle@stratisplatform.com>
2018-05-10 06:00:03 +0000

    Added missing projects for new solution file Imported from Changeset 149

---
+++ src/Stratis.FederationGatewayD/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using NBitcoin;
+using NBitcoin.Protocol;
+using Stratis.Bitcoin.Builder;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Features.Api;
+using Stratis.Bitcoin.Features.BlockStore;
+using Stratis.Bitcoin.Features.Consensus;
+using Stratis.Bitcoin.Features.MemoryPool;
+using Stratis.Bitcoin.Features.Miner;
+using Stratis.Bitcoin.Features.Notifications;
+using Stratis.Bitcoin.Features.RPC;
+using Stratis.Bitcoin.Features.Wallet;
+using Stratis.Bitcoin.Utilities;
+using Stratis.FederatedPeg.Features.FederationGateway;
+
+namespace Stratis.FederationGatewayD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunFederationGatewayAsync(args).Wait();
+        }
+
+        public static async Task RunFederationGatewayAsync(string[] args)
+        {
+            try
+            {
+                Network network = Network.StratisRegTest;
+                NodeSettings nodeSettings = new NodeSettings(network, ProtocolVersion.ALT_PROTOCOL_VERSION, args: args, loadConfiguration: false);
+
+
+                // NOTES: running BTC and STRAT side by side is not possible yet as the flags for serialization are static
+                var node = new FullNodeBuilder()
+                    .UseNodeSettings(nodeSettings)
+                    .UsePosConsensus()
+                    .UseBlockStore()
+                    .UseMempool()
+                    .UseWallet()
+                    .UseTransactionNotification()
+                    .UseBlockNotification()
+                    .AddPowPosMining()
+                    .AddFederationGateway()
+                    .UseApi()
+                    .AddRPC()
+                    .Build();
+
+                if (node != null)
+                    await node.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex.Message);
+            }
+        }
+    }
+}
