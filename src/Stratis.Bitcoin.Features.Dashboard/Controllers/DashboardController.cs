e4d3bb60b01cc2b33ace8361e529479cb7922486
StratisDF <StratisDevelopmentFoundation@gmail.com>
2018-05-11 22:13:02 -0500

    Simple Dashboard Feature
    * Added Dashboard Feature, Controller, Simple endpoint
    * Added Stratis network with HTML UI

---
+++ src/Stratis.Bitcoin.Features.Dashboard/Controllers/DashboardController.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NBitcoin;
+
+namespace Stratis.Bitcoin.Features.Dashboard.Controllers
+{
+    /// <summary>
+    /// Controller providing HTML Dashboard
+    /// </summary>
+    [Route("dashboards")] //declares the endpoint
+    public class DashboardController : Controller
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DashboardController"/> class.
+        /// </summary>
+        public DashboardController()
+        {
+        }
+
+        /// <summary>
+        /// Returns a web page to act as a dashboard
+        /// </summary>
+        /// <returns>text/html content</returns>
+        [HttpGet]
+        [Route("nodedashboard")] // the endpoint name
+		public IActionResult NodeDashboard()
+        {
+            return Content("Hello World", "text/html");
+        }
+
+
+    }
+}
