185f2e64465f385245d36d8d0ea5bcb868c732a5
Carlton Pringle <carlton.pringle@stratisplatform.com>
2018-05-14 04:45:38 +0000

    Moved Payload into feature Imported from Changeset 154

---
+++ src/Stratis.FederatedPeg.Features.FederationGateway/Payload/RequestPartialTransactionPayload.cs
@@ -0,0 +1,47 @@
+﻿using NBitcoin;
+using Stratis.Bitcoin.P2P.Protocol.Payloads;
+
+namespace Stratis.FederatedPeg.Features.FederationGateway
+{
+    [Payload("partial")]
+    public class RequestPartialTransactionPayload : Payload
+    {
+        private uint256 sessionId = uint256.Zero;
+        private Transaction template;
+
+        private Transaction transactionPartial = null;
+        private uint256 bossCard = uint256.Zero;
+
+        public Transaction PartialTransaction => this.transactionPartial;
+        public Transaction TemplateTransaction => this.template;
+
+        public uint256 SessionId => this.sessionId;
+
+        public uint256 BossCard => this.bossCard;
+
+        public RequestPartialTransactionPayload()
+        {
+
+        }
+
+        public RequestPartialTransactionPayload(uint256 sessionId, Transaction template)
+        {
+            this.sessionId = sessionId;
+            this.template = template;
+        }
+
+        public void AddPartial(Transaction partialTransaction, uint256 bossCard)
+        {
+            this.transactionPartial = partialTransaction;
+            this.bossCard = bossCard;
+        }
+
+        public override void ReadWriteCore(BitcoinStream stream)
+        {
+            stream.ReadWrite(ref this.sessionId);
+            stream.ReadWrite(ref this.bossCard);
+            stream.ReadWrite(ref this.transactionPartial);
+            stream.ReadWrite(ref this.template);
+        }
+    }
+}
