a57e364712091c2f55a9f507bdeea7d4cccc694d
Dan Gould <dcsled@gmail.com>
2018-05-16 14:56:45 +0100

    detail provider comments

--- src/Stratis.Bitcoin/Interfaces/IStoreStateProvider.cs
+++ src/Stratis.Bitcoin/Interfaces/IStoreStateProvider.cs
@@ -5,6 +5,7 @@ using NBitcoin;
 
 namespace Stratis.Bitcoin.Interfaces
 {
+    /// <summary>Shares status details from persistant storage.</summary>
     public interface IStoreStateProvider
     {
         /// <summary>Represents the last block stored to disk.</summary>
