714dad2d7b1a6f251c7d7a9c02203cc261f91536
Carlton Pringle <carlton.pringle@stratisplatform.com>
2018-05-21 06:34:40 +0000

    refactor of sessions Imported from Changeset 176

--- src/Stratis.FederatedPeg.Features.FederationGateway/CounterChain/PartialTransactionSession.cs
+++ src/Stratis.FederatedPeg.Features.FederationGateway/CounterChain/PartialTransactionSession.cs
@@ -7,7 +7,7 @@ public class PartialTransactionSession
         private Transaction[] partialTransactions;
         public uint256 SessionId { get; }
 
-        public uint256 CompletedTransactionId { get; set; } = null;
+        public uint256 CompletedTransactionId { get; set; } = uint256.Zero;
 
         public Money Amount { get; }
         public string Destination { get; }
