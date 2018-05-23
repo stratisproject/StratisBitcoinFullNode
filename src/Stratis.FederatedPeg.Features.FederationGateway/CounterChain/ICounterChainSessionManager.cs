d86c7f35f7508c1597474a24e9d86c087bff6cbd
Carlton Pringle <carlton.pringle@stratisplatform.com>
2018-05-23 06:57:08 +0000

    Refactor VerifySession method in CounterChainSessionManager. Imported from Changeset 189

--- src/Stratis.FederatedPeg.Features.FederationGateway/CounterChain/ICounterChainSessionManager.cs
+++ src/Stratis.FederatedPeg.Features.FederationGateway/CounterChain/ICounterChainSessionManager.cs
@@ -14,6 +14,13 @@ namespace Stratis.FederatedPeg.Features.FederationGateway.CounterChain
 
         void ReceivePartial(uint256 sessionId, Transaction partialTransaction, uint256 bossCard);
 
+        /// <summary>
+        /// VerifySession ensures it is safe to sign any inbound partial transaction by performing a
+        /// number of checks against the session data. 
+        /// </summary>
+        /// <param name="sessionId">An id that identifies the session.</param>
+        /// <param name="partialTransactionTemplate">The partial transaction we are asked to sign.</param>
+        /// <returns></returns>
         CounterChainSession VerifySession(uint256 sessionId, Transaction partialTransactionTemplate);
 
         void MarkSessionAsSigned(CounterChainSession session);
