2d23e098965c13552a23380823363f9c32d51fa1
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-05-28 15:00:04 +1000

    Make SmartContractValidator implement ISmartContractValidator

--- src/Stratis.SmartContracts.Core/ContractValidation/SmartContractValidator.cs
+++ src/Stratis.SmartContracts.Core/ContractValidation/SmartContractValidator.cs
@@ -4,7 +4,7 @@ using Stratis.Validators.Net;
 
 namespace Stratis.SmartContracts.Core.ContractValidation
 {
-    public sealed class SmartContractValidator
+    public sealed class SmartContractValidator : ISmartContractValidator
     {
         private readonly IList<ISmartContractValidator> validators;
 
@@ -13,7 +13,7 @@ namespace Stratis.SmartContracts.Core.ContractValidation
             this.validators = validators;
         }
 
-        public ValidationResult ValidateContract(SmartContractDecompilation decompilation)
+        public ValidationResult Validate(SmartContractDecompilation decompilation)
         {
             var endResult = new ValidationResult();
             foreach (ISmartContractValidator validator in this.validators)
--- src/Stratis.SmartContracts.Core/SmartContractExecutor.cs
+++ src/Stratis.SmartContracts.Core/SmartContractExecutor.cs
@@ -160,7 +160,7 @@ namespace Stratis.SmartContracts.Core
 
             // Decompile the contract execution code and validate it.
             SmartContractDecompilation decompilation = SmartContractDecompiler.GetModuleDefinition(this.carrier.ContractExecutionCode);
-            ValidationResult validation = this.validator.ValidateContract(decompilation);
+            ValidationResult validation = this.validator.Validate(decompilation);
 
             // If validation failed, refund the sender any remaining gas.
             if (!validation.IsValid)
