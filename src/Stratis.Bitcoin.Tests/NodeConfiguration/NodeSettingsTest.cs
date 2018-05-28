ac3f2a5b0603c51f23ce81add013f41c21a93603
quantumagi <someguy.fromafrica@gmail.com>
2018-05-28 16:52:38 +1000

    Add Nodesettings test cases

--- src/Stratis.Bitcoin.Tests/NodeConfiguration/NodeSettingsTest.cs
+++ src/Stratis.Bitcoin.Tests/NodeConfiguration/NodeSettingsTest.cs
@@ -1,11 +1,152 @@
-﻿using System;
-using System.Net;
-using Stratis.Bitcoin.Utilities.Extensions;
+﻿using System.IO;
+using NBitcoin;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Tests.Common;
 using Xunit;
 
 namespace Stratis.Bitcoin.Tests.NodeConfiguration
 {
-    public class NodeSettingsTest
-    {       
+    public class NodeSettingsTest : TestBase
+    {
+        public NodeSettingsTest():base(Network.Main)
+        {
+        }
+
+        /// <summary>
+        /// Assert that a setting can be read from the command line.
+        /// </summary>
+        [Fact]
+        public void NodeSettings_CanReadSingleValueFromCmdLine()
+        {
+            // Arrange
+            var nodeSettings = new NodeSettings(args:new[] { "-agentprefix=abc" });
+            // Act
+            string result = nodeSettings.AgentPrefix;
+            // Assert
+            Assert.Equal("abc", result);
+        }
+
+        /// <summary>
+        /// Assert that a setting can be read from the configuration file.
+        /// </summary>
+        [Fact]
+        public void NodeSettings_CanReadSingleValueFromFile()
+        {
+            // Arrange
+            string dataDir = TestBase.CreateDataFolder(this).RootPath;
+            var configFile = Path.Combine(dataDir, "config.txt");
+            File.WriteAllText(configFile, "agentprefix=def");
+            var nodeSettings = new NodeSettings(args: new[] { $"-datadir={dataDir}", $"-conf=config.txt" });
+            // Act
+            string result = nodeSettings.AgentPrefix;
+            // Assert
+            Assert.Equal("def", result);
+        }
+
+        /// <summary>
+        /// If both the commandline and the configuration file supplies a setting then the command line value is taken.
+        /// </summary>
+        [Fact]
+        public void NodeSettings_CanReadSingleValueFromCmdLineAndFile()
+        {
+            // Arrange
+            string dataDir = TestBase.CreateDataFolder(this).RootPath;
+            var configFile = Path.Combine(dataDir, "config.txt");
+            File.WriteAllText(configFile, "agentprefix=def");
+            var nodeSettings = new NodeSettings(args: new[] { $"-datadir={dataDir}", $"-conf=config.txt", "-agentprefix=abc" });
+            // Act
+            string result = nodeSettings.AgentPrefix;
+            // Assert
+            Assert.Equal("abc", result);
+        }
+
+        /// <summary>
+        /// If neither the commandline nor the configuration file supplies a setting then the default value is used.
+        /// </summary>
+        [Fact]
+        public void NodeSettings_CanReadSingleValueFromNone()
+        {
+            // Arrange
+            string dataDir = TestBase.CreateDataFolder(this).RootPath;
+            var configFile = Path.Combine(dataDir, "config.txt");
+            File.WriteAllText(configFile, "");
+            var nodeSettings = new NodeSettings(args: new[] { $"-datadir={dataDir}", $"-conf=config.txt" });
+            // Act
+            string result = nodeSettings.AgentPrefix;
+            // Assert
+            Assert.Equal("", result);
+        }
+
+
+        /// <summary>
+        /// Assert that a multi-value setting can be read from the command line.
+        /// </summary>
+        [Fact]
+        public void NodeSettings_CanReadMultiValueFromCmdLine()
+        {
+            // Arrange
+            var nodeSettings = new NodeSettings(args: new[] { "-addnode=0.0.0.0", "-addnode=0.0.0.1" });
+            // Act
+            var result = nodeSettings.ConfigReader.GetAll("addnode");
+            // Assert
+            Assert.Equal(2, result.Length);
+            Assert.Equal("0.0.0.0", result[0]);
+            Assert.Equal("0.0.0.1", result[1]);
+        }
+
+        /// <summary>
+        /// Assert that a multi-value setting can be read from the configuration file.
+        /// </summary>
+        [Fact]
+        public void NodeSettings_CanReadMultiValueFromFile()
+        {
+            // Arrange
+            string dataDir = TestBase.CreateDataFolder(this).RootPath;
+            var configFile = Path.Combine(dataDir, "config.txt");
+            File.WriteAllText(configFile, "addnode=0.0.0.0\r\naddnode=0.0.0.1");
+            var nodeSettings = new NodeSettings(args: new[] { $"-datadir={dataDir}", $"-conf=config.txt" });
+            // Act
+            var result = nodeSettings.ConfigReader.GetAll("addnode");
+            // Assert
+            Assert.Equal(2, result.Length);
+            Assert.Equal("0.0.0.0", result[0]);
+            Assert.Equal("0.0.0.1", result[1]);
+        }
+
+        /// <summary>
+        /// If both the commandline and the configuration file supplies a setting then both are combined.
+        /// </summary>
+        [Fact]
+        public void NodeSettings_CanReadMultiValueFromCmdLineAndFile()
+        {
+            // Arrange
+            string dataDir = TestBase.CreateDataFolder(this).RootPath;
+            var configFile = Path.Combine(dataDir, "config.txt");
+            File.WriteAllText(configFile, "addnode=0.0.0.0");
+            var nodeSettings = new NodeSettings(args: new[] { $"-datadir={dataDir}", $"-conf=config.txt", "-addnode=0.0.0.1" });
+            // Act
+            var result = nodeSettings.ConfigReader.GetAll("addnode");
+            // Assert
+            Assert.Equal(2, result.Length);
+            Assert.Equal("0.0.0.1", result[0]); // Command-line first
+            Assert.Equal("0.0.0.0", result[1]);
+        }
+
+        /// <summary>
+        /// If neither the commandline nor the configuration file supplies a multi-value setting then no values are returned.
+        /// </summary>
+        [Fact]
+        public void NodeSettings_CanReadSingleMultiFromNone()
+        {
+            // Arrange
+            string dataDir = TestBase.CreateDataFolder(this).RootPath;
+            var configFile = Path.Combine(dataDir, "config.txt");
+            File.WriteAllText(configFile, "");
+            var nodeSettings = new NodeSettings(args: new[] { $"-datadir={dataDir}", $"-conf=config.txt" });
+            // Act
+            var result = nodeSettings.ConfigReader.GetAll("addnode");
+            // Assert
+            Assert.Empty(result);
+        }
     }
 }
