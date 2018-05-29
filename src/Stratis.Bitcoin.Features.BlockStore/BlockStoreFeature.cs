430aa005d6f1ef470a5a7b6211c66c4d02c2638c
noescape0 <noescape0@ya.ru>
2018-05-30 01:50:35 +0300

    Fixed merge conflicts

--- src/Stratis.Bitcoin.Features.BlockStore/BlockStoreFeature.cs
+++ src/Stratis.Bitcoin.Features.BlockStore/BlockStoreFeature.cs
@@ -120,12 +120,6 @@ namespace Stratis.Bitcoin.Features.BlockStore
             return this.blockRepository.GetTrxBlockIdAsync(trxid);
         }
 
-        /// <inheritdoc/>
-        public ChainedHeader GetHighestPersistedBlock()
-        {
-            return this.blockRepository.HighestPersistedBlock;
-        }
-
         public override void Initialize()
         {
             this.logger.LogTrace("()");
