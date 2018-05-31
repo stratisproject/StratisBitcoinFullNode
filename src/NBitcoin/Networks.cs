42d18713041ed77fa3deca1e6bb42521b7f66443
bokobza <bokobza@gmail.com>
2018-05-31 17:49:53 +0100

    Consolidated the CreateGenesis methods.

--- src/NBitcoin/Networks.cs
+++ src/NBitcoin/Networks.cs
@@ -60,11 +60,7 @@ namespace NBitcoin
         {
             string pszTimestamp = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks";
             Script genesisOutputScript = new Script(Op.GetPushOp(Encoders.Hex.DecodeData("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f")), OpcodeType.OP_CHECKSIG);
-            return CreateGenesisBlock(consensusFactory, pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
-        }
 
-        private static Block CreateGenesisBlock(ConsensusFactory consensusFactory, string pszTimestamp, Script genesisOutputScript, uint nTime, uint nNonce, uint nBits, int nVersion, Money genesisReward)
-        {
             Transaction txNew = consensusFactory.CreateTransaction();
             txNew.Version = 1;
             txNew.AddInput(new TxIn()
@@ -94,11 +90,7 @@ namespace NBitcoin
         protected static Block CreateStratisGenesisBlock(ConsensusFactory consensusFactory, uint nTime, uint nNonce, uint nBits, int nVersion, Money genesisReward)
         {
             string pszTimestamp = "http://www.theonion.com/article/olympics-head-priestess-slits-throat-official-rio--53466";
-            return CreateStratisGenesisBlock(consensusFactory, pszTimestamp, nTime, nNonce, nBits, nVersion, genesisReward);
-        }
 
-        private static Block CreateStratisGenesisBlock(ConsensusFactory consensusFactory, string pszTimestamp, uint nTime, uint nNonce, uint nBits, int nVersion, Money genesisReward)
-        {
             Transaction txNew = consensusFactory.CreateTransaction();
             txNew.Version = 1;
             txNew.Time = nTime;
--- src/NBitcoin/Networks/BitcoinMain.cs
+++ src/NBitcoin/Networks/BitcoinMain.cs
@@ -116,5 +116,4 @@ namespace NBitcoin.Networks
             Assert(this.Genesis.Header.HashMerkleRoot == uint256.Parse("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
         }
     }
-
 }
