5b05d9a22264d46a91add1fe40cee913523129ec
Dan Gould <dcsled@gmail.com>
2018-05-31 15:00:09 +0100

    api/general-info log gets full stacktrace

--- src/Stratis.Bitcoin.Features.Wallet/Controllers/WalletController.cs
+++ src/Stratis.Bitcoin.Features.Wallet/Controllers/WalletController.cs
@@ -295,8 +295,8 @@ namespace Stratis.Bitcoin.Features.Wallet.Controllers
             }
             catch (Exception e)
             {
-                this.logger.LogError("Exception occurred: {0}", e.ToString());
-                return ErrorHelpers.BuildErrorResponse(HttpStatusCode.BadRequest, e.Message, e.ToString());
+                this.logger.LogError(e, "Exception occurred: {0}", e.StackTrace);
+                return ErrorHelpers.BuildErrorResponse(HttpStatusCode.BadRequest, e.Message(), e.StackTrace);
             }
         }
 
