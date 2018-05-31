090dc1cf021994e6ef251c4120e3e5284d5a6c6c
Carlton Pringle <carlton.pringle@stratisplatform.com>
2018-05-31 04:14:07 +0000

    Added comments to the CounterChainSessionManager. Imported from Changeset 196

--- src/Stratis.FederatedPeg.Features.FederationGateway/CounterChain/ICounterChainSessionManager.cs
+++ src/Stratis.FederatedPeg.Features.FederationGateway/CounterChain/ICounterChainSessionManager.cs
@@ -6,12 +6,38 @@ using NBitcoin;
 
 namespace Stratis.FederatedPeg.Features.FederationGateway.CounterChain
 {
+    /// <summary>
+    /// The CounterChainSessionManager receives session data from the MonitorChainSessionManager and takes a number of precautionary steps
+    /// before signing the transaction. It also builds and broadcasts the transaction once a quorum of is received from the payload
+    /// </summary>
     public interface ICounterChainSessionManager
     {
+        /// <summary>
+        /// Called from our sister MonitorChainSessionManager.  The monitor is reading our trusted chain and
+        /// this registers the session data that tells us the trusted data we must use to verify we are signing
+        /// a true transaction that has not been corrupted by a rouge or alien actor.
+        /// </summary>
+        /// <param name="sessionId">The transaction hash of the source transaction from the monior chain is used as the sessionId.</param>
+        /// <param name="amount">The amount of the transaction.</param>
+        /// <param name="destinationAddress">The final destination address on this counterchain.</param>
         void CreateSessionOnCounterChain(uint256 transactionId, Money amount, string detinationAddress);
 
+        /// <summary>
+        /// Do the work to process the transaction. In this method we start the process of requesting peer gateways to sign our transaction
+        /// by building the partial transaction template and broadcasting it to peers.
+        /// </summary>
+        /// <param name="transactionId">The transactionId that forms the session Id.</param>
+        /// <param name="amount">The amount of the transaction.</param>
+        /// <param name="destinationAddress">The final destination address.</param>
+        /// <returns></returns>
         Task<uint256> ProcessCounterChainSession(uint256 transactionId, Money amount, string destinationAddress);
 
+        /// <summary>
+        /// Receives a partial transaction inbound from the payload behavior.
+        /// </summary>
+        /// <param name="sessionId">The session we are receiving.</param>
+        /// <param name="partialTransaction">Inbound partial transaction.</param>
+        /// <param name="bossCard">The insertion place in the partial transaction table we store in the session.</param>
         void ReceivePartial(uint256 sessionId, Transaction partialTransaction, uint256 bossCard);
 
         /// <summary>
@@ -23,6 +49,10 @@ namespace Stratis.FederatedPeg.Features.FederationGateway.CounterChain
         /// <returns></returns>
         CounterChainSession VerifySession(uint256 sessionId, Transaction partialTransactionTemplate);
 
+        /// <summary>
+        /// Record that we have signed the session.
+        /// </summary>
+        /// <param name="session">The session.</param>
         void MarkSessionAsSigned(CounterChainSession session);
     }
 }
