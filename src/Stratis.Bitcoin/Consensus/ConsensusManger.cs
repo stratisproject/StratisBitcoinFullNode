95cf9d0814786c919c9e3e82f1e32ab4028b2d1c
Dan Gershony <dan.gershony@gmail.com>
2018-06-06 17:54:47 +0100

    Create ConsensusManager.InitializeAsync() incomplete

---
+++ src/Stratis.Bitcoin/Consensus/ConsensusManger.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NBitcoin;
+using Stratis.Bitcoin.Base;
+using Stratis.Bitcoin.BlockPulling;
+using Stratis.Bitcoin.Configuration.Settings;
+using Stratis.Bitcoin.Utilities;
+
+namespace Stratis.Bitcoin.Consensus
+{
+    public class ConsensusManager
+    {
+        private readonly Network network;
+        private readonly ILogger logger;
+        private readonly IChainedHeaderTree chainedHeaderTree;
+        private readonly IChainState chainState;
+        private readonly ConsensusSettings consensusSettings;
+        private readonly ConcurrentChain concurrentChain;
+        private readonly ILookaheadBlockPuller lookaheadBlockPuller;
+
+        /// <summary>The current tip of the chain that has been validated.</summary>
+        public ChainedHeader Tip { get; private set; }
+
+        private Dictionary<uint256, List<Action>> blocksRequested;
+
+        private AsyncQueue<(ChainedHeader headerFrom, ChainedHeader headerTo)> toDownloadQueue;
+
+        public ConsensusManager(Network network, ILoggerFactory loggerFactory, IChainedHeaderTree chainedHeaderTree, IChainState chainState, ConsensusSettings consensusSettings, ConcurrentChain concurrentChain, ILookaheadBlockPuller lookaheadBlockPuller)
+        {
+            this.network = network;
+            this.chainedHeaderTree = chainedHeaderTree;
+            this.chainState = chainState;
+            this.consensusSettings = consensusSettings;
+            this.concurrentChain = concurrentChain;
+            this.lookaheadBlockPuller = lookaheadBlockPuller;
+            this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
+        }
+
+        public async Task InitializeAsync()
+        {
+            this.logger.LogTrace("()");
+
+            uint256 utxoHash = await this.UTXOSet.GetBlockHashAsync().ConfigureAwait(false);
+            bool blockStoreDisabled = this.chainState.BlockStoreTip == null;
+
+            while (true)
+            {
+                this.Tip = this.concurrentChain.GetBlock(utxoHash);
+
+                if ((this.Tip != null) && (blockStoreDisabled || (this.chainState.BlockStoreTip.Height >= this.Tip.Height)))
+                    break;
+
+                // In case block store initialized behind, rewind until or before the block store tip.
+                // The node will complete loading before connecting to peers so the chain will never know if a reorg happened.
+                utxoHash = await this.UTXOSet.Rewind().ConfigureAwait(false);
+            }
+
+            this.concurrentChain.SetTip(this.Tip);
+
+            this.lookaheadBlockPuller.SetLocation(this.Tip);
+
+            this.chainedHeaderTree.Initialize(this.Tip, !blockStoreDisabled);
+
+            this.logger.LogTrace("(-)");
+        }
+    }
+}
