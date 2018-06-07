96535cfff934da9fa5c05aae16b7a470df66ddca
Jeremy Bokobza <bokobza@users.noreply.github.com>
2018-06-07 17:44:50 +0100

    created Apex definition (#4)

---
+++ src/Stratis.ApexD/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NBitcoin;
+using NBitcoin.Protocol;
+using Stratis.Bitcoin.Builder;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Features.Api;
+using Stratis.Bitcoin.Features.BlockStore;
+using Stratis.Bitcoin.Features.Consensus;
+using Stratis.Bitcoin.Features.MemoryPool;
+using Stratis.Bitcoin.Features.Miner;
+using Stratis.Bitcoin.Features.RPC;
+using Stratis.Bitcoin.Features.Wallet;
+using Stratis.Bitcoin.Utilities;
+
+namespace Stratis.ApexD
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        public static async Task MainAsync(string[] args)
+        {
+            try
+            {
+                NodeSettings nodeSettings = new NodeSettings(ApexNetwork.ApexTest, protocolVersion: ProtocolVersion.ALT_PROTOCOL_VERSION, args: args);
+
+                var node = new FullNodeBuilder()
+                    .UseNodeSettings(nodeSettings)
+                    .UseBlockStore()
+                    .UsePowConsensus() //.UsePosConsensus()
+                    .UseMempool()
+                    .AddRPC()
+                    .UseWallet()
+                    .AddMining() //.AddPowPosMining()
+                    .UseApi()
+                    .Build();
+
+                if (node != null)
+                    await node.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex.Message);
+            }
+        }
+    }
+}
