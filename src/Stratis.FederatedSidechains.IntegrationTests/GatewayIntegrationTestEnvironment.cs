500895a5aa57e6da87a7849c04a727d668fe5f34
monsieurleberre <monsieurleberre@users.noreply.github.com>
2018-06-15 07:33:23 +0100

    Cross chain transfer integration test (#25)
    * upgrade nuget 1.1.7 to 1.1.9 and 4.0.0.57 and 4.0.0.58
    
    * add NLog
    
    * added CoinbaseMaturity, PremineReward, pow reward, pos reward and max reorg for the apex network.
    
    * Add new network properties to Apex
    
    * Update ApexNetwork.cs
    
    * Update ApexNetwork.cs
    
    * New integration test for CrossChainTransfer, the old one and its project will be obsolete soon
    
    * PR comments + other bug fixes
    
    * Dispose of the gateway env, although nodebuilder is already being disposed of ...
    
    * A few more fixes
    
    * Fixed the mining, I still need to understand why CustomNodeRunner creates mining issues.

---
+++ src/Stratis.FederatedSidechains.IntegrationTests/GatewayIntegrationTestEnvironment.cs
@@ -0,0 +1,156 @@
+﻿using FluentAssertions;
+using NBitcoin;
+using Stratis.Bitcoin.Features.Api;
+using Stratis.Bitcoin.Features.BlockStore;
+using Stratis.Bitcoin.Features.Consensus;
+using Stratis.Bitcoin.Features.GeneralPurposeWallet;
+using Stratis.Bitcoin.Features.MemoryPool;
+using Stratis.Bitcoin.Features.Miner;
+using Stratis.Bitcoin.Features.Notifications;
+using Stratis.Bitcoin.Features.RPC;
+using Stratis.Bitcoin.Features.Wallet;
+using Stratis.Bitcoin.IntegrationTests.Common.EnvironmentMockUpHelpers;
+using Stratis.FederatedPeg;
+using Stratis.FederatedPeg.Features.FederationGateway;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Stratis.Bitcoin.IntegrationTests.Common;
+
+namespace Stratis.FederatedSidechains.IntegrationTests
+{
+    public class GatewayIntegrationTestEnvironment : IDisposable
+    {
+        public IDictionary<Chain, Network> Networks { get; }
+        public IDictionary<Chain, Mnemonic> ChainMnemonics { get; }
+        public List<NodeKey> FederationNodeKeys { get; private set; }
+        public IList<FederationMemberKey> FederationMemberKeys { get; private set; }
+        public IDictionary<FederationMemberKey, Mnemonic> FederationMembersMnemonics { get; private set; }
+        public Script RedeemScript { get; private set; }
+        public IDictionary<NodeKey, GeneralPurposeAccount> GpAccountsByKey { get; }
+        public IDictionary<NodeKey, CoreNode> NodesByKey { get; }
+
+        private readonly NodeBuilder nodeBuilder;
+
+        public readonly int FederationMemberCount;
+        public int QuorumSize => FederationMemberCount / 2 + 1;
+
+        public GatewayIntegrationTestEnvironment(NodeBuilder nodeBuilder, Network mainchainNetwork, Network sidechainNetwork, int federationMemberCount = 3)
+        {
+            FederationMembersMnemonics = new Dictionary<FederationMemberKey, Mnemonic>();
+            ChainMnemonics = new Dictionary<Chain, Mnemonic>();
+            NodesByKey = new Dictionary<NodeKey, CoreNode>();
+            GpAccountsByKey = new Dictionary<NodeKey, GeneralPurposeAccount>();
+            Networks = new Dictionary<Chain, Network>
+            {
+                {Chain.Mainchain, mainchainNetwork},
+                {Chain.Sidechain, sidechainNetwork }
+            };
+
+            this.nodeBuilder = nodeBuilder;
+            FederationMemberCount = federationMemberCount;
+            BuildFederationMembersNodeKeys();
+            BuildMnemonics();
+            BuildRedeemScript();
+            BuildFederationNodes();
+            BuildGeneralPurposeWallets();
+        }
+
+        private void BuildFederationMembersNodeKeys()
+        {
+            FederationNodeKeys = new List<NodeKey>();
+            foreach (var chain in Enum.GetValues(typeof(Chain)))
+            {
+                var keys = Enumerable.Range(0, FederationMemberCount)
+                    .Select(i => new NodeKey { Chain = (Chain)chain, Role = NodeRole.Federation, Index = i }).ToList();
+                FederationNodeKeys.AddRange(keys);
+            }
+
+            FederationMemberKeys = FederationNodeKeys.Select(n => n.AsFederationMemberKey()).Distinct().ToList();
+
+            FederationNodeKeys.Count.Should().Be(FederationMemberCount * 2);
+            FederationMemberKeys.Count.Should().Be(FederationMemberCount);
+        }
+
+        private void BuildMnemonics()
+        {
+            foreach (var federationMemberKey in FederationMemberKeys)
+            {
+                var mnemonic = new Mnemonic(Wordlist.English, WordCount.Twelve);
+                FederationMembersMnemonics.Add(federationMemberKey, mnemonic);
+            }
+            //todo: make sure this is needed
+            ChainMnemonics[Chain.Mainchain] = new Mnemonic(Wordlist.English, WordCount.Twelve);
+            ChainMnemonics[Chain.Sidechain] = new Mnemonic(Wordlist.English, WordCount.Twelve);
+        }
+
+        private void BuildRedeemScript()
+        {
+            this.RedeemScript = PayToMultiSigTemplate.Instance.GenerateScriptPubKey(QuorumSize,
+                FederationMembersMnemonics.Values
+                    .Select(m => m.DeriveExtKey().PrivateKey.PubKey).ToArray());
+        }
+
+        private void BuildFederationNodes()
+        {
+            foreach (var key in FederationNodeKeys)
+            {
+                var addParametersAction = new Action<CoreNode>(n =>
+                {
+                    n.ConfigParameters.Add("membername", key.AsFederationMemberKey().Name);
+                    n.ConfigParameters.Add("apiport", key.SelfApiPort.ToString());
+                    n.ConfigParameters.Add("counterchainapiport", key.CounterChainApiPort.ToString());
+                    n.ConfigParameters.Add("redeemscript", this.RedeemScript.ToString());
+                    n.ConfigParameters.Add("federationips",
+                        string.Join(",", FederationNodeKeys
+                            .Where(k => k.Chain == key.Chain && k.Index != key.Index)
+                            .Select(k => $"127.0.0.1:{k.SelfApiPort}")
+                        ));
+                });
+                TestHelper.BuildStartAndRegisterNode(nodeBuilder,
+                    fullNodeBuilder => fullNodeBuilder
+                        .UseBlockStore()
+                        .UsePowConsensus()
+                        .UseMempool()
+                        .UseWallet()
+                        .AddMining()
+                        .AddFederationGateway()
+                        .UseGeneralPurposeWallet()
+                        .UseBlockNotification()
+                        .UseApi()
+                        .AddRPC()
+                        .MockIBD(),
+                    key, NodesByKey, Networks[key.Chain], addParametersAction);
+                //todo: check this is needed
+                //TestHelper.ConnectNodeToOtherNodesInTest(key, nodesByKey);
+            }
+        }
+
+        private void BuildGeneralPurposeWallets()
+        {
+            foreach (var key in FederationNodeKeys)
+            {
+                //todo: change that when GeneralPurposeWallets are ready again
+                //var generalWalletManager = nodesByKey[key].FullNode.NodeService<IGeneralPurposeWalletManager>();
+                //generalWalletManager.CreateWallet(NamingConstants.MultisigPassword, NamingConstants.MultisigWallet)
+                GpAccountsByKey.Add(key, null);
+            }
+        }
+
+        public BitcoinAddress GetMultisigAddress(Chain chain)
+        {
+            return RedeemScript.Hash.GetAddress(Networks[chain]);
+        }
+
+        public Script GetMultisigPubKey(Chain chain)
+        {
+            return GetMultisigAddress(chain).ScriptPubKey;
+        }
+
+        public void Dispose()
+        {
+            nodeBuilder?.Dispose();
+        }
+    }
+}
+
---
+++ src/Stratis.FederatedSidechains.IntegrationTests/NodeKey.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Stratis.FederatedPeg;
+
+namespace Stratis.FederatedSidechains.IntegrationTests
+{
+    public enum NodeRole
+    {
+        Wallet,
+        Federation,
+    }
+
+    public struct FederationMemberKey
+    {
+        public NodeRole Role => NodeRole.Federation;
+        public int Index;
+
+        public string Name => NamingConstants.CrossChain + Role + Index;
+        public string Password => NamingConstants.Password + Name;
+        public string MultisigWalletName => NamingConstants.Multisig + NamingConstants.Wallet;
+
+    }
+    
+    public struct NodeKey
+    {
+        public Chain Chain;
+        public NodeRole Role;
+        public int Index;
+        public int SelfApiPort => (Chain == Chain.Mainchain ? 101 : 102) * 100 + Index;
+        public int CounterChainApiPort => (Chain == Chain.Mainchain ? 102 : 101) * 100 + Index;
+
+        public string Name => Chain.ToString() + Role + Index;
+        public string Password => nameof(Password) + Name;
+        public string WalletName => NamingConstants.Wallet + Name;
+
+        public FederationMemberKey AsFederationMemberKey()
+        {
+            return new FederationMemberKey() {Index = this.Index};
+        }
+    }
+}
