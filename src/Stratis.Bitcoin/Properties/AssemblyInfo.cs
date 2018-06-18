3562521766cdbd4b976ab07463adc8370b91471e
bokobza <bokobza@gmail.com>
2018-06-18 10:19:17 +0100

    readded the version in Stratis.Bitcoin AssemblyInfo.cs

--- src/Stratis.Bitcoin/Properties/AssemblyInfo.cs
+++ src/Stratis.Bitcoin/Properties/AssemblyInfo.cs
@@ -21,4 +21,17 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a6c18cae-7246-41b1-bfd6-c54ba1694ac2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.12.0")]
+[assembly: AssemblyFileVersion("1.1.12.0")]
 [assembly: InternalsVisibleTo("Stratis.Bitcoin.Tests")]
