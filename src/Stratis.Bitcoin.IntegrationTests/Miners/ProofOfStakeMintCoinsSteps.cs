31ee6f8e45d35086099373e95611cfd428773531
Francois de la Rouviere <fassadlr@gmail.com>
2018-06-18 11:37:19 +0100

    Cleanup staking tests

--- src/Stratis.Bitcoin.IntegrationTests/Miners/ProofOfStakeMintCoinsSteps.cs
+++ src/Stratis.Bitcoin.IntegrationTests/Miners/ProofOfStakeMintCoinsSteps.cs
@@ -16,9 +16,9 @@ namespace Stratis.Bitcoin.IntegrationTests.Miners
 
         protected override void AfterTest() { }
 
-        private void a_proof_of_work_node_with_wallet()
+        private void a_proof_of_stake_node_with_wallet()
         {
-            this.proofOfStakeSteps.ProofOfWorkNodeWithWallet();
+            this.proofOfStakeSteps.PremineNodeWithWallet();
         }
 
         private void it_mines_genesis_and_premine_blocks()
@@ -31,39 +31,19 @@ namespace Stratis.Bitcoin.IntegrationTests.Miners
             this.proofOfStakeSteps.MineCoinsToMaturity();
         }
 
-        private void a_proof_of_stake_node_with_wallet()
-        {
-            this.proofOfStakeSteps.ProofOfStakeNodeWithWallet();
-        }
-
-        private void it_syncs_with_proof_work_node()
-        {
-            this.proofOfStakeSteps.SyncWithProofWorkNode();
-        }
-
-        private void sends_a_million_coins_from_pow_wallet_to_pos_wallet()
-        {
-            this.proofOfStakeSteps.SendOneMillionCoinsFromPowWalletToPosWallet();
-        }
-
-        private void pow_wallet_broadcasts_tx_of_million_coins_and_pos_wallet_receives()
-        {
-            this.proofOfStakeSteps.PowWalletBroadcastsTransactionOfOneMillionCoinsAndPosWalletReceives();
-        }
-
         private void pos_node_mines_ten_blocks_more_ensuring_they_can_be_staked()
         {
-            this.proofOfStakeSteps.PosNodeMinesTenBlocksMoreEnsuringTheyCanBeStaked();
+            this.proofOfStakeSteps.PremineNodeMinesTenBlocksMoreEnsuringTheyCanBeStaked();
         }
 
         private void pos_node_starts_staking()
         {
-            this.proofOfStakeSteps.PosNodeStartsStaking();
+            this.proofOfStakeSteps.PremineNodeStartsStaking();
         }
 
         private void pos_node_wallet_has_earned_coins_through_staking()
         {
-            this.proofOfStakeSteps.PosNodeWalletHasEarnedCoinsThroughStaking();
+            this.proofOfStakeSteps.PremineNodeWalletHasEarnedCoinsThroughStaking();
         }
     }
 }
