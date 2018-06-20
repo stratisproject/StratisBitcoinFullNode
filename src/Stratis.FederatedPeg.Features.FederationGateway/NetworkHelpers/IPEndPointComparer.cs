5faef6aa24860f1e839a743d3d1a2a6f4bebc036
monsieurleberre <monsieurleberre@users.noreply.github.com>
2018-06-20 10:36:01 +0100

    Partial transaction for federation members only (#52)
    * check if a Peer is in the federation before registering or sending message to it
    
    * don't send message to non federation peers
    
    * hopefully better way to compare ip endpoints

---
+++ src/Stratis.FederatedPeg.Features.FederationGateway/NetworkHelpers/IPEndPointComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using DBreeze.Utils;
+using Stratis.Bitcoin.Utilities.Extensions;
+
+namespace Stratis.FederatedPeg.Features.FederationGateway.NetworkHelpers
+{
+    public class IPEndPointComparer : IEqualityComparer<IPEndPoint>
+    {
+        public bool Equals(IPEndPoint x, IPEndPoint y)
+        {
+            if (x == null) return y == null;
+
+            return x.MapToIpv6().Equals(y.MapToIpv6());
+        }
+
+        public int GetHashCode(IPEndPoint endPoint)
+        {
+            return endPoint.MapToIpv6().GetHashCode();
+        }
+    }
+}
