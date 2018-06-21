3b1a7c601b52e57986d2d37ae27672978f27d0d4
majikandy <github@andy-davis.co.uk>
2018-06-21 14:37:41 +0100

    added endpoint to show smart contract logs

--- src/Stratis.Bitcoin/Controllers/DashboardController.cs
+++ src/Stratis.Bitcoin/Controllers/DashboardController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Stratis.Bitcoin.Configuration;
 
 namespace Stratis.Bitcoin.Controllers
 {
@@ -9,11 +12,14 @@ namespace Stratis.Bitcoin.Controllers
     [Route("[controller]")]
     public class DashboardController : Controller
     {
+        
         private readonly IFullNode fullNode;
+        private NodeSettings nodeSettings;
 
-        public DashboardController(IFullNode fullNode)
+        public DashboardController(IFullNode fullNode, NodeSettings nodeSettings)
         {
             this.fullNode = fullNode;
+            this.nodeSettings = nodeSettings;
         }
 
         /// <summary>
@@ -21,12 +27,34 @@ namespace Stratis.Bitcoin.Controllers
         /// </summary>
         /// <returns>text/html content</returns>
         [HttpGet]
-        [Route("")] // the endpoint name
+        [Route("")] 
         [Route("Stats")]
         public IActionResult Stats()
         {
             string content = (this.fullNode as FullNode).LastLogOutput;
             return this.Content(content);
+        }
+
+        /// <summary>
+        /// Returns a web page view over the SmartContract Logs
+        /// </summary>
+        /// <returns>text/html content</returns>
+        [HttpGet]
+        [Route("SmartContracts/{numberOfLogEntriesToShow?}")] 
+        public IActionResult SmartContractLogs(int numberOfLogEntriesToShow = 30)
+        {
+            string logPath = Path.Combine(this.nodeSettings.DataDir, @"Logs\smartcontracts.txt");
+
+            if (!System.IO.File.Exists(logPath))
+            {
+                return this.Content($"There is no log file at: {logPath}. An nlog.config is needed in the daemon directory.");
+            }
+
+            string[] logLines = System.IO.File.ReadAllLines(logPath);
+
+            int entriesToSkip = logLines.Length < numberOfLogEntriesToShow ? 0 : logLines.Length - numberOfLogEntriesToShow;
+
+            return this.Content(string.Join("\r\n", logLines.Skip(entriesToSkip)));
         }
     }
 }
