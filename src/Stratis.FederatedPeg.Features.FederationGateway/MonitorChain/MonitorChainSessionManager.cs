8728605d16ff4467120a74a680cc2eab3ae2743e
carlton355 <carlton.pringle@gmail.com>
2018-06-22 16:37:24 +0700

    Added minimum transfer amount constraint. (#63)

--- src/Stratis.FederatedPeg.Features.FederationGateway/MonitorChain/MonitorChainSessionManager.cs
+++ src/Stratis.FederatedPeg.Features.FederationGateway/MonitorChain/MonitorChainSessionManager.cs
@@ -53,6 +53,10 @@ namespace Stratis.FederatedPeg.Features.FederationGateway.MonitorChain
         // The time between sessions.
         private TimeSpan sessionRunInterval = new TimeSpan(hours: 0, minutes: 0, seconds: 30);
 
+        // The minimum transfer amount permissible.
+        // (Prevents spamming of network.)
+        private readonly Money MinimumTransferAmount = new Money(1.0m, MoneyUnit.BTC);
+
         // The logger.
         private readonly ILogger logger;
 
@@ -116,6 +120,13 @@ namespace Stratis.FederatedPeg.Features.FederationGateway.MonitorChain
         public void CreateMonitorSession(CrossChainTransactionInfo crossChainTransactionInfo)
         {
             this.logger.LogTrace("({0}:'{1}',{2}:'{3}')", nameof(crossChainTransactionInfo.CrossChainTransactionId), crossChainTransactionInfo.CrossChainTransactionId, nameof(crossChainTransactionInfo.DestinationAddress), crossChainTransactionInfo.DestinationAddress);
+
+            // Ignore sessions below the MinimumTransferAmount
+            if (crossChainTransactionInfo.Amount < MinimumTransferAmount)
+            {
+                this.logger.LogInformation($"Session {crossChainTransactionInfo.TransactionHash} is less than the MinimumTransferAmount.  Ignoring. ");
+                return;
+            }
 
             var monitorChainSession = new MonitorChainSession(
                 DateTime.Now,
@@ -277,3 +288,4 @@ namespace Stratis.FederatedPeg.Features.FederationGateway.MonitorChain
         }
     }
 }
+
