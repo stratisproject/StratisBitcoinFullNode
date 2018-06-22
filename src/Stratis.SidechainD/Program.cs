b7fb16abbe923780f0dba65b835ad6471af748a1
Jeremy Bokobza <bokobza@users.noreply.github.com>
2018-06-22 03:25:08 +0100

    Improvements (#61)
    * Added seednodes to the SidechainD project
    
    * check publickey is part of the multisig

--- src/Stratis.SidechainD/Program.cs
+++ src/Stratis.SidechainD/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NBitcoin;
 using NBitcoin.Protocol;
 using Stratis.Bitcoin.Builder;
 using Stratis.Bitcoin.Configuration;
@@ -38,6 +40,9 @@ namespace Stratis.SidechainD
 
                 NodeSettings nodeSettings = new NodeSettings(ApexNetwork.Test, protocolVersion: ProtocolVersion.ALT_PROTOCOL_VERSION, args: args);
 
+                string[] seedNodes = { "104.211.178.243", "51.144.35.218", "65.52.5.149", "51.140.231.125", "13.70.81.5" };
+                ApexNetwork.Test.SeedNodes.AddRange(ConvertToNetworkAddresses(seedNodes, ApexNetwork.Test.DefaultPort).ToList());
+
                 var node = new FullNodeBuilder()
                     .UseNodeSettings(nodeSettings)
                     .UseBlockStore()
@@ -54,6 +59,24 @@ namespace Stratis.SidechainD
             catch (Exception ex)
             {
                 Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex.Message);
+            }
+        }
+
+        protected static IEnumerable<NetworkAddress> ConvertToNetworkAddresses(string[] seeds, int defaultPort)
+        {
+            var rand = new Random();
+            TimeSpan oneWeek = TimeSpan.FromDays(7);
+
+            foreach (string seed in seeds)
+            {
+                // It'll only connect to one or two seed nodes because once it connects,
+                // it'll get a pile of addresses with newer timestamps.
+                // Seed nodes are given a random 'last seen time' of between one and two weeks ago.
+                yield return new NetworkAddress
+                {
+                    Time = DateTime.UtcNow - (TimeSpan.FromSeconds(rand.NextDouble() * oneWeek.TotalSeconds)) - oneWeek,
+                    Endpoint = Utils.ParseIpEndpoint(seed, defaultPort)
+                };
             }
         }
     }
