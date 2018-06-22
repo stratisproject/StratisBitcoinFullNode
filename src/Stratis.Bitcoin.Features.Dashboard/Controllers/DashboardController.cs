b5194b8a442d1499b5face015d5c7eb69859504e
dippyeggs <stratdev@protonmail.com>
2018-06-22 11:07:40 +0100

    Dashboard feature initial commit

---
+++ src/Stratis.Bitcoin.Features.Dashboard/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿
+using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Stratis.Bitcoin.Features.Api;
+
+namespace Stratis.Bitcoin.Features.Dashboard.Controllers
+{
+    public class DashboardController : Controller
+    {
+
+        [HttpGet("/")]
+        public ContentResult Index()
+        {
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                StatusCode = (int)HttpStatusCode.OK,
+                Content = System.IO.File.ReadAllText(Path.Combine(DashboardApiSettings.GetRootDirectory(), "wwwroot/index.html"))
+            };
+        }
+
+    }
+}
---
+++ src/Stratis.Bitcoin.Features.Dashboard/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Stratis.Bitcoin.Features.Api;
+
+namespace Stratis.Bitcoin.Features.Dashboard
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(DashboardApiSettings.GetRootDirectory(), "wwwroot")),
+                RequestPath = "/assets"
+            });
+
+            app.UseMvc();
+        }
+    }
+}
