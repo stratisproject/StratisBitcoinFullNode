9a3df04a1f00cbeb2d4a25b4f43247707400206f
Jeremy Bokobza <bokobza@gmail.com>
2018-06-28 17:56:50 +0100

    Removed NOSTRNORMALIZE option

--- src/NBitcoin/BIP39/Mnemonic.cs
+++ src/NBitcoin/BIP39/Mnemonic.cs
@@ -189,44 +189,8 @@ namespace NBitcoin
 
         internal static string NormalizeString(string word)
         {
-#if !NOSTRNORMALIZE
-            if(!SupportOsNormalization())
-            {
-                return KDTable.NormalizeKD(word);
-            }
-            else
-            {
-                return word.Normalize(NormalizationForm.FormKD);
-            }
-#else
             return KDTable.NormalizeKD(word);
-#endif
         }
-
-#if !NOSTRNORMALIZE
-        static bool? _SupportOSNormalization;
-        internal static bool SupportOsNormalization()
-        {
-            if(_SupportOSNormalization == null)
-            {
-                var notNormalized = "あおぞら";
-                var normalized = "あおぞら";
-                if(notNormalized.Equals(normalized, StringComparison.Ordinal))
-                {
-                    _SupportOSNormalization = false;
-                }
-                else
-                {
-                    try
-                    {
-                        _SupportOSNormalization = notNormalized.Normalize(NormalizationForm.FormKD).Equals(normalized, StringComparison.Ordinal);
-                    }
-                    catch { _SupportOSNormalization = false; }
-                }
-            }
-            return _SupportOSNormalization.Value;
-        }
-#endif
 
         public ExtKey DeriveExtKey(string passphrase = null)
         {
