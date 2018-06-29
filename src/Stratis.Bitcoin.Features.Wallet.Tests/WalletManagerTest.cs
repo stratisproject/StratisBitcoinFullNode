a595b615653f3acb3f6de2443bb9825f62e90192
majikandy <github@andy-davis.co.uk>
2018-06-29 15:19:21 +0100

    unused using

--- src/Stratis.Bitcoin.Features.Wallet.Tests/WalletManagerTest.cs
+++ src/Stratis.Bitcoin.Features.Wallet.Tests/WalletManagerTest.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NBitcoin;
