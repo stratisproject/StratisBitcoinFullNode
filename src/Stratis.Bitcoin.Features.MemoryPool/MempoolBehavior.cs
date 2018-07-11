c50ba2d382ffc02c39e16be677bb2818996828b5
Mike Dennis <strykitenterprises@gmail.com>
2018-07-10 22:04:56 -0600

    Fix code review issues.

--- src/Stratis.Bitcoin.Features.MemoryPool/MempoolBehavior.cs
+++ src/Stratis.Bitcoin.Features.MemoryPool/MempoolBehavior.cs
@@ -321,11 +321,17 @@ namespace Stratis.Bitcoin.Features.MemoryPool
                 }
 
                 if (this.isBlocksOnlyMode)
+                {
                     this.logger.LogInformation("Transaction ID '{0}' inventory sent in violation of protocol peer '{1}'.", inv.Hash, peer.RemoteSocketEndpoint);
+                }
                 else if (await this.orphans.AlreadyHaveAsync(inv.Hash))
+                {
                     this.logger.LogDebug("Transaction ID '{0}' already in orphans, skipped.", inv.Hash);
+                }
                 else
-                    send.Inventory.Add(inv);               
+                {
+                    send.Inventory.Add(inv);
+                }
             }
 
             if (peer.IsConnected)
