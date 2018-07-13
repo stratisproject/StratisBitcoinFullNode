dd1a553c0d1dadb1118692d6912c03cee44b6fdd
Francois de la Rouviere <fassadlr@gmail.com>
2018-07-13 13:16:05 +0100

    Changes as per review

--- src/NBitcoin.Tests/ChainTests.cs
+++ src/NBitcoin.Tests/ChainTests.cs
@@ -555,7 +555,7 @@ namespace NBitcoin.Tests
             uint nonce = RandomUtils.GetUInt32();
             foreach (ConcurrentChain chain in chains)
             {
-                Block block = TestUtils.CreateFakeBlock(this.network, this.network.CreateTransaction());
+                Block block = TestUtils.CreateFakeBlock(this.network);
                 block.Header.HashPrevBlock = previous == null ? chain.Tip.HashBlock : previous.HashBlock;
                 block.Header.Nonce = nonce;
                 if (!chain.TrySetTip(block.Header, out last))
