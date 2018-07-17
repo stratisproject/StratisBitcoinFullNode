656f94e13cc36314c9206dfb469de19b21f5982d
herbepau <prjherbert@hotmail.co.uk>
2018-07-17 10:19:50 +0100

    Feature plugins (#1651)
    * Initial commit
    
    * Further work on the new Apps Feature
    
    * Further work on the new Apps Feature
    
    * Stratis.Bitcoin.Features.Apps POC latest
    
    * Stratis.Bitcoin.Features.Apps latest (AppsController)
    
    * Latest Apps feature commit including Blazor apps browser
    
    * Added 'this' prefixes.
    
    * Apps Feature latest
    
    * Apps Feature latest
    
    * Apps Feature latest
    
    * Removed UseApps use from Stratis.Stratis.StratisD/Program.cs
    
    * Changes following PR feedback
    
    * Changes following merge
    
    * Changes following PR feedback:-
    - Removed Stratis.Bitcoin.Features.Apps.Browser project (is now Stratis.Bitcoin.Apps.Browser)
    - Removed redundant spaces from part of Stratis.Bitcoin.Features.Api/Startup.cs
    
    * Futher whitespace removed
    
    * Changes following PR feedback.  In particular:-
    - removed AppsFileService; now use FileStorage in utils
    - Attribute decoration of StratisApp in order to work with FileStorage
    
    * Latest following PR feedback
    
    * Changes following PR feedback.

---
+++ src/Stratis.Bitcoin.Features.Apps.Tests/FullNodeBuilderExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Stratis.Bitcoin.Builder;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Features.Apps.Interfaces;
+using Xunit;
+
+namespace Stratis.Bitcoin.Features.Apps.Tests
+{
+    public class FullNodeBuilderExtensionsTests
+    {
+        private readonly FullNodeBuilder fullNodeBuilder;
+
+        public FullNodeBuilderExtensionsTests()
+        {
+            this.fullNodeBuilder = new FullNodeBuilder(new NodeSettings());
+        }
+
+        [Fact]
+        public void Test_UseApps_adds_the_AppsFeature()
+        {
+            this.fullNodeBuilder.UseApps().Build();
+            var count = this.fullNodeBuilder.Features.FeatureRegistrations.Count(x => x.FeatureType == typeof(AppsFeature));
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public void Test_UseApps_adds_the_AppStore()
+        {
+            this.fullNodeBuilder.UseApps().Build();
+            var count = this.fullNodeBuilder.Services.Count(x => x.ServiceType == typeof(IAppsStore));
+            Assert.Equal(1, count);
+        }        
+
+        [Fact]
+        public void Test_UseApps_adds_the_AppsHost()
+        {
+            this.fullNodeBuilder.UseApps().Build();
+            var count = this.fullNodeBuilder.Services.Count(x => x.ServiceType == typeof(IAppsHost));
+            Assert.Equal(1, count);
+        }        
+
+        [Fact]
+        public void Test_UseApps_adds_the_AppsController()
+        {
+            this.fullNodeBuilder.UseApps().Build();
+            var count = this.fullNodeBuilder.Services.Count(x => x.ServiceType == typeof(AppsController));
+            Assert.Equal(1, count);
+        }
+    }
+}
---
+++ src/Stratis.Bitcoin.Features.Apps/AppsFeature.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Stratis.Bitcoin.Builder;
+using Stratis.Bitcoin.Builder.Feature;
+using Stratis.Bitcoin.Configuration.Logging;
+using Stratis.Bitcoin.Features.Apps.Interfaces;
+
+namespace Stratis.Bitcoin.Features.Apps
+{
+    /// <summary>
+    /// Feature allowing for the dynamic hosting of web-based applications targeting the FullNode.
+    /// </summary>
+    public class AppsFeature : FullNodeFeature
+    {
+        private bool disposed;
+        private readonly ILogger logger;
+        private readonly IAppsStore appsStore;
+        private readonly IAppsHost appsHost;
+
+        public AppsFeature(ILoggerFactory loggerFactory, IAppsStore appsStore, IAppsHost appsHost)
+        {
+            this.appsStore = appsStore;
+            this.appsHost = appsHost;
+            this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
+        }
+
+        public override void Initialize()
+        {
+            this.logger.LogInformation("Initializing {0}", nameof(AppsFeature));
+
+            this.appsHost.Host(this.appsStore.Applications);
+        }
+
+        public override void Dispose()
+        {
+            if (this.disposed)
+                return;
+            this.logger.LogInformation("Disposing {0}", nameof(AppsFeature));
+
+            this.appsHost.Close();
+
+            base.Dispose();
+            this.disposed = true;
+        }
+    }
+
+    public static class FullNodeBuilderExtensions
+    {
+        public static IFullNodeBuilder UseApps(this IFullNodeBuilder fullNodeBuilder)
+        {
+            LoggingConfiguration.RegisterFeatureNamespace<AppsFeature>("apps");
+
+            fullNodeBuilder.ConfigureFeature(features =>
+            {
+                features
+                    .AddFeature<AppsFeature>()
+                    .FeatureServices(services =>
+                    {
+                        services.AddSingleton<IAppsStore, AppsStore>();                        
+                        services.AddSingleton<IAppsHost, AppsHost>();
+                        services.AddSingleton<AppsController>();
+                    });
+            });
+
+            return fullNodeBuilder;
+        }
+    }
+}
