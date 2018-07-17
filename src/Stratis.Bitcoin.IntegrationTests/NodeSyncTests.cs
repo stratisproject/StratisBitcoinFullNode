592ac75f00d2b58820bb8072a75eeac7acc6fc3e
Francois de la Rouviere <fassadlr@gmail.com>
2018-07-17 15:46:36 +0100

    Revert test

--- src/Stratis.Bitcoin.IntegrationTests/NodeSyncTests.cs
+++ src/Stratis.Bitcoin.IntegrationTests/NodeSyncTests.cs
@@ -311,6 +311,7 @@ namespace Stratis.Bitcoin.IntegrationTests
                 Thread.Sleep(1);
 
             // Make sure that miner did not advance yet but connector did.
+            Assert.NotEqual(miner.FullNode.Chain.Tip.HashBlock, networkNode1.FullNode.Chain.Tip.HashBlock);
             Assert.Equal(connector.FullNode.Chain.Tip.HashBlock, networkNode1.FullNode.Chain.Tip.HashBlock);
             Assert.Equal(miner.FullNode.Chain.Tip.Height, networkHeight);
             Assert.Equal(connector.FullNode.Chain.Tip.Height, networkHeight + 1);
@@ -322,6 +323,7 @@ namespace Stratis.Bitcoin.IntegrationTests
             networkHeight++;
 
             // Make sure that at this moment miner's tip != network's and connector's tip.
+            Assert.NotEqual(miner.FullNode.Chain.Tip.HashBlock, networkNode1.FullNode.Chain.Tip.HashBlock);
             Assert.Equal(connector.FullNode.Chain.Tip.HashBlock, networkNode1.FullNode.Chain.Tip.HashBlock);
             Assert.Equal(miner.FullNode.Chain.Tip.Height, networkHeight);
             Assert.Equal(connector.FullNode.Chain.Tip.Height, networkHeight);
