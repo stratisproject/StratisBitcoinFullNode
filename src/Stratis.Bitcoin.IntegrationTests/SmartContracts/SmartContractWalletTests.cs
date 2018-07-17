d9c4376a86e7e49e16dffaf90716a07004055d06
Francois de la Rouviere <fassadlr@gmail.com>
2018-07-17 16:39:17 +0100

    changes as per review

--- src/Stratis.Bitcoin.IntegrationTests/SmartContracts/SmartContractWalletTests.cs
+++ src/Stratis.Bitcoin.IntegrationTests/SmartContracts/SmartContractWalletTests.cs
@@ -89,7 +89,6 @@ namespace Stratis.Bitcoin.IntegrationTests.SmartContracts
 
                 // generate two new blocks do the trx is confirmed
                 scSender.AddToStratisMempool(scSender.FullNode.Network.CreateTransaction(trx.ToBytes()));
-                //scSender.GenerateBlockManually(new List<Transaction>(new[] { scSender.FullNode.Network.CreateTransaction(trx.ToBytes()) }));
                 scSender.GenerateStratisWithMiner(1);
 
                 // wait for block repo for block sync to work
