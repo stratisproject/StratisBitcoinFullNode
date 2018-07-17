600cef08b71ba25d52faa16998c92d47bef5cab7
Francois de la Rouviere <fassadlr@gmail.com>
2018-07-17 08:01:32 +0100

    Small refactor

--- src/Stratis.Bitcoin.IntegrationTests.Common/Extensions/CoreNodeExtensions.cs
+++ src/Stratis.Bitcoin.IntegrationTests.Common/Extensions/CoreNodeExtensions.cs
@@ -63,17 +63,17 @@ namespace Stratis.Bitcoin.IntegrationTests
             DateTimeOffset now = coreNode.MockTime ?? DateTimeOffset.UtcNow;
 
             uint nonce = 0;
-            var block = coreNode.FullNode.Network.Consensus.ConsensusFactory.CreateBlock();
+            var block = coreNode.FullNode.Network.CreateBlock();
             block.Header.HashPrevBlock = coreNode.FullNode.Chain.Tip.HashBlock;
             block.Header.Bits = block.Header.GetWorkRequired(coreNode.FullNode.Network, coreNode.FullNode.Chain.Tip);
             block.Header.UpdateTime(now, coreNode.FullNode.Network, coreNode.FullNode.Chain.Tip);
 
-            var coinbase = coreNode.FullNode.Network.Consensus.ConsensusFactory.CreateTransaction();
+            var coinbase = coreNode.FullNode.Network.CreateTransaction();
             coinbase.AddInput(TxIn.CreateCoinbase(coreNode.FullNode.Chain.Height + 1));
             coinbase.AddOutput(new TxOut(coreNode.FullNode.Network.GetReward(coreNode.FullNode.Chain.Height + 1), receiver.GetAddress()));
             block.AddTransaction(coinbase);
 
-            if (transactions?.Any() ?? false)
+            if (transactions.Any())
             {
                 transactions = Reorder(transactions);
                 block.Transactions.AddRange(transactions);
