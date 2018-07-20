040ae9ac5670b80bea4b82352a616fc361541987
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-07-20 14:52:19 +1000

    Move CreateData to file

---
+++ src/Stratis.SmartContracts.Executor.Reflection/CreateData.cs
@@ -0,0 +1,16 @@
+﻿namespace Stratis.SmartContracts.Executor.Reflection
+{
+    public struct CreateData : ICreateData
+    {
+        public CreateData(Gas gasLimit, byte[] code, object[] methodParameters = null)
+        {
+            this.GasLimit = gasLimit;
+            this.ContractExecutionCode = code;
+            this.MethodParameters = methodParameters;
+        }
+
+        public object[] MethodParameters { get; }
+        public Gas GasLimit { get; }
+        public byte[] ContractExecutionCode { get; }
+    }
+}
