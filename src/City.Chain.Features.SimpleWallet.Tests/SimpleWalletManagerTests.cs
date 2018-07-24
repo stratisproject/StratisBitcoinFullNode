ddb812aafb6af95e1a8514ad616bd823ec8f54a6
SondreB <pixelworld@hotmail.com>
2018-07-25 01:41:39 +0200

    Add SimpleWalletFeature prototype #3 (#4)
    - Prototype of the SimpleWalletFeature.
    - Not fully implemented, just the basic for a functional prototype.
    - Adds a separate City.Chain.sln to be used for City Chain development.

---
+++ src/City.Chain.Features.SimpleWallet.Tests/SimpleWalletManagerTests.cs
@@ -0,0 +1,38 @@
+using System;
+using Xunit;
+
+namespace City.Chain.Features.SimpleWallet.Tests
+{
+    public class SimpleWalletManagerTests
+    {
+        [Fact]
+        public void CreateSimpleWallet()
+        {
+            //SimpleWalletHub hub = new SimpleWalletHub();
+
+            //var connectionId = Guid.NewGuid().ToString();
+
+            //hub.CreateWallet("citychain-mobile-0.0.1", connectionId);
+
+            //// TODO: Consider adding some bloomfilter type of ability, to increase privacy with a method similar to "filterload" message.
+            //hub.Watch("SMsZGWkF9zR5mjxWTYsDq9pU5ZHdUQw1jJ", connectionId);
+
+            //hub.Remove(connectionId);
+        }
+
+        [Fact]
+        public void CreateSimpleWalletAndWatch()
+        {
+            //SimpleWalletHub hub = new SimpleWalletHub();
+
+            //var connectionId = Guid.NewGuid().ToString();
+
+            //hub.CreateWallet("citychain-mobile-0.0.1", connectionId);
+
+            //// TODO: Consider adding some bloomfilter type of ability, to increase privacy with a method similar to "filterload" message.
+            //hub.Watch("SMsZGWkF9zR5mjxWTYsDq9pU5ZHdUQw1jJ", connectionId);
+
+            //hub.Remove(connectionId);
+        }
+    }
+}
