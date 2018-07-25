3bfb9afe3c898263fabd3ca1f9c0dae662b37fa4
Francois de la Rouviere <fassadlr@gmail.com>
2018-07-25 16:04:34 +0100

    Solution builds

--- src/Stratis.Bitcoin.Features.SmartContracts/Networks/SmartContractsRegTest.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/Networks/SmartContractsRegTest.cs
@@ -15,8 +15,8 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Networks
         public SmartContractsRegTest()
         {
             this.Name = "SmartContractsRegTest";
-            this.RootFolderName = NBitcoin.Networks.StratisMain.StratisRootFolderName;
-            this.DefaultConfigFilename = NBitcoin.Networks.StratisMain.StratisDefaultConfigFilename;
+            this.RootFolderName = "stratis";
+            this.DefaultConfigFilename = "stratis.conf";
             this.Magic = 0xDAB5BFFA;
             this.DefaultPort = 18444;
             this.RPCPort = 18332;
