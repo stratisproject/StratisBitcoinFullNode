eeaa1fb9c7622af8307d8035aae47c691464b56e
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-07-26 16:11:19 +1000

    Add Mutable Consensus

---
+++ src/NBitcoin/MutableConsensus.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NBitcoin.BouncyCastle.Math;
+
+namespace NBitcoin
+{
+    /// <summary>
+    /// Used to create a Mutable version of the <see cref="Consensus"/> object during configuration
+    /// </summary>
+    public class MutableConsensus : IConsensus
+    {
+        public long CoinbaseMaturity { get; set; }
+        public Money PremineReward { get; set; }
+        public long PremineHeight { get; set; }
+        public Money ProofOfWorkReward { get; set; }
+        public Money ProofOfStakeReward { get; set; }
+        public uint MaxReorgLength { get; set; }
+        public long MaxMoney { get; set; }
+        public ConsensusOptions Options { get; set; }
+        public Consensus.BuriedDeploymentsArray BuriedDeployments { get; set; }
+        public Consensus.BIP9DeploymentsArray BIP9Deployments { get; set; }
+        public int SubsidyHalvingInterval { get; set; }
+        public int MajorityEnforceBlockUpgrade { get; set; }
+        public int MajorityRejectBlockOutdated { get; set; }
+        public int MajorityWindow { get; set; }
+        public uint256 BIP34Hash { get; set; }
+        public Target PowLimit { get; set; }
+        public TimeSpan PowTargetTimespan { get; set; }
+        public TimeSpan PowTargetSpacing { get; set; }
+        public bool PowAllowMinDifficultyBlocks { get; set; }
+        public bool PowNoRetargeting { get; set; }
+        public uint256 HashGenesisBlock { get; set; }
+        public uint256 MinimumChainWork { get; set; }
+        public long DifficultyAdjustmentInterval { get; }
+        public int MinerConfirmationWindow { get; set; }
+        public int RuleChangeActivationThreshold { get; set; }
+        public int CoinType { get; set; }
+        public BigInteger ProofOfStakeLimit { get; set; }
+        public BigInteger ProofOfStakeLimitV2 { get; set; }
+        public int LastPOWBlock { get; set; }
+        public bool IsProofOfStake { get; set; }
+        public uint256 DefaultAssumeValid { get; set; }
+        public ConsensusFactory ConsensusFactory { get; set; }
+    }
+}
