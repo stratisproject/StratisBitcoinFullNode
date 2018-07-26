75c6842d7871484086fbc4f93b6b3d8a40c5da5a
Jandrews <jordandrews@hotmail.com>
2018-07-26 12:30:01 +1000

    Miner Tests

--- src/Stratis.Bitcoin.Features.Miner.Tests/MinerSettingsTest.cs
+++ src/Stratis.Bitcoin.Features.Miner.Tests/MinerSettingsTest.cs
@@ -82,32 +82,42 @@ namespace Stratis.Bitcoin.Features.Miner.Tests
             // Values assigned as configured
             Assert.Equal( (uint) 150000, minersettings.BlockDefinitionOptions.BlockMaxSize);
             Assert.Equal( (uint) 300000, minersettings.BlockDefinitionOptions.BlockMaxWeight);
+        }
 
+        [Fact]
+        public void Load_MiningEnabled_BlockSize_BlockWeight_Set_BelowMinimum()
+        {
             // Set values below consensus rules
-            nodeSettings = new NodeSettings(args: new string[] {
+            var nodeSettings = new NodeSettings(args: new string[] {
                 "-mine=true",
                 "-blockmaxsize=10",
                 "-blockmaxweight=30"
             });
 
-            minersettings = new MinerSettings(nodeSettings);
-            
-            // Values assigned minimum
-            Assert.Equal( (uint) 1000, minersettings.BlockDefinitionOptions.BlockMaxSize);
-            Assert.Equal( (uint) 4000, minersettings.BlockDefinitionOptions.BlockMaxWeight);
+            var minersettings = new MinerSettings(nodeSettings);
 
+            // Values assigned minimum
+            Assert.Equal((uint)1000, minersettings.BlockDefinitionOptions.BlockMaxSize);
+            Assert.Equal((uint)4000, minersettings.BlockDefinitionOptions.BlockMaxWeight);
+        }
+
+        [Fact]
+        public void Load_MiningEnabled_BlockSize_BlockWeight_Set_AboveMaximum()
+        {
             // Set values above consensus rules
-            nodeSettings = new NodeSettings(args: new string[] {
+            var nodeSettings = new NodeSettings(args: new string[] {
                 "-mine=true",
                 "-blockmaxsize=5000000",
                 "-blockmaxweight=5000000"
             });
 
-            minersettings = new MinerSettings(nodeSettings);
-
+            var minersettings = new MinerSettings(nodeSettings);
+            
             // Values assigned maximum
             Assert.Equal(nodeSettings.Network.Consensus.Options.MaxBlockSerializedSize, minersettings.BlockDefinitionOptions.BlockMaxSize);
             Assert.Equal(nodeSettings.Network.Consensus.Options.MaxBlockWeight, minersettings.BlockDefinitionOptions.BlockMaxWeight);
         }
+
+
     }	
 } 
