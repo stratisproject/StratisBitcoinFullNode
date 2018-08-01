a155f9a3c0fb85cae405d1cab326a7da216891ad
bokobza <bokobza@gmail.com>
2018-08-01 18:07:28 +0100

    Added documentation on MinRelayTxFee and MinTxFee

--- src/NBitcoin/Network.cs
+++ src/NBitcoin/Network.cs
@@ -59,6 +59,11 @@ namespace NBitcoin
         /// Be careful setting this: if you set it to zero then a transaction spammer can cheaply fill blocks using
         /// 1-satoshi-fee transactions. It should be set above the real cost to you of processing a transaction.
         /// </summary>
+        /// <remarks>
+        /// The <see cref="MinRelayTxFee"/> and <see cref="MinTxFee"/> are typically the same value to prevent dos attacks on the network. 
+        /// If <see cref="MinRelayTxFee"/> is less than <see cref="MinTxFee"/>, an attacker can broadcast a lot of transactions with fees between these two values, 
+        /// which will lead to transactions filling the mempool without ever being mined.
+        /// </remarks>
         public long MinTxFee { get; protected set; }
 
         /// <summary>
@@ -69,6 +74,11 @@ namespace NBitcoin
         /// <summary>
         /// The minimum fee under which transactions may be rejected from being relayed.
         /// </summary>
+        /// <remarks>
+        /// The <see cref="MinRelayTxFee"/> and <see cref="MinTxFee"/> are typically the same value to prevent dos attacks on the network. 
+        /// If <see cref="MinRelayTxFee"/> is less than <see cref="MinTxFee"/>, an attacker can broadcast a lot of transactions with fees between these two values, 
+        /// which will lead to transactions filling the mempool without ever being mined.
+        /// </remarks>
         public long MinRelayTxFee { get; protected set; }
 
         /// <summary>
