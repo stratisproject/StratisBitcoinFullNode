bd6fc6d775b5f2b912c3cbce64b7fdb86db1723c
Kevin Loubser <zeptin@gmail.com>
2018-08-02 19:04:06 +0200

    Remove unnecessary param

--- src/Stratis.Bitcoin.Features.Consensus/Rules/CommonRules/HeaderVersionRule.cs
+++ src/Stratis.Bitcoin.Features.Consensus/Rules/CommonRules/HeaderVersionRule.cs
@@ -18,14 +18,13 @@ namespace Stratis.Bitcoin.Features.Consensus.Rules.CommonRules
         /// current set of BIP9 deployments defined in the consensus.
         /// </summary>
         /// <param name="prevChainedHeader">The header of the previous block in the chain.</param>
-        /// <param name="consensus">The current network's consensus parameters.</param>
         /// <remarks>This method is currently used during block creation only. Different nodes may not implement
         /// BIP9, or may disagree about what the current valid set of deployments are. It is therefore not strictly
         /// possible to validate a block version number in anything more than general terms.</remarks>
-        public int ComputeBlockVersion(ChainedHeader prevChainedHeader, NBitcoin.Consensus consensus)
+        public int ComputeBlockVersion(ChainedHeader prevChainedHeader)
         {
             uint version = ThresholdConditionCache.VersionbitsTopBits;
-            var thresholdConditionCache = new ThresholdConditionCache(consensus);
+            var thresholdConditionCache = new ThresholdConditionCache(this.Parent.Network.Consensus);
 
             IEnumerable<BIP9Deployments> deployments = Enum.GetValues(typeof(BIP9Deployments)).OfType<BIP9Deployments>();
 
