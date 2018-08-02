2657195432bc18dbdb264372f4eed72e803a8912
Francois de la Rouviere <fassadlr@gmail.com>
2018-08-02 14:51:52 +0100

    Fix network

--- src/Stratis.Bitcoin.Features.SmartContracts/Networks/SmartContractsRegTest.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/Networks/SmartContractsRegTest.cs
@@ -71,7 +71,7 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Networks
                 maxBlockSigopsCost: 20_000
                 );
 
-            Assert(this.Consensus.HashGenesisBlock == uint256.Parse("93867319cf92c86f957a9652c1fbe7cc8cbe70c53a915ac96ee7c59cb80f94b4"));
+            Assert(consensus.HashGenesisBlock == uint256.Parse("93867319cf92c86f957a9652c1fbe7cc8cbe70c53a915ac96ee7c59cb80f94b4"));
 
             this.Consensus = consensus;
 
