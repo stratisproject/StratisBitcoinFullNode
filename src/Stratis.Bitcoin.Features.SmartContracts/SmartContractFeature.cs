2a7066bed96de4c9d691f53ce35a5b494f64b4ec
Jandrews <jordandrews@hotmail.com>
2018-08-06 13:16:00 +1000

    Remoe unnecessary files

--- src/Stratis.Bitcoin.Features.SmartContracts/SmartContractFeature.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/SmartContractFeature.cs
@@ -193,7 +193,7 @@ namespace Stratis.Bitcoin.Features.SmartContracts
                         services.AddSingleton<IPowMining, PowMining>();
                         services.AddSingleton<IBlockProvider, SmartContractBlockProvider>();
                         services.AddSingleton<BlockDefinition, SmartContractBlockDefinition>();
-                        services.AddSingleton<MiningApiController>();
+                        services.AddSingleton<MiningController>();
                         services.AddSingleton<MiningRpcController>();
                         services.AddSingleton<MinerSettings>();
                     });
@@ -223,9 +223,10 @@ namespace Stratis.Bitcoin.Features.SmartContracts
                         services.AddSingleton<IBlockProvider, SmartContractBlockProvider>();
                         services.AddSingleton<BlockDefinition, SmartContractBlockDefinition>();
                         services.AddSingleton<BlockDefinition, SmartContractPosPowBlockDefinition>();
-                        services.AddSingleton<MiningApiController>();
                         services.AddSingleton<MiningRpcController>();
                         services.AddSingleton<MiningController>();
+                        services.AddSingleton<StakingController>();
+                        services.AddSingleton<StakingRpcController>();
                         services.AddSingleton<MinerSettings>();
                     });
             });
