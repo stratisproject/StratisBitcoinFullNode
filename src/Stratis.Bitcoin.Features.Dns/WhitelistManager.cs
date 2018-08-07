ad18f6a2ff65936a98cac31a0a3de9b722dfcf53
majikandy <github@andy-davis.co.uk>
2018-08-07 10:12:16 +0100

    Logging line when banned doesn't get added to masterfile.

--- src/Stratis.Bitcoin.Features.Dns/WhitelistManager.cs
+++ src/Stratis.Bitcoin.Features.Dns/WhitelistManager.cs
@@ -120,6 +120,7 @@ namespace Stratis.Bitcoin.Features.Dns
             {
                 if (this.peerBanning.IsBanned(whitelistEntry.Endpoint))
                 {
+                    this.logger.LogDebug("{0} is banned, therefore removing from masterfile.");
                     continue;
                 }
 
