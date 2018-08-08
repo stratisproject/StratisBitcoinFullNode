231b557aee2fb335fed57bd19910c7acb04025a7
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-08-08 12:41:50 +1000

    Add AddressGenerator implementation

---
+++ src/Stratis.SmartContracts.Executor.Reflection/AddressGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using NBitcoin;
+
+namespace Stratis.SmartContracts.Executor.Reflection
+{
+    public class AddressGenerator : IAddressGenerator
+    {
+        public uint160 GenerateAddress(ITransactionContext context)
+        {
+            uint256 hash = context.TransactionHash;
+            ulong nonce = context.GetNonceAndIncrement();
+            byte[] toHash = hash.ToBytes().Concat(new uint256(nonce).ToBytes()).ToArray();
+            return new uint160(HashHelper.Keccak256(toHash).Take(20).ToArray());
+        }
+    }
+}
