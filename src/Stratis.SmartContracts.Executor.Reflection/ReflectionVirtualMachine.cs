ccd88c59dc77bcdd733e6da44804bff41b4e6e82
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-08-11 23:51:35 +1000

    Remove unused reference

--- src/Stratis.SmartContracts.Executor.Reflection/ReflectionVirtualMachine.cs
+++ src/Stratis.SmartContracts.Executor.Reflection/ReflectionVirtualMachine.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.Serialization;
 using System.Text;
 using CSharpFunctionalExtensions;
 using Microsoft.Extensions.Logging;
