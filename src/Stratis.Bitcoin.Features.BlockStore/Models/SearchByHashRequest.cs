b1b81f0536f98ae7eeb4fb5761e5dffb7db76a05
SondreB <pixelworld@hotmail.com>
2018-08-15 12:42:41 +0200

    Implement support for returning transaction details in block #1912 (#1913)
    - Add verbose query input for block API, to return transaction details.

--- src/Stratis.Bitcoin.Features.BlockStore/Models/SearchByHashRequest.cs
+++ src/Stratis.Bitcoin.Features.BlockStore/Models/SearchByHashRequest.cs
@@ -11,5 +11,11 @@ namespace Stratis.Bitcoin.Features.BlockStore.Models
     {
         [Required(AllowEmptyStrings = false)]
         public string Hash { get; set; }
+
+        /// <summary>
+        /// Indicates if the Transactions should be returned with all details, or simply be returned as an <see cref="string[]"/> with hashes (txids).
+        /// </summary>
+        /// <remarks>This is not considered when <see cref="RequestBase.OutputJson"/> is set to false.</remarks>
+        public bool ShowTransactionDetails { get; set; }
     }
 }
