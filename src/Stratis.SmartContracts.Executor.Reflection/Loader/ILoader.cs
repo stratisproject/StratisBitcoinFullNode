042ea64471816b2620e304fa6f9ee76d9774ad78
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-08-15 16:40:16 +1000

    Define ILoader and IContractAssembly

---
+++ src/Stratis.SmartContracts.Executor.Reflection/Loader/ILoader.cs
@@ -0,0 +1,9 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace Stratis.SmartContracts.Executor.Reflection.Loader
+{
+    public interface ILoader
+    {
+        Result<IContractAssembly> Load(ContractByteCode bytes);
+    }
+}
