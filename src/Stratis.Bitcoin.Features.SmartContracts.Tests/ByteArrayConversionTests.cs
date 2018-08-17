fa87e6ea4647fd48bd97fab1f5313347f36250d2
Jandrews <jordandrews@hotmail.com>
2018-08-17 15:23:48 +1000

    ulongs and longs

--- src/Stratis.Bitcoin.Features.SmartContracts.Tests/ByteArrayConversionTests.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts.Tests/ByteArrayConversionTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using Xunit;
 
 namespace Stratis.Bitcoin.Features.SmartContracts.Tests
@@ -37,6 +38,34 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
             Assert.Equal(expectedBytes, actualBytes);
         }
 
+        [Fact]
+        public void ULongConversion()
+        {
+            // TODO: Test more values.
+            byte[] max = new byte[] { 255, 255, 255, 255, 255, 255, 255, 255 };
+            ulong expected = BitConverter.ToUInt64(max);
+            ulong actual = ByteArrayConversion.BytesToULong(max);
+            Assert.Equal(expected, actual);
 
+            byte[] expectedBytes = BitConverter.GetBytes(ulong.MaxValue);
+            byte[] actualBytes = ByteArrayConversion.ULongToBytes(ulong.MaxValue);
+            Assert.Equal(expectedBytes, actualBytes);
+        }
+
+        [Fact]
+        public void LongConversion()
+        {
+            // TODO: Test more values.
+            byte[] max = new byte[] { 255, 255, 255, 255, 255, 255, 255, 255 };
+            long expected = BitConverter.ToInt64(max);
+            long actual = ByteArrayConversion.BytesToLong(max);
+            Assert.Equal(expected, actual);
+
+            byte[] expectedBytes = BitConverter.GetBytes(long.MaxValue);
+            byte[] actualBytes = ByteArrayConversion.LongToBytes(long.MaxValue);
+            Assert.Equal(expectedBytes, actualBytes);
+        }
+
+        
     }
 }
--- src/Stratis.Bitcoin.Features.SmartContracts.Tests/SmartContracts/ByteArrayConversion.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts.Tests/SmartContracts/ByteArrayConversion.cs
@@ -16,6 +16,40 @@ public class ByteArrayConversion : SmartContract
         return (uint) value[0] | (uint) (value[1] << 8) | (uint) (value[2] << 16) | (uint) (value[3] << 24);
     }
 
+    public static ulong BytesToULong(byte[] value)
+    {
+        return (ulong) value[0] 
+            | (ulong)(value[1] << 8) 
+            | (ulong)(value[2] << 16) 
+            | (ulong)(value[3] << 24) 
+            | (ulong)(value[4] << 32)
+            | (ulong)(value[5] << 40)
+            | (ulong)(value[6] << 48)
+            | (ulong)(value[7] << 56);
+    }
+
+    public static long BytesToLong(byte[] value)
+    {
+        return (long)value[0]
+            | (long)(value[1] << 8)
+            | (long)(value[2] << 16)
+            | (long)(value[3] << 24)
+            | (long)(value[4] << 32)
+            | (long)(value[5] << 40)
+            | (long)(value[6] << 48)
+            | (long)(value[7] << 56);
+    }
+
+    public static string BytesToString(byte[] value)
+    {
+        string ret = "";
+        for(int i = 0; i < value.Length; i++)
+        {
+            ret += value[i]; 
+        }
+        return ret;
+    }
+
     public static byte[] IntToBytes(int value)
     {
         unchecked
@@ -40,5 +74,49 @@ public class ByteArrayConversion : SmartContract
             bytes[3] = (byte)(value >> 24);
             return bytes;
         }
+    }
+
+    public static byte[] ULongToBytes(ulong value)
+    {
+        unchecked
+        {
+            byte[] bytes = new byte[8];
+            bytes[0] = (byte)value;
+            bytes[1] = (byte)(value >> 8);
+            bytes[2] = (byte)(value >> 16);
+            bytes[3] = (byte)(value >> 24);
+            bytes[4] = (byte)(value >> 32);
+            bytes[5] = (byte)(value >> 40);
+            bytes[6] = (byte)(value >> 48);
+            bytes[7] = (byte)(value >> 56);
+            return bytes;
+        }
+    }
+
+    public static byte[] LongToBytes(long value)
+    {
+        unchecked
+        {
+            byte[] bytes = new byte[8];
+            bytes[0] = (byte)value;
+            bytes[1] = (byte)(value >> 8);
+            bytes[2] = (byte)(value >> 16);
+            bytes[3] = (byte)(value >> 24);
+            bytes[4] = (byte)(value >> 32);
+            bytes[5] = (byte)(value >> 40);
+            bytes[6] = (byte)(value >> 48);
+            bytes[7] = (byte)(value >> 56);
+            return bytes;
+        }
+    }
+
+    public static byte[] StringToBytes(string value)
+    {
+        byte[] ret = new byte[value.Length];
+        for(int i=0; i< value.Length; i++)
+        {
+            ret[i] = (byte) value[i];
+        }
+        return ret;
     }
 }
