e8362a99cd699b3f20fd0fe9fc423a465a5eca9d
Jordan Andrews <jordandrews@hotmail.com>
2018-08-17 14:24:42 +1000

    [SC] Creating contracts implementation (#1923)

--- src/Stratis.Bitcoin.IntegrationTests/SmartContracts/TransferTest.cs
+++ src/Stratis.Bitcoin.IntegrationTests/SmartContracts/TransferTest.cs
@@ -10,18 +10,18 @@ public class TransferTest : SmartContract
 
     public void Test()
     {
-        TransferFunds(new Address("mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn"), 100);
+        Transfer(new Address("mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn"), 100);
     }
 
     public void Test2()
     {
-        TransferFunds(new Address("mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn"), 100);
-        TransferFunds(new Address("n2hyJZj9m8jorD21Nss1tbUtR1NthNHEzg"), 100);
+        Transfer(new Address("mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn"), 100);
+        Transfer(new Address("n2hyJZj9m8jorD21Nss1tbUtR1NthNHEzg"), 100);
     }
 
     public void P2KTest()
     {
-        TransferFunds(new Address("mxKorCkWmtrPoekfWiMzERJPhaT13nnkMy"), 100);
+        Transfer(new Address("mxKorCkWmtrPoekfWiMzERJPhaT13nnkMy"), 100);
     }
 
     public bool DoNothing()
