1b92df749bc163c4c13b7970a9713f1c38f91a87
Jandrews <jordandrews@hotmail.com>
2018-08-18 21:50:26 +1000

    skeleton bones

---
+++ src/Stratis.SmartContracts.Core/Receipts/IReceiptRepository.cs
@@ -0,0 +1,27 @@
+﻿using NBitcoin;
+
+namespace Stratis.SmartContracts.Core.Receipts
+{
+    public interface IReceiptRepository
+    {
+        /*
+         *Validating a block:
+
+          For each transaction:
+            - Generate a receipt
+            - Store receipt in memory
+
+          Hash all receipts, and generate merkle root.
+          Ensure that merkle root matches block receipt root.
+          Store receipt in DBreeze.
+
+          TODO: Worry about pruning old data in case of chain reorg. 
+          TODO: Worry about indexing. 
+         * 
+         */
+
+        Receipt GetReceipt(uint256 txHash);
+
+        void SaveReceipt(Receipt receipt);
+    }
+}
---
+++ src/Stratis.SmartContracts.Core/Receipts/Receipt.cs
@@ -0,0 +1,43 @@
+﻿using NBitcoin;
+
+namespace Stratis.SmartContracts.Core.Receipts
+{
+    public class Receipt
+    {
+        public uint256 TransactionId { get; }
+
+        public uint256 BlockHash { get; }
+
+        public uint160 Sender { get; }
+
+        public uint160 To { get; }
+
+        public uint160 NewContractAddress { get; }
+
+        public ulong GasUsed { get; }
+
+        public bool Success { get; }
+
+        public byte[] ReturnValue { get; }
+
+        public Receipt(
+            uint256 transactionId,
+            uint256 blockHash,
+            uint160 sender,
+            uint160 to,
+            uint160 newContractAddress,
+            ulong gasUsed,
+            bool success,
+            byte[] returnValue)
+        {
+            this.TransactionId = transactionId;
+            this.BlockHash = blockHash;
+            this.Sender = sender;
+            this.To = to;
+            this.NewContractAddress = newContractAddress;
+            this.GasUsed = gasUsed;
+            this.Success = success;
+            this.ReturnValue = returnValue;
+        }
+    }
+}
