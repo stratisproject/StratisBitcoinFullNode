b591321b08803dc274a3af00367ebca234b0f490
Jandrews <jordandrews@hotmail.com>
2018-08-19 22:17:44 +1000

    Receipt Serialization

---
+++ src/Stratis.Bitcoin.Features.SmartContracts.Tests/Receipts/ReceiptSerializationTest.cs
@@ -0,0 +1,52 @@
+﻿using NBitcoin;
+using Stratis.SmartContracts.Core.Receipts;
+using Xunit;
+
+namespace Stratis.Bitcoin.Features.SmartContracts.Tests.Receipts
+{
+    public class ReceiptSerializationTest
+    {
+        [Fact]
+        public void Receipt_Serializes_And_Deserializes()
+        {
+            // create - to is null.
+            var createReceipt = new Receipt(
+                new uint256(123),
+                new uint256(1234),
+                new uint160(12345),
+                null,
+                new uint160(12347),
+                10_000,
+                true,
+                null);
+
+            TestSerializeReceipt(createReceipt);
+
+            // call - newcontract is null.
+            var callReceipt = new Receipt(
+                new uint256(123),
+                new uint256(1234),
+                new uint160(12345),
+                new uint160(12347),
+                null,
+                10_000,
+                true,
+                null);
+
+            TestSerializeReceipt(callReceipt);
+        }
+
+        private void TestSerializeReceipt(Receipt receipt)
+        {
+            byte[] serialized = receipt.ToBytesRlp();
+            Receipt deserialized = Receipt.FromBytesRlp(serialized);
+            Assert.Equal(receipt.BlockHash, deserialized.BlockHash);
+            Assert.Equal(receipt.GasUsed, deserialized.GasUsed);
+            Assert.Equal(receipt.NewContractAddress, deserialized.NewContractAddress);
+            Assert.Equal(receipt.ReturnValue, deserialized.ReturnValue);
+            Assert.Equal(receipt.Sender, deserialized.Sender);
+            Assert.Equal(receipt.To, deserialized.To);
+            Assert.Equal(receipt.TransactionId, deserialized.TransactionId);
+        }
+    }
+}
--- src/Stratis.SmartContracts.Core/Receipts/Receipt.cs
+++ src/Stratis.SmartContracts.Core/Receipts/Receipt.cs
@@ -1,4 +1,7 @@
-﻿using NBitcoin;
+﻿using System;
+using NBitcoin;
+using Nethereum.RLP;
+using Stratis.SmartContracts.Core.Hashing;
 
 namespace Stratis.SmartContracts.Core.Receipts
 {
@@ -38,6 +41,41 @@ namespace Stratis.SmartContracts.Core.Receipts
             this.GasUsed = gasUsed;
             this.Success = success;
             this.ReturnValue = returnValue;
+        }
+
+        public byte[] ToBytesRlp()
+        {
+            return RLP.EncodeList(
+                RLP.EncodeElement(this.TransactionId.ToBytes()),
+                RLP.EncodeElement(this.BlockHash.ToBytes()),
+                RLP.EncodeElement(this.Sender.ToBytes()),
+                RLP.EncodeElement(this.To?.ToBytes()),
+                RLP.EncodeElement(this.NewContractAddress?.ToBytes()),
+                RLP.EncodeElement(BitConverter.GetBytes(this.GasUsed)),
+                RLP.EncodeElement(BitConverter.GetBytes(this.Success)),
+                RLP.EncodeElement(this.ReturnValue)
+            );
+        }
+
+        public static Receipt FromBytesRlp(byte[] bytes)
+        {
+            RLPCollection list = RLP.Decode(bytes);
+            RLPCollection innerList = (RLPCollection)list[0];
+            return new Receipt(
+                new uint256(innerList[0].RLPData),
+                new uint256(innerList[1].RLPData),
+                new uint160(innerList[2].RLPData),
+                innerList[3].RLPData == null ? null : new uint160(innerList[3].RLPData),
+                innerList[4].RLPData == null ? null : new uint160(innerList[4].RLPData),
+                BitConverter.ToUInt64(innerList[5].RLPData),
+                BitConverter.ToBoolean(innerList[6].RLPData),
+                innerList[7].RLPData
+            );
+        }
+
+        public uint256 GetHash()
+        {
+            return new uint256(HashHelper.Keccak256(ToBytesRlp()));
         }
     }
 }
