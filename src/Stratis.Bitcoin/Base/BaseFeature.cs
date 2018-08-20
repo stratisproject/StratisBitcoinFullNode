e593725210113188ff45686778fca51a7ce46c61
noescape0 <noescape0@ya.ru>
2018-08-20 18:38:06 +0300

    load header

--- src/Stratis.Bitcoin/Base/BaseFeature.cs
+++ src/Stratis.Bitcoin/Base/BaseFeature.cs
@@ -237,7 +237,8 @@ namespace Stratis.Bitcoin.Base
             await this.finalizedBlockInfo.LoadFinalizedBlockInfoAsync(this.network).ConfigureAwait(false);
 
             this.logger.LogInformation("Loading chain");
-            await this.chainRepository.LoadAsync(this.chain).ConfigureAwait(false);
+            ChainedHeader chainTip = await this.chainRepository.LoadAsync(this.chain.Genesis).ConfigureAwait(false);
+            this.chain.SetTip(chainTip);
 
             this.logger.LogInformation("Chain loaded at height " + this.chain.Height);
 
