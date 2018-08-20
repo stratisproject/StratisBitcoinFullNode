e9e32a7a3fe059f9b840d81b22b8f92411a2688c
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-08-20 14:52:24 +1000

    Add module def reader implementation

---
+++ src/Stratis.SmartContracts.Executor.Reflection/Compilation/ContractModuleDefinitionReader.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Cecil;
+
+namespace Stratis.SmartContracts.Executor.Reflection.Compilation
+{
+    public class ContractModuleDefinitionReader : IContractModuleDefinitionReader
+    {
+        /// <inheritdoc />
+        public IContractModuleDefinition Read(byte[] bytes, IAssemblyResolver assemblyResolver = null)
+        {
+            return SmartContractDecompiler.GetModuleDefinition(bytes, assemblyResolver);
+        }
+    }
+}
