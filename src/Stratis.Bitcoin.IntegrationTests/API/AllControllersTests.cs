ef0748d4ebe21f13d7ab237f3250f7b2e5455d24
Matthieu Le Berre <monsieur.leberre@gmail.com>
2018-08-22 16:28:56 +0100

    check all posts have parameters

---
+++ src/Stratis.Bitcoin.IntegrationTests/API/AllControllersTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using FluentAssertions;
+using FluentAssertions.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Stratis.Bitcoin.Features.Api;
+using Xunit;
+
+namespace Stratis.Bitcoin.IntegrationTests.API
+{
+    public class AllControllersTests
+    {
+        [Fact]
+        public void AllPostMethodsShouldHaveBody()
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var controllers = assemblies.SelectMany(a => a.GetTypes().Where(t => t.Implements(typeof(Controller))));
+
+            foreach (var controller in controllers)
+            {
+                var postMethods = controller.GetMethods()
+                    .Where(m => m.CustomAttributes.Any(a => a.AttributeType == typeof(HttpPostAttribute)))
+                    .ToList();
+                postMethods.ForEach(mi => mi.GetParameters().Count().Should().BeGreaterOrEqualTo(1, $"HttpPost method {mi.Name} should have at least one parameter to prevent CORS calls from executing it."));
+            }
+        }
+    }
+}
