2abada3e235acf6da5b9213820539c80fb0b4b3a
Andy Davis <github@andy-davis.co.uk>
2018-08-22 18:21:42 +0900

    [Activation] Block to chainedheaderblock (#1966)
    * type changed
    
    * undo accidental commit
    
    * rename variable
    
    * Block to ChainedHeaderBlock in BlockConnected Signal
    
    * removed unneccessary get block header from chain
    
    * Revert named method and nesting reduction
    
    * Log whole chainheaderblock (.ToString())
    
    * Block log reverted to gethash
    
    * block param rename

--- src/Stratis.Bitcoin/Consensus/ConsensusManager.cs
+++ src/Stratis.Bitcoin/Consensus/ConsensusManager.cs
@@ -664,8 +664,7 @@ namespace Stratis.Bitcoin.Consensus
                     }
                 }
 
-                // TODO: change signal to take ChainedHeaderBlock
-                this.signals.SignalBlockConnected(blockToConnect.Block);
+                this.signals.SignalBlockConnected(blockToConnect);
             }
 
             this.logger.LogTrace("(-):'{0}'", connectBlockResult);
