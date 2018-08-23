367b143372fbd72050701e167418cd6853671dfd
Maciej Zaleski <maciej.zaleski@stratisplatform.com>
2018-08-23 12:53:08 +0100

    Fixed DateTimeProviderTest

--- src/Stratis.Bitcoin.Tests/Utilities/DateTimeProviderTest.cs
+++ src/Stratis.Bitcoin.Tests/Utilities/DateTimeProviderTest.cs
@@ -10,9 +10,11 @@ namespace Stratis.Bitcoin.Tests.Utilities
         [Fact]
         public void GetUtcNowReturnsCurrentUtcDateTime()
         {
+            int timeOffset = -96;
+            DateTimeProvider.Default.SetSystemTimeOffset(timeOffset);
             DateTime result = DateTimeProvider.Default.GetUtcNow();
 
-            Assert.Equal(DateTime.UtcNow.ToString("yyyyMMddhhmmss"), result.ToString("yyyyMMddhhmmss"));
+            Assert.Equal(DateTime.UtcNow.AddSeconds(timeOffset).ToString("yyyyMMddhhmmss"), result.ToString("yyyyMMddhhmmss"));
         }
 
         [Fact]
