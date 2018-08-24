847d38ef3d99983ed0abc6997f9af3af63c691ee
Matthieu Le Berre <monsieur.leberre@gmail.com>
2018-08-24 15:36:13 +0100

    removed unused "using"

--- src/Stratis.Bitcoin.Features.Miner/Controllers/StakingController.cs
+++ src/Stratis.Bitcoin.Features.Miner/Controllers/StakingController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Security;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NBitcoin;
