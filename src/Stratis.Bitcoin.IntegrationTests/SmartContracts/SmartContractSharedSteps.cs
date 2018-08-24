86d69d42449e91401a2cb4884c4c51ce20f823f3
Lukasz Krol <lukasz.krol@stratisplatform.com>
2018-08-24 10:40:52 +0100

    Revert "Increased time to spin on mempoool sync in SmartContracts shared, increased detail on error"
    This reverts commit 15b4172678dfdb860a296540954bc9c59d5c3408.

--- src/Stratis.Bitcoin.IntegrationTests/SmartContracts/SmartContractSharedSteps.cs
+++ src/Stratis.Bitcoin.IntegrationTests/SmartContracts/SmartContractSharedSteps.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Threading;
-using Stratis.Bitcoin.Features.SmartContracts.Wallet;
+﻿using Stratis.Bitcoin.Features.SmartContracts.Wallet;
 using Stratis.Bitcoin.Features.Wallet.Models;
 using Stratis.Bitcoin.IntegrationTests.Common;
 using Stratis.Bitcoin.IntegrationTests.Common.EnvironmentMockUpHelpers;
@@ -15,12 +13,7 @@ namespace Stratis.Bitcoin.IntegrationTests.SmartContracts
             {
                 Hex = responseHex
             });
-
-            TimeSpan duration = TimeSpan.FromSeconds(180);
-
-            TestHelper.WaitLoop(() => scReceiver.CreateRPCClient().GetRawMempool().Length > 0,
-                cancellationToken: new CancellationTokenSource(duration).Token,
-                failureReason: $"Failed to retrive Mempool size > 0 whihin {duration}");
+            TestHelper.WaitLoop(() => scReceiver.CreateRPCClient().GetRawMempool().Length > 0);
 
             scReceiver.GenerateStratisWithMiner(2);
             TestHelper.WaitLoop(() => TestHelper.AreNodesSynced(scReceiver, scSender));
