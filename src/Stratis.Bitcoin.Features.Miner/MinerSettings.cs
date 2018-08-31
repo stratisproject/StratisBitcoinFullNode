6e2caeba48c0be0ca62b3f0beef7745cb95172fb
Jandrews <jordandrews@hotmail.com>
2018-08-31 13:45:18 +1000

    Remove MinerSettings virtual

--- src/Stratis.Bitcoin.Features.Miner/MinerSettings.cs
+++ src/Stratis.Bitcoin.Features.Miner/MinerSettings.cs
@@ -64,7 +64,7 @@ namespace Stratis.Bitcoin.Features.Miner
         /// <summary>
         /// Settings for <see cref="BlockDefinition"/>.
         /// </summary>
-        public virtual BlockDefinitionOptions BlockDefinitionOptions { get; }
+        public BlockDefinitionOptions BlockDefinitionOptions { get; }
 
         /// <summary>
         /// Initializes an instance of the object from the default configuration.
