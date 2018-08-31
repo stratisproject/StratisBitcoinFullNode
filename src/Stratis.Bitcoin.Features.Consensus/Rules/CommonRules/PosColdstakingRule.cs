9795c3dd50ad8c51b15ff8d61b970f985b4a2a10
quantumagi <someguy.fromafrica@gmail.com>
2018-08-31 16:08:36 +1000

    Fis missing using

--- src/Stratis.Bitcoin.Features.Consensus/Rules/CommonRules/PosColdstakingRule.cs
+++ src/Stratis.Bitcoin.Features.Consensus/Rules/CommonRules/PosColdstakingRule.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using NBitcoin;
 using Stratis.Bitcoin.Base.Deployments;
+using Stratis.Bitcoin.Consensus;
 using Stratis.Bitcoin.Consensus.Rules;
 namespace Stratis.Bitcoin.Features.Consensus.Rules.CommonRules
 {
