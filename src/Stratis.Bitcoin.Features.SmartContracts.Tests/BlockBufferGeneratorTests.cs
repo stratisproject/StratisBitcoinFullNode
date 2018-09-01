07f53ddadf0fee96711b1f81e9e9679b9533b4d4
Jandrews <jordandrews@hotmail.com>
2018-09-01 14:46:48 +1000

    naming

--- src/Stratis.Bitcoin.Features.SmartContracts.Tests/BlockBufferGeneratorTests.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts.Tests/BlockBufferGeneratorTests.cs
@@ -7,18 +7,18 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
 {
     public class BlockBufferGeneratorTests
     {
-        private readonly BlockBufferGenerator bufferGen;
+        private readonly BlockBufferGenerator bufferGenerator;
 
         public BlockBufferGeneratorTests()
         {
-            this.bufferGen = new BlockBufferGenerator();
+            this.bufferGenerator = new BlockBufferGenerator();
         }
 
         [Fact]
         public void Buffer_50Kb_For_1MB_BlockSize()
         {
             BlockDefinitionOptions optionsFromNetwork = new MinerSettings(new NodeSettings(new SmartContractsTest())).BlockDefinitionOptions;
-            BlockDefinitionOptions newOptions = this.bufferGen.GetOptionsWithBuffer(optionsFromNetwork);
+            BlockDefinitionOptions newOptions = this.bufferGenerator.GetOptionsWithBuffer(optionsFromNetwork);
 
             Assert.Equal((uint) 950_000, newOptions.BlockMaxWeight);
             Assert.Equal((uint) 950_000, newOptions.BlockMaxSize);
--- src/Stratis.Bitcoin.Features.SmartContracts/BlockBufferGenerator.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/BlockBufferGenerator.cs
@@ -6,8 +6,7 @@ namespace Stratis.Bitcoin.Features.SmartContracts
     {
         public BlockDefinitionOptions GetOptionsWithBuffer(BlockDefinitionOptions options)
         {
-            uint percentageBuffer = 5; // For 1MB blocks, 50 KB reserved for generated transactions / txouts
-            uint percentageToBuild = 100 - percentageBuffer;
+            uint percentageToBuild = 95; // For 1MB blocks, 50 KB reserved for generated transactions / txouts
             return new BlockDefinitionOptions(options.BlockMaxSize * percentageToBuild / 100, options.BlockMaxWeight * percentageToBuild / 100);
         }
     }
