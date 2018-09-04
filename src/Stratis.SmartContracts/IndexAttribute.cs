d3749d7b4fef42e028026d770831bfdf76c48b59
Jandrews <jordandrews@hotmail.com>
2018-09-04 11:18:28 +1000

    Docs

--- src/Stratis.SmartContracts/IndexAttribute.cs
+++ src/Stratis.SmartContracts/IndexAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace Stratis.SmartContracts
 {
+    /// <summary>
+    /// Specifies that the value of a field can be used to search for receipts.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Field)]
     public class IndexAttribute : Attribute
     {
