6d223544c7575ee31ec71fa2e6ebdb0a021238a5
quantumagi <someguy.fromafrica@gmail.com>
2018-09-14 11:23:49 +1000

    Add ColdStakingScriptSigParameters

---
+++ src/Stratis.Bitcoin.Features.ColdStaking/ColdStakingScriptSigParameters.cs
@@ -0,0 +1,19 @@
+﻿using NBitcoin;
+
+namespace Stratis.Bitcoin.Features.ColdStaking
+{
+    public class ColdStakingScriptSigParameters : IDestination
+    {
+        public TransactionSignature TransactionSignature { get; set; }
+        public bool ColdPublicKey { get; set; }
+        public PubKey PublicKey { get; set; }
+
+        public virtual TxDestination Hash { get { return this.PublicKey.Hash; } }
+
+        #region IDestination Members
+
+        public Script ScriptPubKey { get { return this.Hash.ScriptPubKey; } }
+
+        #endregion
+    }
+}
