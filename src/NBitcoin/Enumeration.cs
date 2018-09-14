649ac14552f0adfea2fbf813cbf8ea4648354879
ferdeen <ferdeen@gmail.com>
2018-09-14 14:33:17 +0100

    Incorrect namespace.

--- src/NBitcoin/Enumeration.cs
+++ src/NBitcoin/Enumeration.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace Stratis.Bitcoin.Utilities
+namespace NBitcoin
 {
     public abstract class Enumeration : IComparable
     {
