d1c909b5d7c0732601002382808d823fa719a61f
Igor Goldobin <igor.goldobin@gmail.com>
2018-09-17 23:13:31 +1000

    Removed unused class

--- src/Stratis.Bitcoin/P2P/Protocol/Payloads/ProvenHeadersPayload.cs
+++ src/Stratis.Bitcoin/P2P/Protocol/Payloads/ProvenHeadersPayload.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using NBitcoin;
-using NBitcoin.Protocol;
 
 namespace Stratis.Bitcoin.P2P.Protocol.Payloads
 {
@@ -11,34 +9,9 @@ namespace Stratis.Bitcoin.P2P.Protocol.Payloads
     [Payload("provhdr")]
     public class ProvenHeadersPayload : Payload
     {
-        private class ProvenBlockHeaderWithTxCount : IBitcoinSerializable
-        {
-            internal ProvenBlockHeader Header;
+        private List<BlockHeader> headers = new List<BlockHeader>();
 
-            public ProvenBlockHeaderWithTxCount()
-            {
-            }
-
-            public ProvenBlockHeaderWithTxCount(ProvenBlockHeader header)
-            {
-                this.Header = header;
-            }
-
-            public void ReadWrite(BitcoinStream stream)
-            {
-                stream.ReadWrite(ref this.Header);
-                var txCount = new VarInt(0);
-                stream.ReadWrite(ref txCount);
-
-                // Stratis adds an additional byte to the end of a header need to investigate why.
-                if (stream.ConsensusFactory is PosConsensusFactory)
-                {
-                    stream.ReadWrite(ref txCount);
-                }
-            }
-        }
-
-        public List<ProvenBlockHeader> Headers { get; } = new List<ProvenBlockHeader>();
+        public List<BlockHeader> Headers => this.headers;
 
         public ProvenHeadersPayload()
         {
@@ -51,18 +24,7 @@ namespace Stratis.Bitcoin.P2P.Protocol.Payloads
 
         public override void ReadWriteCore(BitcoinStream stream)
         {
-            if (stream.Serializing)
-            {
-                List<ProvenBlockHeaderWithTxCount> headersOff = this.Headers.Select(h => new ProvenBlockHeaderWithTxCount(h)).ToList();
-                stream.ReadWrite(ref headersOff);
-            }
-            else
-            {
-                this.Headers.Clear();
-                var headersOff = new List<ProvenBlockHeaderWithTxCount>();
-                stream.ReadWrite(ref headersOff);
-                this.Headers.AddRange(headersOff.Select(h => h.Header));
-            }
+            stream.ReadWrite(ref this.headers);
         }
     }
 }
