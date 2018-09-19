4e736a43a4f3c979f6921b248d25bedd0244b143
ferdeen <ferdeen@gmail.com>
2018-09-19 12:34:34 +0100

    Initial interface design.

---
+++ src/Stratis.Bitcoin.Features.Consensus/ProvenBlockHeaders/IProvenBlockHeaderStore.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NBitcoin;
+
+namespace Stratis.Bitcoin.Features.Consensus.ProvenBlockHeaders
+{
+    /// <summary>
+    /// Database of <see cref="ProvenBlockHeader"/>s.
+    /// </summary>
+    public interface IProvenBlockHeaderStore
+    {
+        /// <summary>
+        /// Initializes the database table used by <see cref="ProvenBlockHeader"/>.
+        /// </summary>
+        /// <returns><placeholder>A <see cref="Task"/> representing the asynchronous operation.</placeholder></returns>
+        Task InitializeAsync();
+
+        /// <summary>
+        /// Get a <see cref="ProvenBlockHeader"/> corresponding to a block.
+        /// </summary>
+        /// <param name="blockId">Id used to retrieve the <see cref="ProvenBlockHeader"/>.</param>
+        /// <param name="cancellationToken">Cancellation that allows aborting the operation at any stage.</param>
+        /// <returns><see cref="ProvenBlockHeader"/> retrieved from the <see cref="ProvenBlockHeaderStore"/>.</returns>
+        Task<ProvenBlockHeader> GetAsync(uint256 blockId, CancellationToken cancellationToken = default(CancellationToken));
+
+        /// <summary>
+        /// Retrieves the <see cref="ProvenBlockHeader"/> of the current tip.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation that allows aborting the operation at any stage.</param>
+        /// <returns><see cref="ProvenBlockHeader"/></returns>
+        Task<ProvenBlockHeader> GetTipAsync(CancellationToken cancellationToken = default(CancellationToken));
+
+        /// <summary>
+        /// Adds a <see cref="ProvenBlockHeader"/> to the internal concurrent dictionary. 
+        /// </summary>
+        /// <param name="provenBlockHeader"><see cref="ProvenBlockHeader"/> to add to the internal concurrent dictionary.</param>
+        /// <param name="cancellationToken">Cancellation that allows aborting the operation at any stage.</param>
+        /// <returns><placeholder>A <see cref="Task"/> representing the asynchronous operation.</placeholder></returns>
+        Task SetAsync(ProvenBlockHeader provenBlockHeader, CancellationToken cancellationToken = default(CancellationToken));
+
+        /// <summary>
+        /// Flushes the internal dictionary and saves any new items into the <see cref="ProvenBlockHeaderStore"/>. 
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation that allows aborting the operation at any stage.</param>
+        /// <returns><placeholder>A <see cref="Task"/> representing the asynchronous operation.</placeholder></returns>
+        Task FlushAsync(CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
---
+++ src/Stratis.Bitcoin.Features.Consensus/ProvenBlockHeaders/ProvenBlockHeaderStore.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NBitcoin;
+
+namespace Stratis.Bitcoin.Features.Consensus.ProvenBlockHeaders
+{
+    public class ProvenBlockHeaderStore : IProvenBlockHeaderStore, IDisposable
+    {
+        // TODO
+
+        // Add Database key under which the ProvebBlockHeader current tip is stored
+
+        // Add Instance logger
+
+        // Access to DBreeze database
+
+        // Ad Specification of the network the node runs on - regtest/testnet/mainnet
+
+        // A Hash of the block which is currently the tip of the chain
+
+        // Add performance counter
+
+        public Task InitializeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task FlushAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Pop some items to remove a window of 10 % of the threshold.
+            throw new NotImplementedException();
+        }
+
+        public Task<ProvenBlockHeader> GetAsync(uint256 blockId, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProvenBlockHeader> GetTipAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetAsync(ProvenBlockHeader provenBlockHeader, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
