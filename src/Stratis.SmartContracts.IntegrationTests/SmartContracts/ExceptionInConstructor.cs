0f5110e2821110b7c27b4657a843b6347d6bd447
Jandrews <jordandrews@hotmail.com>
2018-09-24 13:59:46 +1000

    Exception in second constructor

--- src/Stratis.SmartContracts.IntegrationTests/SmartContracts/ExceptionInConstructor.cs
+++ src/Stratis.SmartContracts.IntegrationTests/SmartContracts/ExceptionInConstructor.cs
@@ -4,9 +4,22 @@ public class ExceptionInConstructor : SmartContract
 {
     public ExceptionInConstructor(ISmartContractState smartContractState) : base(smartContractState)
     {
-        // Throws OutOfIndexException
+        // Create log that ultimately shouldn't be persisted due to failure.
+        Log(new ArbitraryLog { Id = 1 });
+
+        // Create transfer that ultimately shouldn't be persisted due to failure.
+        Transfer(new Address("mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn"), 1);
+
+        // Create storage that ultimately shouldn't be persisted due to failure. 
+
+        // Throw (unexpected) OutOfIndexException
         var array = new int[25];
         int value = array[26];
     }
 
+    public struct ArbitraryLog
+    {
+        [Index]
+        public int Id;
+    }
 }
---
+++ src/Stratis.SmartContracts.IntegrationTests/SmartContracts/ExceptionInMethod.cs
@@ -0,0 +1,17 @@
+﻿
+using Stratis.SmartContracts;
+
+public class ExceptionInMethod : SmartContract
+{
+    public ExceptionInMethod(ISmartContractState smartContractState) : base(smartContractState)
+    {
+    }
+
+    public void Method()
+    {
+        // Throws OutOfIndexException
+        var array = new int[25];
+        int value = array[26];
+    }
+}
+
