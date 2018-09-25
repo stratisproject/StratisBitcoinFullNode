53e47c81e1f1e858fd3da4ebc5c3c2de5e9bf46f
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-09-25 16:35:59 +1000

    Add spec for internal call success

--- src/Stratis.Bitcoin.Features.SmartContracts.Tests/InternalExecutorSpecification.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts.Tests/InternalExecutorSpecification.cs
@@ -185,9 +185,51 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
             Assert.Equal(default(Address), result.NewContractAddress);
         }
 
-        [Fact(Skip = "TODO")]
+        [Fact]
         public void Call_StateTransition_Success()
         {
+            ulong amount = 100UL;
+            var to = new Address("Sj2p6ZRHdLvywyi43HYoE4bu2TF1nvavjR");
+            var method = "Test";
+            var parameters = new object[] { };
+            var gasLimit = (Gas)100_000;
+
+            var fixture = new InternalExecutorTestFixture();
+
+            fixture.SetGasMeterLimitAbove(gasLimit);
+
+            StateTransitionResult stateTransitionResult = StateTransitionResult.Ok((Gas)1000, uint160.One, new object());
+
+            fixture.StateProcessor
+                .Setup(sp => sp.Apply(It.IsAny<IState>(), It.IsAny<InternalCallMessage>()))
+                .Returns(stateTransitionResult);
+
+            var internalExecutor = new InternalExecutor(
+                fixture.LoggerFactory,
+                fixture.Network,
+                fixture.State.Object,
+                fixture.StateProcessor.Object);
+
+            ITransferResult result = internalExecutor.Call(fixture.SmartContractState, to, amount, method, parameters, gasLimit);
+
+            fixture.State.Verify(s => s.Snapshot(), Times.Once);
+
+            fixture.StateProcessor.Verify(sp =>
+                sp.Apply(fixture.Snapshot, It.Is<InternalCallMessage>(m =>
+                    m.Amount == amount &&
+                    m.Method.Name == method &&
+                    m.Method.Parameters == parameters &&
+                    m.GasLimit == gasLimit &&
+                    m.From == fixture.FromAddress &&
+                    m.To == to.ToUint160(fixture.Network)
+                )));
+
+            fixture.State.Verify(s => s.TransitionTo(fixture.Snapshot));
+
+            fixture.GasMeter.Verify(g => g.Spend(stateTransitionResult.GasConsumed));
+
+            Assert.True(result.Success);
+            Assert.Equal(stateTransitionResult.Success.ExecutionResult, result.ReturnValue);
         }
 
         [Fact(Skip = "TODO")]
