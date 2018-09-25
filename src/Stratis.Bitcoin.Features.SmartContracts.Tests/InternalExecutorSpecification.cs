0cd5cd54cee9a3404c98c9767940d27548b6e5a8
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-09-25 15:32:33 +1000

    Add empty tests

---
+++ src/Stratis.Bitcoin.Features.SmartContracts.Tests/InternalExecutorSpecification.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+
+namespace Stratis.Bitcoin.Features.SmartContracts.Tests
+{
+    public class InternalExecutorSpecification
+    {
+        [Fact(Skip = "TODO")]
+        public void Create_StateTransition_Success()
+        {
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Create_StateTransition_Error()
+        {
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Create_GasRemaining_Error()
+        {
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Call_StateTransition_Success()
+        {
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Call_StateTransition_Error()
+        {
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Call_GasRemaining_Error()
+        {
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Transfer_StateTransition_Success()
+        {
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Transfer_StateTransition_Error()
+        {
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Transfer_GasRemaining_Error()
+        {
+        }
+    }
+}
