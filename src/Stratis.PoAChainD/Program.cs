4a987ca550550d87fd3cac2e3e8a02daf82c8cf8
noescape0 <noescape0@ya.ru>
2018-09-28 17:45:40 +0300

    Added PoAD project

---
+++ src/Stratis.PoAChainD/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Stratis.Bitcoin;
+using Stratis.Bitcoin.Builder;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Features.Api;
+using Stratis.Bitcoin.Features.Apps;
+using Stratis.Bitcoin.Features.BlockStore;
+using Stratis.Bitcoin.Features.Consensus;
+using Stratis.Bitcoin.Features.MemoryPool;
+using Stratis.Bitcoin.Features.Miner;
+using Stratis.Bitcoin.Features.RPC;
+using Stratis.Bitcoin.Features.Wallet;
+using Stratis.Bitcoin.Utilities;
+
+namespace Stratis.PoAChainD
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                var nodeSettings = new NodeSettings(args: args);
+
+                IFullNode node = new FullNodeBuilder()
+                    .UseNodeSettings(nodeSettings)
+                    .UseBlockStore()
+                    .UsePosConsensus() // TODO POA
+                    .UseMempool()
+                    .UseWallet()
+                    .AddPowPosMining() // TODO POA
+                    .UseApi()
+                    .UseApps()
+                    .AddRPC()
+                    .Build();
+
+                if (node != null)
+                {
+                    await node.RunAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex.ToString());
+            }
+        }
+    }
+}
