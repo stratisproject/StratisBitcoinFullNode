82a78664ee6b4ed99fbc6ef3dc23a6ba3b1e4ee7
noescape0 <noescape0@ya.ru>
2018-09-29 14:57:01 +0300

    key tool and tests for it

---
+++ src/Stratis.Bitcoin.Features.PoA.Tests/KeyToolTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using NBitcoin;
+using Xunit;
+
+namespace Stratis.Bitcoin.Features.PoA.Tests
+{
+    public class KeyToolTests
+    {
+        private readonly KeyTool tool;
+
+        public KeyToolTests()
+        {
+            this.tool = new KeyTool();
+        }
+
+        [Fact]
+        public void CanGenerateKey()
+        {
+            Key key1 = this.tool.GeneratePrivateKey();
+            Key key2 = this.tool.GeneratePrivateKey();
+
+            Assert.NotEqual(key1.PubKey, key2.PubKey);
+
+            int bytesCount = key1.PubKey.ToBytes().Length;
+            Assert.Equal(33, bytesCount);
+        }
+
+        [Fact]
+        public void CanSaveLoadKey()
+        {
+            string testPath = Path.Combine(Path.GetTempPath(), this.GetType().Name + "_" + nameof(this.CanSaveLoadKey));
+            Directory.CreateDirectory(testPath);
+
+            string filePath = testPath + @"\key.dat";
+
+            Key key = this.tool.GeneratePrivateKey();
+
+            this.tool.SavePrivateKey(key, filePath);
+            Key loadedKey = this.tool.LoadPrivateKey(filePath);
+
+            Assert.Equal(loadedKey.PubKey, key.PubKey);
+
+            Directory.Delete(testPath, true);
+        }
+    }
+}
---
+++ src/Stratis.Bitcoin.Features.PoA/KeyTool.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using NBitcoin;
+
+namespace Stratis.Bitcoin.Features.PoA
+{
+    public class KeyTool
+    {
+        public Key GeneratePrivateKey()
+        {
+            var mnemonic = new Mnemonic(Wordlist.English, WordCount.Twelve);
+            Key privKey = mnemonic.DeriveExtKey().PrivateKey;
+
+            return privKey;
+        }
+
+        public void SavePrivateKey(Key privKey, string path)
+        {
+            var ms = new MemoryStream();
+            var stream = new BitcoinStream(ms, true);
+            stream.ReadWrite(ref privKey);
+
+            ms.Seek(0, SeekOrigin.Begin);
+            FileStream fileStream = File.Create(path);
+            ms.CopyTo(fileStream);
+            fileStream.Close();
+        }
+
+        public Key LoadPrivateKey(string path)
+        {
+            FileStream readStream = File.OpenRead(path);
+
+            var privKey = new Key();
+            var stream = new BitcoinStream(readStream, false);
+            stream.ReadWrite(ref privKey);
+            readStream.Close();
+
+            return privKey;
+        }
+    }
+}
