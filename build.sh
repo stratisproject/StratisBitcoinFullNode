39182f95f1f52deb6f26f59522c0a95ec06e9584
noescape0 <noescape0@ya.ru>
2018-10-02 12:37:47 +0300

    updated CI configs

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ dotnet --info
 echo disabling Fody because it is not needed for CI
 
 cd src
-dotnet remove Stratis.Bitcoin/Stratis.Bitcoin.csproj package Fody
+dotnet remove Stratis.Bitcoin/Stratis.Bitcoin.csproj package Tracer.Fody
 
 echo STARTED dotnet build
 dotnet build -c Release ${path} -v m
