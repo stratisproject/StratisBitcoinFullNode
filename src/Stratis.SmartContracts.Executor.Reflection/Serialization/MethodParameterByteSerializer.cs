0444f9ac83300fad924a1ba23102e365faef2243
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-10-02 12:28:51 +1000

    Add base for byte serializer

---
+++ src/Stratis.SmartContracts.Executor.Reflection/Serialization/MethodParameterByteSerializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Stratis.SmartContracts.Executor.Reflection.Serialization
+{
+    /// <summary>
+    /// Serializes method parameters in a byte-encoded format using an <see cref="IContractPrimitiveSerializer"/>.
+    /// </summary>
+    public sealed class MethodParameterByteSerializer : IMethodParameterSerializer
+    {
+        private readonly IContractPrimitiveSerializer primitiveSerializer;
+
+        public MethodParameterByteSerializer(IContractPrimitiveSerializer primitiveSerializer)
+        {
+            this.primitiveSerializer = primitiveSerializer;
+        }
+
+        public byte[] Serialize(object[] methodParameters)
+        {
+            return new byte[] { };
+        }
+
+        public object[] Deserialize(string[] parameters)
+        {
+            // Don't bother implementing this as it will be removed soon.
+            throw new NotImplementedException();
+        }
+
+        public object[] Deserialize(byte[] methodParameters)
+        {
+            return new object[] { };
+        }
+    }
+}
