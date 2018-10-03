27f7f79f7dfa1812afb431400f486a4d2f042077
noescape0 <noescape0@ya.ru>
2018-10-03 14:56:36 +0300

    moved validation to a separated class

--- src/Stratis.Bitcoin.Features.PoA/PoABlockHeader.cs
+++ src/Stratis.Bitcoin.Features.PoA/PoABlockHeader.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using NBitcoin;
-using NBitcoin.Crypto;
+﻿using NBitcoin;
 
 namespace Stratis.Bitcoin.Features.PoA
 {
@@ -11,44 +6,16 @@ namespace Stratis.Bitcoin.Features.PoA
     {
         private BlockSignature federationSignature;
 
-        public BlockSignature FederationSignature { get => this.federationSignature; }
-
-        //ECDSASignature signature = coinstakeContext.Key.Sign(block.GetHash());
-        //block.BlockSignature = new BlockSignature { Signature = signature.ToDER() };
-        //BlockSignature
+        public BlockSignature FederationSignature
+        {
+            get => this.federationSignature;
+            set => this.federationSignature = value;
+        }
 
         public override void ReadWrite(BitcoinStream stream)
         {
             base.ReadWrite(stream);
-
             stream.ReadWrite(ref this.federationSignature);
-        }
-
-        // TODO move this code to PoA header validator
-
-        public void Sign(Key key)
-        {
-            ECDSASignature signature = key.Sign(this.GetHash());
-
-            if (!signature.IsLowS)
-                signature = signature.MakeCanonical();
-
-            this.federationSignature = new BlockSignature { Signature = signature.ToDER() };
-        }
-
-        public bool VerifySignature(PubKey pubKey)
-        {
-            if (!ECDSASignature.IsValidDER(this.FederationSignature.Signature))
-                return false;
-
-            ECDSASignature signature = ECDSASignature.FromDER(this.FederationSignature.Signature);
-
-            if (!signature.IsLowS)
-                return false;
-
-            bool res = pubKey.Verify(this.GetHash(), signature);
-
-            return res;
         }
     }
 }
---
+++ src/Stratis.Bitcoin.Features.PoA/PoABlockHeaderValidator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NBitcoin;
+using NBitcoin.Crypto;
+
+namespace Stratis.Bitcoin.Features.PoA
+{
+    public class PoABlockHeaderValidator
+    {
+        public void Sign(Key key, PoABlockHeader header)
+        {
+            ECDSASignature signature = key.Sign(header.GetHash());
+
+            if (!signature.IsLowS)
+                signature = signature.MakeCanonical();
+
+            header.FederationSignature = new BlockSignature { Signature = signature.ToDER() };
+        }
+
+        public bool VerifySignature(PubKey pubKey, PoABlockHeader header)
+        {
+            if (header.FederationSignature.IsEmpty())
+                return false;
+
+
+            if (!ECDSASignature.IsValidDER(header.FederationSignature.Signature))
+                return false;
+
+            ECDSASignature signature = ECDSASignature.FromDER(header.FederationSignature.Signature);
+
+            if (!signature.IsLowS)
+                return false;
+
+            bool isValidSignature = pubKey.Verify(header.GetHash(), signature);
+
+            return isValidSignature;
+        }
+    }
+}
