9217b82c8bf2ec1fcf19bd1e5a81e32dedee40b2
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-10-03 16:45:21 +1000

    Add initial balance to BalanceState

--- src/Stratis.SmartContracts.Executor.Reflection/BalanceState.cs
+++ src/Stratis.SmartContracts.Executor.Reflection/BalanceState.cs
@@ -21,6 +21,21 @@ namespace Stratis.SmartContracts.Executor.Reflection
             this.internalTransfers = internalTransfers;
         }
 
+        public BalanceState(IBalanceRepository repository, IReadOnlyList<TransferInfo> internalTransfers,
+            (ulong, uint160) initialBalance)
+        {
+            this.repository = repository;
+            this.internalTransfers = internalTransfers;
+            this.InitialBalance = initialBalance;
+        }
+
+        public (ulong, uint160) InitialBalance { get; private set; }
+
+        public void AddInitialBalance(ulong messageAmount, uint160 address)
+        {
+            this.InitialBalance = (messageAmount, address);
+        }
+
         public ulong GetBalance(uint160 address)
         {
             return this.repository.GetCurrentBalance(address) 
@@ -29,7 +44,9 @@ namespace Stratis.SmartContracts.Executor.Reflection
 
         private ulong GetPendingBalance(uint160 address)
         {
-            ulong ret = 0;
+            (var balance, var contractAddress) = this.InitialBalance;
+
+            ulong ret = address == contractAddress ? balance : 0UL;
 
             foreach (TransferInfo transfer in this.internalTransfers.Where(x => x.To == address))
             {
