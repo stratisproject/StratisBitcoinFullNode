c19ce2cf6b9c65716f1145173630edcd68ddd85c
noescape0 <NoEscape0@ya.ru>
2018-10-04 15:09:48 +0300

    PoA BlockHeader (#2359)
    Fix: #2308

---
+++ src/Stratis.Bitcoin.Features.PoA/PoABlockHeaderValidator.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Logging;
+using NBitcoin;
+using NBitcoin.Crypto;
+
+namespace Stratis.Bitcoin.Features.PoA
+{
+    public class PoABlockHeaderValidator
+    {
+        private readonly ILogger logger;
+
+        public PoABlockHeaderValidator(ILoggerFactory factory)
+        {
+            this.logger = factory.CreateLogger(this.GetType().FullName);
+        }
+
+        public void Sign(Key key, PoABlockHeader header)
+        {
+            ECDSASignature signature = key.Sign(header.GetHash());
+
+            header.BlockSignature = new BlockSignature { Signature = signature.ToDER() };
+        }
+
+        public bool VerifySignature(PubKey pubKey, PoABlockHeader header)
+        {
+            if ((header.BlockSignature == null) || header.BlockSignature.IsEmpty())
+            {
+                this.logger.LogTrace("(-)[NO_SIGNATURE]");
+                return false;
+            }
+
+            if (!ECDSASignature.IsValidDER(header.BlockSignature.Signature))
+            {
+                this.logger.LogTrace("(-)[INVALID_DER]");
+                return false;
+            }
+
+            ECDSASignature signature = ECDSASignature.FromDER(header.BlockSignature.Signature);
+
+            if (!signature.IsLowS)
+            {
+                this.logger.LogTrace("(-)[NOT_CANONICAL]");
+                return false;
+            }
+
+            bool isValidSignature = pubKey.Verify(header.GetHash(), signature);
+
+            return isValidSignature;
+        }
+    }
+}
