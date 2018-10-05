2d82a4b831733d8ac85b6ab35cb9182cd47e7228
bokobza <bokobza@gmail.com>
2018-10-05 10:25:21 +0100

    Fixed GetGeneralInfo error handling

--- src/Stratis.Bitcoin.Features.Wallet/Controllers/WalletController.cs
+++ src/Stratis.Bitcoin.Features.Wallet/Controllers/WalletController.cs
@@ -344,10 +344,7 @@ namespace Stratis.Bitcoin.Features.Wallet.Controllers
             catch (Exception e)
             {
                 this.logger.LogError(e, "Exception occurred: {0}", e.StackTrace);
-                if (e is System.FormatException)
-                    return ErrorHelpers.BuildErrorResponse(HttpStatusCode.BadRequest, e.Message, e.ToString());
-
-                return ErrorHelpers.BuildErrorResponse(HttpStatusCode.InternalServerError, e.Message, e.ToString());
+                return ErrorHelpers.BuildErrorResponse(HttpStatusCode.BadRequest, e.Message, e.ToString());
             }
         }
 
