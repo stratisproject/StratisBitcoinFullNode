ae7e7d31b0d3578932a91f0af254bca9e7c5240a
bokobza <bokobza@gmail.com>
2018-10-07 23:34:13 +0100

    Check for null transactions ids when removing transactions from wallet.

--- src/Stratis.Bitcoin.Features.Wallet/Controllers/WalletController.cs
+++ src/Stratis.Bitcoin.Features.Wallet/Controllers/WalletController.cs
@@ -988,6 +988,11 @@ namespace Stratis.Bitcoin.Features.Wallet.Controllers
                 }
                 else
                 {
+                    if (request.TransactionsIds == null || request.TransactionsIds.Any(trx => trx == null))
+                    {
+                        throw new WalletException("Transaction ids need to be specified if the 'all' flag is not set.");
+                    }
+
                     IEnumerable<uint256> ids = request.TransactionsIds.Select(uint256.Parse);
                     result = this.walletManager.RemoveTransactionsByIdsLocked(request.WalletName, ids);
                 }
