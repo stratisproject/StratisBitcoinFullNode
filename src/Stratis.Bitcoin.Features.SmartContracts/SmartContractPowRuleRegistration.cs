00f6a5c5b0325931e5830905aac31ea2228c53ba
Jordan Andrews <jordandrews@hotmail.com>
2018-10-08 12:39:47 +1100

    [SC] Amend SmartContractFormatRule so it's used (#2383)
    * Array serialization methods complete
    
    * Add consensusrule correctly

--- src/Stratis.Bitcoin.Features.SmartContracts/SmartContractPowRuleRegistration.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/SmartContractPowRuleRegistration.cs
@@ -4,6 +4,9 @@ using NBitcoin.Rules;
 using Stratis.Bitcoin.Consensus.Rules;
 using Stratis.Bitcoin.Features.Consensus.Rules.CommonRules;
 using Stratis.Bitcoin.Features.SmartContracts.Consensus.Rules;
+using Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Consensus.Rules;
+using Stratis.SmartContracts.Executor.Reflection;
+using Stratis.SmartContracts.Executor.Reflection.Serialization;
 
 namespace Stratis.Bitcoin.Features.SmartContracts
 {
@@ -48,6 +51,7 @@ namespace Stratis.Bitcoin.Features.SmartContracts
                 new TransactionDuplicationActivationRule(), // implements BIP30
                 new TxOutSmartContractExecRule(),
                 new OpSpendRule(),
+                new SmartContractFormatRule(new CallDataSerializer(new MethodParameterStringSerializer())), // Can we inject these serializers?
                 new SmartContractPowCoinviewRule(), // implements BIP68, MaxSigOps and BlockReward 
                 new SmartContractSaveCoinviewRule()
             };
