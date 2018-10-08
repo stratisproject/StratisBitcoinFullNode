a6ae159fc944ca4617109576ebe2fe4325a161b9
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-10-08 17:13:08 +1100

    Add base Serializer

---
+++ src/Stratis.SmartContracts.Executor.Reflection/Serialization/Serializer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Stratis.SmartContracts.Executor.Reflection.Serialization
+{
+    /// <summary>
+    /// Defines the serialization functionality that is exposed to a <see cref="SmartContract"/>.
+    /// </summary>
+    public class Serializer : ISerializer
+    {
+        public byte[] Serialize(Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Serialize(bool b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Serialize(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Serialize(long l)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Serialize(uint u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Serialize(ulong ul)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Serialize(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Serialize(Array a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ToBool(byte[] val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Address ToAddress(byte[] val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ToInt32(byte[] val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public uint ToUInt32(byte[] val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long ToInt64(byte[] val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ulong ToUInt64(byte[] val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToString(byte[] val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] ToArray<T>(byte[] val)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
