a8b61164a58c8e6bfd784a2d4d361966ad8202f6
noescape0 <noescape0@ya.ru>
2018-10-10 18:31:51 +0300

    PoAIntegritySignatureRule

--- src/Stratis.Bitcoin.Features.PoA/ConsensusRules/PoAConsensusErrors.cs
+++ src/Stratis.Bitcoin.Features.PoA/ConsensusRules/PoAConsensusErrors.cs
@@ -7,5 +7,6 @@ namespace Stratis.Bitcoin.Features.PoA.ConsensusRules
         public static readonly ConsensusError InvalidHeaderBits = new ConsensusError("invalid-header-bits", "invalid header bits");
         public static readonly ConsensusError InvalidHeaderTimestamp = new ConsensusError("invalid-header-timestamp", "invalid header timestamp");
         public static readonly ConsensusError InvalidHeaderSignature = new ConsensusError("invalid-header-signature", "invalid header signature");
+        public static readonly ConsensusError InvalidBlockSignature = new ConsensusError("invalid-block-signature", "invalid block signature");
     }
 }
