2722c92f7972c688d8b3316ca612e5b178b9b0a0
Jordan Andrews <jordandrews@hotmail.com>
2018-10-11 14:46:08 +1100

    new test

---
+++ src/Stratis.SmartContracts.IntegrationTests/ContractParameterSerializationTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NBitcoin;
+using Stratis.Bitcoin.Features.SmartContracts.Models;
+using Stratis.SmartContracts.Core.Util;
+using Stratis.SmartContracts.Executor.Reflection.Compilation;
+using Stratis.SmartContracts.Executor.Reflection.Serialization;
+using Stratis.SmartContracts.IntegrationTests.MockChain;
+using Xunit;
+
+namespace Stratis.SmartContracts.IntegrationTests
+{
+    public class ContractParameterSerializationTests : IClassFixture<MockChainFixture>
+    {
+        private readonly Chain mockChain;
+        private readonly Node node1;
+        private readonly Node node2;
+
+        public ContractParameterSerializationTests(MockChainFixture fixture)
+        {
+            this.mockChain = fixture.Chain;
+            this.node1 = this.mockChain.Nodes[0];
+            this.node2 = this.mockChain.Nodes[1];
+        }
+
+        [Fact]
+        public void CreateContract_OneOfEachParameterType()
+        {
+            // Ensure fixture is funded.
+            this.node1.MineBlocks(1);
+
+            double amount = 25;
+            Money senderBalanceBefore = this.node1.WalletSpendableBalance;
+            uint256 currentHash = this.node1.GetLastBlock().GetHash();
+
+            ContractCompilationResult compilationResult = ContractCompiler.CompileFile("SmartContracts/BasicParameters.cs");
+            Assert.True(compilationResult.Success);
+            string[] parameters = new string[]
+            {
+                string.Format("{0}#{1}", (int)MethodParameterDataType.Char, 'c'), // char
+                string.Format("{0}#{1}", (int)MethodParameterDataType.Address, new Address("mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn")), // Address
+                string.Format("{0}#{1}", (int)MethodParameterDataType.Bool, true), // bool
+                string.Format("{0}#{1}", (int)MethodParameterDataType.Int, Int32.MaxValue), // int
+                string.Format("{0}#{1}", (int)MethodParameterDataType.ULong, UInt64.MaxValue), // long
+                string.Format("{0}#{1}", (int)MethodParameterDataType.ULong, UInt64.MaxValue), // uint
+                string.Format("{0}#{1}", (int)MethodParameterDataType.ULong, UInt64.MaxValue), // ulong
+                string.Format("{0}#{1}", (int)MethodParameterDataType.ULong, UInt64.MaxValue), // string
+                string.Format("{0}#{1}", (int)MethodParameterDataType.ULong, UInt64.MaxValue), // byte[]
+            };
+            BuildCreateContractTransactionResponse response = this.node1.SendCreateContractTransaction(compilationResult.Compilation, amount, parameters);
+            this.node2.WaitMempoolCount(1);
+            this.node2.MineBlocks(1);
+            Block lastBlock = this.node1.GetLastBlock();
+        }
+    }
+}
---
+++ src/Stratis.SmartContracts.IntegrationTests/SmartContracts/CreateWithAllParameters.cs
@@ -0,0 +1,75 @@
+﻿using Stratis.SmartContracts;
+
+public class CreateWithAllParameters : SmartContract
+{
+    public CreateWithAllParameters(ISmartContractState state,
+        char aChar,
+        Address anAddress,
+        bool aBool,
+        int anInt,
+        long aLong,
+        uint aUint,
+        ulong aUlong,
+        string aString,
+        byte[] bytes) : base(state)
+    {
+        PersistentState.SetChar("char", aChar);
+        PersistentState.SetAddress("Address", anAddress);
+        PersistentState.SetBool("bool", aBool);
+        PersistentState.SetInt32("int", anInt);
+        PersistentState.SetInt64("long", aLong);
+        PersistentState.SetUInt32("uint", aUint);
+        PersistentState.SetUInt64("ulong",aUlong);
+        PersistentState.SetString("string", aString);
+        PersistentState.SetBytes("bytes", bytes);
+        this.Log(new Log
+        {
+            Char = aChar,
+            Address = anAddress,
+            Bool = aBool,
+            Int = anInt,
+            Long = aLong,
+            Uint = aUint,
+            Ulong = aUlong,
+            String = aString,
+            Bytes = bytes
+        });
+        Assert(PersistentState.GetChar("char") == aChar);
+        Assert(PersistentState.GetAddress("Address") == anAddress);
+        Assert(PersistentState.GetBool("bool") == aBool);
+        Assert(PersistentState.GetInt32("int") == anInt);
+        Assert(PersistentState.GetInt64("long") == aLong);
+        Assert(PersistentState.GetString("string") == aString);
+        Assert(PersistentState.GetBytes("bytes") == bytes);
+    }
+
+    public struct Log
+    {
+        [Index]
+        public char Char;
+
+        [Index]
+        public Address Address;
+
+        [Index]
+        public bool Bool;
+
+        [Index]
+        public int Int;
+
+        [Index]
+        public long Long;
+
+        [Index]
+        public uint Uint;
+
+        [Index]
+        public ulong Ulong;
+
+        [Index]
+        public string String;
+
+        [Index]
+        public byte[] Bytes;
+    }
+}
