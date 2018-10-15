417cd209cfeea59649fc442d70095882b1f860b9
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-10-15 16:50:48 +1100

    Add get docs

--- src/Stratis.SmartContracts/IPersistentState.cs
+++ src/Stratis.SmartContracts/IPersistentState.cs
@@ -7,26 +7,69 @@ namespace Stratis.SmartContracts
     /// </summary>
     public interface IPersistentState
     {
+        /// <summary>
+        /// Gets the bytes set at the value pointed to by the given key.
+        /// </summary>
         byte[] GetBytes(string key);
 
+        /// <summary>
+        /// Gets the bytes set at the value pointed to by the given key, and uses <see cref="ISerializer.ToChar"/> to
+        /// deserialize the value to a char. If deserialization is unsuccessful, returns default(char).
+        /// </summary>
         char GetChar(string key);
 
+        /// <summary>
+        /// Gets the bytes set at the value pointed to by the given key, and uses <see cref="ISerializer.ToAddress"/> to
+        /// deserialize the value to an Address. If deserialization is unsuccessful, returns default(Address).
+        /// </summary>
         Address GetAddress(string key);
 
+        /// <summary>
+        /// Gets the bytes set at the value pointed to by the given key, and uses <see cref="ISerializer.ToBool"/> to
+        /// deserialize the value to a bool. If deserialization is unsuccessful, returns default(bool).
+        /// </summary>
         bool GetBool(string key);
 
+        /// <summary>
+        /// Gets the bytes set at the value pointed to by the given key, and uses <see cref="ISerializer.ToInt32"/> to
+        /// deserialize the value to an int. If deserialization is unsuccessful, returns default(int).
+        /// </summary>
         int GetInt32(string key);
 
+        /// <summary>
+        /// Gets the bytes set at the value pointed to by the given key, and uses <see cref="ISerializer.ToUInt32"/> to
+        /// deserialize the value to a uint. If deserialization is unsuccessful, returns default(uint).
+        /// </summary>
         uint GetUInt32(string key);
 
+        /// <summary>
+        /// Gets the bytes set at the value pointed to by the given key, and uses <see cref="ISerializer.ToInt64"/> to
+        /// deserialize the value to a long. If deserialization is unsuccessful, returns default(long).
+        /// </summary>
         long GetInt64(string key);
 
+        /// <summary>
+        /// Gets the bytes set at the value pointed to by the given key, and uses <see cref="ISerializer.ToUInt64"/> to
+        /// deserialize the value to a ulong. If deserialization is unsuccessful, returns default(ulong).
+        /// </summary>
         ulong GetUInt64(string key);
 
+        /// <summary>
+        /// Gets the bytes set at the value pointed to by the given key, and uses <see cref="ISerializer.ToString(byte[])"/> to
+        /// deserialize the value to a string. If deserialization is unsuccessful, returns <see cref="string.Empty"/>.
+        /// </summary>
         string GetString(string key);
 
+        /// <summary>
+        /// Gets the bytes set at the value pointed to by the given key, and uses <see cref="ISerializer.ToStruct{T}(byte[])"/> to
+        /// deserialize the value to a struct. If deserialization is unsuccessful, returns default(T).
+        /// </summary>
         T GetStruct<T>(string key) where T : struct;
 
+        /// <summary>
+        /// Gets the bytes set at the value pointed to by the given key, and uses <see cref="ISerializer.ToArray{T}(byte[])"/> to
+        /// deserialize the value to an Array. If deserialization is unsuccessful, returns new T[0].
+        /// </summary>
         T[] GetArray<T>(string key);
 
         void SetBytes(string key, byte[] value);
