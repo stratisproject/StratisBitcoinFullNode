ddf5f164118ccf13382631e39d7abc45eeef95ab
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-10-16 15:33:22 +1100

    Remove namespace

--- src/Stratis.SmartContracts.Token/StandardToken.cs
+++ src/Stratis.SmartContracts.Token/StandardToken.cs
@@ -1,123 +1,123 @@
-﻿namespace Stratis.SmartContracts.Token
+﻿using Stratis.SmartContracts;
+using Stratis.SmartContracts.Token;
+
+public class StandardToken : SmartContract, IStandardToken
 {
-    public class StandardToken : SmartContract, IStandardToken
+    public StandardToken(ISmartContractState smartContractState, uint totalSupply) 
+        : base(smartContractState)
     {
-        public StandardToken(ISmartContractState smartContractState, uint totalSupply) 
-            : base(smartContractState)
+        this.TotalSupply = totalSupply;
+    }
+
+    public uint TotalSupply
+    {
+        get => PersistentState.GetUInt32(nameof(this.TotalSupply));
+        private set => PersistentState.SetUInt32(nameof(this.TotalSupply), value);
+    }
+
+    public uint GetBalance(Address address)
+    {
+        return PersistentState.GetUInt32($"Balance:{address}");
+    }
+
+    private void SetBalance(Address address, uint value)
+    {
+        PersistentState.SetUInt32($"Balance:{address}", value);
+    }
+
+    public bool Transfer(Address to, uint amount)
+    {
+        if (amount == 0)
         {
-            this.TotalSupply = totalSupply;
-        }
-
-        public uint TotalSupply
-        {
-            get => PersistentState.GetUInt32(nameof(this.TotalSupply));
-            private set => PersistentState.SetUInt32(nameof(this.TotalSupply), value);
-        }
-
-        public uint GetBalance(Address address)
-        {
-            return PersistentState.GetUInt32($"Balance:{address}");
-        }
-
-        private void SetBalance(Address address, uint value)
-        {
-            PersistentState.SetUInt32($"Balance:{address}", value);
-        }
-
-        public bool Transfer(Address to, uint amount)
-        {
-            if (amount == 0)
-            {
-                Log(new TransferLog { From = Message.Sender, To = to, Amount = 0 });
-
-                return true;
-            }
-
-            uint senderBalance = GetBalance(Message.Sender);
-            
-            if (senderBalance < amount)
-            {
-                return false;
-            }
-
-            uint toBalance = GetBalance(to);
-
-            SetBalance(Message.Sender, senderBalance - amount);
-
-            checked
-            {
-                SetBalance(to, toBalance + amount);
-            }
-
-            Log(new TransferLog { From = Message.Sender, To = to, Amount = amount });
+            Log(new TransferLog { From = Message.Sender, To = to, Amount = 0 });
 
             return true;
         }
 
-        public bool TransferFrom(Address from, Address to, uint amount)
+        uint senderBalance = GetBalance(Message.Sender);
+        
+        if (senderBalance < amount)
         {
-            if (amount == 0)
-            {
-                Log(new TransferLog { From = from, To = to, Amount = 0 });
+            return false;
+        }
 
-                return true;
-            }
+        uint toBalance = GetBalance(to);
 
-            uint senderAllowance = Allowance(from, Message.Sender);
-            uint fromBalance = GetBalance(from);
+        SetBalance(Message.Sender, senderBalance - amount);
 
-            if (senderAllowance < amount || fromBalance < amount)
-            {
-                return false;
-            }
+        checked
+        {
+            SetBalance(to, toBalance + amount);
+        }
 
-            uint toBalance = GetBalance(to);
+        Log(new TransferLog { From = Message.Sender, To = to, Amount = amount });
 
-            SetApproval(from, Message.Sender, senderAllowance - amount);
-            SetBalance(from, fromBalance - amount);
+        return true;
+    }
 
-            checked
-            {
-                SetBalance(to, toBalance + amount);
-            }
-
-            Log(new TransferLog { From = from, To = to, Amount = amount });
+    public bool TransferFrom(Address from, Address to, uint amount)
+    {
+        if (amount == 0)
+        {
+            Log(new TransferLog { From = from, To = to, Amount = 0 });
 
             return true;
         }
 
-        /// <inheritdoc />
-        public bool Approve(Address spender, uint amount)
+        uint senderAllowance = Allowance(from, Message.Sender);
+        uint fromBalance = GetBalance(from);
+
+        if (senderAllowance < amount || fromBalance < amount)
         {
-            SetApproval(Message.Sender, spender, amount);
-
-            Log(new ApprovalLog { Owner = Message.Sender, Spender = spender, Amount = amount});
-
-            return true;
+            return false;
         }
 
-        private void SetApproval(Address owner, Address spender, uint value)
+        uint toBalance = GetBalance(to);
+
+        SetApproval(from, Message.Sender, senderAllowance - amount);
+        SetBalance(from, fromBalance - amount);
+
+        checked
         {
-            PersistentState.SetUInt32($"Allowance:{owner}:{spender}", value);
+            SetBalance(to, toBalance + amount);
         }
 
-        public uint Allowance(Address owner, Address spender)
-        {
-            return PersistentState.GetUInt32($"Allowance:{owner}:{spender}");
-        }
+        Log(new TransferLog { From = from, To = to, Amount = amount });
 
-        public struct TransferLog
-        {
-            public Address From;
-            public Address To;
-            public uint Amount;
-        }
+        return true;
+    }
 
-        public struct ApprovalLog
-        {
-            public Address Owner;
-            public Address Spender;
-            public uint Amount;
-        }
+    /// <inheritdoc />
+    public bool Approve(Address spender, uint amount)
+    {
+        SetApproval(Message.Sender, spender, amount);
+
+        Log(new ApprovalLog { Owner = Message.Sender, Spender = spender, Amount = amount});
+
+        return true;
+    }
+
+    private void SetApproval(Address owner, Address spender, uint value)
+    {
+        PersistentState.SetUInt32($"Allowance:{owner}:{spender}", value);
+    }
+
+    public uint Allowance(Address owner, Address spender)
+    {
+        return PersistentState.GetUInt32($"Allowance:{owner}:{spender}");
+    }
+
+    public struct TransferLog
+    {
+        public Address From;
+        public Address To;
+        public uint Amount;
+    }
+
+    public struct ApprovalLog
+    {
+        public Address Owner;
+        public Address Spender;
+        public uint Amount;
     }
 }
