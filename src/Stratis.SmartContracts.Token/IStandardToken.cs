5950aa0ca225766e3344556f52b4bcb9436a7877
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-10-16 15:07:18 +1100

    Add standard token contract and tests

---
+++ src/Stratis.SmartContracts.Token/IStandardToken.cs
@@ -0,0 +1,53 @@
+﻿namespace Stratis.SmartContracts.Token
+{
+    /// <summary>
+    /// Interface for a standard smart contract token
+    /// </summary>
+    public interface IStandardToken
+    {
+        /// <summary>
+        /// Gets the total supply of tokens.
+        /// </summary>
+        uint TotalSupply { get; }
+
+        /// <summary>
+        /// Gets the balance of the specified address.
+        /// </summary>
+        /// <param name="address">The address to check balance for.</param>
+        /// <returns>Balance for the given address</returns>
+        uint GetBalance(Address address);
+
+        /// <summary>
+        /// Transfers tokens from current address to specified address.
+        /// </summary>
+        /// <param name="to">Address you want to send tokens to.</param>
+        /// <param name="amount">Address to transfer tokens to.</param>
+        /// <returns>Result of the transfer operation</returns>
+        bool Transfer(Address to, uint amount);
+
+        /// <summary>
+        /// Transfers tokens from one address to another.
+        /// </summary>
+        /// <param name="from">Address to transfer tokens from</param>
+        /// <param name="to">Address to transfer tokens to.</param>
+        /// <param name="amount">Amount of tokens to transfer.</param>
+        /// <returns>Result of the transfer operation</returns>
+        bool TransferFrom(Address from, Address to, uint amount);
+
+        /// <summary>
+        /// The caller of the approve function approves spender to spend the allowance of tokens.
+        /// </summary>
+        /// <param name="spender"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        bool Approve(Address spender, uint value);
+
+        /// <summary>
+        /// Returns the amount of tokens owned by the owner that the spender is able to spend.
+        /// </summary>
+        /// <param name="owner">The address of the owner of the tokens.</param>
+        /// <param name="spender">The address of the spender of the tokens.</param>
+        /// <returns></returns>
+        uint Allowance(Address owner, Address spender);
+    }
+}
---
+++ src/Stratis.SmartContracts.Token/StandardToken.cs
@@ -0,0 +1,97 @@
+﻿namespace Stratis.SmartContracts.Token
+{
+    public class StandardToken : SmartContract, IStandardToken
+    {
+        public StandardToken(ISmartContractState smartContractState, uint totalSupply) 
+            : base(smartContractState)
+        {
+            this.TotalSupply = totalSupply;
+        }
+
+        public uint TotalSupply
+        {
+            get => PersistentState.GetUInt32(nameof(this.TotalSupply));
+            private set => PersistentState.SetUInt32(nameof(this.TotalSupply), value);
+        }
+
+        public uint GetBalance(Address address)
+        {
+            return PersistentState.GetUInt32($"Balance:{address}");
+        }
+
+        private void SetBalance(Address address, uint value)
+        {
+            PersistentState.SetUInt32($"Balance:{address}", value);
+        }
+
+        public bool Transfer(Address to, uint amount)
+        {
+            if (amount == 0)
+                return true;
+
+            uint senderBalance = GetBalance(Message.Sender);
+            
+            if (senderBalance < amount)
+            {
+                return false;
+            }
+
+            uint toBalance = GetBalance(to);
+
+            SetBalance(Message.Sender, senderBalance - amount);
+
+            checked
+            {
+                SetBalance(to, toBalance + amount);
+            }
+
+            return true;
+        }
+
+        public bool TransferFrom(Address from, Address to, uint amount)
+        {
+            if (amount == 0)
+            {
+                return true;
+            }
+
+            uint senderAllowance = Allowance(from, Message.Sender);
+            uint fromBalance = GetBalance(from);
+
+            if (senderAllowance < amount || fromBalance < amount)
+            {
+                return false;
+            }
+
+            uint toBalance = GetBalance(to);
+
+            SetApproval(from, Message.Sender, senderAllowance - amount);
+            SetBalance(from, fromBalance - amount);
+
+            checked
+            {
+                SetBalance(to, toBalance + amount);
+            }
+
+            return true;
+        }
+
+        /// <inheritdoc />
+        public bool Approve(Address spender, uint value)
+        {
+            SetApproval(Message.Sender, spender, value);
+
+            return true;
+        }
+
+        private void SetApproval(Address owner, Address spender, uint value)
+        {
+            PersistentState.SetUInt32($"Allowance:{owner}:{spender}", value);
+        }
+
+        public uint Allowance(Address owner, Address spender)
+        {
+            return PersistentState.GetUInt32($"Allowance:{owner}:{spender}");
+        }
+    }
+}
