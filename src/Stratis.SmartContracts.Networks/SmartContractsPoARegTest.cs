830446fe06a2072df220c02e641d5c820bb3963f
Jordan Andrews <jordandrews@hotmail.com>
2018-10-17 11:47:02 +1100

    Docs

--- src/Stratis.SmartContracts.Networks/SmartContractsPoARegTest.cs
+++ src/Stratis.SmartContracts.Networks/SmartContractsPoARegTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using NBitcoin;
 using NBitcoin.DataEncoders;
+using NBitcoin.Protocol;
 using Stratis.Bitcoin.Features.PoA;
 using Stratis.Bitcoin.Features.SmartContracts.Consensus;
 
@@ -16,21 +17,10 @@ namespace Stratis.SmartContracts.Networks
     {
         public SmartContractsPoARegTest()
         {
-
             this.Name = "SmartContractsPoARegTest";
             this.CoinTicker = "SCPOA";
 
             var consensusFactory = new SmartContractPoAConsensusFactory();
-
-            // Configure federation public keys.
-            // Keep in mind that order in which keys are added to this list is important
-            // and should be the same for all nodes operating on this network.
-            this.FederationPublicKeys = new List<PubKey>()
-            {
-                new PubKey("03e6f19ea3dc6c145d98a0e0838af952755798e5bc3950bbca4f9485aa23873d7f"),
-                new PubKey("02ddebcf18207072bdd172a25f85f2ea12e2de1d9d794f136722634aad08400fcb"),
-                new PubKey("02067b38d777690aaaf23a5b371a819e6ddc6d2aae734b0199fe59df28dc056dd7")
-            };
 
             // Create the genesis block.
             this.GenesisTime = 1513622125;
@@ -108,29 +98,17 @@ namespace Stratis.SmartContracts.Networks
             this.Base58Prefixes[(int)Base58Type.ASSET_ID] = new byte[] { 115 };
             this.Base58Prefixes[(int)Base58Type.COLORED_ADDRESS] = new byte[] { 0x13 };
 
-            this.Checkpoints = new Dictionary<int, CheckpointInfo>
-            {
-                { 0, new CheckpointInfo(new uint256("0x0621b88fb7a99c985d695be42e606cb913259bace2babe92970547fa033e4076")) },
-            };
-
-            var encoder = new Bech32Encoder("bc");
+            Bech32Encoder encoder = Encoders.Bech32("tb");
             this.Bech32Encoders = new Bech32Encoder[2];
             this.Bech32Encoders[(int)Bech32Type.WITNESS_PUBKEY_ADDRESS] = encoder;
             this.Bech32Encoders[(int)Bech32Type.WITNESS_SCRIPT_ADDRESS] = encoder;
 
-            // No DNS seeds.
-            this.DNSSeeds = new List<DNSSeedData> { };
+            this.Checkpoints = new Dictionary<int, CheckpointInfo>();
 
-            // No seed nodes.
-            string[] seedNodes = { };
-            this.SeedNodes = this.ConvertToNetworkAddresses(seedNodes, this.DefaultPort).ToList();
+            this.DNSSeeds = new List<DNSSeedData>();
+            this.SeedNodes = new List<NetworkAddress>();
 
-            // TODO: Hash is different because of SC fields. Do we need asserts?
-
-            if ((this.FederationPublicKeys == null) || (this.FederationPublicKeys.Count == 0))
-            {
-                throw new Exception("No keys for federation members are configured!");
-            }
+            // TODO: Do we need Asserts for block hash
         }
     }
 }
