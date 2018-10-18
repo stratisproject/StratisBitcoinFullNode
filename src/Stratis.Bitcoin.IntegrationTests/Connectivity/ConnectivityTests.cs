0d6055d2f422d3df249611075aa682f6beedf9c3
Ferdeen Mughal <Ferdeen.Mughal@StratisPlatform.com>
2018-10-18 10:44:54 +0100

    Changes after PR review.
    Removed await peerAddNodeConnector.OnConnectAsync() to allow the nodes to connect naturally.
    
    Removed pointless assertions.
    
    Started each node individually.
    
    New'd up the StratisRegTest network, rather then using KnownNetworks.

--- src/Stratis.Bitcoin.IntegrationTests/Connectivity/ConnectivityTests.cs
+++ src/Stratis.Bitcoin.IntegrationTests/Connectivity/ConnectivityTests.cs
@@ -7,8 +7,8 @@ using NBitcoin;
 using Stratis.Bitcoin.Connection;
 using Stratis.Bitcoin.IntegrationTests.Common;
 using Stratis.Bitcoin.IntegrationTests.Common.EnvironmentMockUpHelpers;
+using Stratis.Bitcoin.Networks;
 using Stratis.Bitcoin.P2P;
-using Stratis.Bitcoin.Tests.Common;
 using Xunit;
 
 namespace Stratis.Bitcoin.IntegrationTests.Connectivity
@@ -19,7 +19,7 @@ namespace Stratis.Bitcoin.IntegrationTests.Connectivity
 
         public ConnectivityTests()
         {
-            this.Network = KnownNetworks.StratisRegTest;
+            this.Network = new StratisRegTest();
         }
 
         [Fact]
@@ -27,39 +27,31 @@ namespace Stratis.Bitcoin.IntegrationTests.Connectivity
         {
             using (NodeBuilder builder = NodeBuilder.Create(this))
             {
-                CoreNode node1 = builder.CreateStratisPosNode(this.Network).NotInIBD();
-                CoreNode node2 = builder.CreateStratisPosNode(this.Network).NotInIBD();
-                CoreNode node3 = builder.CreateStratisPosNode(this.Network).NotInIBD();
-
-                CoreNode syncerNode = builder.CreateStratisPosNode(this.Network).NotInIBD();
-
-                builder.StartAll();
+                CoreNode node1 = builder.CreateStratisPosNode(this.Network).NotInIBD().Start();
+                CoreNode node2 = builder.CreateStratisPosNode(this.Network).NotInIBD().Start();
+                CoreNode node3 = builder.CreateStratisPosNode(this.Network).NotInIBD().Start();
+                CoreNode syncerNode = builder.CreateStratisPosNode(this.Network).NotInIBD().Start();
 
                 // Connects with AddNode inside Connect().
                 TestHelper.Connect(node1, syncerNode);
 
-                node1.FullNode.ConnectionManager.ConnectedPeers.Count().Should().Be(1);
                 node2.FullNode.ConnectionManager.ConnectedPeers.Count().Should().Be(0);
                 node3.FullNode.ConnectionManager.ConnectedPeers.Count().Should().Be(0);
-                syncerNode.FullNode.ConnectionManager.ConnectedPeers.Count().Should().Be(1);
 
                 var syncerConnectionMgr = syncerNode.FullNode.NodeService<IConnectionManager>();
 
                 var peerAddNodeConnector = syncerConnectionMgr.PeerConnectors.
                     Where(p => p.GetType() == typeof(PeerConnectorAddNode)).First() as PeerConnectorAddNode;
 
-                peerAddNodeConnector.Should().NotBeNull();
-                peerAddNodeConnector.Should().BeOfType<PeerConnectorAddNode>();
-
                 // Adding these endpoints will add the items to the internal address manager.
                 peerAddNodeConnector.ConnectionSettings.AddNode = new List<IPEndPoint>() { node2.Endpoint, node3.Endpoint };
                 peerAddNodeConnector.Initialize(syncerConnectionMgr);
-                await peerAddNodeConnector.OnConnectAsync();
+
+                TestHelper.WaitLoop(() => syncerConnectionMgr.ConnectedPeers.Count() == 3);
 
                 node1.FullNode.ConnectionManager.ConnectedPeers.Count().Should().Be(1);
                 node2.FullNode.ConnectionManager.ConnectedPeers.Count().Should().Be(1);
                 node3.FullNode.ConnectionManager.ConnectedPeers.Count().Should().Be(1);
-                syncerConnectionMgr.ConnectedPeers.Count().Should().Be(3);
             }
         }
     }
