971bc7a72e1f3bf32b05b045cf4dc8e7e1538e8e
Mithril Man <red.angel@libero.it>
2018-10-18 17:30:23 +0200

    restored ProvenHeadersConsensusManagerBehavior to not have PR merge changes

--- src/Stratis.Bitcoin/Consensus/ProvenHeadersConsensusManagerBehavior.cs
+++ src/Stratis.Bitcoin/Consensus/ProvenHeadersConsensusManagerBehavior.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using NBitcoin;
@@ -11,29 +14,26 @@ using Stratis.Bitcoin.Utilities;
 
 namespace Stratis.Bitcoin.Consensus
 {
-    /// <summary>
-    /// Behavior that takes care of proven headers protocol. It also keeps the notion of peer's consensus tip.
-    /// </summary>
+    /// <summary>Behavior that takes care of proven headers protocol. It also keeps the notion of peer's consensus tip.</summary>
     public class ProvenHeadersConsensusManagerBehavior : ConsensusManagerBehavior
     {
         private readonly IInitialBlockDownloadState initialBlockDownloadState;
         private readonly IConsensusManager consensusManager;
         private readonly IPeerBanning peerBanning;
         private readonly ILoggerFactory loggerFactory;
-        private readonly Network network;
+        private readonly PosConsensusFactory consensusFactory;
 
         /// <summary>Instance logger.</summary>
         private readonly ILogger logger;
 
-        public ProvenHeadersConsensusManagerBehavior(ConcurrentChain chain, IInitialBlockDownloadState initialBlockDownloadState,
-            IConsensusManager consensusManager, IPeerBanning peerBanning, ILoggerFactory loggerFactory, Network network) : base(chain, initialBlockDownloadState, consensusManager, peerBanning, loggerFactory)
+        public ProvenHeadersConsensusManagerBehavior(ConcurrentChain chain, IInitialBlockDownloadState initialBlockDownloadState, IConsensusManager consensusManager, IPeerBanning peerBanning, ILoggerFactory loggerFactory) : base(chain, initialBlockDownloadState, consensusManager, peerBanning, loggerFactory)
         {
             this.chain = chain;
             this.initialBlockDownloadState = initialBlockDownloadState;
             this.consensusManager = consensusManager;
             this.peerBanning = peerBanning;
-            this.network = network;
             this.loggerFactory = loggerFactory;
+            this.consensusFactory = new PosConsensusFactory();
             this.logger = loggerFactory.CreateLogger(this.GetType().FullName, $"[{this.GetHashCode():x}] ");
         }
 
@@ -80,7 +80,7 @@ namespace Stratis.Bitcoin.Consensus
             foreach (ChainedHeader header in this.chain.EnumerateToTip(fork).Skip(1))
             {
                 var posBock = new PosBlock(header.Header);
-                ProvenBlockHeader provenBlockHeader = ((PosConsensusFactory)this.network.Consensus.ConsensusFactory).CreateProvenBlockHeader(posBock);
+                ProvenBlockHeader provenBlockHeader = this.consensusFactory.CreateProvenBlockHeader(posBock);
                 lastHeader = header;
                 headers.Headers.Add(provenBlockHeader);
 
@@ -99,8 +99,7 @@ namespace Stratis.Bitcoin.Consensus
         /// <inheritdoc />
         public override object Clone()
         {
-            return new ProvenHeadersConsensusManagerBehavior(
-                this.chain, this.initialBlockDownloadState, this.consensusManager, this.peerBanning, this.loggerFactory, this.network);
+            return new ProvenHeadersConsensusManagerBehavior(this.chain, this.initialBlockDownloadState, this.consensusManager, this.peerBanning, this.loggerFactory);
         }
     }
 }
