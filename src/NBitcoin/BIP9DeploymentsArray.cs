192afef000da52000c44c38cfe683bdaa72a0bc9
quantumagi <someguy.fromafrica@gmail.com>
2018-10-20 13:06:30 +1100

    Fix comments in BIP9DeploymentsArray

--- src/NBitcoin/BIP9DeploymentsArray.cs
+++ src/NBitcoin/BIP9DeploymentsArray.cs
@@ -21,8 +21,21 @@
     /// </summary>
     public interface IBIP9DeploymentsArray
     {
-        BIP9DeploymentFlags GetFlags(int deployment);
+        /// <summary>The number of elements/deployments in the array.</summary>
         int Length { get; }
+
+        /// <summary>
+        /// Gets the flags to set when the deployment goes active.
+        /// </summary>
+        /// <param name="deployment">The deployment number (element index in array).</param>
+        /// <returns>The flags to set.</returns>
+        BIP9DeploymentFlags GetFlags(int deployment);
+
+        /// <summary>
+        /// Gets or sets the deployment parameters for a deployment.
+        /// </summary>
+        /// <param name="deployment">The deployment number (element index in array).</param>
+        /// <returns>The deployment parameters if this is a get.</returns>
         BIP9DeploymentsParameters this[int deployment] { get; set; }
     }
 
@@ -33,31 +46,42 @@
     {
         protected readonly BIP9DeploymentsParameters[] parameters;
 
+        /// <summary>
+        /// Constructs a deployments array of the given length.
+        /// </summary>
+        /// <param name="length">The length of the deployments array to construct.</param>
         public BIP9DeploymentsArray(int length)
         {
             this.parameters = new BIP9DeploymentsParameters[length];
         }
 
+        /// <inheritdoc />
         public abstract BIP9DeploymentFlags GetFlags(int deployment);
 
+        /// <inheritdoc />
         public BIP9DeploymentsParameters this[int deployment]
         {
             get { return this.parameters[deployment]; }
             set { this.parameters[deployment] = value; }
         }
 
+        /// <inheritdoc />
         public int Length => this.parameters.Length;
     }
 
     /// <summary>
-    /// Used by networks that don't define any deployments or deployment pararameters.
+    /// Used by networks that don't define any deployments or deployment parameters.
     /// </summary>
     public class NoBIP9Deployments : BIP9DeploymentsArray
     {
+        /// <summary>
+        /// Constructs a zero-length deployments array.
+        /// </summary>
         public NoBIP9Deployments() : base(0)
         {
         }
 
+        /// <inheritdoc />
         public override BIP9DeploymentFlags GetFlags(int deployment)
         {
             return new BIP9DeploymentFlags();
