1f5fbd8cf3da61da6b13604145037fffa5c13520
noescape0 <NoEscape0@ya.ru>
2018-10-21 16:20:31 +0300

    added integration tests project

---
+++ src/Stratis.Bitcoin.Features.PoA.IntegrationTests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Stratis.Bitcoin.Features.PoA.IntegrationTests
+{
+    public class Program
+    {
+        [Fact]
+        public void DoSmth()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
