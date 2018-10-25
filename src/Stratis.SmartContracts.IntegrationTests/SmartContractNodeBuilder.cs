fae75154888a14572d79df044f69fc82b856a5c8
Jordan Andrews <jordandrews@hotmail.com>
2018-10-25 16:59:34 +1100

    SC PoA start - broken

---
+++ src/Stratis.SmartContracts.IntegrationTests/SmartContractNodeBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NBitcoin;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Features.PoA;
+using Stratis.Bitcoin.Features.PoA.IntegrationTests.Tools;
+using Stratis.Bitcoin.Features.SmartContracts.Networks;
+using Stratis.Bitcoin.IntegrationTests.Common.EnvironmentMockUpHelpers;
+using Stratis.Bitcoin.IntegrationTests.Common.Runners;
+using Stratis.SmartContracts.Networks;
+
+namespace Stratis.SmartContracts.IntegrationTests
+{
+    public class SmartContractNodeBuilder : NodeBuilder
+    {
+        public EditableTimeProvider TimeProvider { get; }
+
+        public SmartContractNodeBuilder(string rootFolder) : base(rootFolder)
+        {
+            this.TimeProvider = new EditableTimeProvider();
+        }
+
+        public CoreNode CreateSmartContractPoANode(Key key)
+        {
+            Network network = new SmartContractsPoARegTest();
+            string dataFolder = this.GetNextDataFolderName();
+            CoreNode node = this.CreateNode(new PoANodeRunner(dataFolder, network, this.TimeProvider), "poa.conf");
+
+            var settings = new NodeSettings(network, args: new string[] { "-conf=poa.conf", "-datadir=" + dataFolder });
+            var tool = new KeyTool(settings.DataFolder);
+            tool.SavePrivateKey(key);
+
+            return node;
+            Network network = new SmartContractsPoARegTest();
+            return CreateNode(new SmartContractPoARunner(this.GetNextDataFolderName(), network), "stratis.conf");
+        }
+
+        public CoreNode CreateSmartContractPowNode()
+        {
+            Network network = new SmartContractsRegTest();
+            return CreateNode(new StratisSmartContractNode(this.GetNextDataFolderName(), network), "stratis.conf");
+        }
+
+        public CoreNode CreateSmartContractPosNode()
+        {
+            Network network = new SmartContractPosRegTest();
+            return CreateNode(new StratisSmartContractPosNode(this.GetNextDataFolderName(), network), "stratis.conf");
+        }
+    }
+}
