6f3ba3868cc7615957a3c8e8a14d3035ec542f64
mikedennis <mike.dennis@stratisplatform.com>
2018-10-26 04:50:49 -0600

    RPC getbalance - only use mature coins (#2487)
    Bitcoin core only shows balance of coins that are mature with getbalance. Do similar.

--- src/Stratis.SmartContracts.IntegrationTests/PoW/SmartContractWalletTests.cs
+++ src/Stratis.SmartContracts.IntegrationTests/PoW/SmartContractWalletTests.cs
@@ -488,7 +488,7 @@ namespace Stratis.SmartContracts.IntegrationTests.PoW
         /// </summary>
         private static int GetSpendableBlocks(int mined, int maturity)
         {
-            return mined - (maturity - 1);
+            return mined - maturity;
         }
     }
 }
