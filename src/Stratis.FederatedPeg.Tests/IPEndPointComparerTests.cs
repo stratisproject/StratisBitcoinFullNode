c6d3f9fad671d0dbd2b73f53547297c28ce51807
monsieurleberre <monsieurleberre@users.noreply.github.com>
2018-10-30 14:56:30 +0300

    upgrades libraries (#144)
    * upgrades to
    Stratis.Bitcoin.* 1.2.3-beta
    NStratis 4.0.0.67
    Microsoft.NET.Test.Sdk 15.9.0
    Xunit 2.4.1
    
    That meant removing unused Integration test project which used corrupted Stratis.Bitcoin.IntegrationTests.Common temporarily

--- src/Stratis.FederatedPeg.Tests/IPEndPointComparerTests.cs
+++ src/Stratis.FederatedPeg.Tests/IPEndPointComparerTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using DBreeze.Utils;
+﻿using System.Net;
 using FluentAssertions;
 using Stratis.FederatedPeg.Features.FederationGateway.NetworkHelpers;
 using Xunit;
