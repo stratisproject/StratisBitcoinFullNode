76aab18b03e5b6b3a6f1031bdd217fbf555c3d6c
Dan Gould <dan@Dans-MacBook-Pro.local>
2018-11-01 16:36:26 -0400

    add BosStrat net

--- src/Stratis.Bitcoin.Networks/Networks.cs
+++ src/Stratis.Bitcoin.Networks/Networks.cs
@@ -22,5 +22,13 @@ namespace Stratis.Bitcoin.Networks
                 return new NetworksSelector(() => new StratisMain(), () => new StratisTest(), () => new StratisRegTest());
             }
         }
+
+        public static NetworksSelector BosStrat
+        {
+            get
+            {
+                return new NetworksSelector(() => new StratisMain(), () => new BostonStrat(), () => new StratisRegTest());
+            }
+        }
     }
 }
