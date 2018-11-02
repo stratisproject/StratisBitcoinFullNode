25601d12892c4d601e757667e38f775a75cf5689
Jordan Andrews <jordandrews@hotmail.com>
2018-11-02 11:51:21 +1100

    [SC] zero as coinbase if get fails. More integration tests. 3 second poa times on reg tests. (#2651)
    * done for poa
    
    * Still passing
    
    * PoA all tests running
    
    * Remove unused test

--- src/Stratis.SmartContracts.Networks/SmartContractsPoARegTest.cs
+++ src/Stratis.SmartContracts.Networks/SmartContractsPoARegTest.cs
@@ -35,18 +35,19 @@ namespace Stratis.SmartContracts.Networks
 
             this.Genesis = genesisBlock;
 
+            // Keeping the 3rd there in case we use it in future. For our integration tests we use 2 nodes currently.
             this.FederationKeys = new Key[]
             {
                 new Mnemonic("lava frown leave wedding virtual ghost sibling able mammal liar wide wisdom").DeriveExtKey().PrivateKey,
                 new Mnemonic("idle power swim wash diesel blouse photo among eager reward govern menu").DeriveExtKey().PrivateKey,
-                new Mnemonic("high neither night category fly wasp inner kitchen phone current skate hair").DeriveExtKey().PrivateKey
+                //new Mnemonic("high neither night category fly wasp inner kitchen phone current skate hair").DeriveExtKey().PrivateKey
             };
 
             var federationPubKeys = new List<PubKey>
             {
                 this.FederationKeys[0].PubKey, // 029528e83f065153d7fa655e73a07fc96fc759162f1e2c8936fa592f2942f39af0
                 this.FederationKeys[1].PubKey, // 03b539807c64abafb2d14c52a0d1858cc29d7c7fad0598f92a1274789c18d74d2d
-                this.FederationKeys[2].PubKey  // 02d6792cf941b68edd1e9056653573917cbaf974d46e9eeb9801d6fcedf846477a
+                // this.FederationKeys[2].PubKey  // 02d6792cf941b68edd1e9056653573917cbaf974d46e9eeb9801d6fcedf846477a
             };
 
             var consensusOptions = new PoAConsensusOptions(
@@ -56,7 +57,7 @@ namespace Stratis.SmartContracts.Networks
                 maxBlockSigopsCost: 20_000,
                 maxStandardTxSigopsCost: 20_000 / 5,
                 federationPublicKeys: federationPubKeys,
-                targetSpacingSeconds: this.ConsensusOptions.TargetSpacingSeconds
+                targetSpacingSeconds: 3
             );
 
             var buriedDeployments = new BuriedDeploymentsArray
@@ -86,7 +87,7 @@ namespace Stratis.SmartContracts.Networks
                 defaultAssumeValid: null,
                 maxMoney: long.MaxValue,
                 coinbaseMaturity: 1,
-                premineHeight: 10,
+                premineHeight: 5,
                 premineReward: Money.Coins(100_000_000),
                 proofOfWorkReward: Money.Coins(0),
                 powTargetTimespan: TimeSpan.FromSeconds(14 * 24 * 60 * 60), // two weeks
