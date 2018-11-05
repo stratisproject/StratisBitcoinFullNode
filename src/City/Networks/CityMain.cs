aac7a5be7840727b8958b3f586d9b0eea820577b
Sondre Bjellås <pixelworld@hotmail.com>
2018-11-05 20:53:04 +0100

    Last POW block will be at height 2500.
    - Mandatory update, make sure you update any nodes running City Chain before height 2500.

--- src/City/Networks/CityMain.cs
+++ src/City/Networks/CityMain.cs
@@ -101,7 +101,7 @@ namespace City.Networks
                 powLimit: new Target(new uint256("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")),
                 minimumChainWork: null,
                 isProofOfStake: true,
-                lastPowBlock: 12500,
+                lastPowBlock: 2500,
                 proofOfStakeLimit: new BigInteger(uint256.Parse("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff").ToBytes(false)),
                 proofOfStakeLimitV2: new BigInteger(uint256.Parse("000000000000ffffffffffffffffffffffffffffffffffffffffffffffffffff").ToBytes(false)),
                 proofOfStakeReward: Money.Coins(20)
