c0986f2d40e7ea28ff4b70838dd5a1168152ea9d
quantumagi <someguy.fromafrica@gmail.com>
2018-11-06 16:36:28 +1100

    Add Apex NetworkSelector (#148)

---
+++ src/Stratis.Sidechains.Networks/ApexNetworks.cs
@@ -0,0 +1,15 @@
+﻿using NBitcoin;
+
+namespace Stratis.Sidechains.Networks
+{
+    public static class ApexNetworks
+    {
+        public static NetworksSelector Apex
+        {
+            get
+            {
+                return new NetworksSelector(() => new ApexMain(), () => new ApexTest(), () => new ApexRegTest());
+            }
+        }
+    }
+}
