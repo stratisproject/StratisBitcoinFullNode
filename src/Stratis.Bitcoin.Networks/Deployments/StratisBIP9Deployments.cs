fd292c6e26af2d1c405757fff892ebdbbc05ceb4
quantumagi <someguy.fromafrica@gmail.com>
2018-11-07 03:02:29 +1100

    [ColdStaking] Merge coldstaking with fullnode (#2453)
    This PR merges the coldstaking master into the fullnode master.

--- src/Stratis.Bitcoin.Networks/Deployments/StratisBIP9Deployments.cs
+++ src/Stratis.Bitcoin.Networks/Deployments/StratisBIP9Deployments.cs
@@ -9,9 +9,10 @@ namespace Stratis.Bitcoin.Networks.Deployments
     {
         // The position of each deployment in the deployments array.
         public const int TestDummy = 0;
+        public const int ColdStaking = 2;
 
         // The number of deployments.
-        public const int NumberOfDeployments = 1;
+        public const int NumberOfDeployments = ColdStaking + 1;
 
         /// <summary>
         /// Constructs the BIP9 deployments array.
@@ -27,7 +28,16 @@ namespace Stratis.Bitcoin.Networks.Deployments
         /// <returns>The deployment flags.</returns>
         public override BIP9DeploymentFlags GetFlags(int deployment)
         {
-            return new BIP9DeploymentFlags();
+            var flags = new BIP9DeploymentFlags();
+
+            switch (deployment)
+            {
+                case ColdStaking:
+                    flags.ScriptFlags |= ScriptVerify.CheckColdStakeVerify;
+                    break;
+            }
+
+            return flags;
         }
     }
 }
