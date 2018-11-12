73501a65bbd59269d4af9db95908d9914145fd1c
monsieurleberre <monsieurleberre@users.noreply.github.com>
2018-11-12 19:54:41 +0300

    fix the serialisation of the hash, add test (#164)

--- src/Stratis.FederatedPeg.Features.FederationGateway/Interfaces/IBlockTip.cs
+++ src/Stratis.FederatedPeg.Features.FederationGateway/Interfaces/IBlockTip.cs
@@ -1,10 +1,13 @@
 ﻿using NBitcoin;
+using Newtonsoft.Json;
 using Stratis.Bitcoin.Utilities;
+using Stratis.Bitcoin.Utilities.JsonConverters;
 
 namespace Stratis.FederatedPeg.Features.FederationGateway.Interfaces
 {
     public interface IBlockTip
     {
+        [JsonConverter(typeof(UInt256JsonConverter))]
         uint256 Hash { get; }
 
         int Height { get; }
---
+++ src/Stratis.FederatedPeg.Tests/BlockTipModelTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Stratis.FederatedPeg.Features.FederationGateway.Interfaces;
+using Stratis.FederatedPeg.Features.FederationGateway.Models;
+using Stratis.FederatedPeg.Tests.Utils;
+using Xunit;
+
+namespace Stratis.FederatedPeg.Tests
+{
+    public class BlockTipModelTests
+    {
+        public static IBlockTip PrepareBlockTip()
+        {
+            var blockHash = TestingValues.GetUint256();
+            var blockHeight = TestingValues.GetPositiveInt();
+
+            var blockTip = new BlockTipModel(blockHash, blockHeight);
+            return blockTip;
+        }
+
+        [Fact]
+        public void ShouldSerialiseAsJson()
+        {
+            var blockTip = PrepareBlockTip();
+            var asJson = blockTip.ToString();
+
+            var reconverted = JsonConvert.DeserializeObject<BlockTipModel>(asJson);
+
+            reconverted.Hash.Should().Be(blockTip.Hash);
+            reconverted.Height.Should().Be(blockTip.Height);
+        }
+    }
+}
