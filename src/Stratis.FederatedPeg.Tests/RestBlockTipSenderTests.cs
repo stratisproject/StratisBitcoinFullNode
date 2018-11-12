3991441e3afeece48a715ba8db48090a5776ed7b
Ferdeen Mughal <ferdeen@gmail.com>
2018-11-12 16:59:47 +0000

    Test for RestBlockTipSender(). (#162)
    * Test for RestBlockTipSender().
    
    * Move PrepareWorkingHttpClient() and PrepareFailingHttpClient() into a shared class.
    
    Add SendBlockTip_Should_Log_Error_When_Failing_To_Send_IBlockTipAsync() to RestBlockTipSenderTests.

--- src/Stratis.FederatedPeg.Tests/RestBlockTipSenderTests.cs
+++ src/Stratis.FederatedPeg.Tests/RestBlockTipSenderTests.cs
@@ -4,13 +4,14 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
 using Stratis.FederatedPeg.Features.FederationGateway;
+using Stratis.FederatedPeg.Features.FederationGateway.Models;
 using Stratis.FederatedPeg.Features.FederationGateway.SourceChain;
 using Stratis.FederatedPeg.Tests.Utils;
 using Xunit;
 
 namespace Stratis.FederatedPeg.Tests
 {
-    public class RestMaturedBlockSenderTests : IDisposable
+    public class RestBlockTipSenderTests : IDisposable
     {
         private IHttpClientFactory httpClientFactory;
 
@@ -24,7 +25,7 @@ namespace Stratis.FederatedPeg.Tests
 
         private ILogger logger;
 
-        public RestMaturedBlockSenderTests()
+        public RestBlockTipSenderTests()
         {
             this.loggerFactory = Substitute.For<ILoggerFactory>();
             this.logger = Substitute.For<ILogger>();
@@ -33,29 +34,29 @@ namespace Stratis.FederatedPeg.Tests
         }
 
         [Fact]
-        public async Task SendMaturedBlockDeposits_Should_Be_Able_To_Send_IMaturedBlockDepositsAsync()
+        public async Task SendBlockTip_Should_Be_Able_To_Send_IBlockTipAsync()
         {
             TestingHttpClient.PrepareWorkingHttpClient(ref this.messageHandler, ref this.httpClient, ref this.httpClientFactory);
 
-            var maturedBlockDeposits = MaturedBlockDepositModelTests.PrepareMaturedBlockDeposits();
+            var restSender = new RestBlockTipSender(this.loggerFactory, this.federationSettings, this.httpClientFactory);
 
-            var restSender = new RestMaturedBlockSender(this.loggerFactory, this.federationSettings, this.httpClientFactory);
+            var blockTip = new BlockTipModel(TestingValues.GetUint256(), TestingValues.GetPositiveInt());
 
-            await restSender.SendMaturedBlockDepositsAsync(maturedBlockDeposits).ConfigureAwait(false);
+            await restSender.SendBlockTipAsync(blockTip).ConfigureAwait(false);
 
             this.logger.Received(0).Log<object>(LogLevel.Error, 0, Arg.Any<object>(), Arg.Any<Exception>(), Arg.Any<Func<object, Exception, string>>());
         }
 
         [Fact]
-        public async Task SendMaturedBlockDeposits_Should_Log_Error_When_Failing_To_Send_MaturedBlockDepositAsync()
+        public async Task SendBlockTip_Should_Log_Error_When_Failing_To_Send_IBlockTipAsync()
         {
             TestingHttpClient.PrepareFailingHttpClient(ref this.messageHandler, ref this.httpClient, ref this.httpClientFactory);
 
-            var maturedBlockDeposits = MaturedBlockDepositModelTests.PrepareMaturedBlockDeposits();
+            var restSender = new RestBlockTipSender(this.loggerFactory, this.federationSettings, this.httpClientFactory);
 
-            var restSender = new RestMaturedBlockSender(this.loggerFactory, this.federationSettings, this.httpClientFactory);
+            var blockTip = new BlockTipModel(TestingValues.GetUint256(), TestingValues.GetPositiveInt());
 
-            await restSender.SendMaturedBlockDepositsAsync(maturedBlockDeposits).ConfigureAwait(false);
+            await restSender.SendBlockTipAsync(blockTip).ConfigureAwait(false);
 
             this.logger.Received(1).Log<object>(LogLevel.Error, 0, Arg.Any<object>(), Arg.Is<Exception>(e => e != null), Arg.Any<Func<object, Exception, string>>());
         }
---
+++ src/Stratis.FederatedPeg.Tests/Utils/TestingHttpClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+
+namespace Stratis.FederatedPeg.Tests.Utils
+{
+    public static class TestingHttpClient
+    {
+        public static void PrepareWorkingHttpClient(ref HttpMessageHandler httpMessageHandler, ref HttpClient httpClient, ref IHttpClientFactory httpClientFactory)
+        {
+            PrepareHttpClient(ref httpMessageHandler, ref httpClient, ref httpClientFactory);
+        }
+
+        public static void PrepareFailingHttpClient(ref HttpMessageHandler httpMessageHandler, ref HttpClient httpClient, ref IHttpClientFactory httpClientFactory)
+        {
+            PrepareHttpClient(ref httpMessageHandler, ref httpClient, ref httpClientFactory, true);
+        }
+
+        private static void PrepareHttpClient(ref HttpMessageHandler httpMessageHandler, ref HttpClient httpClient, ref IHttpClientFactory httpClientFactory, bool failingClient = false)
+        {
+            httpMessageHandler = Substitute.ForPartsOf<HttpMessageHandler>();
+
+            var sendCall = httpMessageHandler.Protected("SendAsync", Arg.Any<HttpRequestMessage>(), Arg.Any<CancellationToken>());
+
+            if (failingClient)
+                sendCall.ThrowsForAnyArgs(new Exception("failed"));
+            else
+                sendCall.Returns(Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)));
+
+            httpClient = new HttpClient(httpMessageHandler);
+            httpClientFactory = Substitute.For<IHttpClientFactory>();
+            httpClientFactory.CreateClient(Arg.Any<string>()).Returns(httpClient);
+        }
+    }
+}
