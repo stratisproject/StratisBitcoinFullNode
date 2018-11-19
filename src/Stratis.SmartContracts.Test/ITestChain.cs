4ed85a02151be8e81f0a9d70f1a7c3db8423105a
Jordan Andrews <jordandrews@hotmail.com>
2018-11-19 23:16:14 +1100

    ITestChain

--- src/Stratis.SmartContracts.Test/ITestChain.cs
+++ src/Stratis.SmartContracts.Test/ITestChain.cs
@@ -9,7 +9,9 @@ using Stratis.Bitcoin.Features.BlockStore;
 using Stratis.Bitcoin.Features.MemoryPool;
 using Stratis.Bitcoin.Features.RPC;
 using Stratis.Bitcoin.Features.SmartContracts;
+using Stratis.Bitcoin.Features.SmartContracts.Models;
 using Stratis.Bitcoin.Features.SmartContracts.PoA;
+using Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Consensus.Rules;
 using Stratis.Bitcoin.Features.SmartContracts.Wallet;
 using Stratis.Bitcoin.Utilities;
 using Stratis.SmartContracts.Networks;
@@ -18,17 +20,59 @@ namespace Stratis.SmartContracts.Test
 {
     public interface ITestChain
     {
-        IReadOnlyList<string> Addresses { get; }
+        /// <summary>
+        /// 10 addresses that come preloaded with funds.
+        /// </summary>
+        IReadOnlyList<string> PreloadedAddresses { get; }
+
+        /// <summary>
+        /// Get the balance in stratoshis for an address.
+        /// </summary>
+        /// <param name="address">Address to get the balance for.</param>
+        /// <returns>Balance for this address.</returns>
+        ulong GetBalanceInStratoshis(string address);
 
         /// <summary>
         /// Performs all required setup for network.
         /// </summary>
         void Initialize();
-        
+
         /// <summary>
         /// Mine blocks on the network. Miner selection algorithm depends on consensus model.
         /// </summary>
         /// <param name="num">Number of blocks to mine.</param>
         void MineBlocks(int num);
+
+        BuildCreateContractTransactionResponse SendCreateContractTransaction(
+            string from,
+            byte[] contractCode,
+            double amount,
+            string[] parameters = null,
+            ulong gasLimit = SmartContractFormatRule.GasLimitMaximum / 2, // half of maximum
+            ulong gasPrice = SmartContractMempoolValidator.MinGasPrice,
+            double feeAmount = 0.01);
+
+        BuildCallContractTransactionResponse SendCallContractTransaction(
+            string from,
+            string methodName,
+            string contractAddress,
+            double amount,
+            string[] parameters = null,
+            ulong gasLimit = SmartContractFormatRule.GasLimitMaximum / 2, // half of maximum
+            ulong gasPrice = SmartContractMempoolValidator.MinGasPrice,
+            double feeAmount = 0.01);
+
+        /// <summary>
+        /// Get the bytecode stored at a particular contract address.
+        /// </summary>
+        /// <param name="contractAddress">Address of the contract to get code for.</param>
+        /// <returns>Code at this address.</returns>
+        byte[] GetCode(string contractAddress);
+
+        /// <summary>
+        /// Get the newest block mined on the network.
+        /// </summary>
+        /// <returns>Newest block mined on the network.</returns>
+        Block GetLastBlock();
     }
 }
