03bcdbfc388e940c621e7b5b07997723a889bbd8
Dan Gershony <dan.gershony@gmail.com>
2018-11-20 23:32:15 +0000

    Remove duplicate resync

--- src/Stratis.Bitcoin/Consensus/ConsensusManagerBehavior.cs
+++ src/Stratis.Bitcoin/Consensus/ConsensusManagerBehavior.cs
@@ -73,16 +73,6 @@ namespace Stratis.Bitcoin.Consensus
         /// <summary>Protects write access to the <see cref="BestSentHeader"/>.</summary>
         private readonly object bestSentHeaderLock;
 
-        /// <summary>
-        /// The last time we forced a resync
-        /// </summary>
-        private DateTime lastForcedResync = DateTime.MinValue;
-
-        /// <summary>
-        /// The minimum time it has to pass, between two forced resync.
-        /// </summary>
-        private TimeSpan forceResyncThreshold = TimeSpan.FromMinutes(1);
-
         public ConsensusManagerBehavior(ConcurrentChain chain, IInitialBlockDownloadState initialBlockDownloadState, IConsensusManager consensusManager, IPeerBanning peerBanning, ILoggerFactory loggerFactory)
         {
             this.loggerFactory = loggerFactory;
@@ -299,7 +289,6 @@ namespace Stratis.Bitcoin.Consensus
                     {
                         this.logger.LogTrace("Header {0} could not be connected to last cached header {1}, clear cache and resync.", headers[0].GetHash(), lastCachedHeaderHash);
                         this.logger.LogTrace("(-)[FAILED_TO_ATTACH_TO_CACHE]");
-                        this.lastForcedResync = DateTime.UtcNow;
                         this.cachedHeaders.Clear();
                         await this.ResyncAsync().ConfigureAwait(false);
                         return;
@@ -310,15 +299,6 @@ namespace Stratis.Bitcoin.Consensus
 
                 if (result == null)
                 {
-                    // If a peer has a new block and our current peer is not exactly at the tip,
-                    // we try to trigger a resync, this will bring us to the tip of the peer.
-                    if (headers.Count == 1 && (DateTime.UtcNow - this.lastForcedResync) >= this.forceResyncThreshold)
-                    {
-                        this.lastForcedResync = DateTime.UtcNow;
-                        this.logger.LogTrace("Header {0} could not be connected try to trigger a resync.", headers[0].GetHash());
-                        await this.ResyncAsync().ConfigureAwait(false);
-                    }
-
                     this.logger.LogTrace("Processing of {0} headers failed.", headers.Count);
                     this.logger.LogTrace("(-)[PROCESSING_FAILED]");
 
