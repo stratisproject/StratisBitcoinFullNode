43c2aa1024d107775f26d00ce4242fbe05c715ca
Rowan de Haas <rowandh@users.noreply.github.com>
2018-11-22 13:23:20 +1100

    Remove unused components

--- src/Stratis.SmartContracts.Executor.Reflection/Exceptions/DisallowedAssemblyReferenceException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Stratis.SmartContracts.Executor.Reflection.Exceptions
-{
-    public class DisallowedAssemblyReferenceException : StratisCompilationException
-    {
-        public DisallowedAssemblyReferenceException() { }
-        public DisallowedAssemblyReferenceException(string message) : base(message) { }
-    }
-}
