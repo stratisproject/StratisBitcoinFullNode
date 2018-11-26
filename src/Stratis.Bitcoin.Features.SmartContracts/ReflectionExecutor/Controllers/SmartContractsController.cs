9407bbe629ca1ec48f6d8fa9201aba17bb2e49bd
Jordan Andrews <jordandrews@hotmail.com>
2018-11-26 13:08:05 +1100

    Contract Decompilation out + upgrade Mono.Cecil

--- src/Stratis.Bitcoin.Features.SmartContracts/ReflectionExecutor/Controllers/SmartContractsController.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/ReflectionExecutor/Controllers/SmartContractsController.cs
@@ -5,8 +5,6 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
-using ICSharpCode.Decompiler;
-using ICSharpCode.Decompiler.CSharp;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Mono.Cecil;
@@ -22,6 +20,7 @@ using Stratis.Bitcoin.Utilities.JsonErrors;
 using Stratis.Bitcoin.Utilities.ModelStateErrors;
 using Stratis.SmartContracts;
 using Stratis.SmartContracts.Core;
+using Stratis.SmartContracts.Core.Decompilation;
 using Stratis.SmartContracts.Core.Receipts;
 using Stratis.SmartContracts.Core.State;
 using Stratis.SmartContracts.Executor.Reflection;
@@ -43,6 +42,7 @@ namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Controllers
         private readonly IBroadcasterManager broadcasterManager;
         private readonly IBlockStore blockStore;
         private readonly ConcurrentChain chain;
+        private readonly IContractDecompiler contractDecompiler;
         private readonly ILogger logger;
         private readonly Network network;
         private readonly IStateRepositoryRoot stateRoot;
@@ -55,7 +55,7 @@ namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Controllers
         public SmartContractsController(IBroadcasterManager broadcasterManager,
             IBlockStore blockStore,
             ConcurrentChain chain,
-            IConsensusManager consensus,
+            IContractDecompiler contractDecompiler,
             IDateTimeProvider dateTimeProvider,
             ILoggerFactory loggerFactory,
             Network network,
@@ -67,6 +67,7 @@ namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Controllers
             ISmartContractTransactionService smartContractTransactionService)
         {
             this.stateRoot = stateRoot;
+            this.contractDecompiler = contractDecompiler;
             this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
             this.network = network;
             this.chain = chain;
@@ -94,20 +95,14 @@ namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Controllers
                 });
             }
 
-            using (var memStream = new MemoryStream(contractCode))
+            Result<string> sourceResult = this.contractDecompiler.GetSource(contractCode);
+
+            return Json(new GetCodeResponse
             {
-                var modDefinition = ModuleDefinition.ReadModule(memStream);
-                var decompiler = new CSharpDecompiler(modDefinition, new DecompilerSettings { });
-                // TODO: Update decompiler to display all code, not just this rando FirstOrDefault (given we now allow multiple types)
-                string cSharp = decompiler.DecompileAsString(modDefinition.Types.FirstOrDefault(x => x.FullName != "<Module>"));
-                
-                return Json(new GetCodeResponse
-                {
-                    Message = string.Format("Contract execution code retrieved at {0}", address),
-                    Bytecode = contractCode.ToHexString(),
-                    CSharp = cSharp
-                });
-            }
+                Message = string.Format("Contract execution code retrieved at {0}", address),
+                Bytecode = contractCode.ToHexString(),
+                CSharp = sourceResult.IsSuccess ? sourceResult.Value : sourceResult.Error // Show the source, or the reason why the source couldn't be retrieved.
+            });
         }
 
         [Route("balance")]
---
+++ src/Stratis.SmartContracts.Executor.Reflection/Decompilation/CSharpContractDecompiler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CSharpFunctionalExtensions;
+using ICSharpCode.Decompiler;
+using ICSharpCode.Decompiler.CSharp;
+using Mono.Cecil;
+using Stratis.SmartContracts.Core.Decompilation;
+
+namespace Stratis.SmartContracts.Executor.Reflection.Decompilation
+{
+    public class CSharpContractDecompiler : IContractDecompiler
+    {
+        public Result<string> GetSource(byte[] bytecode)
+        {
+            using (var memStream = new MemoryStream(bytecode))
+            {
+                var modDefinition = ModuleDefinition.ReadModule(memStream);
+                var decompiler = new CSharpDecompiler(modDefinition, new DecompilerSettings { });
+                // TODO: Update decompiler to display all code, not just this rando FirstOrDefault (given we now allow multiple types)
+                string cSharp = decompiler.DecompileAsString(modDefinition.Types.FirstOrDefault(x => x.FullName != "<Module>"));
+                return Result.Ok(cSharp);
+            }
+        }
+    }
+}
