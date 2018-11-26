a11b1e9a4322479cc08bf3c6fafd6e635438014c
Dan Gershony <dan.gershony@gmail.com>
2018-11-26 12:53:43 +0000

    Load the tip from data store, in case it was rewinded.

--- src/Stratis.Bitcoin.Features.Consensus/ProvenBlockHeaders/RewindDataIndexStore.cs
+++ src/Stratis.Bitcoin.Features.Consensus/ProvenBlockHeaders/RewindDataIndexStore.cs
@@ -41,6 +41,11 @@ namespace Stratis.Bitcoin.Features.Consensus.ProvenBlockHeaders
         /// <inheritdoc />
         public async Task InitializeAsync(IConsensus consensusParameters, ChainedHeader tip, ICoinView coinView)
         {
+            // A temporary hack until tip manage will be introduced.
+            var breezeCoinView = (DBreezeCoinView)((CachedCoinView)coinView).Inner;
+            uint256 hash = await breezeCoinView.GetTipHashAsync().ConfigureAwait(false);
+            tip = tip.FindAncestorOrSelf(hash);
+
             this.numberOfBlocksToKeep = (int)consensusParameters.MaxReorgLength;
 
             int heightToSyncTo = tip.Height > this.numberOfBlocksToKeep ? tip.Height - this.numberOfBlocksToKeep : 0;
