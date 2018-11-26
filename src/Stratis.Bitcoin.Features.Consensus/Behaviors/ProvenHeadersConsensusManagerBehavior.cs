12f9cd74b5e9023e908f6f0733650056713dcd54
noescape0 <NoEscape0@ya.ru>
2018-11-26 18:16:25 +0300

    updated comment

--- src/Stratis.Bitcoin.Features.Consensus/Behaviors/ProvenHeadersConsensusManagerBehavior.cs
+++ src/Stratis.Bitcoin.Features.Consensus/Behaviors/ProvenHeadersConsensusManagerBehavior.cs
@@ -135,7 +135,7 @@ namespace Stratis.Bitcoin.Features.Consensus.Behaviors
                     {
                         // Proven header is not available yet for this header.
                         // This can happen in case headers were requested by the peer right after we advanced consensus tip
-                        // So at this moment proven header is not created yet for the block connected.
+                        // So at this moment proven header is not created or not yet saved to headers store for the block connected.
                         this.logger.LogTrace("(-)[NO_PH_AVAILABLE]:{header}", header);
                         break;
                     }
