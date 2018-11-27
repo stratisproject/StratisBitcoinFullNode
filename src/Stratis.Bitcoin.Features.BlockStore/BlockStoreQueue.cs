c89d8e267dc45801d51b319599ac1e2dba74a11e
Francois de la Rouviere <fassadlr@gmail.com>
2018-11-27 17:31:18 +0000

    [BlockStore] Dequeue all items from the async queue on dispose (#2837)
    * Add items from async queue to batch before saving
    
    * Fixes as per review
    
    * Changes as per review

--- src/Stratis.Bitcoin.Features.BlockStore/BlockStoreQueue.cs
+++ src/Stratis.Bitcoin.Features.BlockStore/BlockStoreQueue.cs
@@ -394,6 +394,21 @@ namespace Stratis.Bitcoin.Features.BlockStore
                 }
             }
 
+            await this.FlushAllCollectionsAsync();
+        }
+
+        /// <summary>
+        /// Ensures that any blocks queued in <see cref="blocksQueue"/> gets added to <see cref="batch"/>
+        /// so that it can be persisted on dispose.
+        /// </summary>
+        private async Task FlushAllCollectionsAsync()
+        {
+            ChainedHeaderBlock chainedHeaderBlock = null;
+            while (this.blocksQueue.TryDequeue(out chainedHeaderBlock))
+            {
+                this.batch.Add(chainedHeaderBlock);
+            }
+
             if (this.batch.Count != 0)
                 await this.SaveBatchAsync().ConfigureAwait(false);
         }
