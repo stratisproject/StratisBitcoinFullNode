bb9a45f94c8c895c05f8376c61cde14e14202d19
Ferdeen Mughal <ferdeen@gmail.com>
2018-11-27 09:17:54 +0000

    Federation Setup Console. (#189)
    * Add GenesisMiner and MultisigAddressCreator to console.
    
    Renamed console.
    
    * Changes after PR review.
    
    Changed switch values.
    
    Allow end user to select network.
    
    Allow end user to select quorum.
    
    * Quorum and Fed member argument validation.
    
    * Console OutputUsage update.
    
    * Reduced commandLineArgs in debug from 6 fed addresses down to 5.
    
    Removed actual keys from FederationSetup.OutputUsage().
    
    MultisigAddressCreator now takes fed pubkeys from argument list not hard wired as before.
    
    Generating Fed pubkeys creates two lots.  One for transfer of funds to sidechain and the other for sidechain mining.
    
    * Removed ReadLine() switch check.

--- MultisigAddressCreator.cs
+++ MultisigAddressCreator.cs
@@ -1,23 +1,21 @@
 ﻿using System;
-
+using System.Text;
 using NBitcoin;
 using NBitcoin.DataEncoders;
-
-using Stratis.Bitcoin.Features.PoA;
 using Stratis.Bitcoin.Networks;
 using Stratis.Sidechains.Networks;
-
 using Xunit;
 using Xunit.Abstractions;
 
-namespace FedKeyPairGen
+namespace FederationSetup
 {
     public class MultisigAddressCreator
     {
         private readonly ITestOutputHelper output;
 
-        public MultisigAddressCreator(ITestOutputHelper output)
+        public MultisigAddressCreator(ITestOutputHelper output = null)
         {
+            if (output == null) return;
             this.output = output;
         }
 
@@ -28,37 +26,35 @@ namespace FedKeyPairGen
             var mainchainNetwork = Networks.Stratis.Testnet();
             var sidechainNetwork = FederatedPegNetwork.NetworksSelector.Testnet();
 
-            this.CreateMultisigAddresses(mainchainNetwork, sidechainNetwork);
+            string password = "mypassword";
+
+            // Create a mnemonic and get the corresponding pubKey.
+            PubKey[] pubKeys = new PubKey[1];
+            var mnemonic = new Mnemonic(Wordlist.English, WordCount.Twelve);
+            pubKeys[0] = mnemonic.DeriveExtKey().PrivateKey.PubKey;
+
+            this.output.WriteLine($"Mnemonic - Please note the following 12 words down in a secure place: {string.Join(" ", mnemonic.Words)}");
+            this.output.WriteLine($"PubKey   - Please share the following public key with the person responsible for the sidechain generation: {Encoders.Hex.EncodeData((pubKeys[0]).ToBytes(false))}");
+
+            this.output.WriteLine(this.CreateMultisigAddresses(mainchainNetwork, sidechainNetwork, pubKeys));
         }
 
-        public void CreateMultisigAddresses(Network mainchainNetwork, Network sidechainNetwork, int quorum = 2, int keysCount = 5)
+        public string CreateMultisigAddresses(Network mainchainNetwork, Network sidechainNetwork, PubKey[] pubKeys, int quorum = 3)
         {
-            PubKey[] pubKeys = new PubKey[keysCount];
-
-            for (int i = 0; i < keysCount; i++)
-            {
-                string password = "mypassword";
-
-                // Create a mnemonic and get the corresponding pubKey.
-                Mnemonic mnemonic = new Mnemonic(Wordlist.English, WordCount.Twelve);
-                var pubKey = mnemonic.DeriveExtKey().PrivateKey.PubKey;
-                pubKeys[i] = pubKey;
-
-                this.output.WriteLine($"Mnemonic - Please note the following 12 words down in a secure place: {string.Join(" ", mnemonic.Words)}");
-                this.output.WriteLine($"PubKey   - Please share the following public key with the person responsible for the sidechain generation: {Encoders.Hex.EncodeData((pubKey).ToBytes(false))}");
-                this.output.WriteLine(Environment.NewLine);
-            }
+            var output = new StringBuilder();
 
             Script payToMultiSig = PayToMultiSigTemplate.Instance.GenerateScriptPubKey(quorum, pubKeys);
-            this.output.WriteLine("Redeem script: " + payToMultiSig.ToString());
+            output.AppendLine("Redeem script: " + payToMultiSig.ToString());
 
             BitcoinAddress sidechainMultisigAddress = payToMultiSig.Hash.GetAddress(sidechainNetwork);
-            this.output.WriteLine("Sidechan P2SH: " + sidechainMultisigAddress.ScriptPubKey);
-            this.output.WriteLine("Sidechain Multisig address: " + sidechainMultisigAddress);
+            output.AppendLine("Sidechan P2SH: " + sidechainMultisigAddress.ScriptPubKey);
+            output.AppendLine("Sidechain Multisig address: " + sidechainMultisigAddress);
 
             BitcoinAddress mainchainMultisigAddress = payToMultiSig.Hash.GetAddress(mainchainNetwork);
-            this.output.WriteLine("Mainchain P2SH: " + mainchainMultisigAddress.ScriptPubKey);
-            this.output.WriteLine("Mainchain Multisig address: " + mainchainMultisigAddress);
+            output.AppendLine("Mainchain P2SH: " + mainchainMultisigAddress.ScriptPubKey);
+            output.AppendLine("Mainchain Multisig address: " + mainchainMultisigAddress);
+
+            return output.ToString();
         }
     }
 }
