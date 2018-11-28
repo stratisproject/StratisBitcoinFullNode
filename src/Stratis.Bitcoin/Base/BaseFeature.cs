914e2549e62735ffcf09b2dc21b90544082930ab
Francois de la Rouviere <fassadlr@gmail.com>
2018-11-28 18:56:18 +0000

    [ProvenHeaders] Set PHBS tip to ChainTip if its ahead (#2853)
    * Fix issue with exceptions on startup
    
    * Formatting
    
    * Move PHBS tip to ChainTip
    
    * Fixes as per review
    
    * Logging fixes
    
    * Final

--- src/Stratis.Bitcoin/Base/BaseFeature.cs
+++ src/Stratis.Bitcoin/Base/BaseFeature.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
