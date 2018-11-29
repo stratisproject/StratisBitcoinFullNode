4c3086777943c7dfc73c4856e3cf5c0d092ad21e
Rowan de Haas <rowandh@users.noreply.github.com>
2018-11-29 17:45:09 +1100

    Add default connection params to SC networks (#2858)

--- src/Stratis.SmartContracts.Networks/SmartContractPosTest.cs
+++ src/Stratis.SmartContracts.Networks/SmartContractPosTest.cs
@@ -4,7 +4,6 @@ using NBitcoin;
 using NBitcoin.BouncyCastle.Math;
 using NBitcoin.DataEncoders;
 using NBitcoin.Protocol;
-using Stratis.Bitcoin.Networks.Deployments;
 using Stratis.Bitcoin.Features.SmartContracts.PoS;
 
 namespace Stratis.Bitcoin.Features.SmartContracts.Networks
@@ -18,6 +17,8 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Networks
             this.DefaultConfigFilename = SmartContractNetwork.StratisDefaultConfigFilename;
             this.Magic = 0x0709110F; // Incremented 09/08
             this.DefaultPort = 18333;
+            this.DefaultMaxOutboundConnections = 16;
+            this.DefaultMaxInboundConnections = 109;
             this.RPCPort = 18332;
             this.MaxTipAge = SmartContractNetwork.BitcoinDefaultMaxTipAgeInSeconds;
             this.MinTxFee = 1000;
