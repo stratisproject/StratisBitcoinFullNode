7254c0d5410856586e6894fdab36fed84b608815
monsieurleberre <monsieurleberre@users.noreply.github.com>
2018-11-30 13:22:32 -0300

    add runner special for sidechain network (#204)
    (where the premine goes to multisig)

---
+++ src/Stratis.FederatedPeg.IntegrationTests/Utils/SidechainNodeBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+using NBitcoin;
+
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Features.PoA;
+using Stratis.Bitcoin.Features.PoA.IntegrationTests.Common;
+using Stratis.Bitcoin.IntegrationTests.Common.EnvironmentMockUpHelpers;
+using Stratis.Bitcoin.Tests.Common;
+
+namespace Stratis.FederatedPeg.IntegrationTests.Utils
+{
+    public class SidechainNodeBuilder : NodeBuilder
+    {
+        public EditableTimeProvider TimeProvider { get; }
+
+        private SidechainNodeBuilder(string rootFolder) : base(rootFolder)
+        {
+            this.TimeProvider = new EditableTimeProvider();
+        }
+
+        public static SidechainNodeBuilder CreatePoANodeBuilder(object caller, [CallerMemberName] string callingMethod = null)
+        {
+            string testFolderPath = TestBase.CreateTestDir(caller, callingMethod);
+            SidechainNodeBuilder builder = new SidechainNodeBuilder(testFolderPath);
+            builder.WithLogsDisabled();
+
+            return builder;
+        }
+
+        public CoreNode CreatePoANode(PoANetwork network)
+        {
+            return this.CreateNode(new PoANodeRunner(this.GetNextDataFolderName(), network, this.TimeProvider), "poa.conf");
+        }
+
+        public CoreNode CreatePoANode(PoANetwork network, Key key)
+        {
+            string dataFolder = this.GetNextDataFolderName();
+            CoreNode node = this.CreateNode(new PoANodeRunner(dataFolder, network, this.TimeProvider), "poa.conf");
+
+            var settings = new NodeSettings(network, args: new string[] { "-conf=poa.conf", "-datadir=" + dataFolder });
+            var tool = new KeyTool(settings.DataFolder);
+            tool.SavePrivateKey(key);
+
+            return node;
+        }
+    }
+}
---
+++ src/Stratis.FederatedPeg.IntegrationTests/Utils/SidechainNodeRunner.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Stratis.Bitcoin;
+using Stratis.Bitcoin.Builder;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Features.Api;
+using Stratis.Bitcoin.Features.BlockStore;
+using Stratis.Bitcoin.Features.MemoryPool;
+using Stratis.Bitcoin.Features.PoA;
+using Stratis.Bitcoin.Features.PoA.IntegrationTests.Common;
+using Stratis.Bitcoin.Features.RPC;
+using Stratis.Bitcoin.Features.Wallet;
+using Stratis.Bitcoin.IntegrationTests.Common;
+using Stratis.Bitcoin.IntegrationTests.Common.Runners;
+using Stratis.Bitcoin.Utilities;
+using Stratis.FederatedPeg.Features.FederationGateway;
+
+namespace Stratis.FederatedPeg.IntegrationTests.Utils
+{
+    public class SidechainNodeRunner : NodeRunner
+    {
+        private IDateTimeProvider timeProvider;
+
+        public SidechainNodeRunner(string dataDir, PoANetwork network, EditableTimeProvider timeProvider)
+            : base(dataDir)
+        {
+            this.Network = network;
+            this.timeProvider = timeProvider;
+        }
+
+        public override void BuildNode()
+        {
+            var settings = new NodeSettings(this.Network, args: new string[] { "-conf=poa.conf", "-datadir=" + this.DataFolder });
+
+            this.FullNode = (FullNode)new FullNodeBuilder()
+                .UseNodeSettings(settings)
+                .UseBlockStore()
+                .UseFederatedPegPoAMining()
+                .UseMempool()
+                .UseWallet()
+                .UseApi()
+                .AddRPC()
+                .MockIBD()
+                .ReplaceTimeProvider(this.timeProvider)
+                .AddFastMiningCapability()
+                .Build();
+        }
+    }
+}
