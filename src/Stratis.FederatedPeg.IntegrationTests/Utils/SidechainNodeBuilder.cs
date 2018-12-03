99faee8336fbcbaf430c7a2f5aa2035ec909a37e
monsieurleberre <monsieurleberre@users.noreply.github.com>
2018-12-03 10:12:14 -0300

    Allow minimum deposit confirmations to be changed from config param (for testing) (#206)
    * add stratis-dev feed
    and remove powershell-core which has become useless for this project
    
    * 1.2.5-beta to 5547
    
    * upgrade from 1.2.5-beta to 1.2.6-beta
    
    * allow MinimumDepositConfirmations from settings

--- src/Stratis.FederatedPeg.IntegrationTests/Utils/SidechainNodeBuilder.cs
+++ src/Stratis.FederatedPeg.IntegrationTests/Utils/SidechainNodeBuilder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 
 using NBitcoin;
 
@@ -17,12 +18,14 @@ namespace Stratis.FederatedPeg.IntegrationTests.Utils
     {
         public EditableTimeProvider TimeProvider { get; }
 
+        public static int agentCount;
+
         private SidechainNodeBuilder(string rootFolder) : base(rootFolder)
         {
             this.TimeProvider = new EditableTimeProvider();
         }
 
-        public static SidechainNodeBuilder CreatePoANodeBuilder(object caller, [CallerMemberName] string callingMethod = null)
+        public static SidechainNodeBuilder CreateSidechainNodeBuilder(object caller, [CallerMemberName] string callingMethod = null)
         {
             string testFolderPath = TestBase.CreateTestDir(caller, callingMethod);
             SidechainNodeBuilder builder = new SidechainNodeBuilder(testFolderPath);
@@ -31,15 +34,17 @@ namespace Stratis.FederatedPeg.IntegrationTests.Utils
             return builder;
         }
 
-        public CoreNode CreatePoANode(PoANetwork network)
+        public CoreNode CreateSidechainNode(Network network)
         {
-            return this.CreateNode(new PoANodeRunner(this.GetNextDataFolderName(), network, this.TimeProvider), "poa.conf");
+            var agentName = $"sidechain{Interlocked.Increment(ref agentCount)}";
+            return this.CreateNode(new SidechainNodeRunner(this.GetNextDataFolderName(agentName), agentName, network, this.TimeProvider), "poa.conf");
         }
 
-        public CoreNode CreatePoANode(PoANetwork network, Key key)
+        public CoreNode CreateSidechainNode(Network network, Key key)
         {
-            string dataFolder = this.GetNextDataFolderName();
-            CoreNode node = this.CreateNode(new PoANodeRunner(dataFolder, network, this.TimeProvider), "poa.conf");
+            var agentName = $"sidechain{Interlocked.Increment(ref agentCount)}";
+            string dataFolder = this.GetNextDataFolderName(agentName);
+            CoreNode node = this.CreateNode(new SidechainNodeRunner(dataFolder, agentName, network, this.TimeProvider), "poa.conf");
 
             var settings = new NodeSettings(network, args: new string[] { "-conf=poa.conf", "-datadir=" + dataFolder });
             var tool = new KeyTool(settings.DataFolder);
