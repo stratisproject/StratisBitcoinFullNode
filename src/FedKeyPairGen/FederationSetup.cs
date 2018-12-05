826b9c0fa63ff2131991b0a5485b09b9f71902f1
Jeremy Bokobza <bokobza@users.noreply.github.com>
2018-12-05 10:09:03 +0000

    Adapted the console app to have a menu (#216)
    Changed the console app to have a menu rather than being a command line tool.
    The reason for that is that this utility is to be used by federation members and this change makes that easier.
    Also changed it so that it loops until the user exits
    
    ![image](https://user-images.githubusercontent.com/1867877/49459731-a276c980-f7e7-11e8-8db3-2e8ce39ec441.png)

--- src/FedKeyPairGen/FederationSetup.cs
+++ src/FedKeyPairGen/FederationSetup.cs
@@ -6,48 +6,50 @@ namespace FederationSetup
     // The FedKeyPairGenManager handles console output and input.
     internal static class FederationSetup
     {
-        // Standard information header.
+        /// <summary>
+        /// Print the utility's header and menu.
+        /// </summary>
         public static void OutputHeader()
         {
-            Console.WriteLine($"Stratis Federation Set up v{Assembly.GetEntryAssembly().GetName().Version.ToString()}");
-            Console.WriteLine("* Creates genesis blocks for Main/Test/Reg networks.");
-            Console.WriteLine("* Generates cryptographic private and public keys for Sidechain Federation Members.");
-            Console.WriteLine("* Create multisig address.");
             Console.WriteLine(Environment.NewLine);
-            Console.WriteLine("For help : federationsetup -h");
-            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine($"Stratis Federation Set up v{Assembly.GetEntryAssembly().GetName().Version}");
             Console.WriteLine("Copyright (c) 2018 Stratis Group Limited");
             Console.WriteLine(Environment.NewLine);
         }
 
-        // A standard usage message with examples.  This is output on -h command and also in some cases if validation fails.
-        public static void OutputUsage()
+        /// <summary>
+        /// Shows the help message woth examples.
+        /// This is output on -h command and also in some cases if validation fails.
+        /// </summary>
+        public static void OutputMenu()
         {
-            Console.WriteLine("usage: federationsetup [-h] [-g] [-p] [-m]");
-            Console.WriteLine(Environment.NewLine);
-            Console.WriteLine("Show help message:                   -h");
-            Console.WriteLine("Create genesis blocks:               -g");
-            Console.WriteLine("Create private and public keys:      -p");  // ask members to create public and private -p (for the specfic network)  - 1 pubpriv for signing transactions and 1 for pubpriv key for mining
-            Console.WriteLine("Create multi signature addresses:    -m");
-            Console.WriteLine("                                     [network] [-fedpubkeys] [-quorum]");
-            Console.WriteLine("                                     network:    TestNet or RegTest (default MainNet).");
-            Console.WriteLine("                                     quorum:     The minimum number of federated members.");  // fed admin will do -m and number (3 qurom + the public keys for the signing of transactions
-            Console.WriteLine("                                     fedpubkeys: Federation members' public keys.");
-            Console.WriteLine("                                                 Must have an odd number of up to fifteen members.");  // fed admin will do -m and number (3 qurom + the public keys for the signing of transactions
-            Console.WriteLine("                                     Example:    federationsetup -m testnet -keys=PublicKey1,");
-            Console.WriteLine("                                                     PublicKey2, PublicKey3, PublicKey4, PublicKey5");
-            Console.WriteLine("                                                     -quorum=2");
+            Console.WriteLine("Menu:");
+            Console.WriteLine("g       Create genesis blocks for Mainnet, Testnet and Regtest.");
+            Console.WriteLine("p       Create private and public keys for federation members.");  // ask members to create public and private -p (for the specfic network)  - 1 pubpriv for signing transactions and 1 for pubpriv key for mining
+            Console.WriteLine("m       Create multi signature addresses for the federation wallets.");
+            Console.WriteLine("        args: [<network>] [-quorum=<quorum>] [-fedpubkeys=<pubkey1, pubkey2, ..>]");
+            Console.WriteLine("              network:    testnet or regtest or mainnet (default).");
+            Console.WriteLine("              quorum:     The minimum number of federated members needed to sign transactions.");
+            Console.WriteLine("              fedpubkeys: Federation members' public keys. Must have an odd number of up to fifteen members."); // // fed admin will do -m and number (3 qurom + the public keys for the signing of transactions)
+            Console.WriteLine("              Example:    federationsetup -m testnet -quorum=2 -fedpubkeys=PublicKey1, PublicKey2, PublicKey3, PublicKey4, PublicKey5");
+            Console.WriteLine("menu    Show this menu.");
+            Console.WriteLine("exit    Close the utility.");
             Console.WriteLine(Environment.NewLine);
         }
 
-        // Output completion message and secret warning.
+        /// <summary>
+        ///  Output completion message and secret warning.
+        /// </summary>
         public static void OutputSuccess()
         {
-            Console.WriteLine();
             Console.WriteLine("Done!");
+            Console.WriteLine();
         }
 
-        // On error we output in red.
+        /// <summary>
+        /// Shows an error message, in red.
+        /// </summary>
+        /// <param name="message">The message to show.</param>
         public static void OutputErrorLine(string message)
         {
             var colorSaved = Console.ForegroundColor;
