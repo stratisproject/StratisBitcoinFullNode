5c8c08f9a6e7c88d29c0d8b2be86ae621649e9f7
Jordan Andrews <jordandrews@hotmail.com>
2018-12-06 10:41:10 +1100

    Powershell NuGet fix + version updates (#2900)

--- PushSCNuget.ps1
+++ PushSCNuget.ps1
@@ -1,4 +1,4 @@
-$packageNames = @("Stratis.SmartContracts.Core", "Stratis.SmartContracts.CLR", "Stratis.SmartContracts.CLR.Validation", "Stratis.SmartContracts.Standards", "Stratis.Bitcoin.Features.SmartContracts")
+$packageNames = @("RuntimeObserver", "Stratis.SmartContracts.Core", "Stratis.SmartContracts.CLR", "Stratis.SmartContracts.CLR.Validation", "Stratis.SmartContracts.Standards", "Stratis.Bitcoin.Features.SmartContracts")
 
 # A little gross to have to enter src/ and then go back after, but this is where the file is atm 
 cd "src"
@@ -7,7 +7,7 @@ foreach ($packageName in $packageNames){
 	cd $packageName
 	rm "bin\debug\" -Recurse -Force -ErrorAction Ignore
 	dotnet pack --configuration Debug --include-source --include-symbols 
-	dotnet nuget push "bin\Release\*.symbols.nupkg" --source "https://api.nuget.org/v3/index.json" 
+	dotnet nuget push "bin\Debug\*.symbols.nupkg" --source "https://api.nuget.org/v3/index.json"
 	cd ..
 }
 
