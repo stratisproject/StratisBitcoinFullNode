e2af0c0256285093f97a6773de12e5664c670cc8
Rowan de Haas <rowandh@users.noreply.github.com>
2018-12-06 15:11:18 +1100

    Add version to network folder name (#2915)

--- src/Stratis.SmartContracts.Networks/SmartContractsPoATest.cs
+++ src/Stratis.SmartContracts.Networks/SmartContractsPoATest.cs
@@ -15,7 +15,7 @@ namespace Stratis.SmartContracts.Networks
     {
         public SmartContractsPoATest()
         {
-            this.Name = "SmartContractsPoATest";
+            this.Name = "SmartContractsPoATest-0.13.0-beta";
             this.CoinTicker = "SCPOA";
 
             var consensusFactory = new SmartContractPoAConsensusFactory();
