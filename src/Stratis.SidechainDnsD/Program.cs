8e1652c5fd9c2c45b46be2f57d8fe5480000071d
MithrilMan <red.angel@libero.it>
2018-12-13 14:43:16 +0100

    DNS seeder #246 (#255)
    * Sidechain DNS node
    
    * fix as per review
    
    * removed AssemblyInfo.cs

---
+++ src/Stratis.SidechainDnsD/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using NBitcoin.Protocol;
+using Stratis.Bitcoin;
+using Stratis.Bitcoin.Builder;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Features.Api;
+using Stratis.Bitcoin.Features.BlockStore;
+using Stratis.Bitcoin.Features.Dns;
+using Stratis.Bitcoin.Features.MemoryPool;
+using Stratis.Bitcoin.Features.RPC;
+using Stratis.Bitcoin.Features.SmartContracts;
+using Stratis.Bitcoin.Features.SmartContracts.PoA;
+using Stratis.Bitcoin.Features.SmartContracts.Wallet;
+using Stratis.Bitcoin.Utilities;
+using Stratis.Sidechains.Networks;
+
+namespace Stratis.SidechainDnsD
+{
+    /// <summary>
+    /// Starts a console app that includes the sidechain network parameters and the Dns feature.
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        /// <summary>
+        /// The async entry point for the Stratis Dns process.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        /// <returns>A task used to await the operation.</returns>
+        public static async Task MainAsync(string[] args)
+        {
+            try
+            {
+                var nodeSettings = new NodeSettings(networksSelector: FederatedPegNetwork.NetworksSelector, protocolVersion: ProtocolVersion.ALT_PROTOCOL_VERSION, args: args);
+
+                var dnsSettings = new DnsSettings(nodeSettings);
+
+                if (string.IsNullOrWhiteSpace(dnsSettings.DnsHostName) || string.IsNullOrWhiteSpace(dnsSettings.DnsNameServer) || string.IsNullOrWhiteSpace(dnsSettings.DnsMailBox))
+                    throw new ConfigurationException("When running as a DNS Seed service, the -dnshostname, -dnsnameserver and -dnsmailbox arguments must be specified on the command line.");
+
+                // Build the Dns full node.
+                IFullNode node = new FullNodeBuilder()
+                    .UseNodeSettings(nodeSettings)
+                    .UseBlockStore()
+                    .AddSmartContracts()
+                    .UseSmartContractPoAConsensus()
+                    .UseSmartContractPoAMining()
+                    .UseSmartContractWallet()
+                    .UseReflectionExecutor()
+                    .UseMempool()
+                    .UseApi()
+                    .AddRPC()
+                    .UseDns()
+                    .Build();
+
+                // Run node.
+                if (node != null)
+                    await node.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex.ToString());
+            }
+        }
+    }
+}
