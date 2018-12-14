ab7c2a7b124af0ddcb6d0fb97c0b44104e6901f2
zeptin <zeptin@gmail.com>
2018-12-14 11:20:36 +0200

    Add remove-transactions endpoint (#271)

--- src/Stratis.FederatedPeg.Features.FederationGateway/Models/RequestModels.cs
+++ src/Stratis.FederatedPeg.Features.FederationGateway/Models/RequestModels.cs
@@ -38,4 +38,15 @@ namespace Stratis.FederatedPeg.Features.FederationGateway.Models
         [Required(ErrorMessage = "A password is required.")]
         public string Password { get; set; }
     }
+
+    /// <summary>
+    /// Model object to use as input to the Api request for removing transactions from a wallet.
+    /// </summary>
+    /// <seealso cref="RequestModel" />
+    public class RemoveFederationTransactionsModel : RequestModel
+    {
+        [Required(ErrorMessage = "The reSync flag is required.")]
+        [JsonProperty(PropertyName = "reSync")]
+        public bool ReSync { get; set; }
+    }
 }
