e5c626ea60b90ae8eb886cfe9701079f9bdd7e40
Jeremy Bokobza <bokobza@users.noreply.github.com>
2018-12-21 11:13:37 +0000

    updated to SBFN 3.0.0.4 beta (#355)

--- src/Stratis.FederatedPeg.Features.FederationGateway/Wallet/FederationWalletSyncManager.cs
+++ src/Stratis.FederatedPeg.Features.FederationGateway/Wallet/FederationWalletSyncManager.cs
@@ -60,7 +60,7 @@ namespace Stratis.FederatedPeg.Features.FederationGateway.Wallet
             // if the wallet falls behind the block puller.
             // To support pruning the wallet will need to be
             // able to download blocks from peers to catch up.
-            if (this.storeSettings.Prune)
+            if (this.storeSettings.PruningEnabled)
                 throw new WalletException("Wallet can not yet run on a pruned node");
 
             this.logger.LogInformation("WalletSyncManager initialized. Wallet at block {0}.", this.walletManager.LastBlockHeight());
