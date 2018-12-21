2c66cb69d268930e100257489eef3bb5057b84dd
Rowan de Haas <rowandh@users.noreply.github.com>
2018-12-21 19:56:24 +1100

    Separate out check for IsContractType

--- src/Stratis.SmartContracts.CLR.Validation/ModuleDefinitionExtensions.cs
+++ src/Stratis.SmartContracts.CLR.Validation/ModuleDefinitionExtensions.cs
@@ -11,14 +11,22 @@ namespace Stratis.SmartContracts.CLR.Validation
     public static class ModuleDefinitionExtensions
     {
         /// <summary>
-        /// Get the 'real' types from the module. i.e. not added by the compiler or framework.
+        /// Determines whether a type is a contract type.
+        /// </summary>
+        public static bool IsContractType(this TypeDefinition typeDefinition)
+        {
+            return typeDefinition.IsClass &&
+                   !typeDefinition.IsAbstract &&
+                   typeDefinition.BaseType != null &&
+                   typeDefinition.BaseType.FullName == InheritsSmartContractValidator.SmartContractType;
+        }
+
+        /// <summary>
+        /// Get the contract types from the module.
         /// </summary>
         public static IEnumerable<TypeDefinition> GetContractTypes(this ModuleDefinition moduleDefinition)
-        {            
-            return moduleDefinition.Types
-                .Where(x => x.BaseType != null)
-                .Where(x => x.IsClass && !x.IsAbstract)
-                .Where(x => x.BaseType.FullName == InheritsSmartContractValidator.SmartContractType);
+        {
+            return moduleDefinition.Types.Where(IsContractType);
         }
     }
 }
