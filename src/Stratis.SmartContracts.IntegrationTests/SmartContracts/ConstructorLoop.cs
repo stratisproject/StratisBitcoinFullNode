204872452b6677f712e4071652aeea9e0fa5a1ec
Jordan Andrews <jordandrews@hotmail.com>
2018-12-23 22:40:41 +1100

    [SC] ObserverRewriter update (#3003)
    * Rewriter functionality
    
    * Extra test
    
    * Assert.Create
    
    * Integration test included

---
+++ src/Stratis.SmartContracts.IntegrationTests/SmartContracts/ConstructorLoop.cs
@@ -0,0 +1,23 @@
+﻿using Stratis.SmartContracts;
+
+[Deploy]
+public class ConstructorLoop : SmartContract
+{
+    public ConstructorLoop(ISmartContractState state) : base(state)
+    {
+        // This will only allow contract creation to happen if the inner call does run out of gas.
+        Assert(!Create<Other>().Success);
+    }
+}
+
+public class Other : SmartContract
+{
+    public Other(ISmartContractState state) : base(state)
+    {
+        var i = 1;
+        while (true)
+        {
+            PersistentState.SetInt32("i", i++);
+        }
+    }
+}
