b0e9cdd7a10d2c105004bdf474c8ec103c3843fb
Jordan Andrews <jordandrews@hotmail.com>
2018-12-23 17:14:51 +1000

    Extra test

---
+++ src/Stratis.SmartContracts.IntegrationTests/SmartContracts/ConstructorLoop.cs
@@ -0,0 +1,22 @@
+﻿using Stratis.SmartContracts;
+
+[Deploy]
+public class ConstructorLoop : SmartContract
+{
+    public ConstructorLoop(ISmartContractState state) : base(state)
+    {
+        Create<Other>();
+    }
+}
+
+public class Other : SmartContract
+{
+    public Other(ISmartContractState state) : base(state)
+    {
+        var i = 1;
+        while (true)
+        {
+            PersistentState.SetInt32("i", i++);
+        }
+    }
+}
