6fe5d4742a6fe8071c48d6bbfddfeacdbbe4ea65
quantumagi <someguy.fromafrica@gmail.com>
2019-01-08 22:20:59 +1100

    Fix ProvenHeaderCoinStakeRule

--- src/Stratis.Bitcoin.Features.Consensus/Rules/CommonRules/PosBlockSignatureRule.cs
+++ src/Stratis.Bitcoin.Features.Consensus/Rules/CommonRules/PosBlockSignatureRule.cs
@@ -51,19 +51,24 @@ namespace Stratis.Bitcoin.Features.Consensus.Rules.CommonRules
                 return res;
             }
 
-            if (block.BlockSignature.IsEmpty())
+            return CheckPOSSignature(block.BlockSignature, block.GetHash(), block.Transactions[1], this.Logger);
+        }
+
+        public static bool CheckPOSSignature(BlockSignature signature, uint256 blockHash, Transaction coinStake, ILogger logger)
+        {
+            if (signature.IsEmpty())
             {
-                this.Logger.LogTrace("(-)[EMPTY]:false");
+                logger.LogTrace("(-)[EMPTY]:false");
                 return false;
             }
 
-            TxOut txout = block.Transactions[1].Outputs[1];
+            TxOut txout = coinStake.Outputs[1];
 
             if (PayToPubkeyTemplate.Instance.CheckScriptPubKey(txout.ScriptPubKey))
             {
                 PubKey pubKey = PayToPubkeyTemplate.Instance.ExtractScriptPubKeyParameters(txout.ScriptPubKey);
-                bool res = pubKey.Verify(block.GetHash(), new ECDSASignature(block.BlockSignature.Signature));
-                this.Logger.LogTrace("(-)[P2PK]:{0}", res);
+                bool res = pubKey.Verify(blockHash, new ECDSASignature(signature.Signature));
+                logger.LogTrace("(-)[P2PK]:{0}", res);
                 return res;
             }
 
@@ -73,19 +78,19 @@ namespace Stratis.Bitcoin.Features.Consensus.Rules.CommonRules
             List<Op> ops = txout.ScriptPubKey.ToOps().ToList();
             if (!ops.Any()) // script.GetOp(pc, opcode, vchPushValue))
             {
-                this.Logger.LogTrace("(-)[NO_OPS]:false");
+                logger.LogTrace("(-)[NO_OPS]:false");
                 return false;
             }
 
             if (ops.ElementAt(0).Code != OpcodeType.OP_RETURN) // OP_RETURN)
             {
-                this.Logger.LogTrace("(-)[NO_OP_RETURN]:false");
+                logger.LogTrace("(-)[NO_OP_RETURN]:false");
                 return false;
             }
 
             if (ops.Count != 2)
             {
-                this.Logger.LogTrace("(-)[INVALID_OP_COUNT]:false");
+                logger.LogTrace("(-)[INVALID_OP_COUNT]:false");
                 return false;
             }
 
@@ -93,17 +98,17 @@ namespace Stratis.Bitcoin.Features.Consensus.Rules.CommonRules
 
             if (data.Length > MaxPushDataSize)
             {
-                this.Logger.LogTrace("(-)[PUSH_DATA_TOO_LARGE]:false");
+                logger.LogTrace("(-)[PUSH_DATA_TOO_LARGE]:false");
                 return false;
             }
 
             if (!ScriptEvaluationContext.IsCompressedOrUncompressedPubKey(data))
             {
-                this.Logger.LogTrace("(-)[NO_PUSH_DATA]:false");
+                logger.LogTrace("(-)[NO_PUSH_DATA]:false");
                 return false;
             }
 
-            bool verifyRes = new PubKey(data).Verify(block.GetHash(), new ECDSASignature(block.BlockSignature.Signature));
+            bool verifyRes = new PubKey(data).Verify(blockHash, new ECDSASignature(signature.Signature));
             return verifyRes;
         }
     }
