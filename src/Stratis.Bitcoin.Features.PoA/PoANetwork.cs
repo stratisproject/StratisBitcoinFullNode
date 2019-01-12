0051dad1f3f8c019c4ac62dff4ef406b69ea43ad
Kevin Loubser <zeptin@gmail.com>
2019-01-12 13:34:27 +0200

    Add standard scripts to PoA

--- src/Stratis.Bitcoin.Features.PoA/PoANetwork.cs
+++ src/Stratis.Bitcoin.Features.PoA/PoANetwork.cs
@@ -160,6 +160,8 @@ namespace Stratis.Bitcoin.Features.PoA
             string[] seedNodes = { };
             this.SeedNodes = this.ConvertToNetworkAddresses(seedNodes, this.DefaultPort).ToList();
 
+            this.StandardScriptsRegistry = new PoAStandardScriptsRegistry();
+
             Assert(this.Consensus.HashGenesisBlock == uint256.Parse("0x0621b88fb7a99c985d695be42e606cb913259bace2babe92970547fa033e4076"));
             Assert(this.Genesis.Header.HashMerkleRoot == uint256.Parse("0x9928b372fd9e4cf62a31638607344c03c48731ba06d24576342db9c8591e1432"));
 
