734471471ef06517070dea9decf1756f282ea8c2
quantumagi <someguy.fromafrica@gmail.com>
2019-01-16 22:42:29 +1100

    Avoid multiple constructor calls 2 (#3105)

--- src/Stratis.Bitcoin.Features.PoA/PoAConsensusErrors.cs
+++ src/Stratis.Bitcoin.Features.PoA/PoAConsensusErrors.cs
@@ -5,9 +5,9 @@ namespace Stratis.Bitcoin.Features.PoA
     /// <summary>Rules that might be thrown by consensus rules that are specific to PoA consensus.</summary>
     public static class PoAConsensusErrors
     {
-        public static readonly ConsensusError InvalidHeaderBits = new ConsensusError("invalid-header-bits", "invalid header bits");
-        public static readonly ConsensusError InvalidHeaderTimestamp = new ConsensusError("invalid-header-timestamp", "invalid header timestamp");
-        public static readonly ConsensusError InvalidHeaderSignature = new ConsensusError("invalid-header-signature", "invalid header signature");
-        public static readonly ConsensusError InvalidBlockSignature = new ConsensusError("invalid-block-signature", "invalid block signature");
+        public static ConsensusError InvalidHeaderBits => new ConsensusError("invalid-header-bits", "invalid header bits");
+        public static ConsensusError InvalidHeaderTimestamp => new ConsensusError("invalid-header-timestamp", "invalid header timestamp");
+        public static ConsensusError InvalidHeaderSignature => new ConsensusError("invalid-header-signature", "invalid header signature");
+        public static ConsensusError InvalidBlockSignature => new ConsensusError("invalid-block-signature", "invalid block signature");
     }
 }
