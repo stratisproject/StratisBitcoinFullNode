9f907cc454c70a9577895d760ad8ea7713673b1a
quantumagi <someguy.fromafrica@gmail.com>
2019-01-17 19:52:21 +1100

    Upgrade CLI to use network selector (#3113)

--- src/Stratis.Bitcoin.Cli/Program.cs
+++ src/Stratis.Bitcoin.Cli/Program.cs
@@ -5,12 +5,13 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using NBitcoin;
-using NBitcoin.Networks;
+using NBitcoin.Protocol;
 using Newtonsoft.Json;
 using Stratis.Bitcoin.Configuration;
 using Stratis.Bitcoin.Features.RPC;
-using Stratis.Bitcoin.Networks;
+using Stratis.Bitcoin.Features.Api;
 using Stratis.Bitcoin.Utilities.Extensions;
+
 
 namespace Stratis.Bitcoin.Cli
 {
@@ -66,7 +67,7 @@ namespace Stratis.Bitcoin.Cli
                     builder.AppendLine();
                     builder.AppendLine("Command line arguments:");
                     builder.AppendLine();
-                    builder.AppendLine("[network-name]                     Name of the network - e.g. \"stratis\", \"stratismain\", \"stratistest\", \"bitcoinmain\", \"bitcointest\".");
+                    builder.AppendLine("[network-name]                     Name of the network - e.g. \"stratis\" or \"bitcoin\".");
                     builder.AppendLine("[options]                          Options for the CLI (optional) - e.g. -help, -rpcuser, see below.");
                     builder.AppendLine("[command]                          Name of RPC method or API <controller>/<method>.");
                     builder.AppendLine("[arguments]                        Argument by position (RPC) or Name = Value pairs (API) (optional).");
@@ -80,7 +81,7 @@ namespace Stratis.Bitcoin.Cli
                     builder.AppendLine();
                     builder.AppendLine("Examples:");
                     builder.AppendLine();
-                    builder.AppendLine("dotnet run stratis Wallet/history WalletName=testwallet - Lists all the historical transactions of the wallet called 'testwallet'.");
+                    builder.AppendLine("dotnet run stratis -testnet Wallet/history WalletName=testwallet - Lists all the historical transactions of the wallet called 'testwallet' on the stratis test network.");
                     builder.AppendLine("dotnet run stratis -rpcuser=stratistestuser -rpcpassword=stratistestpassword -rpcconnect=127.0.0.3 -rpcport=26174 getinfo - Displays general information about the Stratis node on the 127.0.0.3:26174, authenticating with the RPC specified user.");
                     builder.AppendLine("dotnet run bitcoin -rpcuser=btctestuser -rpcpassword=btctestpass getbalance - Displays the current balance of the opened wallet on the 127.0.0.1:8332 node, authenticating with the RPC specified user.");
                     Console.WriteLine(builder);
@@ -88,18 +89,15 @@ namespace Stratis.Bitcoin.Cli
                 }
 
                 // Determine API port.
-                int defaultRestApiPort = 0;
-                Network network = null;
+                NetworksSelector networksSelector = null;
 
                 if (networkName.Contains("stratis"))
                 {
-                    defaultRestApiPort = 37221;
-                    network = NetworkRegistration.Register(new StratisMain());
+                    networksSelector = Networks.Networks.Stratis;
                 }
                 else
                 {
-                    defaultRestApiPort = 37220;
-                    network = NetworkRegistration.Register(new BitcoinMain());
+                    networksSelector = Networks.Networks.Bitcoin;
                 }
 
                 // API calls require both the contoller name and the method name separated by "/".
@@ -110,10 +108,13 @@ namespace Stratis.Bitcoin.Cli
                     try
                     {
                         string[] options = optionList.Append("-server").ToArray();
-
-                        var nodeSettings = new NodeSettings(network, args:options);
+                        var nodeSettings = new NodeSettings(networksSelector: networksSelector, protocolVersion: ProtocolVersion.PROVEN_HEADER_VERSION, args: options)
+                        {
+                            MinProtocolVersion = ProtocolVersion.ALT_PROTOCOL_VERSION
+                        };
 
                         var rpcSettings = new RpcSettings(nodeSettings);
+                        Network network = nodeSettings.Network;
 
                         // Find the binding to 127.0.0.1 or the first available. The logic in RPC settings ensures there will be at least 1.
                         System.Net.IPEndPoint nodeEndPoint = rpcSettings.Bind.FirstOrDefault(b => b.Address.ToString() == "127.0.0.1") ?? rpcSettings.Bind[0];
@@ -160,9 +161,17 @@ namespace Stratis.Bitcoin.Cli
                     // Process API call.
                     using (var client = new HttpClient())
                     {
+                        string[] options = optionList.ToArray();
+                        var nodeSettings = new NodeSettings(networksSelector: networksSelector, protocolVersion: ProtocolVersion.PROVEN_HEADER_VERSION, args: options)
+                        {
+                            MinProtocolVersion = ProtocolVersion.ALT_PROTOCOL_VERSION
+                        };
+
+                        var apiSettings = new ApiSettings(nodeSettings);
+
                         client.DefaultRequestHeaders.Accept.Clear();
                         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                        string url = $"http://localhost:{defaultRestApiPort}/api/{command}";
+                        string url = $"http://localhost:{apiSettings.ApiPort}/api/{command}";
                         if (commandArgList.Any()) url += $"?{string.Join("&", commandArgList)}";
                         try
                         {
