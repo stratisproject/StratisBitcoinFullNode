530c49a028720272445845deb74314c41ea0af78
noescape0 <NoEscape0@ya.ru>
2019-01-29 17:22:27 +0300

    Unfinished classes

---
+++ src/Stratis.Bitcoin.Features.PoA.Tests/Rules/PoAVotingCoinbaseOutputFormatRuleTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NBitcoin;
+using Stratis.Bitcoin.Consensus;
+using Stratis.Bitcoin.Features.PoA.Voting;
+using Stratis.Bitcoin.Features.PoA.Voting.ConsensusRules;
+using Xunit;
+
+namespace Stratis.Bitcoin.Features.PoA.Tests.Rules
+{
+    public class PoAVotingCoinbaseOutputFormatRuleTests : PoARulesTestsBase
+    {
+        private readonly PoAVotingCoinbaseOutputFormatRule votingFormatRule;
+
+        public PoAVotingCoinbaseOutputFormatRuleTests()
+        {
+            this.votingFormatRule = new PoAVotingCoinbaseOutputFormatRule();
+            this.votingFormatRule.Parent = this.rulesEngine;
+            this.votingFormatRule.Logger = this.loggerFactory.CreateLogger(this.votingFormatRule.GetType().FullName);
+            this.votingFormatRule.Initialize();
+        }
+
+        // TODO add tests
+    }
+}
---
+++ src/Stratis.Bitcoin.Features.PoA/Voting/ConsensusRules/PoAVotingCoinbaseOutputFormatRule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NBitcoin;
+using Stratis.Bitcoin.Consensus.Rules;
+
+namespace Stratis.Bitcoin.Features.PoA.Voting.ConsensusRules
+{
+    /// <summary>Validates <see cref="VotingData"/> collection format if voting output is present in the coinbase transaction.</summary>
+    public class PoAVotingCoinbaseOutputFormatRule : PartialValidationConsensusRule
+    {
+        private VotingDataEncoder votingDataEncoder;
+
+        public override void Initialize()
+        {
+            this.votingDataEncoder = new VotingDataEncoder(this.Parent.LoggerFactory);
+
+            base.Initialize();
+        }
+
+        public override Task RunAsync(RuleContext context)
+        {
+            Transaction coinbase = context.ValidationContext.BlockToValidate.Transactions[0];
+
+            byte[] votingDataBytes = this.votingDataEncoder.ExtractRawVotingData(coinbase);
+
+            if (votingDataBytes == null)
+            {
+                this.Logger.LogTrace("(-)[NO_VOTING_DATA]");
+                return Task.CompletedTask;
+            }
+
+            List<VotingData> votingDataList = this.votingDataEncoder.Decode(votingDataBytes);
+
+            // TODO verify format of voting data
+
+            // find voting output. exit if not found. if found check that it's not the first output, then check the format
+            // there could be only 1 voting output
+
+            // max op return data is ushort.maxValue
+
+            // TODO
+
+            throw new NotImplementedException();
+        }
+    }
+}
