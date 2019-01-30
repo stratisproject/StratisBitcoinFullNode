4868953765b47ed144ff0d0a5f386a4a4a4939aa
noescape0 <NoEscape0@ya.ru>
2019-01-30 16:12:27 +0300

    kv repo- add working with bytes support

--- src/Stratis.Bitcoin/Utilities/KeyValueRepository.cs
+++ src/Stratis.Bitcoin/Utilities/KeyValueRepository.cs
@@ -10,9 +10,13 @@ namespace Stratis.Bitcoin.Utilities
     /// <summary>Allows saving and loading single values to and from key-value storage.</summary>
     public interface IKeyValueRepository : IDisposable
     {
+        void SaveBytes(string key, byte[] bytes);
+
         void SaveValue<T>(string key, T value);
 
         T LoadValue<T>(string key);
+
+        byte[] LoadBytes(string key);
     }
 
     public class KeyValueRepository : IKeyValueRepository
@@ -36,20 +40,26 @@ namespace Stratis.Bitcoin.Utilities
         }
 
         /// <inheritdoc />
-        public void SaveValue<T>(string key, T value)
+        public void SaveBytes(string key, byte[] bytes)
         {
             byte[] keyBytes = Encoding.ASCII.GetBytes(key);
 
             using (DBreeze.Transactions.Transaction transaction = this.dbreeze.GetTransaction())
             {
-                transaction.Insert<byte[], byte[]>(TableName, keyBytes, this.dBreezeSerializer.Serialize(value));
+                transaction.Insert<byte[], byte[]>(TableName, keyBytes, bytes);
 
                 transaction.Commit();
             }
         }
 
         /// <inheritdoc />
-        public T LoadValue<T>(string key)
+        public void SaveValue<T>(string key, T value)
+        {
+            this.SaveBytes(key, this.dBreezeSerializer.Serialize(value));
+        }
+
+        /// <inheritdoc />
+        public byte[] LoadBytes(string key)
         {
             byte[] keyBytes = Encoding.ASCII.GetBytes(key);
 
@@ -60,11 +70,22 @@ namespace Stratis.Bitcoin.Utilities
                 Row<byte[], byte[]> row = transaction.Select<byte[], byte[]>(TableName, keyBytes);
 
                 if (!row.Exists)
-                    return default(T);
+                    return null;
 
-                T value = this.dBreezeSerializer.Deserialize<T>(row.Value);
-                return value;
+                return row.Value;
             }
+        }
+
+        /// <inheritdoc />
+        public T LoadValue<T>(string key)
+        {
+            byte[] bytes = this.LoadBytes(key);
+
+            if (bytes == null)
+                return default(T);
+
+            T value = this.dBreezeSerializer.Deserialize<T>(bytes);
+            return value;
         }
 
         /// <inheritdoc />
