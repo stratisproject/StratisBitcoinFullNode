54ddfc88d43af9270fd3f3097936c0ef0429b9f4
noescape0 <NoEscape0@ya.ru>
2019-02-01 14:37:24 +0300

    replace old signals with new

--- src/Stratis.Bitcoin/Signals/Signals.cs
+++ src/Stratis.Bitcoin/Signals/Signals.cs
@@ -1,137 +1,49 @@
-﻿using System;
+﻿using System.Threading.Tasks;
 using NBitcoin;
 using Stratis.Bitcoin.Primitives;
-using Stratis.Bitcoin.Utilities;
 
 namespace Stratis.Bitcoin.Signals
 {
-    /// <summary>
-    /// Provider of notifications of new blocks and transactions.
-    /// </summary>
     public interface ISignals
     {
-        /// <summary>
-        /// Notify subscribers about a new chained header block being available.
-        /// </summary>
-        /// <param name="chainedHeaderBlock">Newly added chained header block.</param>
-        void SignalBlockConnected(ChainedHeaderBlock chainedHeaderBlock);
+        event Signals.BlockDelegate OnBlockConnected;
 
-        /// <summary>
-        /// Notify subscribers about a chained header block being disconnected.
-        /// </summary>
-        /// <param name="chainedHeaderBlock">Chained Header Block that was disconnected.</param>
-        void SignalBlockDisconnected(ChainedHeaderBlock chainedHeaderBlock);
+        event Signals.BlockDelegate OnBlockDisconnected;
 
-        /// <summary>
-        /// Notify subscribers about a new transaction being available.
-        /// </summary>
-        /// <param name="trx">Newly added transaction.</param>
-        void SignalTransaction(Transaction trx);
+        event Signals.TransactionDelegate OnTransactionAvailable;
 
-        /// <summary>
-        /// Subscribes to receive notifications when a new block is available.
-        /// </summary>
-        /// <param name="observer">Observer to be subscribed to receive signaler's messages.</param>
-        /// <returns>Disposable object to allow observer to unsubscribe from the signaler.</returns>
-        IDisposable SubscribeForBlocksConnected(IObserver<ChainedHeaderBlock> observer);
+        void TriggerBlockDisconnected(ChainedHeaderBlock chainedHeaderBlock);
 
-        /// <summary>
-        /// Subscribes to receive notifications when a block was disconnected.
-        /// </summary>
-        /// <param name="observer">Observer to be subscribed to receive signaler's messages.</param>
-        /// <returns>Disposable object to allow observer to unsubscribe from the signaler.</returns>
-        IDisposable SubscribeForBlocksDisconnected(IObserver<ChainedHeaderBlock> observer);
+        void TriggerBlockConnected(ChainedHeaderBlock chainedHeaderBlock);
 
-        /// <summary>
-        /// Subscribes to receive notifications when a new transaction is available.
-        /// </summary>
-        /// <param name="observer">Observer to be subscribed to receive signaler's messages.</param>
-        /// <returns>Disposable object to allow observer to unsubscribe from the signaler.</returns>
-        IDisposable SubscribeForTransactions(IObserver<Transaction> observer);
+        void TriggerTransactionAvailable(Transaction transaction);
     }
 
-    /// <inheritdoc />
     public class Signals : ISignals
     {
-        /// <summary>
-        /// Initializes the object with newly created instances of signalers.
-        /// </summary>
-        public Signals() : this(new Signaler<ChainedHeaderBlock>(), new Signaler<ChainedHeaderBlock>(), new Signaler<Transaction>())
+        public delegate Task BlockDelegate(ChainedHeaderBlock chainedHeaderBlock);
+
+        public delegate Task TransactionDelegate(Transaction transaction);
+
+        public event BlockDelegate OnBlockConnected;
+
+        public event BlockDelegate OnBlockDisconnected;
+
+        public event TransactionDelegate OnTransactionAvailable;
+
+        public void TriggerBlockDisconnected(ChainedHeaderBlock chainedHeaderBlock)
         {
+            this.OnBlockDisconnected?.Invoke(chainedHeaderBlock);
         }
 
-        /// <summary>
-        /// Initializes the object with specific signalers.
-        /// </summary>
-        /// <param name="blockConnectedSignaler">Signaler providing notifications about newly available blocks to its subscribers.</param>
-        /// <param name="blockDisonnectedSignaler">Signaler providing notifications about a block being disconnected to its subscribers.</param>
-        /// <param name="transactionSignaler">Signaler providing notifications about newly available transactions to its subscribers.</param>
-        public Signals(ISignaler<ChainedHeaderBlock> blockConnectedSignaler, ISignaler<ChainedHeaderBlock> blockDisonnectedSignaler, ISignaler<Transaction> transactionSignaler)
+        public void TriggerBlockConnected(ChainedHeaderBlock chainedHeaderBlock)
         {
-            Guard.NotNull(blockConnectedSignaler, nameof(blockConnectedSignaler));
-            Guard.NotNull(blockDisonnectedSignaler, nameof(blockDisonnectedSignaler));
-            Guard.NotNull(transactionSignaler, nameof(transactionSignaler));
-
-            this.BlocksConnected = blockConnectedSignaler;
-            this.BlocksDisconnected = blockDisonnectedSignaler;
-            this.Transactions = transactionSignaler;
+            this.OnBlockConnected?.Invoke(chainedHeaderBlock);
         }
 
-        /// <summary>Signaler providing notifications about newly available blocks to its subscribers.</summary>
-        private ISignaler<ChainedHeaderBlock> BlocksConnected { get; }
-
-        /// <summary>Signaler providing notifications about blocks being disconnected to its subscribers.</summary>
-        private ISignaler<ChainedHeaderBlock> BlocksDisconnected { get; }
-
-        /// <summary>Signaler providing notifications about newly available transactions to its subscribers.</summary>
-        private ISignaler<Transaction> Transactions { get; }
-
-        /// <inheritdoc />
-        public void SignalBlockConnected(ChainedHeaderBlock chainedHeaderBlock)
+        public void TriggerTransactionAvailable(Transaction transaction)
         {
-            Guard.NotNull(chainedHeaderBlock, nameof(chainedHeaderBlock));
-
-            this.BlocksConnected.Broadcast(chainedHeaderBlock);
-        }
-
-        /// <inheritdoc />
-        public void SignalBlockDisconnected(ChainedHeaderBlock chainedHeaderBlock)
-        {
-            Guard.NotNull(chainedHeaderBlock, nameof(chainedHeaderBlock));
-
-            this.BlocksDisconnected.Broadcast(chainedHeaderBlock);
-        }
-
-        /// <inheritdoc />
-        public void SignalTransaction(Transaction trx)
-        {
-            Guard.NotNull(trx, nameof(trx));
-
-            this.Transactions.Broadcast(trx);
-        }
-
-        /// <inheritdoc />
-        public IDisposable SubscribeForBlocksConnected(IObserver<ChainedHeaderBlock> observer)
-        {
-            Guard.NotNull(observer, nameof(observer));
-
-            return this.BlocksConnected.Subscribe(observer);
-        }
-
-        /// <inheritdoc />
-        public IDisposable SubscribeForBlocksDisconnected(IObserver<ChainedHeaderBlock> observer)
-        {
-            Guard.NotNull(observer, nameof(observer));
-
-            return this.BlocksDisconnected.Subscribe(observer);
-        }
-
-        /// <inheritdoc />
-        public IDisposable SubscribeForTransactions(IObserver<Transaction> observer)
-        {
-            Guard.NotNull(observer, nameof(observer));
-
-            return this.Transactions.Subscribe(observer);
+            this.OnTransactionAvailable?.Invoke(transaction);
         }
     }
 }
