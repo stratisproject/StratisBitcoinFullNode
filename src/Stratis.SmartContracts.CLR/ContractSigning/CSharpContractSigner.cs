cdba6a7f5005c72638188e2c07ccac4b97b6fd6e
Jordan Andrews <jordandrews@hotmail.com>
2019-02-04 17:18:11 +1100

    CSharp Signing Tool

---
+++ src/Stratis.SmartContracts.CLR/ContractSigning/CSharpContractSigner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NBitcoin;
+using Stratis.SmartContracts.CLR.Compilation;
+using Stratis.SmartContracts.Core.ContractSigning;
+
+namespace Stratis.SmartContracts.CLR.ContractSigning
+{
+    public class CSharpContractSigner
+    {
+        private readonly IContractSigner contractSigner;
+
+        public CSharpContractSigner(IContractSigner contractSigner)
+        {
+            this.contractSigner = contractSigner;
+        }
+
+        /// <summary>
+        /// Compile and then sign a .cs file to enable use as a smart contract.
+        /// </summary>
+        public (byte[] contractCode, byte[] signature) SignCSharpFile(Key privKey, string path)
+        {
+            ContractCompilationResult compilationResult = ContractCompiler.CompileFile(path);
+
+            if (!compilationResult.Success)
+                throw new Exception("Contract compilation failed.");
+
+            byte[] signature = this.contractSigner.Sign(privKey, compilationResult.Compilation);
+
+            return (compilationResult.Compilation, signature);
+        } 
+    }
+}
