292f2dacb2592fbf4a49842cd78da10103db29f9
Jordan Andrews <jordandrews@hotmail.com>
2019-02-04 12:16:38 +1100

    New network for signing contracts

---
+++ src/Stratis.SmartContracts.Networks/SignedContractsPoARegTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NBitcoin;
+
+namespace Stratis.SmartContracts.Networks
+{
+    public class SignedContractsPoARegTest : SmartContractsPoARegTest
+    {
+        public Key SigningContractPrivKey { get;}
+
+        public PubKey SigningContractPubKey { get;}
+
+        public SignedContractsPoARegTest()
+        {
+            this.SigningContractPrivKey = new Mnemonic("lava frown leave wedding virtual ghost sibling able mammal liar wide wisdom").DeriveExtKey().PrivateKey;
+            this.SigningContractPubKey = this.SigningContractPrivKey.PubKey;
+        }
+    }
+}
