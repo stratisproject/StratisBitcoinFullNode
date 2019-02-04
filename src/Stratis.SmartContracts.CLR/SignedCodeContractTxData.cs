eef8d32b387ebc2edc1de94f909f39a1d4ee8dc0
Rowan de Haas <rowandehaas@outlook.com>
2019-02-04 12:56:13 +1100

    Add signed code tx data

---
+++ src/Stratis.SmartContracts.CLR/SignedCodeContractTxData.cs
@@ -0,0 +1,28 @@
+﻿using NBitcoin;
+
+namespace Stratis.SmartContracts.CLR
+{
+    /// <summary>
+    /// Fields that are serialized and sent as data with a smart contract transaction. Includes a digital signature used to authenticate
+    /// the contract code provided.
+    /// </summary>
+    public class SignedCodeContractTxData : ContractTxData
+    {
+        public SignedCodeContractTxData(int vmVersion, ulong gasPrice, RuntimeObserver.Gas gasLimit, uint160 contractAddress, 
+            string method, object[] methodParameters = null) : base(vmVersion, gasPrice, gasLimit, contractAddress, method, methodParameters)
+        {
+        }
+
+        public SignedCodeContractTxData(int vmVersion, ulong gasPrice, RuntimeObserver.Gas gasLimit, byte[] code, 
+            byte[] codeSignature,
+            object[] methodParameters = null) : base(vmVersion, gasPrice, gasLimit, code, methodParameters)
+        {
+            this.CodeSignature = codeSignature;
+        }
+
+        /// <summary>
+        /// Digital signature used to authenticate the contract execution code provided.
+        /// </summary>
+        public byte[] CodeSignature { get; }
+    }
+}
