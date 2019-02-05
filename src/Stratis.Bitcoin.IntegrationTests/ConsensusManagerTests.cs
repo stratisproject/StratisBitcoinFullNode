cee681f4cfde8052a983cc025497f8b93d624338
Rowan de Haas <rowandehaas@outlook.com>
2019-02-05 17:04:46 +1100

    Remove extra TestRuleRegistration

--- src/Stratis.Bitcoin.IntegrationTests/ConsensusManagerTests.cs
+++ src/Stratis.Bitcoin.IntegrationTests/ConsensusManagerTests.cs
@@ -15,6 +15,7 @@ using Stratis.Bitcoin.IntegrationTests.Common.EnvironmentMockUpHelpers;
 using Stratis.Bitcoin.IntegrationTests.Common.ReadyData;
 using Stratis.Bitcoin.Interfaces;
 using Stratis.Bitcoin.Networks;
+using Stratis.Bitcoin.Tests.Common;
 using Stratis.Bitcoin.Utilities;
 using Xunit;
 
@@ -713,35 +714,4 @@ namespace Stratis.Bitcoin.IntegrationTests
             }
         }
     }
-
-    public class TestRuleRegistration : IRuleRegistration
-    {
-        public TestRuleRegistration(IRuleRegistration existing)
-        {
-            var rules = existing.CreateRules();
-            this.HeaderValidationRules = new List<HeaderValidationConsensusRule>(rules.HeaderValidationRules);
-            this.IntegrityValidationRules = new List<IntegrityValidationConsensusRule>(rules.IntegrityValidationRules);
-            this.PartialValidationRules = new List<PartialValidationConsensusRule>(rules.PartialValidationRules);
-            this.FullValidationRules = new List<FullValidationConsensusRule>(rules.FullValidationRules);
-        }
-
-        public TestRuleRegistration()
-        {
-            this.HeaderValidationRules = new List<HeaderValidationConsensusRule>();
-            this.IntegrityValidationRules = new List<IntegrityValidationConsensusRule>();
-            this.PartialValidationRules = new List<PartialValidationConsensusRule>();
-            this.FullValidationRules = new List<FullValidationConsensusRule>();
-        }
-
-        public List<HeaderValidationConsensusRule> HeaderValidationRules { get; }
-        public List<IntegrityValidationConsensusRule> IntegrityValidationRules { get; }
-        public List<PartialValidationConsensusRule> PartialValidationRules { get; }
-        public List<FullValidationConsensusRule> FullValidationRules { get; }
-
-        public RuleContainer CreateRules()
-        {
-            return new RuleContainer(this.FullValidationRules, this.PartialValidationRules, this.HeaderValidationRules, this.IntegrityValidationRules);
-        }
-    }
-
 }
