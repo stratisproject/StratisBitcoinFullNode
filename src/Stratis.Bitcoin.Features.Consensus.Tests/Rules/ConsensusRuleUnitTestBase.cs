09cf3f65746e3d6b06fbd854079c140c1891385b
Rowan de Haas <rowandehaas@outlook.com>
2019-02-05 17:03:30 +1100

    Remove extra TestRuleRegistration

--- src/Stratis.Bitcoin.Features.Consensus.Tests/Rules/ConsensusRuleUnitTestBase.cs
+++ src/Stratis.Bitcoin.Features.Consensus.Tests/Rules/ConsensusRuleUnitTestBase.cs
@@ -115,36 +115,6 @@ namespace Stratis.Bitcoin.Features.Consensus.Tests.Rules
         }
     }
 
-    public class TestRuleRegistration : IRuleRegistration
-    {
-        public TestRuleRegistration(IRuleRegistration existing)
-        {
-            var rules = existing.CreateRules();
-            this.HeaderValidationRules = new List<HeaderValidationConsensusRule>(rules.HeaderValidationRules);
-            this.IntegrityValidationRules = new List<IntegrityValidationConsensusRule>(rules.IntegrityValidationRules);
-            this.PartialValidationRules = new List<PartialValidationConsensusRule>(rules.PartialValidationRules);
-            this.FullValidationRules = new List<FullValidationConsensusRule>(rules.FullValidationRules);
-        }
-
-        public TestRuleRegistration()
-        {
-            this.HeaderValidationRules = new List<HeaderValidationConsensusRule>();
-            this.IntegrityValidationRules = new List<IntegrityValidationConsensusRule>();
-            this.PartialValidationRules = new List<PartialValidationConsensusRule>();
-            this.FullValidationRules = new List<FullValidationConsensusRule>();
-        }
-
-        public List<HeaderValidationConsensusRule> HeaderValidationRules { get; }
-        public List<IntegrityValidationConsensusRule> IntegrityValidationRules { get; }
-        public List<PartialValidationConsensusRule> PartialValidationRules { get; }
-        public List<FullValidationConsensusRule> FullValidationRules { get; }
-
-        public RuleContainer CreateRules()
-        {
-            return new RuleContainer(this.FullValidationRules, this.PartialValidationRules, this.HeaderValidationRules, this.IntegrityValidationRules);
-        }
-    }
-
     public class ConsensusRuleUnitTestBase<T> where T : ConsensusRuleEngine
     {
         protected Network network;
