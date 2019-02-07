61a35bd4f64a7f556f8c9b0e44574eefad8f296e
Jordan Andrews <jordandrews@hotmail.com>
2019-02-07 16:11:21 +1100

    Validate LogsBloom (#3177)

--- src/Stratis.Bitcoin.Features.SmartContracts/SmartContractConsensusErrors.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/SmartContractConsensusErrors.cs
@@ -7,6 +7,7 @@ namespace Stratis.Bitcoin.Features.SmartContracts
         public static ConsensusError UserOpSpend => new ConsensusError("op-spend-by-user", "op spend opcode invoked by a user");
         public static ConsensusError UserInternalCall => new ConsensusError("op-spend-by-user", "op internalcall opcode invoked by a user");
         public static ConsensusError UnequalCondensingTx => new ConsensusError("invalid-condensing-tx", "condensing tx generated didn't match tx in block");
+        public static ConsensusError UnequalLogsBloom => new ConsensusError("invalid-logs-bloom", "bloom filter not matching after block execution");
         public static ConsensusError UnequalStateRoots => new ConsensusError("invalid-state-roots", "contract state root not matching after block execution");
         public static ConsensusError UnequalReceiptRoots => new ConsensusError("invalid-receipt-roots", "contract receipt roots not matching after block execution");
         public static ConsensusError UnequalRefundAmounts => new ConsensusError("invalid-refund-amount", "contract execution refunded a different amount or to a different address");
