cc1be7f67e678df4db229ab147bd7ad17d0b5fb8
quantumagi <someguy.fromafrica@gmail.com>
2019-02-15 02:19:36 +1100

    Do not disconnect peers that don't support required services (#3241)
    * Do not disconnect peers that don't support required services
    
    * Refactor
    
    * Refactor
    
    * Changes based on feedback
    
    * Remove empty line
    
    * Add tests for Check method

---
+++ src/Stratis.Bitcoin.Tests/P2P/NetworkPeerTests.cs
@@ -0,0 +1,86 @@
+﻿using NBitcoin.Protocol;
+using Stratis.Bitcoin.P2P.Peer;
+using Stratis.Bitcoin.P2P.Protocol.Payloads;
+using Xunit;
+
+namespace Stratis.Bitcoin.Tests.P2P
+{
+    public sealed class NetworkPeerTests
+    {
+        public NetworkPeerTests()
+        {
+        }
+
+        [Fact]
+        public void NetworkPeerRequirementCheckForOutboundWithValidVersionAndValidServiceReturnsTrue()
+        {
+            NetworkPeerRequirement networkPeerRequirement = new NetworkPeerRequirement();
+            networkPeerRequirement.MinVersion = ProtocolVersion.ALT_PROTOCOL_VERSION;
+            networkPeerRequirement.RequiredServices = NetworkPeerServices.Network;
+            Assert.True(networkPeerRequirement.Check(new VersionPayload() { Services = NetworkPeerServices.Network, Version = ProtocolVersion.ALT_PROTOCOL_VERSION }, false, out string reason));
+        }
+
+        [Fact]
+        public void NetworkPeerRequirementCheckForOutboundWithValidVersionAndInvalidServiceReturnsFalse()
+        {
+            NetworkPeerRequirement networkPeerRequirement = new NetworkPeerRequirement();
+            networkPeerRequirement.MinVersion = ProtocolVersion.ALT_PROTOCOL_VERSION;
+            networkPeerRequirement.RequiredServices = NetworkPeerServices.Network;
+            Assert.False(networkPeerRequirement.Check(new VersionPayload() { Services = NetworkPeerServices.Nothing, Version = ProtocolVersion.ALT_PROTOCOL_VERSION }, false, out string reason));
+        }
+
+        [Fact]
+        public void NetworkPeerRequirementCheckForOutboundWithInvalidVersionAndValidServiceReturnsFalse()
+        {
+            NetworkPeerRequirement networkPeerRequirement = new NetworkPeerRequirement();
+            networkPeerRequirement.MinVersion = ProtocolVersion.PROTOCOL_VERSION;
+            networkPeerRequirement.RequiredServices = NetworkPeerServices.Network;
+            Assert.False(networkPeerRequirement.Check(new VersionPayload() { Services = NetworkPeerServices.Network, Version = ProtocolVersion.ALT_PROTOCOL_VERSION }, false, out string reason));
+        }
+
+        [Fact]
+        public void NetworkPeerRequirementCheckForOutboundWithInvalidVersionAndInvalidServiceReturnsFalse()
+        {
+            NetworkPeerRequirement networkPeerRequirement = new NetworkPeerRequirement();
+            networkPeerRequirement.MinVersion = ProtocolVersion.PROTOCOL_VERSION;
+            networkPeerRequirement.RequiredServices = NetworkPeerServices.Network;
+            Assert.False(networkPeerRequirement.Check(new VersionPayload() { Services = NetworkPeerServices.Nothing, Version = ProtocolVersion.ALT_PROTOCOL_VERSION }, false, out string reason));
+        }
+
+        [Fact]
+        public void NetworkPeerRequirementCheckForInboundWithValidVersionAndValidServiceReturnsTrue()
+        {
+            NetworkPeerRequirement networkPeerRequirement = new NetworkPeerRequirement();
+            networkPeerRequirement.MinVersion = ProtocolVersion.ALT_PROTOCOL_VERSION;
+            networkPeerRequirement.RequiredServices = NetworkPeerServices.Network;
+            Assert.True(networkPeerRequirement.Check(new VersionPayload() { Services = NetworkPeerServices.Network, Version = ProtocolVersion.ALT_PROTOCOL_VERSION }, true, out string reason));
+        }
+
+        [Fact]
+        public void NetworkPeerRequirementCheckForInboundWithValidVersionAndInvalidServiceReturnsTrue()
+        {
+            NetworkPeerRequirement networkPeerRequirement = new NetworkPeerRequirement();
+            networkPeerRequirement.MinVersion = ProtocolVersion.ALT_PROTOCOL_VERSION;
+            networkPeerRequirement.RequiredServices = NetworkPeerServices.Network;
+            Assert.True(networkPeerRequirement.Check(new VersionPayload() { Services = NetworkPeerServices.Nothing, Version = ProtocolVersion.ALT_PROTOCOL_VERSION }, true, out string reason));
+        }
+
+        [Fact]
+        public void NetworkPeerRequirementCheckForInboundWithInvalidVersionAndValidServiceReturnsFalse()
+        {
+            NetworkPeerRequirement networkPeerRequirement = new NetworkPeerRequirement();
+            networkPeerRequirement.MinVersion = ProtocolVersion.PROTOCOL_VERSION;
+            networkPeerRequirement.RequiredServices = NetworkPeerServices.Network;
+            Assert.False(networkPeerRequirement.Check(new VersionPayload() { Services = NetworkPeerServices.Network, Version = ProtocolVersion.ALT_PROTOCOL_VERSION }, true, out string reason));
+        }
+
+        [Fact]
+        public void NetworkPeerRequirementCheckForInboundWithInvalidVersionAndInvalidServiceReturnsFalse()
+        {
+            NetworkPeerRequirement networkPeerRequirement = new NetworkPeerRequirement();
+            networkPeerRequirement.MinVersion = ProtocolVersion.PROTOCOL_VERSION;
+            networkPeerRequirement.RequiredServices = NetworkPeerServices.Network;
+            Assert.False(networkPeerRequirement.Check(new VersionPayload() { Services = NetworkPeerServices.Nothing, Version = ProtocolVersion.ALT_PROTOCOL_VERSION }, true, out string reason));
+        }
+    }
+}
