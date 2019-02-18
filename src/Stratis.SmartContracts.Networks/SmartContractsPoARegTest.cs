c842140a76726fde26360e51cbefbaa72b011326
noescape0 <NoEscape0@ya.ru>
2019-02-18 10:29:24 +0300

    tests

--- src/Stratis.SmartContracts.Networks/SmartContractsPoARegTest.cs
+++ src/Stratis.SmartContracts.Networks/SmartContractsPoARegTest.cs
@@ -58,7 +58,7 @@ namespace Stratis.SmartContracts.Networks
                 maxBlockSigopsCost: 20_000,
                 maxStandardTxSigopsCost: 20_000 / 5,
                 federationPublicKeys: federationPubKeys,
-                targetSpacingSeconds: 16,
+                targetSpacingSeconds: 60,
                 votingEnabled: true
             );
 
