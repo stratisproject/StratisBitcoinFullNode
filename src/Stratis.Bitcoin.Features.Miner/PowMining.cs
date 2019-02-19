fba98c32c27d266a1288ee40fa3b27ceddb64c5f
Kevin Loubser <zeptin@gmail.com>
2019-02-19 18:18:19 +0200

    Relax IBD check for regtest to allow mining at genesis

--- src/Stratis.Bitcoin.Features.Miner/PowMining.cs
+++ src/Stratis.Bitcoin.Features.Miner/PowMining.cs
@@ -196,6 +196,12 @@ namespace Stratis.Bitcoin.Features.Miner
             this.miningCancellationTokenSource.Token.ThrowIfCancellationRequested();
 
             context.ChainTip = this.consensusManager.Tip;
+
+            // Genesis on a regtest network is a special case. We need to regard ourselves as outside of IBD to
+            // bootstrap the mining.
+            if (this.network.IsRegTest() && (context.ChainTip.Height == 0))
+                return true;
+
             if (this.initialBlockDownloadState.IsInitialBlockDownload())
             {
                 Task.Delay(TimeSpan.FromMinutes(1), this.nodeLifetime.ApplicationStopping).GetAwaiter().GetResult();
