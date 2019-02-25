387593bad8f640aabd2330661bdd910750f24734
Dan Gershony <dan.gershony@gmail.com>
2019-02-25 18:39:33 +0000

    Update to fallback network ips (#3271)
    * update to fallback network ips
    
    * Some changes to ips list
    
    * Update src/Stratis.Bitcoin.Networks/StratisTest.cs
    
    Co-Authored-By: dangershony <dan.gershony@gmail.com>
    
    * Update src/Stratis.Bitcoin.Networks/StratisMain.cs
    
    Co-Authored-By: dangershony <dan.gershony@gmail.com>
    
    * Update src/Stratis.Bitcoin.Networks/StratisTest.cs
    
    Co-Authored-By: dangershony <dan.gershony@gmail.com>
    
    * Fix tests

--- src/Stratis.Bitcoin.Networks/StratisTest.cs
+++ src/Stratis.Bitcoin.Networks/StratisTest.cs
@@ -138,10 +138,9 @@ namespace Stratis.Bitcoin.Networks
 
             this.SeedNodes = new List<NetworkAddress>
             {
-                new NetworkAddress(IPAddress.Parse("51.140.231.125"), this.DefaultPort), // danger cloud node
-                new NetworkAddress(IPAddress.Parse("13.70.81.5"), 3389), // beard cloud node
-                new NetworkAddress(IPAddress.Parse("191.235.85.131"), 3389), // fassa cloud node
-                new NetworkAddress(IPAddress.Parse("52.232.58.52"), 26178), // neurosploit public node
+                new NetworkAddress(IPAddress.Parse("51.140.231.125"), 26178), // danger cloud node
+                new NetworkAddress(IPAddress.Parse("13.70.81.5"), 26178), // beard cloud node
+                new NetworkAddress(IPAddress.Parse("191.235.85.131"), 26178), // fassa cloud node
             };
 
             this.StandardScriptsRegistry = new StratisStandardScriptsRegistry();
