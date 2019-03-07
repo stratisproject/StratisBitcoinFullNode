06bf3057c759f6a369f49fd54f4b2b04e77295c6
Rowan de Haas <rowandh@users.noreply.github.com>
2019-03-07 14:03:18 +1100

    Add unit tests

---
+++ src/Stratis.Bitcoin.Features.PoA/Voting/IWhitelistedHashesRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using NBitcoin;
+
+namespace Stratis.Bitcoin.Features.PoA.Voting
+{
+    public interface IWhitelistedHashesRepository
+    {
+        void AddHash(uint256 hash);
+
+        void RemoveHash(uint256 hash);
+
+        List<uint256> GetHashes();
+    }
+}
--- src/Stratis.Bitcoin.Features.SmartContracts/PoA/Rules/AllowedCodeHashLogic.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/PoA/Rules/AllowedCodeHashLogic.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using NBitcoin;
+using NBitcoin.Crypto;
+using Stratis.Bitcoin.Features.PoA.Voting;
 using Stratis.Bitcoin.Features.SmartContracts.Rules;
 using Stratis.SmartContracts.CLR;
 
@@ -9,9 +12,28 @@ namespace Stratis.Bitcoin.Features.SmartContracts.PoA.Rules
     /// </summary>
     public class AllowedCodeHashLogic : IContractTransactionValidationLogic
     {
+        private readonly IWhitelistedHashChecker whitelistedHashChecker;
+        private readonly IHashingStrategy hashingStrategy;
+
+        public AllowedCodeHashLogic(IWhitelistedHashChecker whitelistedHashChecker, IHashingStrategy hashingStrategy)
+        {
+            this.whitelistedHashChecker = whitelistedHashChecker;
+            this.hashingStrategy = hashingStrategy;
+        }
+
         public void CheckContractTransaction(ContractTxData txData, Money suppliedBudget)
         {
             throw new System.NotImplementedException();
         }
+    }
+
+    public interface IWhitelistedHashChecker
+    {
+        bool CheckHashWhitelisted(byte[] hash);
+    }
+
+    public interface IHashingStrategy
+    {
+        byte[] Hash(byte[] data);
     }
 }
