000c261866a59bfd6098454956bc14b5a021f0aa
Rowan de Haas <rowandh@users.noreply.github.com>
2019-03-11 16:00:46 +1100

    Remove unused

--- src/Stratis.Bitcoin.Features.SmartContracts/PoA/IFullNodeBuilderExtensions.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/PoA/IFullNodeBuilderExtensions.cs
@@ -68,46 +68,6 @@ namespace Stratis.Bitcoin.Features.SmartContracts.PoA
         }
 
         /// <summary>
-        /// Configures the node with the smart contract proof of authority consensus model.
-        /// </summary>
-        public static IFullNodeBuilder UseSignedContractPoAConsensus(this IFullNodeBuilder fullNodeBuilder)
-        {
-            LoggingConfiguration.RegisterFeatureNamespace<ConsensusFeature>("consensus");
-
-            fullNodeBuilder.ConfigureFeature(features =>
-            {
-                features
-                    .AddFeature<ConsensusFeature>()
-                    .DependOn<SmartContractFeature>()
-                    .FeatureServices(services =>
-                    {
-                        services.AddSingleton<DBreezeCoinView>();
-                        services.AddSingleton<ICoinView, CachedCoinView>();
-                        services.AddSingleton<ConsensusController>();
-                        services.AddSingleton<VotingManager>();
-                        services.AddSingleton<WhitelistedHashesRepository>();
-                        services.AddSingleton<IPollResultExecutor, PollResultExecutor>();
-
-                        // Replace serializer
-                        fullNodeBuilder.Services.AddSingleton<ICallDataSerializer, SignedCodeCallDataSerializer>();
-
-                        services.AddSingleton<PoAConsensusRuleEngine>();
-                        services.AddSingleton<IRuleRegistration, SmartContractPoARuleRegistration>();
-
-                        services.AddSingleton<IConsensusRuleEngine>(f =>
-                        {
-                            var concreteRuleEngine = f.GetService<PoAConsensusRuleEngine>();
-                            var ruleRegistration = f.GetService<IRuleRegistration>();
-                            
-                            return new DiConsensusRuleEngine(concreteRuleEngine, ruleRegistration);
-                        });
-                    });
-            });
-
-            return fullNodeBuilder;
-        }
-
-        /// <summary>
         /// Adds mining to the smart contract node when on a proof-of-authority network.
         /// </summary>
         public static IFullNodeBuilder UseSmartContractPoAMining(this IFullNodeBuilder fullNodeBuilder)
