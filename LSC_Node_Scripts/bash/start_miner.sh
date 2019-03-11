61b92d2d87873589b7eac49441c1c0e21f197e12
Justin Topham <jtopham@Justins-MacBook-Air.local>
2019-03-11 11:52:27 +0000

    Updated miner scripts with a bootstrap config option.

--- LSC_Node_Scripts/bash/start_miner.sh
+++ LSC_Node_Scripts/bash/start_miner.sh
@@ -7,4 +7,4 @@ git checkout release/3.0.1.0-LSC
 cd src/Stratis.LocalSmartContracts.MinerD
 echo "Running miner 1..."
 echo "**Data held in $NodesDirectory/miner1**"
-dotnet run -datadir=$NodesDirectory/miner1 -port=36201 -apiport=38201 -txindex=1 connect=0 listen=1 -bind=127.0.0.1
+dotnet run -bootstrap -datadir=$NodesDirectory/miner1 -port=36201 -apiport=38201 -txindex=1 connect=0 listen=1 -bind=127.0.0.1
--- LSC_Node_Scripts/batch/start_miner.bat
+++ LSC_Node_Scripts/batch/start_miner.bat
@@ -10,4 +10,4 @@ cd src\Stratis.LocalSmartContracts.MinerD
 @ECHO ON
 echo Running miner 1...
 echo **Data held in %NodesDirectory%\miner1**
-dotnet run -datadir=%NodesDirectory%\miner1 -port=36201 -apiport=38201 -txindex=1 connect=0 listen=1 -bind=127.0.0.1
+dotnet run -bootstrap -datadir=%NodesDirectory%\miner1 -port=36201 -apiport=38201 -txindex=1 connect=0 listen=1 -bind=127.0.0.1
