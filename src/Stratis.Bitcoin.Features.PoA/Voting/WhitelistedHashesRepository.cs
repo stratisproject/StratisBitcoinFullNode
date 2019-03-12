7b130e582eef6ccda731002bf934b36f91602da0
Rowan de Haas <rowandh@users.noreply.github.com>
2019-03-12 21:00:54 +1100

    Move interface

--- src/Stratis.Bitcoin.Features.PoA/Voting/WhitelistedHashesRepository.cs
+++ src/Stratis.Bitcoin.Features.PoA/Voting/WhitelistedHashesRepository.cs
@@ -77,4 +77,15 @@ namespace Stratis.Bitcoin.Features.PoA.Voting
             }
         }
     }
+
+    public interface IWhitelistedHashesRepository
+    {
+        void AddHash(uint256 hash);
+
+        void RemoveHash(uint256 hash);
+
+        List<uint256> GetHashes();
+
+        void Initialize();
+    }
 }
