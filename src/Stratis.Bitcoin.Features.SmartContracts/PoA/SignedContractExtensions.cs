84dc2a678ead2387df723317cda7d4f4b6eee70b
Rowan de Haas <rowandh@users.noreply.github.com>
2019-03-12 12:45:48 +1100

    Add signed contract extensions

---
+++ src/Stratis.Bitcoin.Features.SmartContracts/PoA/SignedContractExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Stratis.Bitcoin.Features.SmartContracts.PoA.Rules;
+using Stratis.Bitcoin.Features.SmartContracts.Rules;
+
+namespace Stratis.Bitcoin.Features.SmartContracts.PoA
+{
+    public static class SignedContractExtensions
+    {
+        /// <summary>
+        /// Adds a consensus rule ensuring only contracts with hashes that are on the PoA whitelist are able to be deployed.
+        /// The PoA feature must be installed for this to function correctly.
+        /// </summary>
+        public static SmartContractOptions UsePoAWhitelistedContracts(this SmartContractOptions options)
+        {
+            IServiceCollection services = options.Services;
+
+            services.AddSingleton<IWhitelistedHashChecker, WhitelistedHashChecker>();
+            services.AddSingleton<IContractCodeHashingStrategy, Sha256CodeHashingStrategy>();
+
+            // Registers an additional contract tx validation consensus rule which checks whether the hash of the contract being deployed is whitelisted.
+            services.AddTransient<IContractTransactionValidationLogic, AllowedCodeHashLogic>();
+
+            return options;
+        }
+    }
+}
