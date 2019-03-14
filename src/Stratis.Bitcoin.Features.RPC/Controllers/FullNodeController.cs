016ff25caac05ccc1fe11a9e769cf2af55aedd7f
Francois de la Rouviere <fassadlr@gmail.com>
2019-03-14 16:09:40 +0000

    [RPC] Update ValidateAddress to align with Core (#3372)
    * Updates to ValidateAddress
    
    * Update FullNodeController.cs
    
    * Update FullNodeController.cs
    
    * Update FullNodeControllerTest.cs

--- src/Stratis.Bitcoin.Features.RPC/Controllers/FullNodeController.cs
+++ src/Stratis.Bitcoin.Features.RPC/Controllers/FullNodeController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -154,7 +153,7 @@ namespace Stratis.Bitcoin.Features.RPC.Controllers
                     throw new RPCServerException(RPCErrorCode.RPC_INVALID_ADDRESS_OR_KEY, "No such transaction found in the provided block.");
                 }
             }
-            
+
             if (verbose != 0)
             {
                 ChainedHeader block = chainedHeaderBlock != null ? chainedHeaderBlock.ChainedHeader : await this.GetTransactionBlockAsync(trxid);
@@ -293,44 +292,53 @@ namespace Stratis.Bitcoin.Features.RPC.Controllers
         }
 
         /// <summary>
-        /// Returns information about a bitcoin address
+        /// Returns information about a bitcoin address and it's validity.
         /// </summary>
-        /// <param name="address">bech32 or base58 BitcoinAddress to validate.</param>
-        /// <returns>ValidatedAddress containing a boolean indicating address validity</returns>
-        /// <exception cref="ArgumentNullException">Thrown if address provided is null/empty.</exception>
+        /// <param name="address">The bech32 or base58 <see cref="BitcoinAddress"/> to validate.</param>
+        /// <returns><see cref="ValidatedAddress"/> instance containing information about the bitcoin address and it's validity.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if address provided is null or empty.</exception>
         [ActionName("validateaddress")]
         [ActionDescription("Returns information about a bech32 or base58 bitcoin address")]
         public ValidatedAddress ValidateAddress(string address)
         {
-            if (string.IsNullOrEmpty(address))
-                throw new ArgumentNullException("address");
+            Guard.NotEmpty(address, nameof(address));
 
-            var res = new ValidatedAddress();
-            res.IsValid = false;
+            var result = new ValidatedAddress
+            {
+                IsValid = false,
+                Address = address,
+            };
 
             // P2WPKH
             if (BitcoinWitPubKeyAddress.IsValid(address, this.Network, out Exception _))
             {
-                res.IsValid = true;
+                result.IsValid = true;
             }
-            // P2WSH
+            // P2WSH (unsupported)
             else if (BitcoinWitScriptAddress.IsValid(address, this.Network, out Exception _))
             {
-                // We don't support P2WSH addresses yet
-                res.IsValid = false;
+                result.IsValid = true;
             }
             // P2PKH
             else if (BitcoinPubKeyAddress.IsValid(address, this.Network))
             {
-                res.IsValid = true;
+                result.IsValid = true;
             }
             // P2SH
             else if (BitcoinScriptAddress.IsValid(address, this.Network))
             {
-                res.IsValid = true;
+                result.IsValid = true;
+                result.IsScript = true;
             }
 
-            return res;
+            if (result.IsValid)
+            {
+                var scriptPubKey = BitcoinAddress.Create(address, this.Network).ScriptPubKey;
+                result.ScriptPubKey = scriptPubKey.ToHex();
+                result.IsWitness = scriptPubKey.IsWitness(this.Network);
+            }
+
+            return result;
         }
 
         /// <summary>
@@ -361,7 +369,7 @@ namespace Stratis.Bitcoin.Features.RPC.Controllers
                 Version = this.FullNode?.Version?.ToUint() ?? 0,
                 SubVersion = this.Settings?.Agent,
                 ProtocolVersion = (uint)(this.Settings?.ProtocolVersion ?? NodeSettings.SupportedProtocolVersion),
-                IsLocalRelay = this.ConnectionManager?.Parameters?.IsRelay ?? false,            
+                IsLocalRelay = this.ConnectionManager?.Parameters?.IsRelay ?? false,
                 TimeOffset = this.ConnectionManager?.ConnectedPeers?.GetMedianTimeOffset() ?? 0,
                 Connections = this.ConnectionManager?.ConnectedPeers?.Count(),
                 IsNetworkActive = true,
@@ -389,7 +397,7 @@ namespace Stratis.Bitcoin.Features.RPC.Controllers
                 Headers = (uint)(this.Chain?.Height ?? 0),
                 BestBlockHash = this.ChainState?.ConsensusTip?.HashBlock,
                 Difficulty = this.GetNetworkDifficulty()?.Difficulty ?? 0.0,
-                MedianTime = this.ChainState?.ConsensusTip?.GetMedianTimePast().ToUnixTimeSeconds() ?? 0, 
+                MedianTime = this.ChainState?.ConsensusTip?.GetMedianTimePast().ToUnixTimeSeconds() ?? 0,
                 VerificationProgress = 0.0,
                 IsInitialBlockDownload = !this.ChainState?.IsAtBestChainTip ?? true,
                 Chainwork = this.ChainState?.ConsensusTip?.ChainWork,
