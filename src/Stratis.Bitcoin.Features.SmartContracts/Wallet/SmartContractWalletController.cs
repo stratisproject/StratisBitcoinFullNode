505d3fcfc544d304bcc0ac0218eb4d5831064385
justintopham <36919735+justintopham@users.noreply.github.com>
2019-03-18 00:16:24 +0000

    Smart Contract API Documentation (#3248)
    * Smart Contract and Smart Cotnract Wallet API documentation first draft.
    
    CS XML documentation markup added for all classes used by the Swagger API.
    
    * Updated API call documentation based on discussions with Jordan and Rowan.
    
    * Fixed spelling mistakes and clarrified, when STRAT are mentioned, that we could be talking about a sidechain coin.
    
    * Reverted endpoint name changes and implemented suggestions from the last pull request.
    
    * Responding to Rowan's comments.
    
    * Fixed documentation where it said SmartContractsController.GetBalance() returns the balance in Satoshi
    
    Standardized this for all "Money" mentions. The possibility the value could be a sidechain token and not STRAT is taken into account.

--- src/Stratis.Bitcoin.Features.SmartContracts/Wallet/SmartContractWalletController.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/Wallet/SmartContractWalletController.cs
@@ -63,6 +63,32 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Wallet
                 .Select(grouping => grouping.Key);
         }
 
+        /// <summary>
+        /// Gets a smart contract account address.
+        /// This is a single address to use for all smart contract interactions.
+        /// Smart contracts send funds to and store data at this address. For example, an ERC-20 token
+        /// would store tokens allocated to a user at this address, although the actual data
+        /// could, in fact, be anything. The address stores a history of smart contract create/call transactions.   
+        /// It also holds a UTXO list/balance based on UTXOs sent to it from smart contracts or user wallets.
+        /// Once a smart contract has written data to this address, you need to use the address to
+        /// provide gas and fees for smart contract calls involving that stored data (for that smart contract deployment).
+        /// In the case of specific ERC-20 tokens allocated to you, using this address would be
+        /// a requirement if you were to, for example, send some of the tokens to an exchange.  
+        /// It is therefore recommended that in order to keep an intact history and avoid complications,
+        /// you use the single smart contract address provided by this function for all interactions with smart contracts.
+        /// In addition, a smart contract address can be used to identify a contract deployer.
+        /// Some methods, such as a withdrawal method on an escrow smart contract, should only be executed
+        /// by the deployer, and in this case, it is the smart contract account address that identifies the deployer.
+        ///  
+        /// Note that this account differs from "account 0", which is the "default
+        /// holder of multiple addresses". Other address holding accounts can be created,
+        /// but they should not be confused with the smart contract account, which is represented
+        /// by a single address.
+        /// </summary>
+        /// 
+        /// <param name="walletName">The name of the wallet to retrieve a smart contract account address for.</param>
+        /// 
+        /// <returns>A smart contract account address to use for the wallet.</returns>
         [Route("account-addresses")]
         [HttpGet]
         public IActionResult GetAccountAddresses(string walletName)
@@ -94,6 +120,16 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Wallet
             }
         }
 
+        /// <summary>
+        /// Gets the balance at a specific wallet address in STRAT (or the sidechain coin).
+        /// This method gets the UTXOs at the address that the wallet can spend.
+        /// The function can be used to query the balance at a smart contract account address
+        /// supplied by /api/SmartContractWallet/account-addresses.
+        /// </summary>
+        ///
+        /// <param name="walletName">The address at which to retrieve the balance.</param>
+        /// 
+        /// <returns>The balance at a specific wallet address in STRAT (or the sidechain coin).</returns>
         [Route("address-balance")]
         [HttpGet]
         public IActionResult GetAddressBalance(string address)
@@ -103,6 +139,22 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Wallet
             return this.Json(balance.AmountConfirmed.ToUnit(MoneyUnit.Satoshi));
         }
 
+
+        /// <summary>
+        /// Gets the history of a specific wallet address.
+        /// This includes the smart contract create and call transactions
+        /// This method can be used to query the balance at a smart contract account address
+        /// supplied by /api/SmartContractWallet/account-addresses. Indeed,
+        /// it is advisable to use /api/SmartContractWallet/account-addresses
+        /// to generate an address for all smart contract interactions.
+        /// If this has been done, and that address is supplied to this method,
+        /// a list of all smart contract interactions for a wallet will be returned.
+        /// </summary>
+        /// 
+        /// <param name="walletName">The name of the wallet holding the address.</param>
+        /// <param name="address">The address to retrieve the history for.</param>
+        /// 
+        /// <returns>A list of smart contract create and call transaction items as well as transaction items at a specific wallet address.</returns>
         [Route("history")]
         [HttpGet]
         public IActionResult GetHistory(string walletName, string address)
@@ -210,7 +262,16 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Wallet
                 return ErrorHelpers.BuildErrorResponse(HttpStatusCode.BadRequest, e.Message, e.ToString());
             }
         }
-
+         
+        /// <summary>
+        /// Builds a transaction to create a smart contract and then broadcasts the transaction to the network.
+        /// If the deployment is successful, methods on the smart contract can be subsequently called.
+        /// </summary>
+        /// 
+        /// <param name="request">An object containing the necessary parameters to build the transaction.</param>
+        /// 
+        /// <returns>A hash of the transaction used to create the smart contract. The result of the transaction broadcast is not returned,
+        /// and you should check for a transaction receipt to see if it was successful.</returns>
         [Route("create")]
         [HttpPost]
         public IActionResult Create([FromBody] BuildCreateContractTransactionRequest request)
@@ -230,6 +291,16 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Wallet
             return this.Json(response.TransactionId);
         }
 
+        /// <summary>
+        /// Builds a transaction to call a smart contract method and then broadcasts the transaction to the network.
+        /// If the call is successful, any changes to the smart contract balance or persistent data are propagated
+        /// across the network.
+        /// </summary>
+        /// 
+        /// <param name="request">An object containing the necessary parameters to build the transaction.</param>
+        ///
+        /// <returns>The transaction used to call a smart contract method. The result of the transaction broadcast is not returned,
+        /// and you should check for a transaction receipt to see if it was successful.</returns>
         [Route("call")]
         [HttpPost]
         public IActionResult Call([FromBody] BuildCallContractTransactionRequest request)
@@ -248,6 +319,15 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Wallet
             return this.Json(response);
         }
 
+        /// <summary>
+        /// Broadcasts a transaction, which either creates a smart contract or calls a method on a smart contract.
+        /// If the contract deployment or method call are successful gas and fees are consumed.
+        /// </summary>
+        /// 
+        /// <param name="request">An object containing the necessary parameters to send the transaction.</param>
+        /// 
+        /// <returns>A model of the transaction which the Broadcast Manager broadcasts. The result of the transaction broadcast is not returned,
+        /// and you should check for a transaction receipt to see if it was successful.</returns>
         [Route("send-transaction")]
         [HttpPost]
         public IActionResult SendTransaction([FromBody] SendTransactionRequest request)
