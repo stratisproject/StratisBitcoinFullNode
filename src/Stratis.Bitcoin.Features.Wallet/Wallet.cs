06b89a302143f6de0f4fde7ac7db2ea1ba44ee94
Rowan de Haas <rowandh@users.noreply.github.com>
2019-03-20 07:20:36 -0600

    Update src/Stratis.Bitcoin.Features.Wallet/Wallet.cs
    Co-Authored-By: DennisAMenace <41244965+DennisAMenace@users.noreply.github.com>

--- src/Stratis.Bitcoin.Features.Wallet/Wallet.cs
+++ src/Stratis.Bitcoin.Features.Wallet/Wallet.cs
@@ -346,7 +346,7 @@ namespace Stratis.Bitcoin.Features.Wallet
             // Check if the wallet contains the address.
             if (hdAddress == null)
             {
-                throw new WalletException("Address not found on wallet.");
+                throw new WalletException("Address not found in wallet.");
             }
 
             return hdAddress;
