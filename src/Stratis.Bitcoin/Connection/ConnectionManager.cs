e37e62725d266e4b6516a4277d9ba270e8e276c1
quantumagi <someguy.fromafrica@gmail.com>
2019-03-26 21:47:15 +1100

    Changes based on feedback

--- src/Stratis.Bitcoin/Connection/ConnectionManager.cs
+++ src/Stratis.Bitcoin/Connection/ConnectionManager.cs
@@ -87,8 +87,6 @@ namespace Stratis.Bitcoin.Connection
         /// <summary>Traffic statistics from peers that have been disconnected.</summary>
         private readonly PerformanceCounter disconnectedPerfCounter;
 
-        public IPeerBanning PeerBanning { get; private set; }
-
         public ConnectionManager(IDateTimeProvider dateTimeProvider,
             ILoggerFactory loggerFactory,
             Network network,
@@ -118,7 +116,6 @@ namespace Stratis.Bitcoin.Connection
             this.ConnectionSettings = connectionSettings;
             this.networkPeerDisposer = new NetworkPeerDisposer(this.loggerFactory);
             this.Servers = new List<NetworkPeerServer>();
-            this.PeerBanning = new PeerBanning(this, loggerFactory, dateTimeProvider, peerAddressManager);
 
             this.Parameters = parameters;
             this.Parameters.ConnectCancellation = this.nodeLifetime.ApplicationStopping;
@@ -407,9 +404,6 @@ namespace Stratis.Bitcoin.Connection
         {
             Guard.NotNull(ipEndpoint, nameof(ipEndpoint));
 
-            if (this.PeerBanning.IsBanned(ipEndpoint))
-                throw new InvalidOperationException("Can't perform 'addnode' for a banned peer.");
-
             this.peerAddressManager.AddPeer(ipEndpoint.MapToIpv6(), IPAddress.Loopback);
 
             if (!this.ConnectionSettings.AddNode.Any(p => p.Match(ipEndpoint)))
@@ -448,9 +442,6 @@ namespace Stratis.Bitcoin.Connection
 
         public async Task<INetworkPeer> ConnectAsync(IPEndPoint ipEndpoint)
         {
-            if (this.PeerBanning.IsBanned(ipEndpoint))
-                throw new InvalidOperationException("Can't connect to a banned peer.");
-
             var existingConnection = this.connectedPeers.FirstOrDefault(connectedPeer => connectedPeer.PeerEndPoint.Match(ipEndpoint));
 
             if (existingConnection != null)
