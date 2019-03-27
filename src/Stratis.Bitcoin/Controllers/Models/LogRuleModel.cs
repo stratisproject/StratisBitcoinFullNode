9710b99b2e4af120bb3bdde85619d7e62a5c85a2
bokobza <bokobza@gmail.com>
2019-03-27 17:26:10 +0000

    Added an endpoint to get the enabled log rules

---
+++ src/Stratis.Bitcoin/Controllers/Models/LogRuleModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Stratis.Bitcoin.Controllers.Models
+{
+    /// <summary>
+    /// A class representing a log rule as found in NLog.config.
+    /// </summary>
+    public class LogRuleModel
+    {
+        /// <summary>
+        /// The name of the rule.
+        /// </summary>
+        public string RuleName { get; set; }
+
+        /// <summary>
+        /// The log level.
+        /// </summary>
+        public string LogLevel { get; set; }
+
+        /// <summary>
+        /// The full path of the log file.
+        /// </summary>
+        public string FileName { get; set; }
+    }
+}
