c9a95340fdafbe5cd74431b6b51ac45ee86084af
noescape0 <NoEscape0@ya.ru>
2019-03-27 20:03:33 +0300

    TransactionIndexer outline

---
+++ src/Stratis.Bitcoin/TxIndexing/TransactionIndexer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NBitcoin;
+using Stratis.Bitcoin.Configuration;
+
+namespace Stratis.Bitcoin.TxIndexing
+{
+    public class TransactionIndexer
+    {
+        public TransactionIndexer(NodeSettings nodeSettings)
+        {
+
+        }
+
+        /// <summary>Returns balance of the given address confirmed with at least <paramref name="minConfirmations"/> confirmations.</summary>
+        public Money GetAddressBalance(BitcoinAddress address, int minConfirmations = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>Returns the total amount received by the given address in transactions with at least <paramref name="minConfirmations"/> confirmations.</summary>
+        public Money GetReceivedByAddress(BitcoinAddress address, int minConfirmations = 0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
