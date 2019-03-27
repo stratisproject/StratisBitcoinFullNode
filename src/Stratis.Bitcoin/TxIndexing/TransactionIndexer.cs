90464e1e1d5e147b605abd2747fbdb5abbc4cd6c
noescape0 <NoEscape0@ya.ru>
2019-03-27 20:32:49 +0300

    TODOs

--- src/Stratis.Bitcoin/TxIndexing/TransactionIndexer.cs
+++ src/Stratis.Bitcoin/TxIndexing/TransactionIndexer.cs
@@ -1,16 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using NBitcoin;
 using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Signals;
 
 namespace Stratis.Bitcoin.TxIndexing
 {
-    public class TransactionIndexer
+    public class TransactionIndexer : IDisposable
     {
-        public TransactionIndexer(NodeSettings nodeSettings)
+        public TransactionIndexer(NodeSettings nodeSettings, ISignals signals)
         {
-
+            // TODO class:
+            // throw if indexing is disabled but any method is called
+            // once indexing is enabled we require first block to be on top of the tip.
+            // save the tip of the component
         }
 
         /// <summary>Returns balance of the given address confirmed with at least <paramref name="minConfirmations"/> confirmations.</summary>
@@ -23,6 +25,21 @@ namespace Stratis.Bitcoin.TxIndexing
         public Money GetReceivedByAddress(BitcoinAddress address, int minConfirmations = 0)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>Returns the total amount spent from the given address in transactions with at least <paramref name="minConfirmations"/> confirmations.</summary>
+        public Money GetSpentByAddress(BitcoinAddress address, int minConfirmations = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        // KV where key is address and value is a list of operations.
+        //operation: type (spend\deposit), block height, amount
+
+        public void Dispose()
+        {
+            // TODO
         }
     }
 }
