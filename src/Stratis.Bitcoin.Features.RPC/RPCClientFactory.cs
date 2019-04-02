ef9acb3e70c53863be23c71a7eea564485177949
quantumagi <someguy.fromafrica@gmail.com>
2019-04-02 17:53:48 +1100

    Refactor RpcClient

--- src/Stratis.Bitcoin.Features.RPC/RPCClientFactory.cs
+++ src/Stratis.Bitcoin.Features.RPC/RPCClientFactory.cs
@@ -15,7 +15,8 @@ namespace Stratis.Bitcoin.Features.RPC
         /// <param name="authenticationString">username:password or the content of the .cookie file or null to auto configure.</param>
         /// <param name="address">The binding address.</param>
         /// <param name="network">The network.</param>
-        IRPCClient Create(string authenticationString, Uri address, Network network);
+        /// <param name="rpcSettings">The RPC settings.</param>
+        IRPCClient Create(string authenticationString, Uri address, Network network, RpcSettings rpcSettings);
     }
 
     /// <summary>
@@ -24,13 +25,14 @@ namespace Stratis.Bitcoin.Features.RPC
     public class RPCClientFactory : IRPCClientFactory
     {
         /// <inheritdoc/>
-        public IRPCClient Create(string authenticationString, Uri address, Network network)
+        public IRPCClient Create(string authenticationString, Uri address, Network network, RpcSettings rpcSettings)
         {
             Guard.NotNull(authenticationString, nameof(authenticationString));
             Guard.NotNull(address, nameof(address));
             Guard.NotNull(network, nameof(network));
+            Guard.NotNull(rpcSettings, nameof(rpcSettings));
 
-            return new RPCClient(authenticationString, address, network);
+            return new RPCClient(rpcSettings, address.ToString(), network);
         }
     }
 }
