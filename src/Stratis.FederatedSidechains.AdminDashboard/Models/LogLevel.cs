e82d284c2343f028eb39e16b6cd4eac71529e271
Clint.Network <me@clint.network>
2019-04-06 15:33:32 +0200

    Add Log Levels modal

---
+++ src/Stratis.FederatedSidechains.AdminDashboard/Models/LogLevel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stratis.FederatedSidechains.AdminDashboard.Models
+{
+    public class LogItem
+    {
+        public string Name { get; set; }
+        public LogLevel MinLevel { get; set; }
+        public string Filename { get; set; }
+    }
+
+    public enum LogLevel
+    {
+        Trace,
+        Informations,
+        Warning,
+        Error,
+        Critical
+    }
+}
