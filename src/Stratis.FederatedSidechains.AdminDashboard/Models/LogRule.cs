a5453003a78e66c5f2337378ff6654dc3c9420e7
Clint.Network <me@clint.network>
2019-04-09 02:06:52 +0200

    Add json property name to LogRule

--- src/Stratis.FederatedSidechains.AdminDashboard/Models/LogRule.cs
+++ src/Stratis.FederatedSidechains.AdminDashboard/Models/LogRule.cs
@@ -2,22 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Stratis.FederatedSidechains.AdminDashboard.Models
 {
     public class LogRule
     {
+        [JsonProperty("ruleName")]
         public string Name { get; set; }
+
+        [JsonProperty("logLevel")]
+        [JsonConverter(typeof(StringEnumConverter))]
         public LogLevel MinLevel { get; set; } = LogLevel.Trace;
+
+        [JsonProperty("filename")]
         public string Filename { get; set; }
     }
 
     public enum LogLevel
     {
         Trace,
-        Informations,
-        Warning,
+        Debug,
+        Info,
+        Warn,
         Error,
-        Critical
+        Fatal
     }
 }
