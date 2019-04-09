caede041bcf3586bd4ce9aaff4a9dc8355f2dbf1
Igor <igor.goldobin@gmail.com>
2019-04-09 14:25:50 +1000

    Added launch files

---
+++ src/Stratis.Bitcoin.LightD/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Stratis.Bitcoin.LightD")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("82A8ED3E-81C2-4D03-94B9-EA02CC9DCDFA")]
