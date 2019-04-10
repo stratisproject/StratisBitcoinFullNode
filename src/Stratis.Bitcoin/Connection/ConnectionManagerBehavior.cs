7f3860a95a909d04a455c6b6c9aff1a74ab06810
Fabio Angela <fabio.angela@waynet.it>
2019-04-10 19:32:56 +0200

    added eventual exception message in console when a peer get disconnected because of on an unhandled exception

--- src/Stratis.Bitcoin/Connection/ConnectionManagerBehavior.cs
+++ src/Stratis.Bitcoin/Connection/ConnectionManagerBehavior.cs
@@ -8,17 +8,14 @@ using Stratis.Bitcoin.Utilities;
 using Stratis.Bitcoin.Utilities.Extensions;
 using TracerAttributes;
 
-namespace Stratis.Bitcoin.Connection
-{
-    public interface IConnectionManagerBehavior : INetworkPeerBehavior
-    {
+namespace Stratis.Bitcoin.Connection {
+    public interface IConnectionManagerBehavior : INetworkPeerBehavior {
         bool Whitelisted { get; }
 
         bool OneTry { get; }
     }
 
-    public class ConnectionManagerBehavior : NetworkPeerBehavior, IConnectionManagerBehavior
-    {
+    public class ConnectionManagerBehavior : NetworkPeerBehavior, IConnectionManagerBehavior {
         /// <summary>Logger factory to create loggers.</summary>
         private readonly ILoggerFactory loggerFactory;
 
@@ -37,8 +34,7 @@ namespace Stratis.Bitcoin.Connection
 
         public bool OneTry { get; internal set; }
 
-        public ConnectionManagerBehavior(IConnectionManager connectionManager, ILoggerFactory loggerFactory)
-        {
+        public ConnectionManagerBehavior(IConnectionManager connectionManager, ILoggerFactory loggerFactory) {
             this.logger = loggerFactory.CreateLogger(this.GetType().FullName, $"[{this.GetHashCode():x}] ");
             this.infoLogger = loggerFactory.CreateLogger(this.GetType().FullName);
             this.loggerFactory = loggerFactory;
@@ -47,57 +43,46 @@ namespace Stratis.Bitcoin.Connection
         }
 
         [NoTrace]
-        public override object Clone()
-        {
-            return new ConnectionManagerBehavior(this.connectionManager, this.loggerFactory)
-            {
+        public override object Clone() {
+            return new ConnectionManagerBehavior(this.connectionManager, this.loggerFactory) {
                 OneTry = this.OneTry,
                 Whitelisted = this.Whitelisted,
             };
         }
 
         [NoTrace]
-        protected override void AttachCore()
-        {
+        protected override void AttachCore() {
             this.AttachedPeer.StateChanged.Register(this.OnStateChangedAsync);
 
             INetworkPeer peer = this.AttachedPeer;
-            if (peer != null)
-            {
-                if (this.connectionManager.ConnectionSettings.Whitelist.Exists(e => e.Match(peer.PeerEndPoint)))
-                {
+            if (peer != null) {
+                if (this.connectionManager.ConnectionSettings.Whitelist.Exists(e => e.Match(peer.PeerEndPoint))) {
                     this.Whitelisted = true;
                 }
             }
         }
 
-        private async Task OnStateChangedAsync(INetworkPeer peer, NetworkPeerState oldState)
-        {
-            try
-            {
-                if (peer.State == NetworkPeerState.HandShaked)
-                {
+        private async Task OnStateChangedAsync(INetworkPeer peer, NetworkPeerState oldState) {
+            try {
+                if (peer.State == NetworkPeerState.HandShaked) {
                     this.connectionManager.AddConnectedPeer(peer);
                     this.infoLogger.LogInformation("Peer '{0}' connected ({1}), agent '{2}', height {3}", peer.RemoteSocketEndpoint, peer.Inbound ? "inbound" : "outbound", peer.PeerVersion.UserAgent, peer.PeerVersion.StartHeight);
 
                     peer.SendMessage(new SendHeadersPayload());
                 }
 
-                if ((peer.State == NetworkPeerState.Failed) || (peer.State == NetworkPeerState.Offline))
-                {
-                    this.infoLogger.LogInformation("Peer '{0}' offline, reason: '{1}'.", peer.RemoteSocketEndpoint, peer.DisconnectReason?.Reason ?? "unknown");
+                if ((peer.State == NetworkPeerState.Failed) || (peer.State == NetworkPeerState.Offline)) {
+                    this.infoLogger.LogInformation("Peer '{0}' offline, reason: '{1}'.{2}", peer.RemoteSocketEndpoint, peer.DisconnectReason?.Reason ?? "unknown", peer.DisconnectReason?.Exception?.Message ?? string.Empty);
 
                     this.connectionManager.RemoveConnectedPeer(peer, "Peer offline");
                 }
             }
-            catch (OperationCanceledException)
-            {
+            catch (OperationCanceledException) {
             }
         }
 
         [NoTrace]
-        protected override void DetachCore()
-        {
+        protected override void DetachCore() {
             this.AttachedPeer.StateChanged.Unregister(this.OnStateChangedAsync);
             this.connectionManager.PeerDisconnected(this.AttachedPeer.Connection.Id);
         }
