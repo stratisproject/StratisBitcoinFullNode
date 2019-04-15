b493925582262ee002436102eb2f4e823229c2be
noescape0 <NoEscape0@ya.ru>
2019-04-15 17:22:16 +0300

    GetAddressBalancesAsync client implementation

--- src/Stratis.Bitcoin.Features.BlockStore/ControllersAndClients/BlockStoreClient.cs
+++ src/Stratis.Bitcoin.Features.BlockStore/ControllersAndClients/BlockStoreClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -10,8 +11,11 @@ namespace Stratis.Bitcoin.Features.BlockStore.ControllersAndClients
     /// <summary>Rest client for <see cref="BlockStoreController"/>.</summary>
     public interface IBlockStoreClient
     {
-        /// <summary><see cref="BlockStoreController.GetReceivedByAddress"/></summary>
+        /// <summary><see cref="BlockStoreController.GetAddressBalance"/></summary>
         Task<Money> GetAddressBalanceAsync(string address, int minConfirmations, CancellationToken cancellation = default(CancellationToken));
+
+        /// <summary><see cref="BlockStoreController.GetAddressesBalances"/></summary>
+        Task<Dictionary<string, Money>> GetAddressBalancesAsync(IEnumerable<string> addresses, int minConfirmations, CancellationToken cancellation = default(CancellationToken));
     }
 
     /// <inheritdoc cref="IBlockStoreClient"/>
@@ -28,6 +32,16 @@ namespace Stratis.Bitcoin.Features.BlockStore.ControllersAndClients
             string arguments = $"{nameof(address)}={address},{nameof(minConfirmations)}={minConfirmations}";
 
             return this.SendGetRequestAsync<Money>(BlockStoreRouteEndPoint.GetAddressBalance, arguments, cancellation);
+        }
+
+        /// <inheritdoc />
+        public Task<Dictionary<string, Money>> GetAddressBalancesAsync(IEnumerable<string> addresses, int minConfirmations, CancellationToken cancellation = default(CancellationToken))
+        {
+            string addrString = string.Join(",", addresses);
+
+            string arguments = $"{nameof(addresses)}={addrString}&{nameof(minConfirmations)}={minConfirmations}";
+
+            return this.SendGetRequestAsync<Dictionary<string, Money>>(BlockStoreRouteEndPoint.GetAddressesBalances, arguments, cancellation);
         }
     }
 }
