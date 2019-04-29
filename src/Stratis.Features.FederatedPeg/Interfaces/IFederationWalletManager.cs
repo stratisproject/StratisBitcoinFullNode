4490d3a0ee8fafea88ddd23aa9a886f3f1e6a6c7
Jordan Andrews <jordandrews@hotmail.com>
2019-04-29 15:57:59 +1000

    Remove propagation property

--- src/Stratis.Features.FederatedPeg/Interfaces/IFederationWalletManager.cs
+++ src/Stratis.Features.FederatedPeg/Interfaces/IFederationWalletManager.cs
@@ -58,7 +58,7 @@ namespace Stratis.Features.FederatedPeg.Interfaces
         /// <param name="block">The block in which this transaction was included.</param>
         /// <param name="isPropagated">Transaction propagation state.</param>
         /// <returns>A value indicating whether this transaction affects the wallet.</returns>
-        bool ProcessTransaction(Transaction transaction, int? blockHeight = null, Block block = null, bool isPropagated = true);
+        bool ProcessTransaction(Transaction transaction, int? blockHeight = null, Block block = null);
 
         /// <summary>
         /// Verifies that the transaction's input UTXO's have been reserved by the wallet.
