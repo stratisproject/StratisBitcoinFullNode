bd40961d8f9f701619d2b9fe1d3ab6a5e632103f
Francois de la Rouviere <fassadlr@gmail.com>
2019-05-06 11:56:34 +0100

    Update AssemblyInfo.cs

--- src/Stratis.Bitcoin/Properties/AssemblyInfo.cs
+++ src/Stratis.Bitcoin/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Stratis.Bitcoin")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
