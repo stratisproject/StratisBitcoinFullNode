dc069987c2a5c94d18fc64c9fa754b0875fced8c
Francois de la Rouviere <fassadlr@gmail.com>
2019-05-08 08:01:27 +0100

    Added comments

--- src/Stratis.Bitcoin.Features.BlockStore/Controllers/BlockStoreClient.cs
+++ src/Stratis.Bitcoin.Features.BlockStore/Controllers/BlockStoreClient.cs
@@ -21,6 +21,12 @@ namespace Stratis.Bitcoin.Features.BlockStore.Controllers
     /// <inheritdoc cref="IBlockStoreClient"/>
     public class BlockStoreClient : RestApiClientBase, IBlockStoreClient
     {
+        /// <summary>
+        /// Currently the <paramref name="url"/> is required as it needs to be configurable for testing.
+        /// <para>
+        /// In a production/live scenario the sidechain and mainnet federation nodes should run on the same machine.
+        /// </para>
+        /// </summary>
         public BlockStoreClient(ILoggerFactory loggerFactory, IHttpClientFactory httpClientFactory, string url, int port)
             : base(loggerFactory, httpClientFactory, port, "BlockStore", url)
         {
