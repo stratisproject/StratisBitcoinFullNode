a8479f1777d926a5c76df3baa475ab7df00f513c
Jordan Andrews <jordandrews@hotmail.com>
2019-05-22 14:56:06 +1000

    Add CounterChainSettings

---
+++ src/Stratis.Features.FederatedPeg/CounterChainSettings.cs
@@ -0,0 +1,31 @@
+﻿using NBitcoin;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Utilities;
+using Stratis.Features.FederatedPeg.Interfaces;
+
+namespace Stratis.Features.FederatedPeg
+{
+    public class CounterChainSettings : ICounterChainSettings
+    {
+        public const string CounterChainApiHostParam = "counterchainapihost";
+
+        public const string CounterChainApiPortParam = "counterchainapiport";
+
+        public string CounterChainApiHost { get; set; }
+
+        public int CounterChainApiPort { get; set; }
+
+        public Network CounterChainNetwork { get; set; }
+
+        public CounterChainSettings(NodeSettings nodeSettings, Network counterChainNetwork)
+        {
+            Guard.NotNull(nodeSettings, nameof(nodeSettings));
+
+            TextFileConfiguration configReader = nodeSettings.ConfigReader;
+
+            this.CounterChainApiHost = configReader.GetOrDefault(CounterChainApiHostParam, "localhost");
+            this.CounterChainApiPort = configReader.GetOrDefault(CounterChainApiPortParam, counterChainNetwork.DefaultAPIPort);
+            this.CounterChainNetwork = counterChainNetwork;
+        }
+    }
+}
