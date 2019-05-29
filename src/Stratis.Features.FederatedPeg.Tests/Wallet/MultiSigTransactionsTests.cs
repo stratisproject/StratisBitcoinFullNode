a37186bc508fe83b96d1b43a5bc1d380151832ba
quantumagi <someguy.fromafrica@gmail.com>
2019-05-29 21:12:36 +1000

    Tidy up tests

--- src/Stratis.Features.FederatedPeg.Tests/Wallet/MultiSigTransactionsTests.cs
+++ src/Stratis.Features.FederatedPeg.Tests/Wallet/MultiSigTransactionsTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using NBitcoin;
 using Stratis.Features.FederatedPeg.Wallet;
 using Xunit;
@@ -7,72 +9,28 @@ namespace Stratis.Features.FederatedPeg.Tests.Wallet
 {
     public class MultiSigTransactionsTests
     {
-        // TODO: Update this to create a MultiSigTransactions with 64 different TransactionData entries.
+        private class TestData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                for (int mask = 0; mask < 64; mask++)
+                {
+                    yield return new object[] {
+                    (mask & 32) != 0,
+                    (mask & 16) != 0,
+                    (mask & 8) != 0,
+                    (mask & 4) != 0,
+                    (mask & 2) != 0,
+                    (mask & 1) != 0
+                };
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
         [Theory]
-        [InlineData(false, false, false, false, false, false)]
-        [InlineData(false, false, false, false, false, true)]
-        [InlineData(false, false, false, false, true, false)]
-        [InlineData(false, false, false, false, true, true)]
-        [InlineData(false, false, false, true, false, false)]
-        [InlineData(false, false, false, true, false, true)]
-        [InlineData(false, false, false, true, true, false)]
-        [InlineData(false, false, false, true, true, true)]
-        [InlineData(false, false, true, false, false, false)]
-        [InlineData(false, false, true, false, false, true)]
-        [InlineData(false, false, true, false, true, false)]
-        [InlineData(false, false, true, false, true, true)]
-        [InlineData(false, false, true, true, false, false)]
-        [InlineData(false, false, true, true, false, true)]
-        [InlineData(false, false, true, true, true, false)]
-        [InlineData(false, false, true, true, true, true)]
-        [InlineData(false, true, false, false, false, false)]
-        [InlineData(false, true, false, false, false, true)]
-        [InlineData(false, true, false, false, true, false)]
-        [InlineData(false, true, false, false, true, true)]
-        [InlineData(false, true, false, true, false, false)]
-        [InlineData(false, true, false, true, false, true)]
-        [InlineData(false, true, false, true, true, false)]
-        [InlineData(false, true, false, true, true, true)]
-        [InlineData(false, true, true, false, false, false)]
-        [InlineData(false, true, true, false, false, true)]
-        [InlineData(false, true, true, false, true, false)]
-        [InlineData(false, true, true, false, true, true)]
-        [InlineData(false, true, true, true, false, false)]
-        [InlineData(false, true, true, true, false, true)]
-        [InlineData(false, true, true, true, true, false)]
-        [InlineData(false, true, true, true, true, true)]
-        [InlineData(true, false, false, false, false, false)]
-        [InlineData(true, false, false, false, false, true)]
-        [InlineData(true, false, false, false, true, false)]
-        [InlineData(true, false, false, false, true, true)]
-        [InlineData(true, false, false, true, false, false)]
-        [InlineData(true, false, false, true, false, true)]
-        [InlineData(true, false, false, true, true, false)]
-        [InlineData(true, false, false, true, true, true)]
-        [InlineData(true, false, true, false, false, false)]
-        [InlineData(true, false, true, false, false, true)]
-        [InlineData(true, false, true, false, true, false)]
-        [InlineData(true, false, true, false, true, true)]
-        [InlineData(true, false, true, true, false, false)]
-        [InlineData(true, false, true, true, false, true)]
-        [InlineData(true, false, true, true, true, false)]
-        [InlineData(true, false, true, true, true, true)]
-        [InlineData(true, true, false, false, false, false)]
-        [InlineData(true, true, false, false, false, true)]
-        [InlineData(true, true, false, false, true, false)]
-        [InlineData(true, true, false, false, true, true)]
-        [InlineData(true, true, false, true, false, false)]
-        [InlineData(true, true, false, true, false, true)]
-        [InlineData(true, true, false, true, true, false)]
-        [InlineData(true, true, false, true, true, true)]
-        [InlineData(true, true, true, false, false, false)]
-        [InlineData(true, true, true, false, false, true)]
-        [InlineData(true, true, true, false, true, false)]
-        [InlineData(true, true, true, false, true, true)]
-        [InlineData(true, true, true, true, false, false)]
-        [InlineData(true, true, true, true, false, true)]
-        [InlineData(true, true, true, true, true, false)]
-        [InlineData(true, true, true, true, true, true)]
+        [ClassData(typeof(TestData))]
         public void TransactionDataAddedToMultiSigTransactionsExistsInExpectedLookups(bool hasBlockHeight, bool hasSpendingDetails, bool hasWithdrawalDetails, bool flipBlockHeight, bool flipSpendingDetails, bool flipWithdrawalDetails)
         {
             uint256 transactionId = 1;
