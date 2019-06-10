ba2bed2b3111528a481d4ac316de36e843c1efa6
Jordan Andrews <jordandrews@hotmail.com>
2019-06-10 11:04:49 +1000

    Remove 1 subscription

--- src/Stratis.Features.FederatedPeg/Interfaces/IInputConsolidator.cs
+++ src/Stratis.Features.FederatedPeg/Interfaces/IInputConsolidator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using NBitcoin;
+using Stratis.Bitcoin.Primitives;
 using Stratis.Features.FederatedPeg.TargetChain;
 
 namespace Stratis.Features.FederatedPeg.Interfaces
@@ -11,5 +12,7 @@ namespace Stratis.Features.FederatedPeg.Interfaces
         void StartConsolidation();
 
         ConsolidationSignatureResult CombineSignatures(Transaction partialTransaction);
+
+        void ProcessBlock(ChainedHeaderBlock chainedHeaderBlock);
     }
 }
