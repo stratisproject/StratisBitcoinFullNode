b75023d6a4bfb1c7875d5cd1d19512a77cbb525b
noescape0 <NoEscape0@ya.ru>
2019-06-12 17:30:02 +0300

    respect last pow block and checkpoints

--- src/Stratis.Bitcoin.Features.Consensus/Rules/CommonRules/PoSTransactionsOrderRule.cs
+++ src/Stratis.Bitcoin.Features.Consensus/Rules/CommonRules/PoSTransactionsOrderRule.cs
@@ -11,17 +11,35 @@ namespace Stratis.Bitcoin.Features.Consensus.Rules.CommonRules
         /// <inheritdoc />
         /// <exception cref="ConsensusErrors.InvalidTxCount">Thrown in case block has less than 2 transaction.</exception>
         /// <exception cref="ConsensusErrors.PHCoinstakeMissmatch">Thrown in case coinstake transaction from the proven header missmatches 2nd block transaction.</exception>
+        /// <exception cref="ConsensusErrors.ProofOfWorkTooHigh">Thrown in case block is a PoW block but created after the last pow height.</exception>
         public override void Run(RuleContext context)
         {
             Block block = context.ValidationContext.BlockToValidate;
+
+            var header = context.ValidationContext.ChainedHeaderToValidate.Header as ProvenBlockHeader;
+
+            // In case we are in PoW era there might be no coinstake tx.
+            // We have no way of telling if the block was supposed to be PoW or PoS so attacker
+            // can trick us into thinking that all of them are PoW so no PH is required.
+            if (header == null || !header.Coinstake.IsCoinStake)
+            {
+                // If the header represents a POW block we don't do any validation and just verify the header is not passed the last pow height and last checkpoint's height.
+                if ((context.ValidationContext.ChainedHeaderToValidate.Height > this.Parent.Network.Consensus.LastPOWBlock) &&
+                    (context.ValidationContext.ChainedHeaderToValidate.Height > this.Parent.Checkpoints.GetLastCheckpointHeight()))
+                {
+                    this.Logger.LogTrace("(-)[NOT_PH_POW_TOO_HIGH]");
+                    ConsensusErrors.ProofOfWorkTooHigh.Throw();
+                }
+
+                this.Logger.LogTrace("(-)[NOT_PH]");
+                return;
+            }
 
             if (block.Transactions.Count < 2)
             {
                 this.Logger.LogTrace("(-)[BAD_TX_COUNT]");
                 ConsensusErrors.InvalidTxCount.Throw();
             }
-
-            var header = context.ValidationContext.ChainedHeaderToValidate.Header as ProvenBlockHeader;
 
             uint256 headerCoinstakeHash = header.Coinstake.GetHash();
             uint256 blockCoinstakeHash = block.Transactions[1].GetHash();
