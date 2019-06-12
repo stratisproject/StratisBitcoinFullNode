df57440a4683b02f0739aa5a136a661e66187c71
Clint.Network <me@clint.network>
2019-06-12 17:52:02 +0200

    Change ApiRequester to a depedency injection component with logging

--- src/Stratis.FederatedSidechains.AdminDashboard/Services/ApiRequester.cs
+++ src/Stratis.FederatedSidechains.AdminDashboard/Services/ApiRequester.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using RestSharp;
 using Stratis.FederatedSidechains.AdminDashboard.Entities;
@@ -8,22 +9,30 @@ using Stratis.FederatedSidechains.AdminDashboard.Helpers;
 
 namespace Stratis.FederatedSidechains.AdminDashboard.Services
 {
-    public static class ApiRequester
+    public class ApiRequester
     {
+        private readonly ILogger<ApiRequester> logger;
+
+        public ApiRequester(ILogger<ApiRequester> logger)
+        {
+            this.logger = logger;
+        }
+
         /// <summary>
         /// Make a HTTP request to a specified node
         /// </summary>
         /// <param name="endpoint">HTTP node endpoint</param>
         /// <param name="path">URL</param>
         /// <returns>An ApiResponse object</returns>
-        public static async Task<ApiResponse> GetRequestAsync(string endpoint, string path, string query = null)
+        public async Task<ApiResponse> GetRequestAsync(string endpoint, string path, string query = null)
         {
             var restClient = new RestClient(UriHelper.BuildUri(endpoint, path, query));
             var restRequest = new RestRequest(Method.GET);
             IRestResponse restResponse = await restClient.ExecuteTaskAsync(restRequest);
+            var isSuccess = restResponse.StatusCode.Equals(HttpStatusCode.OK);
             return new ApiResponse
             {
-                IsSuccess = restResponse.StatusCode.Equals(HttpStatusCode.OK),
+                IsSuccess = isSuccess,
                 Content = JsonConvert.DeserializeObject(restResponse.Content)
             };
         }
@@ -35,16 +44,17 @@ namespace Stratis.FederatedSidechains.AdminDashboard.Services
         /// <param name="path">URL</param>
         /// <param name="body">Specify the body request</param>
         /// <returns>An ApiResponse object</returns>
-        public static async Task<ApiResponse> PostRequestAsync(string endpoint, string path, object body)
+        public async Task<ApiResponse> PostRequestAsync(string endpoint, string path, object body, Method method = Method.POST)
         {
             var restClient = new RestClient(UriHelper.BuildUri(endpoint, path));
-            var restRequest = new RestRequest(Method.POST);
+            var restRequest = new RestRequest(method);
             restRequest.AddHeader("Content-type", "application/json");
             restRequest.AddJsonBody(body);
             IRestResponse restResponse = await restClient.ExecuteTaskAsync(restRequest);
+            var isSuccess = restResponse.StatusCode.Equals(HttpStatusCode.OK);
             return new ApiResponse
             {
-                IsSuccess = restResponse.StatusCode.Equals(HttpStatusCode.OK),
+                IsSuccess = isSuccess,
                 Content = JsonConvert.DeserializeObject(restResponse.Content)
             };
         }
