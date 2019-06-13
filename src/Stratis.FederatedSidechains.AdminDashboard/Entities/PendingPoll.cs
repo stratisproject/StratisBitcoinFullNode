72e8c389c94ff6c59d0b458c22348515f89c147d
Clint.Network <me@clint.network>
2019-06-13 15:15:09 +0200

    Create PendingPoll Model

---
+++ src/Stratis.FederatedSidechains.AdminDashboard/Entities/PendingPoll.cs
@@ -0,0 +1,12 @@
+﻿namespace Stratis.FederatedSidechains.AdminDashboard.Entities
+{
+    public class PendingPoll
+    {
+        public int Id { get; set; }
+        public bool IsPending { get; set; }
+        public bool IsExecuted { get; set; }
+        public string PollStartBlockData { get; set; }
+        public string PollVotedInFavorBlockData { get; set; }
+        public string PubKeysHexVotedInFavor { get; set; }
+    }
+}
