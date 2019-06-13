2a5076513c58d3c99adaca822f3b750cf986377a
Clint.Network <me@clint.network>
2019-06-13 15:15:37 +0200

    Add Pending Polls to the Sidechain Model

--- src/Stratis.FederatedSidechains.AdminDashboard/Models/SidechainNodelModel.cs
+++ src/Stratis.FederatedSidechains.AdminDashboard/Models/SidechainNodelModel.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
+using Stratis.FederatedSidechains.AdminDashboard.Entities;
+
 namespace Stratis.FederatedSidechains.AdminDashboard.Models
 {
     public class SidechainNodelModel : StratisNodeModel
     {
+        public List<PendingPoll> PoAPendingPolls { get; set; }
     }
 }
