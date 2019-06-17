c811f00b8954aaba9155e9a4b3536073323c3a79
Rowan de Haas <rowandh@users.noreply.github.com>
2019-06-17 15:15:11 +1000

    Docs

--- src/Stratis.Features.FederatedPeg/FedMultiSigWithdrawalTransactionBuilder.cs
+++ src/Stratis.Features.FederatedPeg/FedMultiSigWithdrawalTransactionBuilder.cs
@@ -10,7 +10,7 @@ using Recipient = Stratis.Features.FederatedPeg.Wallet.Recipient;
 namespace Stratis.Features.FederatedPeg
 {
     /// <summary>
-    /// A handler that has various functionalities related to transaction operations.
+    /// Uses this node's federation wallet and the current network fee policy to build withdrawal transactions for the federation multisig.
     /// </summary>
     public class FedMultiSigWithdrawalTransactionBuilder
     {
@@ -34,10 +34,20 @@ namespace Stratis.Features.FederatedPeg
             this.walletFeePolicy = walletFeePolicy;
         }
 
+        /// <summary>
+        /// Builds a withdrawal transaction from the federation multisig to the given recipients.
+        /// Determines which coins from the federation wallet to spend for the total amount being sent to recipients.
+        /// Calculates the fee based on the wallet fee policy.
+        /// Sets the change address to be the multisig redeem script hash.
+        /// Signs the transaction using the given private keys.
+        /// </summary>
+        /// <param name="recipients">The recipients of the transaction.</param>
+        /// <param name="privateKeys">The private keys of the multisig.</param>
+        /// <returns></returns>
         public Transaction BuildTransaction(List<Recipient> recipients, Key[] privateKeys)
         {
-            // FederationWalletTransactionHandler only supports signing with a single key - the fed wallet key.
-            // However we still want to use it to determine what coins we need, so hack this together here to pass in what FederationWalletTransactionHandler.DetermineCoins.
+            // FederationWalletTransactionHandler only supports signing with a single key - the fed wallet key - so we don't use it to build the transaction.
+            // However we still want to use it to determine what coins we need, so hack this together here to pass in to FederationWalletTransactionHandler.DetermineCoins.
             var multiSigContext = new Wallet.TransactionBuildContext(recipients);
 
             (List<Coin> coins, List<Wallet.UnspentOutputReference> _) = FederationWalletTransactionHandler.DetermineCoins(this.federationWalletManager, this.network, multiSigContext, this.federatedPegSettings);
