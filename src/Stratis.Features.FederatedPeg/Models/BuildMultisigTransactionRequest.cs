3963db977fad25a43f56047b6b38412af823c45a
Rowan de Haas <rowandh@users.noreply.github.com>
2019-06-17 14:36:07 +1000

    Remove unused params

--- src/Stratis.Features.FederatedPeg/Models/BuildMultisigTransactionRequest.cs
+++ src/Stratis.Features.FederatedPeg/Models/BuildMultisigTransactionRequest.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Stratis.Bitcoin.Features.Wallet.Models;
 
 namespace Stratis.Features.FederatedPeg.Models
@@ -6,8 +7,17 @@ namespace Stratis.Features.FederatedPeg.Models
     /// <summary>
     /// A class containing the necessary parameters for a build transaction request.
     /// </summary>
-    public class BuildMultisigTransactionRequest : BuildTransactionRequest
+    public class BuildMultisigTransactionRequest
     {
+        /// <summary>
+        /// A list of transaction recipients. For each recipient, specify the Pubkey script and the amount the
+        /// recipient will receive in STRAT (or a sidechain coin). If the transaction was realized,
+        /// both the values would be used to create the UTXOs for the transaction recipients.
+        /// </summary> 
+        [Required(ErrorMessage = "A list of recipients is required.")]
+        [MinLength(1)]
+        public List<RecipientModel> Recipients { get; set; }
+
         [Required(ErrorMessage = "Mnemonic phrases are required.")]
         public SecretModel[] Secrets { get; set; }
     }
