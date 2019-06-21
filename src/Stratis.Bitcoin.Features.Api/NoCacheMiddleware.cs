df46fae57a7553be3d0a5deb560a9b4e38ceee74
Rowan de Haas <rowandh@users.noreply.github.com>
2019-06-21 15:02:17 +1000

    Fix code style

--- src/Stratis.Bitcoin.Features.Api/NoCacheMiddleware.cs
+++ src/Stratis.Bitcoin.Features.Api/NoCacheMiddleware.cs
@@ -8,18 +8,18 @@ namespace Stratis.Bitcoin.Features.Api
     /// </summary>
     public class NoCacheMiddleware
     {
-        private readonly RequestDelegate _next;
+        private readonly RequestDelegate next;
 
         public NoCacheMiddleware(RequestDelegate next)
         {
-            this._next = next;
+            this.next = next;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
             context.Response.Headers["Cache-Control"] = "no-cache";
 
-            await this._next(context);
+            await this.next(context);
         }
     }
 }
