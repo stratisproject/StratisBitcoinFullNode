0ff4177897b68d4fa6f6269582df5cd3c258e6ca
Rowan de Haas <rowandh@users.noreply.github.com>
2019-06-21 20:00:02 +1000

    Set Swagger API response cache control header (#3743)
    * Prevent api response caching
    
    * Fix code style
    
    * Apply middleware everywhere

---
+++ src/Stratis.Bitcoin.Features.Api/NoCacheMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Stratis.Bitcoin.Features.Api
+{
+    /// <summary>
+    /// Middleware to set the response Cache-Control to no-cache.
+    /// </summary>
+    public class NoCacheMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public NoCacheMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Response.Headers["Cache-Control"] = "no-cache";
+
+            await this.next(context);
+        }
+    }
+}
