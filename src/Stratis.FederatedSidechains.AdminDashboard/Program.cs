a9287eb3367b62e62700165ae3e1a4d7b3b730f1
Clint.Network <me@clint.network>
2019-06-21 14:18:47 +0200

    Add command line options for endpoints

--- src/Stratis.FederatedSidechains.AdminDashboard/Program.cs
+++ src/Stratis.FederatedSidechains.AdminDashboard/Program.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Stratis.FederatedSidechains.AdminDashboard.Settings;
 
 namespace Stratis.FederatedSidechains.AdminDashboard
 {
@@ -7,11 +12,37 @@ namespace Stratis.FederatedSidechains.AdminDashboard
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var app = new CommandLineApplication
+            {
+                Name = "Stratis.FederatedSidechains.AdminDashboard",
+                Description = "Stratis Federation Members Dashboard"
+            };
+            
+            app.HelpOption(true);
+            
+            var mainchainportOption = app.Option<int>("--mainchainport <PORT>", "Specify the port that you want to use for the Main Chain", CommandOptionType.SingleValue);
+            var sidechainportOption = app.Option<int>("--sidechainport <PORT>", "Specify the port that you want to use for the Side Chain", CommandOptionType.SingleValue);
+
+            app.OnExecute(() =>
+            {
+                CreateWebHostBuilder(args, mainchainportOption, sidechainportOption).Build().Run();
+            });
+
+            app.Execute(args);
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args, CommandOption<int> mainchainportOption, CommandOption<int> sidechainportOption)
+        {
+            var webHostBuilder = WebHost.CreateDefaultBuilder(args);
+            if(mainchainportOption.HasValue())
+            {
+                webHostBuilder.UseSetting("DefaultEndpoints:StratisNode", $"http://localhost:{mainchainportOption.Value()}");
+            }
+            if(sidechainportOption.HasValue())
+            {
+                webHostBuilder.UseSetting("DefaultEndpoints:SidechainNode", $"http://localhost:{sidechainportOption.Value()}");
+            }
+            return webHostBuilder.UseStartup<Startup>();
+        }
     }
 }
