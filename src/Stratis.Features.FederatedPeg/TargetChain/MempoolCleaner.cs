9ca4b926bc2b6f751d9affa07eea3514fe32c7cb
quantumagi <someguy.fromafrica@gmail.com>
2019-06-24 18:49:34 +1000

    Changes based on feedback

--- src/Stratis.Features.FederatedPeg/TargetChain/MempoolCleaner.cs
+++ src/Stratis.Features.FederatedPeg/TargetChain/MempoolCleaner.cs
@@ -8,6 +8,7 @@ using Stratis.Bitcoin.AsyncWork;
 using Stratis.Bitcoin.Features.MemoryPool;
 using Stratis.Bitcoin.Utilities;
 using Stratis.Features.FederatedPeg.Interfaces;
+using Stratis.Features.FederatedPeg.Wallet;
 
 namespace Stratis.Features.FederatedPeg.TargetChain
 {
@@ -38,6 +39,7 @@ namespace Stratis.Features.FederatedPeg.TargetChain
         private readonly ICrossChainTransferStore store;
         private readonly IAsyncProvider asyncProvider;
         private readonly INodeLifetime nodeLifetime;
+        private readonly IFederationWalletManager federationWalletManager;
 
         private IAsyncLoop asyncLoop;
 
@@ -46,29 +48,59 @@ namespace Stratis.Features.FederatedPeg.TargetChain
             MempoolOrphans mempoolOrphans,
             ICrossChainTransferStore crossChainTransferStore,
             IAsyncProvider asyncProvider,
-            INodeLifetime nodeLifetime)
+            INodeLifetime nodeLifetime,
+            IFederationWalletManager federationWalletManager)
         {
             Guard.NotNull(loggerFactory, nameof(loggerFactory));
             this.mempoolOrphans = Guard.NotNull(mempoolOrphans, nameof(mempoolOrphans));
             this.store = Guard.NotNull(crossChainTransferStore, nameof(crossChainTransferStore));
             this.asyncProvider = Guard.NotNull(asyncProvider, nameof(asyncProvider));
             this.nodeLifetime = Guard.NotNull(nodeLifetime, nameof(nodeLifetime));
+            this.federationWalletManager = Guard.NotNull(federationWalletManager, nameof(federationWalletManager));
 
             this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
         }
 
+        private IEnumerable<Transaction> CompletedTransactions(IEnumerable<Transaction> transactionsToCheck)
+        {
+            FederationWallet wallet = this.federationWalletManager.GetWallet();
+            MultiSigTransactions walletTxs = wallet.MultiSigAddress.Transactions;
+
+            HashSet<uint256> spendingTransactions = walletTxs
+                .Where(t => t.SpendingDetails?.BlockHeight > 0)
+                .Select(t => t.SpendingDetails?.TransactionId)
+                .ToHashSet();
+
+            foreach (Transaction tx in transactionsToCheck)
+            {
+                uint256 hash = tx.GetHash();
+
+                // If there is a spendable output for this tx and it has a block height then the tx is in completed state.
+                if (walletTxs.TryGetTransaction(hash, 0, out TransactionData tData) && tData.BlockHeight > 0)
+                    yield return tx;
+                // If this is a confirmed spending transaction then it is in completed state.
+                else if (spendingTransactions.Contains(hash))
+                    yield return tx;
+            }
+        }
+
         private async Task CleanMempoolAsync()
         {
-            IEnumerable<Transaction> transactionsToCheck = this.mempoolOrphans.OrphansList().Select(i => i.Tx);
-
-            List<Transaction> transactionsToRemove = this.store.CompletedWithdrawals(transactionsToCheck);
-
-            if (transactionsToRemove.Count > 0)
+            this.federationWalletManager.Synchronous(() =>
             {
-                this.mempoolOrphans.RemoveForBlock(transactionsToRemove);
+                IEnumerable<Transaction> transactionsToCheck = this.mempoolOrphans.OrphansList().Select(i => i.Tx);
 
-                this.logger.LogDebug("Removed {0} transactions from mempool", transactionsToRemove.Count);
-            }
+                List<Transaction> transactionsToRemove = this.store.CompletedWithdrawals(transactionsToCheck)
+                    .Union(CompletedTransactions(transactionsToCheck))
+                    .ToList();
+
+                if (transactionsToRemove.Count > 0)
+                {
+                    this.mempoolOrphans.RemoveForBlock(transactionsToRemove);
+
+                    this.logger.LogDebug("Removed {0} transactions from mempool", transactionsToRemove.Count);
+                }
+            });
         }
 
         /// <inheritdoc />
