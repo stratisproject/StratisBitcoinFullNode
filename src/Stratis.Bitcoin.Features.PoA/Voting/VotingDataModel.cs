6e903dd77bcdfea78e1b1c33da4135722ae3a0f6
Mithril Man <red.angel@libero.it>
2019-06-27 18:18:05 +0200

    fixed VotingDataModel

--- src/Stratis.Bitcoin.Features.PoA/Voting/VotingDataModel.cs
+++ src/Stratis.Bitcoin.Features.PoA/Voting/VotingDataModel.cs
@@ -1,16 +1,25 @@
 ﻿using NBitcoin;
+using Newtonsoft.Json;
 
 namespace Stratis.Bitcoin.Features.PoA.Voting
 {
     public class VotingDataModel
     {
-        public string key { get; private set; }
-        public string hash { get; private set; }
+        [JsonProperty("key")]
+        public string Key { get; private set; }
+
+        [JsonProperty("hash")]
+        public string Hash { get; private set; }
 
         public VotingDataModel(VotingData votingData)
         {
-            this.key = votingData.Key.ToString();
-            this.hash = (new uint256(votingData.Data)).ToString();
+            this.Key = votingData.Key.ToString();
+            this.Hash = (new uint256(votingData.Data)).ToString();
+        }
+
+        [JsonConstructor]
+        private VotingDataModel()
+        {
         }
     }
 }
