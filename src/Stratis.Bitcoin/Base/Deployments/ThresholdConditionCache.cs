ca6a00a50dede252dcc86c7b041d7840cea8046c
Kevin Loubser <zeptin@gmail.com>
2019-07-07 22:15:03 +0200

    Remove using

--- src/Stratis.Bitcoin/Base/Deployments/ThresholdConditionCache.cs
+++ src/Stratis.Bitcoin/Base/Deployments/ThresholdConditionCache.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
 using NBitcoin;
 using Stratis.Bitcoin.Base.Deployments.Models;
 using Stratis.Bitcoin.Utilities;
