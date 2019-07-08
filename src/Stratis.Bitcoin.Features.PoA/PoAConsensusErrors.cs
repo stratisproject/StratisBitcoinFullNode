7ade1a29db388b52ad93795b358d52472e22f5ad
Francois de la Rouviere <fassadlr@gmail.com>
2019-07-08 09:04:43 +0100

    Update PoAConsensusErrors.cs

--- src/Stratis.Bitcoin.Features.PoA/PoAConsensusErrors.cs
+++ src/Stratis.Bitcoin.Features.PoA/PoAConsensusErrors.cs
@@ -21,8 +21,8 @@ namespace Stratis.Bitcoin.Features.PoA
         // Collateral related errors.
         public static ConsensusError InvalidCollateralAmount => new ConsensusError("invalid-collateral-amount", "collateral requirement is not fulfilled");
 
-        public static ConsensusError InvalidCollateralAmountNoCommitment => new ConsensusError("invalid-collateral-amount", "collateral commitment not found");
+        public static ConsensusError InvalidCollateralAmountNoCommitment => new ConsensusError("collateral-commitment-not-found", "collateral commitment not found");
 
-        public static ConsensusError InvalidCollateralAmountCommitmentTooNew => new ConsensusError("invalid-collateral-amount", "collateral commitment too new");
+        public static ConsensusError InvalidCollateralAmountCommitmentTooNew => new ConsensusError("collateral-commitment-too-new", "collateral commitment too new");
     }
 }
