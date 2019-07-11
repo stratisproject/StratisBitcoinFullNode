ec9658a616be0cb4c285a34aa67d55ae5ec4a51c
Jordan Andrews <jordandrews@hotmail.com>
2019-07-11 13:37:57 +1000

    Made this test a little more useful

--- src/Stratis.Bitcoin.Features.PoA.IntegrationTests/HeaderTimeTests.cs
+++ src/Stratis.Bitcoin.Features.PoA.IntegrationTests/HeaderTimeTests.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Stratis.Bitcoin.Features.PoA.IntegrationTests.Common;
 using Stratis.Bitcoin.IntegrationTests.Common;
 using Stratis.Bitcoin.IntegrationTests.Common.EnvironmentMockUpHelpers;
+using Stratis.Bitcoin.Utilities;
 using Xunit;
 
 namespace Stratis.Bitcoin.Features.PoA.IntegrationTests
@@ -23,8 +24,11 @@ namespace Stratis.Bitcoin.Features.PoA.IntegrationTests
             // They can connect, they agree on genesis.
             TestHelper.Connect(node1, node2);
 
-            // When one mines a block, his time will be pushed forwards by TargetSpacing.
+            // When one mines a block, his time will be pushed forwards by multiple TargetSpacings.
             await node1.MineBlocksAsync(1);
+            long dateTime1 = node1.FullNode.NodeService<IDateTimeProvider>().GetAdjustedTimeAsUnixTimestamp();
+            long dateTime2 = node2.FullNode.NodeService<IDateTimeProvider>().GetAdjustedTimeAsUnixTimestamp();
+            Assert.True(dateTime1 > dateTime2 + network.ConsensusOptions.TargetSpacingSeconds);
 
             // Give the other node plenty of time such that he should have also synced if he thought the block was valid.
             Thread.Sleep(3000);
