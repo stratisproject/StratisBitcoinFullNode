1af7b14cb6c096c7b98580292fa720d1be470e9b
Francois de la Rouviere <fassadlr@gmail.com>
2019-07-18 14:22:15 +0100

    Fix tests

--- src/Stratis.Bitcoin.Features.SmartContracts.Tests/Consensus/Rules/CanGetSenderRuleTest.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts.Tests/Consensus/Rules/CanGetSenderRuleTest.cs
@@ -7,6 +7,7 @@ using Stratis.Bitcoin.AsyncWork;
 using Stratis.Bitcoin.Base;
 using Stratis.Bitcoin.Base.Deployments;
 using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Configuration.Logging;
 using Stratis.Bitcoin.Configuration.Settings;
 using Stratis.Bitcoin.Consensus;
 using Stratis.Bitcoin.Consensus.Rules;
@@ -33,6 +34,9 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests.Consensus.Rules
 
         public CanGetSenderRuleTest()
         {
+            var loggerFactory = new ExtendedLoggerFactory();
+            loggerFactory.AddConsoleWithFilters();
+
             this.network = new SmartContractsRegTest();
             this.senderRetriever = new Mock<ISenderRetriever>();
             this.rule = new CanGetSenderRule(this.senderRetriever.Object)
@@ -45,7 +49,7 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests.Consensus.Rules
                 new NodeDeployments(KnownNetworks.RegTest, new ChainIndexer(this.network)),
                 new ConsensusSettings(NodeSettings.Default(this.network)), new Mock<ICheckpoints>().Object, new Mock<ICoinView>().Object, new Mock<IChainState>().Object,
                 new InvalidBlockHashStore(null),
-                new NodeStats(null, null),
+                new NodeStats(null, loggerFactory),
                 new AsyncProvider(new Mock<ILoggerFactory>().Object, new Mock<ISignals>().Object, new NodeLifetime()))
             };
 
