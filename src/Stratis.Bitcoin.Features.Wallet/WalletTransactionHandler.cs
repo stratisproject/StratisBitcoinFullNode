0452a7acabcf786d6f60ad18de688e862738732e
Rowan de Haas <rowandh@users.noreply.github.com>
2019-07-26 16:19:46 +1000

    Enable change address to be set in build-transaction API

--- src/Stratis.Bitcoin.Features.Wallet/WalletTransactionHandler.cs
+++ src/Stratis.Bitcoin.Features.Wallet/WalletTransactionHandler.cs
@@ -233,6 +233,13 @@ namespace Stratis.Bitcoin.Features.Wallet
         /// <param name="context">The context associated with the current transaction being built.</param>
         protected void FindChangeAddress(TransactionBuildContext context)
         {
+            if (context.ChangeAddress != null)
+            {
+                // A change address has been specified.
+                context.TransactionBuilder.SetChange(context.ChangeAddress.ScriptPubKey);
+                return;
+            }
+
             // Get an address to send the change to.
             context.ChangeAddress = this.walletManager.GetUnusedChangeAddress(new WalletAccountReference(context.AccountReference.WalletName, context.AccountReference.AccountName));
             context.TransactionBuilder.SetChange(context.ChangeAddress.ScriptPubKey);
