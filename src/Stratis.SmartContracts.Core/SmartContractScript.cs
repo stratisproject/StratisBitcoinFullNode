e7d6dd18d328ed9b865f8ebce6b2df9918ba6b56
Jordan Andrews <jordandrews@hotmail.com>
2019-08-05 16:09:34 +1000

    Revert "[SC] Micro optimisations for SC script tests"
    This reverts commit 30a33446d1dd132dff295b6dbe4eebe189d7b3f7.

--- src/Stratis.SmartContracts.Core/SmartContractScript.cs
+++ src/Stratis.SmartContracts.Core/SmartContractScript.cs
@@ -1,62 +1,43 @@
 ﻿using NBitcoin;
-using TracerAttributes;
 
 namespace Stratis.SmartContracts.Core
 {
     public static class SmartContractScript
     {
-        [NoTrace]
         public static bool IsSmartContractExec(this Script script)
         {
-            return TestFirstByte(script, new byte[]{ (byte)ScOpcodeType.OP_CALLCONTRACT, (byte)ScOpcodeType.OP_CREATECONTRACT });
+            return script.IsSmartContractCall() || script.IsSmartContractCreate();
         }
 
-        [NoTrace]
         public static bool IsSmartContractCall(this Script script)
         {
-            return TestFirstByte(script,  new byte[]{(byte)ScOpcodeType.OP_CALLCONTRACT });
+            return TestFirstByte(script, (byte)ScOpcodeType.OP_CALLCONTRACT);
         }
 
-        [NoTrace]
         public static bool IsSmartContractCreate(this Script script)
         {
-            return TestFirstByte(script, new byte[] {(byte)ScOpcodeType.OP_CREATECONTRACT });
+            return TestFirstByte(script, (byte)ScOpcodeType.OP_CREATECONTRACT);
         }
 
-        [NoTrace]
         public static bool IsSmartContractSpend(this Script script)
         {
-            return TestFirstByte(script, new byte[] {(byte)ScOpcodeType.OP_SPEND });
+            return TestFirstByte(script, (byte)ScOpcodeType.OP_SPEND);
+
         }
 
-        [NoTrace]
         public static bool IsSmartContractInternalCall(this Script script)
         {
-            return TestFirstByte(script, new byte[]{(byte) ScOpcodeType.OP_INTERNALCONTRACTTRANSFER });
+            return TestFirstByte(script, (byte) ScOpcodeType.OP_INTERNALCONTRACTTRANSFER);
         }
 
-        /// <summary>
-        /// Tests whether a script starts with any of the given opcodes.
-        /// </summary>
-        /// <param name="script">The script to test.</param>
-        /// <param name="opcodes">A list of opcodes in byte form to test against.</param>
-        /// <returns>True if the script starts with one of the given opcodes.</returns>
-        [NoTrace]
-        private static bool TestFirstByte(Script script, byte[] opcodes)
+        private static bool TestFirstByte(Script script, byte opcode)
         {
-            // The bytes aren't touched and don't leave this method so use unsafe for performance (no array copy).
-            var scriptBytes = script.ToBytes(true);
+            var scriptBytes = script.ToBytes();
 
             if (scriptBytes.Length == 0)
                 return false;
 
-            foreach (byte opcode in opcodes)
-            {
-                if (scriptBytes[0] == opcode)
-                    return true;
-            }
-
-            return false;
+            return scriptBytes[0] == opcode;
         }
     }
 
