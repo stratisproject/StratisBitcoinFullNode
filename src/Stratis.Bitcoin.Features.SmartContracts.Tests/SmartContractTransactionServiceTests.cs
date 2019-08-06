f0261b628a278728bf6f36fa61d3036dcf406732
Rowan de Haas <rowandh@users.noreply.github.com>
2019-08-06 15:26:02 +1000

    [SC] Build transfer transactions (#3870)

--- src/Stratis.Bitcoin.Features.SmartContracts.Tests/SmartContractTransactionServiceTests.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts.Tests/SmartContractTransactionServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using CSharpFunctionalExtensions;
 using Moq;
 using NBitcoin;
@@ -6,8 +7,10 @@ using Stratis.Bitcoin.Features.SmartContracts.Models;
 using Stratis.Bitcoin.Features.SmartContracts.Wallet;
 using Stratis.Bitcoin.Features.Wallet;
 using Stratis.Bitcoin.Features.Wallet.Interfaces;
+using Stratis.Bitcoin.Features.Wallet.Models;
 using Stratis.SmartContracts.CLR;
 using Stratis.SmartContracts.CLR.Serialization;
+using Stratis.SmartContracts.Core.State;
 using Stratis.SmartContracts.Networks;
 using Stratis.SmartContracts.RuntimeObserver;
 using Xunit;
@@ -22,6 +25,7 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
         private readonly Mock<IMethodParameterStringSerializer> stringSerializer;
         private readonly Mock<ICallDataSerializer> callDataSerializer;
         private readonly Mock<IAddressGenerator> addressGenerator;
+        private readonly Mock<IStateRepositoryRoot> stateRepository;
 
         public SmartContractTransactionServiceTests()
         {
@@ -31,9 +35,9 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
             this.stringSerializer = new Mock<IMethodParameterStringSerializer>();
             this.callDataSerializer = new Mock<ICallDataSerializer>();
             this.addressGenerator = new Mock<IAddressGenerator>();
+            this.stateRepository = new Mock<IStateRepositoryRoot>();
         }
-
-
+        
         [Fact]
         public void CanChooseInputsForCall()
         {
@@ -55,9 +59,9 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
                 WalletName = "wallet",
                 Password = "password",
                 Sender = senderAddress,
-                Outpoints = new List<OutpointRequestModel>
+                Outpoints = new List<OutpointRequest>
                 {
-                    new OutpointRequestModel
+                    new OutpointRequest
                     {
                         Index = utxoIndex,
                         TransactionId = utxoId.ToString()
@@ -131,7 +135,8 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
                 this.walletTransactionHandler.Object,
                 this.stringSerializer.Object,
                 this.callDataSerializer.Object,
-                this.addressGenerator.Object);
+                this.addressGenerator.Object,
+                this.stateRepository.Object);
 
             BuildCallContractTransactionResponse result = service.BuildCallTx(request);
 
@@ -159,9 +164,9 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
                 WalletName = "wallet",
                 Password = "password",
                 Sender = senderAddress,
-                Outpoints = new List<OutpointRequestModel>
+                Outpoints = new List<OutpointRequest>
                 {
-                    new OutpointRequestModel
+                    new OutpointRequest
                     {
                         Index = utxoIndex,
                         TransactionId = new uint256(64).ToString() // A tx we don't have.
@@ -210,7 +215,8 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
                 this.walletTransactionHandler.Object,
                 this.stringSerializer.Object,
                 this.callDataSerializer.Object,
-                this.addressGenerator.Object);
+                this.addressGenerator.Object,
+                this.stateRepository.Object);
 
             BuildCallContractTransactionResponse result = service.BuildCallTx(request);
             Assert.False(result.Success);
@@ -237,9 +243,9 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
                 WalletName = "wallet",
                 Password = "password",
                 Sender = senderAddress,
-                Outpoints = new List<OutpointRequestModel>
+                Outpoints = new List<OutpointRequest>
                 {
-                    new OutpointRequestModel
+                    new OutpointRequest
                     {
                         Index = utxoIndex,
                         TransactionId = utxoId.ToString()
@@ -316,12 +322,374 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
                 this.walletTransactionHandler.Object,
                 this.stringSerializer.Object,
                 this.callDataSerializer.Object,
-                this.addressGenerator.Object);
+                this.addressGenerator.Object,
+                this.stateRepository.Object);
 
             BuildCreateContractTransactionResponse result = service.BuildCreateTx(request);
 
             this.walletTransactionHandler.Verify(x => x.BuildTransaction(It.Is<TransactionBuildContext>(y => y.SelectedInputs.Count == 1)));
         }
 
+        [Fact]
+        public void BuildTransferContext_SenderNotInWallet_Fails()
+        {
+            string senderAddress = uint160.Zero.ToBase58Address(this.network);
+
+            SmartContractTransactionService service = new SmartContractTransactionService(
+                this.network,
+                this.walletManager.Object,
+                this.walletTransactionHandler.Object,
+                this.stringSerializer.Object,
+                this.callDataSerializer.Object,
+                this.addressGenerator.Object,
+                this.stateRepository.Object);
+
+            var request = new BuildContractTransactionRequest
+            {
+                AccountName = "account 0",
+                WalletName = "wallet",
+                Password = "password",
+                Sender = senderAddress,
+            };
+
+            // Create a wallet but without the sender address
+            this.walletManager.Setup(x => x.GetWallet(request.WalletName))
+                .Returns(new Features.Wallet.Wallet
+                {
+                    AccountsRoot = new List<AccountRoot>
+                    {
+                        new AccountRoot
+                        {
+                            Accounts = new List<HdAccount>
+                            {
+                                new HdAccount
+                                {
+                                    ExternalAddresses = new List<HdAddress>(),
+                                    Name = request.AccountName
+                                }
+                            }
+                        }
+                    }
+                });
+
+            BuildContractTransactionResult result = service.BuildTx(request);
+
+            Assert.Equal(SmartContractTransactionService.SenderNotInWalletError, result.Error);
+            Assert.NotNull(result.Message);
+            Assert.Null(result.Response);
+        }
+
+        [Fact]
+        public void BuildTransferContext_AccountNotInWallet_Fails()
+        {
+            string senderAddress = uint160.Zero.ToBase58Address(this.network);
+
+            SmartContractTransactionService service = new SmartContractTransactionService(
+                this.network,
+                this.walletManager.Object,
+                this.walletTransactionHandler.Object,
+                this.stringSerializer.Object,
+                this.callDataSerializer.Object,
+                this.addressGenerator.Object,
+                this.stateRepository.Object);
+
+            var request = new BuildContractTransactionRequest
+            {
+                AccountName = "account 0",
+                WalletName = "wallet",
+                Password = "password",
+                Sender = senderAddress,
+            };
+
+            // Create a wallet but without the correct account name
+            this.walletManager.Setup(x => x.GetWallet(request.WalletName))
+                .Returns(new Features.Wallet.Wallet
+                {
+                    AccountsRoot = new List<AccountRoot>
+                    {
+                        new AccountRoot
+                        {
+                            Accounts = new List<HdAccount>
+                            {
+                                new HdAccount
+                                {
+                                    ExternalAddresses = new List<HdAddress>(),
+                                    Name = "account 1"
+                                }
+                            }
+                        }
+                    }
+                });
+
+            BuildContractTransactionResult result = service.BuildTx(request);
+
+            Assert.Equal(SmartContractTransactionService.AccountNotInWalletError, result.Error);
+            Assert.NotNull(result.Message);
+            Assert.Null(result.Response);
+        }
+
+        [Fact]
+        public void BuildTransferContext_SenderHasNoBalance_Fails()
+        {
+            string senderAddress = uint160.Zero.ToBase58Address(this.network);
+
+            SmartContractTransactionService service = new SmartContractTransactionService(
+                this.network,
+                this.walletManager.Object,
+                this.walletTransactionHandler.Object,
+                this.stringSerializer.Object,
+                this.callDataSerializer.Object,
+                this.addressGenerator.Object,
+                this.stateRepository.Object);
+
+            var request = new BuildContractTransactionRequest
+            {
+                AccountName = "account 0",
+                WalletName = "wallet",
+                Password = "password",
+                Sender = senderAddress,
+            };
+
+            this.walletManager.Setup(x => x.GetWallet(request.WalletName))
+                .Returns(new Features.Wallet.Wallet
+                {
+                    AccountsRoot = new List<AccountRoot>
+                    {
+                        new AccountRoot
+                        {
+                            Accounts = new List<HdAccount>
+                            {
+                                new HdAccount
+                                {
+                                    ExternalAddresses = new List<HdAddress>
+                                    {
+                                        new HdAddress
+                                        {
+                                            Address = senderAddress
+                                        }
+                                    },
+                                    Name = request.AccountName,                                    
+                                }
+                            }
+                        }
+                    }
+                });
+
+            this.walletManager.Setup(x => x.GetAddressBalance(request.Sender))
+                .Returns(new AddressBalance { Address = request.Sender, AmountConfirmed = 0, AmountUnconfirmed = 0 });
+
+            BuildContractTransactionResult result = service.BuildTx(request);
+
+            Assert.Equal(SmartContractTransactionService.InsufficientBalanceError, result.Error);
+            Assert.NotNull(result.Message);
+            Assert.Null(result.Response);
+        }
+
+        [Fact]
+        public void BuildTransferContext_RecipientIsKnownContract_Fails()
+        {
+            const int utxoIndex = 0;
+            uint256 utxoId = uint256.Zero;
+            uint256 utxoIdUnused = uint256.One;
+            string senderAddress = uint160.Zero.ToBase58Address(this.network);
+            string recipientAddress = uint160.One.ToBase58Address(this.network);
+
+            SmartContractTransactionService service = new SmartContractTransactionService(
+                this.network,
+                this.walletManager.Object,
+                this.walletTransactionHandler.Object,
+                this.stringSerializer.Object,
+                this.callDataSerializer.Object,
+                this.addressGenerator.Object,
+                this.stateRepository.Object);
+
+            var request = new BuildContractTransactionRequest
+            {
+                AccountName = "account 0",
+                WalletName = "wallet",
+                Password = "password",
+                Sender = senderAddress,
+                Recipients = new List<RecipientModel>
+                {
+                    new RecipientModel { Amount = "1", DestinationAddress = recipientAddress}
+                }
+            };
+
+            this.walletManager.Setup(x => x.GetWallet(request.WalletName))
+                .Returns(new Features.Wallet.Wallet
+                {
+                    AccountsRoot = new List<AccountRoot>
+                    {
+                        new AccountRoot
+                        {
+                            Accounts = new List<HdAccount>
+                            {
+                                new HdAccount
+                                {
+                                    ExternalAddresses = new List<HdAddress>
+                                    {
+                                        new HdAddress
+                                        {
+                                            Address = senderAddress
+                                        }
+                                    },
+                                    Name = request.AccountName,
+                                }
+                            }
+                        }
+                    }
+                });
+
+            this.walletManager.Setup(x => x.GetAddressBalance(request.Sender))
+                .Returns(new AddressBalance { Address = request.Sender, AmountConfirmed = 10, AmountUnconfirmed = 0 });
+
+            this.walletManager.Setup(x => x.GetSpendableTransactionsInWallet(It.IsAny<string>(), 0))
+                .Returns(new List<UnspentOutputReference>
+                {
+                    new UnspentOutputReference
+                    {
+                        Address = new HdAddress
+                        {
+                            Address = senderAddress
+                        },
+                        Transaction = new TransactionData
+                        {
+                            Id = utxoId,
+                            Index = utxoIndex,
+                        }
+                    }, new UnspentOutputReference
+                    {
+                        Address = new HdAddress
+                        {
+                            Address = senderAddress
+                        },
+                        Transaction = new TransactionData
+                        {
+                            Id = utxoIdUnused,
+                            Index = utxoIndex,
+                        }
+                    }
+                });
+
+            this.stateRepository.Setup(s => s.IsExist(It.IsAny<uint160>())).Returns(true);
+
+            BuildContractTransactionResult result = service.BuildTx(request);
+
+            Assert.Equal(SmartContractTransactionService.TransferFundsToContractError, result.Error);
+            Assert.NotNull(result.Message);
+            Assert.Null(result.Response);
+        }
+
+        [Fact]
+        public void BuildTransferContextCorrectly()
+        {
+            const int utxoIndex = 0;
+            uint256 utxoId = uint256.Zero;
+            uint256 utxoIdUnused = uint256.One;
+            string senderAddress = uint160.Zero.ToBase58Address(this.network);
+            string recipientAddress = uint160.One.ToBase58Address(this.network);
+            string changeAddress = new uint160(2).ToBase58Address(this.network);
+
+            var amount = 1234.567M;
+
+            var request = new BuildContractTransactionRequest
+            {
+                AccountName = "account 0",
+                FeeAmount = "0.01",
+                WalletName = "wallet",
+                Password = "password",
+                Sender = senderAddress,
+                ShuffleOutputs = true,
+                AllowUnconfirmed = true,
+                ChangeAddress = changeAddress,
+                Recipients = new List<RecipientModel>
+                {
+                    new RecipientModel { Amount = amount.ToString(), DestinationAddress = recipientAddress}
+                }
+            };
+
+            SmartContractTransactionService service = new SmartContractTransactionService(
+                this.network,
+                this.walletManager.Object,
+                this.walletTransactionHandler.Object,
+                this.stringSerializer.Object,
+                this.callDataSerializer.Object,
+                this.addressGenerator.Object,
+                this.stateRepository.Object);
+
+            var senderHdAddress = new HdAddress { Address = senderAddress };
+
+            this.walletManager.Setup(x => x.GetWallet(request.WalletName))
+                .Returns(new Features.Wallet.Wallet
+                {
+                    AccountsRoot = new List<AccountRoot>
+                    {
+                        new AccountRoot
+                        {
+                            Accounts = new List<HdAccount>
+                            {
+                                new HdAccount
+                                {
+                                    ExternalAddresses = new List<HdAddress>
+                                    {
+                                        senderHdAddress 
+                                    },
+                                    Name = request.AccountName,
+                                }
+                            }
+                        }
+                    }
+                });
+
+            this.walletManager.Setup(x => x.GetAddressBalance(request.Sender))
+                .Returns(new AddressBalance { Address = request.Sender, AmountConfirmed = Money.FromUnit(amount, MoneyUnit.BTC), AmountUnconfirmed = 0 });
+
+            var outputs = new List<UnspentOutputReference>
+                {
+                    new UnspentOutputReference
+                    {
+                        Address = new HdAddress
+                        {
+                            Address = senderAddress
+                        },
+                        Transaction = new TransactionData
+                        {
+                            Id = utxoId,
+                            Index = utxoIndex,
+                        }
+                    }, new UnspentOutputReference
+                    {
+                        Address = new HdAddress
+                        {
+                            Address = senderAddress
+                        },
+                        Transaction = new TransactionData
+                        {
+                            Id = utxoIdUnused,
+                            Index = utxoIndex,
+                        }
+                    }
+                };
+
+            this.walletManager.Setup(x => x.GetSpendableTransactionsInWallet(It.IsAny<string>(), 0)).Returns(outputs);
+
+            this.walletTransactionHandler.Setup(x => x.BuildTransaction(It.IsAny<TransactionBuildContext>()))
+                .Returns(new Transaction());
+
+            BuildContractTransactionResult result = service.BuildTx(request);
+
+            // Check that the transaction builder is invoked, and that we:
+            // - Ignore shuffleOutputs,
+            // - Set inputs from sender
+            // - Set recipients,
+            // - Set change to sender
+            this.walletTransactionHandler.Verify(w => w.BuildTransaction(It.Is<TransactionBuildContext>(context =>
+                context.AllowOtherInputs == false &&
+                context.Shuffle == false &&
+                context.SelectedInputs.All(i => outputs.Select(o => o.Transaction.Id).Contains(i.Hash)) &&
+                context.Recipients.Single().Amount == Money.FromUnit(amount, MoneyUnit.BTC) &&
+                context.ChangeAddress == senderHdAddress
+            )));
+        }
     }
 }
