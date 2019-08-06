9e43e2e41ca2baaff12a3cc7cabae48979712415
Rowan de Haas <rowandh@users.noreply.github.com>
2019-08-06 15:26:02 +1000

    [SC] Build transfer transactions (#3870)

---
+++ src/Stratis.Bitcoin.Features.SmartContracts/Models/BuildContractTransactionResult.cs
@@ -0,0 +1,34 @@
+﻿using Stratis.Bitcoin.Features.Wallet.Models;
+
+namespace Stratis.Bitcoin.Features.SmartContracts.Models
+{
+    public class BuildContractTransactionResult
+    {
+        private BuildContractTransactionResult(WalletBuildTransactionModel model)
+        {
+            this.Response = model;
+        }
+
+        private BuildContractTransactionResult(string error, string message)
+        {
+            this.Error = error;
+            this.Message = message;
+        }
+
+        public WalletBuildTransactionModel Response { get; }
+
+        public string Error { get; }
+
+        public string Message { get; }
+
+        public static BuildContractTransactionResult Success(WalletBuildTransactionModel model)
+        {
+            return new BuildContractTransactionResult(model);
+        }
+
+        public static BuildContractTransactionResult Failure(string error, string message)
+        {
+            return new BuildContractTransactionResult(error, message);
+        }
+    }
+}
