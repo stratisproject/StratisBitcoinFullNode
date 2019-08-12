829fed973a4c0e2196a710a8abe98cec92743775
Francois de la Rouviere <fassadlr@gmail.com>
2019-08-12 16:32:53 +0100

    Update AddressIndexerOutpointsRepository.cs

--- src/Stratis.Bitcoin.Features.BlockStore/AddressIndexing/AddressIndexerOutpointsRepository.cs
+++ src/Stratis.Bitcoin.Features.BlockStore/AddressIndexing/AddressIndexerOutpointsRepository.cs
@@ -147,6 +147,8 @@ namespace Stratis.Bitcoin.Features.BlockStore.AddressIndexing
                     // Put the spent outputs back into the cache.
                     foreach (OutPointData outPointData in rewindData.SpentOutputs)
                         this.AddOutPointData(outPointData);
+
+                    this.addressIndexerOutPointData.Delete(rewindData.BlockHash);
                 }
             }
         }
