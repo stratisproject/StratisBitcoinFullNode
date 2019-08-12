e500694b8ee7fe2c06ecf48a4aec9b8cd521c87e
Jordan Andrews <jordandrews@hotmail.com>
2019-08-12 13:49:12 +1000

    Update src/Stratis.Bitcoin.Features.SmartContracts/ReflectionExecutor/Controllers/SmartContractsController.cs

--- src/Stratis.Bitcoin.Features.SmartContracts/ReflectionExecutor/Controllers/SmartContractsController.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/ReflectionExecutor/Controllers/SmartContractsController.cs
@@ -31,7 +31,7 @@ using Stratis.SmartContracts.Core.State;
 
 namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Controllers
 {
-    [ApiVersion("1.0")]
+    [ApiVersion("1")]
     [Route("api/[controller]")]
     public class SmartContractsController : Controller
     {
