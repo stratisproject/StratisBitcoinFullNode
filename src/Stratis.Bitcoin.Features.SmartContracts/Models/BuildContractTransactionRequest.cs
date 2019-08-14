b1ba1e470dcaccfd4efe92fc82336a8242303a43
Rowan de Haas <rowandh@users.noreply.github.com>
2019-08-14 16:32:07 +1000

    [SC] Add tx fee estimation endpoint (#3883)

--- src/Stratis.Bitcoin.Features.SmartContracts/Models/BuildContractTransactionRequest.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/Models/BuildContractTransactionRequest.cs
@@ -4,6 +4,13 @@ using Stratis.Bitcoin.Features.Wallet.Validations;
 
 namespace Stratis.Bitcoin.Features.SmartContracts.Models
 {
+    public class ScTxFeeEstimateRequest : TxFeeEstimateRequest
+    {
+        [Required(ErrorMessage = "Sender is required.")]
+        [IsBitcoinAddress]
+        public string Sender { get; set; }
+    }
+
     public class BuildContractTransactionRequest : BuildTransactionRequest
     {
         [Required(ErrorMessage = "Sender is required.")]
--- src/Stratis.Bitcoin.Features.SmartContracts/Wallet/ISmartContractTransactionService.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/Wallet/ISmartContractTransactionService.cs
@@ -5,6 +5,7 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Wallet
 {
     public interface ISmartContractTransactionService
     {
+        EstimateFeeResult EstimateFee(ScTxFeeEstimateRequest request);
         BuildContractTransactionResult BuildTx(BuildContractTransactionRequest request);
         BuildCallContractTransactionResponse BuildCallTx(BuildCallContractTransactionRequest request);
         BuildCreateContractTransactionResponse BuildCreateTx(BuildCreateContractTransactionRequest request);
