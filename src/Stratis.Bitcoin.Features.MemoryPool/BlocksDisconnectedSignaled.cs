2b55819e5b9c33213781821e920d0e6062a73e80
Jordan Andrews <jordandrews@hotmail.com>
2019-08-20 11:49:20 +1000

    The fix

--- src/Stratis.Bitcoin.Features.MemoryPool/BlocksDisconnectedSignaled.cs
+++ src/Stratis.Bitcoin.Features.MemoryPool/BlocksDisconnectedSignaled.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using NBitcoin;
 using Stratis.Bitcoin.EventBus;
 using Stratis.Bitcoin.EventBus.CoreEvents;
+using Stratis.Bitcoin.Features.MemoryPool.Interfaces;
 using Stratis.Bitcoin.Signals;
 
 namespace Stratis.Bitcoin.Features.MemoryPool
@@ -13,6 +15,7 @@ namespace Stratis.Bitcoin.Features.MemoryPool
     /// </summary>
     public class BlocksDisconnectedSignaled : IDisposable
     {
+        private readonly ITxMempool mempool;
         private readonly IMempoolValidator mempoolValidator;
         private readonly MempoolSchedulerLock mempoolLock;
         private readonly ISignals signals;
@@ -20,8 +23,14 @@ namespace Stratis.Bitcoin.Features.MemoryPool
 
         private SubscriptionToken blockDisconnectedSubscription;
 
-        public BlocksDisconnectedSignaled(IMempoolValidator mempoolValidator, MempoolSchedulerLock mempoolLock, ILoggerFactory loggerFactory, ISignals signals)
+        public BlocksDisconnectedSignaled(
+            ITxMempool mempool,
+            IMempoolValidator mempoolValidator,
+            MempoolSchedulerLock mempoolLock,
+            ILoggerFactory loggerFactory,
+            ISignals signals)
         {
+            this.mempool = mempool;
             this.mempoolValidator = mempoolValidator;
             this.mempoolLock = mempoolLock;
             this.signals = signals;
@@ -35,6 +44,7 @@ namespace Stratis.Bitcoin.Features.MemoryPool
 
         private void OnBlockDisconnected(BlockDisconnected blockDisconnected)
         {
+            this.RemoveInvalidTransactionsAsync(blockDisconnected.DisconnectedBlock.Block).ConfigureAwait(false).GetAwaiter().GetResult();
             this.AddBackToMempoolAsync(blockDisconnected.DisconnectedBlock.Block).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
@@ -62,6 +72,28 @@ namespace Stratis.Bitcoin.Features.MemoryPool
                         this.logger.LogDebug("Transaction with hash '{0}' accepted back to mempool.", transaction.GetHash());
                 }
 
+            }).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// If there are any transactions in the mempool that depend on transactions no longer in the chain, remove them.
+        /// </summary>
+        private async Task RemoveInvalidTransactionsAsync(Block block)
+        {
+            // Invalid transactions would have spent the coinbase. The other transactions can be put back into the mempool and be fine.
+            uint256 coinbaseId = block.Transactions[0].GetHash();
+
+            await this.mempoolLock.WriteAsync(async () =>
+            {
+                foreach (TxMempoolEntry mempoolEntry in this.mempool.MapTx.SpendsCoinbase.ToList())
+                {
+                    if (mempoolEntry.Transaction.Inputs.Any(x => x.PrevOut.Hash == coinbaseId))
+                    {
+                        this.logger.LogDebug("Removing transaction '{0}' from the mempool as it spends the coinbase transaction '{1}'", mempoolEntry.TransactionHash, coinbaseId);
+                        this.mempool.RemoveRecursive(mempoolEntry.Transaction);
+                    }
+
+                }
             }).ConfigureAwait(false);
         }
 
