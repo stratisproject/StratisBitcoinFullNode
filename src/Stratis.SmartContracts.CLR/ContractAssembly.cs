466b71da9def309cff7fb0e0e27dd53111a51b62
Rowan de Haas <rowandh@users.noreply.github.com>
2019-10-02 16:07:18 +1000

    Begin adding path factory and tests

---
+++ src/Stratis.SmartContracts.CLR/ContractAssembly.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Stratis.SmartContracts.CLR
+{
+    public class ContractAssembly
+    {
+        private readonly Assembly assembly;
+
+        public ContractAssembly(byte[] code)
+            : this(Assembly.Load(code))
+        {
+        }
+
+        public ContractAssembly(Assembly assembly)
+        {
+            this.assembly = assembly;
+        }
+
+        public IEnumerable<MethodInfo> GetPublicMethods()
+        {
+            Type deployedType = this.assembly.ExportedTypes.FirstOrDefault(t => t.GetCustomAttribute<DeployAttribute>() != null) ?? this.assembly.ExportedTypes.FirstOrDefault();
+
+            if (deployedType == null)
+            {
+                return Enumerable.Empty<MethodInfo>();
+            }
+
+            return deployedType.GetMethods();
+        }
+    }
+}
