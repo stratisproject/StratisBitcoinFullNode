dfbb8e349bcdea9a2bcb0f38292366008dca48e8
Rowan de Haas <rowandh@users.noreply.github.com>
2019-10-05 16:01:38 +1000

    Test schema generation classes

---
+++ src/Stratis.Bitcoin.Features.SmartContracts/ReflectionExecutor/ContractSwaggerDocMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor
+{
+    public class ContractSwaggerDocMapper
+    {
+        public SwaggerDocument Map(Assembly assembly)
+        {
+            return null;
+        }
+    }
+}
