b6eff093fe27af2e013e04813ad67a010049b352
Rowan de Haas <rowandh@users.noreply.github.com>
2019-10-07 18:59:58 +1100

    Add mapper class

---
+++ src/Stratis.SmartContracts.CLR/Serialization/JObjectParameterInfoMapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+
+namespace Stratis.SmartContracts.CLR.Serialization
+{
+    /// <summary>
+    /// Maps a JObject of values to the parameters on a method.
+    /// </summary>
+    public class JObjectParameterInfoMapper
+    {
+        public string[] Map(JObject obj, ParameterInfo[] parameters)
+        {
+            var result = new List<string>();
+
+            foreach (var parameter in parameters)
+            {
+                var jObParam = obj[parameter.Name];
+
+                if (jObParam == null)
+                    throw new Exception("Couldn't map all params");
+
+                var prefix = Prefix.ForType(parameter.ParameterType);
+
+                result.Add($"{prefix.Value}#{jObParam}");
+            }
+
+            return result.ToArray();
+        }
+    }
+}
