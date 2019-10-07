ae1051a5528af20d545321077e6f322c25ce7d3d
Rowan de Haas <rowandh@users.noreply.github.com>
2019-10-07 15:07:14 +1100

    Add contract metadata to calls

--- src/Stratis.Bitcoin.Features.SmartContracts/ReflectionExecutor/Controllers/DynamicContractController.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/ReflectionExecutor/Controllers/DynamicContractController.cs
@@ -1,6 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Controllers
 {
@@ -10,6 +13,8 @@ namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Controllers
         [HttpPost]
         public IActionResult CallMethod([FromRoute] string address, [FromRoute] string method)
         {
+            var headers = Request.Headers;
+
             string requestBody;
             using (StreamReader reader = new StreamReader(this.Request.Body, Encoding.UTF8))
             {
@@ -17,6 +22,16 @@ namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Controllers
             }
 
             // TODO map request body to JSON object, extract transaction-related params, build new request model, then call the regular SC controller.
+            JObject requestData;
+
+            try
+            {
+                requestData = JObject.Parse(requestBody);
+            }
+            catch (JsonReaderException e)
+            {
+                return BadRequest(e.Message);
+            }
 
             // Map parameters to our contract object and try to invoke it.
             // This will need to proxy to the actual SC controller
