96b60bde6877da084d568a32df3c0af8e2a97da8
Rowan de Haas <rowandh@users.noreply.github.com>
2019-10-07 15:48:44 +1100

    Populate default wallet and address

--- src/Stratis.Bitcoin.Features.SmartContracts/ReflectionExecutor/Controllers/ContractSwaggerController.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts/ReflectionExecutor/Controllers/ContractSwaggerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
@@ -7,6 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using NBitcoin;
 using Newtonsoft.Json;
+using Stratis.Bitcoin.Features.Wallet;
+using Stratis.Bitcoin.Features.Wallet.Interfaces;
 using Stratis.SmartContracts.CLR;
 using Stratis.SmartContracts.CLR.Loader;
 using Stratis.SmartContracts.Core.State;
@@ -20,6 +23,7 @@ namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Controllers
     public class ContractSwaggerController : Controller
     {
         private readonly ILoader loader;
+        private readonly IWalletManager walletmanager;
         private readonly IStateRepositoryRoot stateRepository;
         private readonly Network network;
         private readonly SwaggerGeneratorOptions options;
@@ -27,12 +31,14 @@ namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Controllers
 
         public ContractSwaggerController(
             ILoader loader,
+            IWalletManager walletmanager,
             IOptions<MvcJsonOptions> mvcJsonOptions,
             IOptions<SwaggerGeneratorOptions> options,
             IStateRepositoryRoot stateRepository,
             Network network)
         {
             this.loader = loader;
+            this.walletmanager = walletmanager;
             this.stateRepository = stateRepository;
             this.network = network;
             this.options = options.Value;
@@ -56,7 +62,14 @@ namespace Stratis.Bitcoin.Features.SmartContracts.ReflectionExecutor.Controllers
 
             IContractAssembly assembly = assemblyLoadResult.Value;
 
-            var swaggerGen = new ContractSwaggerDocGenerator(this.options, address, assembly);
+            // Default wallet is the first wallet as ordered by name.
+            string defaultWalletName = this.walletmanager.GetWalletsNames().OrderBy(n => n).First();
+
+            // Default address is the first address with a balance, or string.Empty if no addresses have been created.
+            // Ordering this way is consistent with the wallet UI, ie. whatever appears first in the wallet will appear first here.
+            string defaultAddress = this.walletmanager.GetAccountAddressesWithBalance(defaultWalletName).FirstOrDefault()?.Address ?? string.Empty;
+            
+            var swaggerGen = new ContractSwaggerDocGenerator(this.options, address, assembly, defaultWalletName, defaultAddress);
 
             SwaggerDocument doc = swaggerGen.GetSwagger("contracts");
 
