b95151b2d27c403ff90d4d9ffed4d6c3b736aca8
Kevin Loubser <zeptin@gmail.com>
2019-10-26 19:48:38 +0200

    Fix merge error

--- src/Stratis.SmartContracts.IntegrationTests/PoW/SmartContractMinerTests.cs
+++ src/Stratis.SmartContracts.IntegrationTests/PoW/SmartContractMinerTests.cs
@@ -72,7 +72,6 @@ namespace Stratis.SmartContracts.IntegrationTests.PoW
                 testContext.network,
                 new SenderRetriever(),
                 testContext.StateRoot,
-                new NodeDeployments(testContext.network, testContext.ChainIndexer),
                 testContext.executionCache, 
                 testContext.callDataSerializer,
                 new NodeDeployments(testContext.network, testContext.ChainIndexer));
