9966ad641dbf86ead479cac64e0973a4ba8e8a22
quantumagi <someguy.fromafrica@gmail.com>
2019-11-16 00:42:48 +1100

    Clean up DBLock and add comments (#4078)

--- src/Stratis.Features.SQLiteWalletRepository/DBLock.cs
+++ src/Stratis.Features.SQLiteWalletRepository/DBLock.cs
@@ -5,12 +5,12 @@ using Stratis.Bitcoin.Utilities;
 namespace Stratis.Features.SQLiteWalletRepository
 {
     /// <summary>
-    /// Used to lock access to database objects accross threads.
+    /// Used to lock access to database objects across threads.
     /// </summary>
     /// <remarks>
     /// Lock behavior:
-    /// 1) If the thread that acquired the lock calls Wait again these subsequent calls should be ignored.
-    /// 2) If the thread that released the lock calls Release again these subsequent calls should be ignored.
+    /// 1) If the thread that acquired the lock calls Wait again these subsequent calls should be ignored apart from incrementing the lock depth.
+    /// 2) When calling Release decrease the lock depth and only act when lock depth is 1.
     /// 3) Keeps track of the number of threads waiting to acquire the lock.
     /// </remarks>
     internal class DBLock
@@ -24,8 +24,10 @@ namespace Stratis.Features.SQLiteWalletRepository
         private int lockDepth;
         private object lockObj;
 
+        /// <summary>The number of threads waiting to acquire the lock.</summary>
         public int WaitingThreads => this.waitingThreads;
 
+        /// <summary>Indicates whether the lock is available.</summary>
         public bool IsAvailable => this.lastClaimedByThread == -1;
 
         public DBLock()
@@ -36,6 +38,12 @@ namespace Stratis.Features.SQLiteWalletRepository
             this.lockDepth = 0;
         }
 
+        /// <summary>
+        /// Releases the lock.
+        /// </summary>
+        /// <remarks>
+        /// When calling Release decrease the lock depth and only act when lock depth is 1.
+        /// </remarks>
         public void Release()
         {
             int threadId = Thread.CurrentThread.ManagedThreadId;
@@ -58,7 +66,16 @@ namespace Stratis.Features.SQLiteWalletRepository
             }
         }
 
-        public bool Wait(bool wait = true, int timeoutSeconds = 120, [System.Runtime.CompilerServices.CallerMemberName] string memberName = "", [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0)
+        /// <summary>
+        /// Acquires the lock.
+        /// </summary>
+        /// <param name="wait">Set to <c>true</c> to wait until the lock becomes available. Otherwise returns immediately.</param>
+        /// <param name="timeoutSeconds">The maximum time that the lock will be held.</param>
+        /// <returns>Return <c>true</c> if the lock could be acquired and <c>false</c> otherwise.</returns>
+        /// <remarks>
+        /// If the thread that acquired the lock calls Wait again these subsequent calls should be ignored apart from incrementing the lock depth.
+        /// </remarks>
+        public bool Wait(bool wait = true, int timeoutSeconds = 120)
         {
             int threadId = Thread.CurrentThread.ManagedThreadId;
 
@@ -71,33 +88,40 @@ namespace Stratis.Features.SQLiteWalletRepository
                 }
             }
 
-            Interlocked.Increment(ref this.waitingThreads);
+            if (wait)
+                Interlocked.Increment(ref this.waitingThreads);
 
-            while (true)
+            try
             {
-                lock (this.lockObj)
+                while (true)
                 {
-                    if (this.lastClaimedByThread == -1)
+                    lock (this.lockObj)
                     {
-                        this.firstClaimedStackTrace = System.Environment.StackTrace;
-                        this.firstClaimedAt = DateTime.Now;
-                        this.firstClaimedPromise = timeoutSeconds;
-                        this.firstClaimedByThread = threadId;
-                        this.lastClaimedByThread = threadId;
-                        this.lockDepth = 1;
-                        break;
+                        if (this.lastClaimedByThread == -1)
+                        {
+                            this.firstClaimedStackTrace = System.Environment.StackTrace;
+                            this.firstClaimedAt = DateTime.Now;
+                            this.firstClaimedPromise = timeoutSeconds;
+                            this.firstClaimedByThread = threadId;
+                            this.lastClaimedByThread = threadId;
+                            this.lockDepth = 1;
+                            break;
+                        }
+                        else if (this.firstClaimedAt.AddSeconds(this.firstClaimedPromise) <= DateTime.Now)
+                            throw new SystemException($"Lock held by thread {this.firstClaimedByThread} has not been released after {this.firstClaimedPromise} seconds as promised. The stack trace when the lock was taken is '{this.firstClaimedStackTrace}`.");
                     }
-                    else if (this.firstClaimedAt.AddSeconds(this.firstClaimedPromise) <= DateTime.Now)
-                        throw new SystemException($"Lock held by thread {this.firstClaimedByThread} has not been released after {this.firstClaimedPromise} seconds as promised. The stack trace when the lock was taken is '{this.firstClaimedStackTrace}`.");
+
+                    if (wait)
+                        Thread.Yield();
+                    else
+                        break;
                 }
-
-                if (wait)
-                    Thread.Yield();
-                else
-                    break;
             }
-
-            Interlocked.Decrement(ref this.waitingThreads);
+            finally
+            {
+                if (wait)
+                    Interlocked.Decrement(ref this.waitingThreads);
+            }
 
             return this.lastClaimedByThread == threadId;
         }
