5ca92c991b6ec079886ef6f5d8aca9f65b84fc97
quantumagi <someguy.fromafrica@gmail.com>
2019-11-20 15:10:45 +1100

    Enable parsing of scientific notation by Money.TryParse

---
+++ src/Stratis.Bitcoin.Tests/Utilities/MoneyTest.cs
@@ -0,0 +1,15 @@
+﻿using NBitcoin;
+using Xunit;
+
+namespace Stratis.Bitcoin.Tests.Utilities
+{
+    public class MoneyTest
+    {
+        [Fact]
+        public void MoneyTryParseCanParseScientificNotation()
+        {
+            Assert.True(Money.TryParse("1e8", out Money value));
+            Assert.Equal(new Money(100000000m, MoneyUnit.BTC), value);
+        }
+    }
+}
