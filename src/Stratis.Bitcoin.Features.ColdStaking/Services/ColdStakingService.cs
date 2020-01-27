0e5c327ba294e84b186eb00bd064212955db23c5
dev0tion <dev0tion@hotmail.com>
2020-01-27 16:48:04 +0100

    Add cold staking info support to SignalR

---
+++ src/Stratis.Bitcoin.Features.ColdStaking/Services/ColdStakingService.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Stratis.Bitcoin.Features.ColdStaking.Models;
+using Stratis.Bitcoin.Features.Wallet.Interfaces;
+using Stratis.Bitcoin.Features.Wallet.Models;
+
+namespace Stratis.Bitcoin.Features.ColdStaking.Services
+{
+    public class ColdStakingService : IColdStakingService
+    {
+        private readonly ColdStakingManager coldStakingManager;
+
+        public ColdStakingService(
+            ColdStakingManager coldStakingManager)
+        {
+            this.coldStakingManager = coldStakingManager;
+        }
+
+        public async Task<GetColdStakingInfoResponse> GetColdStakingInfo(string walletName, CancellationToken cancellationToken)
+        {
+            return await Task.Run(() =>
+            {
+                GetColdStakingInfoResponse coldStakingInfo = this.coldStakingManager.GetColdStakingInfo(walletName);
+
+                return new GetColdStakingInfoResponse
+                {
+                    ColdWalletAccountExists = coldStakingInfo.ColdWalletAccountExists,
+                    HotWalletAccountExists = coldStakingInfo.HotWalletAccountExists
+                };
+            }, cancellationToken);
+        }
+    }
+}
---
+++ src/Stratis.Bitcoin.Features.ColdStaking/Services/IColdStakingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Stratis.Bitcoin.Features.ColdStaking.Models;
+
+namespace Stratis.Bitcoin.Features.ColdStaking.Services
+{
+    public interface IColdStakingService
+    {
+        Task<GetColdStakingInfoResponse> GetColdStakingInfo(string walletName, CancellationToken cancellationToken);
+    }
+}
