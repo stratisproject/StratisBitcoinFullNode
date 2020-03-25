1b14e24235c682268d1f8e0efe43a823f29b4c54
StratisIain <32906542+StratisIain@users.noreply.github.com>
2020-03-25 14:50:20 +0000

    Update StratisMain.cs
    Update ColdStaking Window

--- src/Stratis.Bitcoin.Networks/StratisMain.cs
+++ src/Stratis.Bitcoin.Networks/StratisMain.cs
@@ -90,8 +90,8 @@ namespace Stratis.Bitcoin.Networks
             var bip9Deployments = new StratisBIP9Deployments()
             {
                 [StratisBIP9Deployments.ColdStaking] = new BIP9DeploymentsParameters("ColdStaking", 2,
-                    new DateTime(2018, 12, 1, 0, 0, 0, DateTimeKind.Utc),
-                    new DateTime(2019, 12, 1, 0, 0, 0, DateTimeKind.Utc),
+                    new DateTime(2019, 12, 2, 0, 0, 0, DateTimeKind.Utc),
+                    new DateTime(2020, 12, 2, 0, 0, 0, DateTimeKind.Utc),
                     BIP9DeploymentsParameters.DefaultMainnetThreshold)
             };
 
