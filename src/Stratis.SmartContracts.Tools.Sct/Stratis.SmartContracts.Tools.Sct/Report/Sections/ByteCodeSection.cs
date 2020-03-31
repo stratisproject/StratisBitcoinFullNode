e14f46b718d5b0c17635f7ed3ad252c53ac86690
Rowan de Haas <rowandh@users.noreply.github.com>
2020-03-31 12:06:19 +1100

    Add SCT

---
+++ src/Stratis.SmartContracts.Tools.Sct/Stratis.SmartContracts.Tools.Sct/Report/Sections/ByteCodeSection.cs
@@ -0,0 +1,28 @@
+﻿using NBitcoin;
+using Stratis.SmartContracts.Core.Hashing;
+using Stratis.SmartContracts.Tools.Sct.Report.Elements;
+using System.Collections.Generic;
+
+namespace Stratis.SmartContracts.Tools.Sct.Report.Sections
+{
+    /// <summary>
+    /// Represents the section of a smart contract validation report
+    /// that outputs the bytecode of a compiled contract.
+    /// </summary>
+    public class ByteCodeSection : IReportSection
+    {
+        public IEnumerable<IReportElement> CreateSection(ValidationReportData data)
+        {
+            if (data.CompilationSuccess && data.FormatValid && data.DeterminismValid)
+            {
+                yield return new ReportElement("Hash");
+                byte[] hash = HashHelper.Keccak256(data.CompilationBytes);
+                uint256 hashDisplay = new uint256(hash);
+                yield return new ReportElement(hashDisplay.ToString());
+                yield return new NewLineElement();
+                yield return new ReportElement("ByteCode");
+                yield return new ReportElement(data.CompilationBytes.ToHexString());
+            }
+        }
+    }
+}
