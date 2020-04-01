860c772f00ff751afb3beb61e129f2a5000618e4
Rowan de Haas <rowandh@users.noreply.github.com>
2020-04-01 16:34:29 +1100

    [Unrestricted] Add hackathon SCT (#4162)
    * Add SCT
    
    * Remove references
    
    * Use latest SCT
    
    * Fix references
    
    * Use new validator

---
+++ src/Stratis.SmartContracts.Tools.Sct/Stratis.SmartContracts.Tools.Sct.Tests/Utils/ConsoleOutput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Stratis.SmartContracts.Tools.Sct.Tests.Utils
+{
+    public class ConsoleOutput : IDisposable
+    {
+        private StringWriter stringWriter;
+        private TextWriter originalOutput;
+
+        public ConsoleOutput()
+        {
+            stringWriter = new StringWriter();
+            originalOutput = Console.Out;
+            Console.SetOut(stringWriter);
+        }
+
+        public string GetOuput()
+        {
+            return stringWriter.ToString();
+        }
+
+        public void Dispose()
+        {
+            Console.SetOut(originalOutput);
+            stringWriter.Dispose();
+        }
+    }
+}
---
+++ src/Stratis.SmartContracts.Tools.Sct/Stratis.SmartContracts.Tools.Sct/Validation/Validator.cs
@@ -0,0 +1,146 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Stratis.SmartContracts.CLR;
+using Stratis.SmartContracts.CLR.Compilation;
+using Stratis.SmartContracts.CLR.Validation;
+using Stratis.SmartContracts.Tools.Sct.Report;
+using Stratis.SmartContracts.Tools.Sct.Report.Sections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stratis.SmartContracts.Tools.Sct.Validation
+{
+    [Command(Description = "Validates smart contracts for structure and determinism")]
+    [HelpOption]
+    class Validator
+    {
+        [Argument(0, Description = "The paths of the files to validate",
+            Name = "[FILES]")]
+        public List<string> InputFiles { get; }
+
+        [Option("-sb|--showbytes", CommandOptionType.NoValue,
+            Description = "Show contract compilation bytes")]
+        public bool ShowBytes { get; }
+
+        private int OnExecute(CommandLineApplication app, IConsole console)
+        {
+            if (!this.InputFiles.Any())
+            {
+                app.ShowHelp();
+                return 1;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Smart Contract Validator");
+            Console.WriteLine();
+
+            var formatValidator = new ExpandedFormatPolicyValidator();
+            var warningValidator = new SmartContractWarningValidator();
+
+            var reportData = new List<ValidationReportData>();
+
+            foreach (string file in this.InputFiles)
+            {
+                var validationData = new ValidationReportData
+                {
+                    FileName = file,
+                    CompilationErrors = new List<CompilationError>(),
+                    DeterminismValidationErrors = new List<ValidationResult>(),
+                    FormatValidationErrors = new List<ValidationError>(),
+                    Warnings = new List<Warning>()
+                };
+
+                reportData.Add(validationData);
+
+                ContractCompilationResult compilationResult = CompilationLoader.CompileFromFileOrDirectoryName(file, console);
+
+                // Check if the file was found.
+                if (compilationResult == null)
+                {
+                    return 1;
+                }
+
+                validationData.CompilationSuccess = compilationResult.Success;
+
+                if (!compilationResult.Success)
+                {
+                    Console.WriteLine("Compilation failed!");
+                    Console.WriteLine();
+
+                    validationData.CompilationErrors
+                        .AddRange(compilationResult
+                            .Diagnostics
+                            .Select(d => new CompilationError { Message = d.ToString() }));
+
+                    continue;
+                }
+
+                validationData.CompilationBytes = compilationResult.Compilation;
+
+                Console.WriteLine($"Compilation OK");
+                Console.WriteLine();
+
+                byte[] compilation = compilationResult.Compilation;
+
+                Console.WriteLine("Building ModuleDefinition");
+
+                IContractModuleDefinition moduleDefinition = ContractDecompiler.GetModuleDefinition(compilation, new DotNetCoreAssemblyResolver()).Value;
+
+                Console.WriteLine("ModuleDefinition built successfully");
+                Console.WriteLine();
+
+                Console.WriteLine($"Validating file {file}...");
+                Console.WriteLine();
+
+                SmartContractValidationResult formatValidationResult = formatValidator.Validate(moduleDefinition.ModuleDefinition);
+
+                validationData.FormatValid = formatValidationResult.IsValid;
+
+                validationData
+                    .FormatValidationErrors
+                    .AddRange(formatValidationResult
+                        .Errors
+                        .Select(e => new ValidationError { Message = e.Message }));
+
+                SmartContractValidationResult determinismValidationResult = new SmartContractValidationResult(Enumerable.Empty<ValidationResult>());
+
+                validationData.DeterminismValid = determinismValidationResult.IsValid;
+
+                validationData
+                    .DeterminismValidationErrors
+                    .AddRange(determinismValidationResult.Errors);
+
+                SmartContractValidationResult warningResult = warningValidator.Validate(moduleDefinition.ModuleDefinition);
+
+                validationData
+                    .Warnings
+                    .AddRange(warningResult
+                        .Errors
+                        .Select(e => new Warning { Message = e.Message }));
+            }
+
+            List<IReportSection> reportStructure = new List<IReportSection>();
+            reportStructure.Add(new HeaderSection());
+            reportStructure.Add(new CompilationSection());
+
+            reportStructure.Add(new FormatSection());
+            reportStructure.Add(new DeterminismSection());
+
+            reportStructure.Add(new WarningsSection());
+
+            if (this.ShowBytes)
+                reportStructure.Add(new ByteCodeSection());
+
+            reportStructure.Add(new FooterSection());
+
+            var renderer = new StreamTextRenderer(Console.Out);
+
+            foreach (ValidationReportData data in reportData)
+            {
+                renderer.Render(reportStructure, data);
+            }
+
+            return 1;
+        }
+    }
+}
