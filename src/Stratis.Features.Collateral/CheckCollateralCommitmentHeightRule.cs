5ab19c7d124b2fb3a673f19f50eec8fff241346c
Rowan de Haas <rowandh@users.noreply.github.com>
2020-04-01 19:11:58 +1100

    Disable collateral commitment check

--- src/Stratis.Features.Collateral/CheckCollateralCommitmentHeightRule.cs
+++ src/Stratis.Features.Collateral/CheckCollateralCommitmentHeightRule.cs
@@ -16,21 +16,6 @@ namespace Stratis.Features.Collateral
     {
         public override Task RunAsync(RuleContext context)
         {
-            // The genesis block won't contain any commitment data.
-            if (context.ValidationContext.ChainedHeaderToValidate.Height == 0)
-                return Task.CompletedTask;
-
-            var commitmentHeightEncoder = new CollateralHeightCommitmentEncoder(this.Logger);
-
-            int? commitmentHeight = commitmentHeightEncoder.DecodeCommitmentHeight(context.ValidationContext.BlockToValidate.Transactions.First());
-            if (commitmentHeight == null)
-            {
-                // Every PoA miner on a sidechain network is forced to include commitment data to the blocks mined.
-                // Not having a commitment should always result in a permanent ban of the block.
-                this.Logger.LogTrace("(-)[COLLATERAL_COMMITMENT_HEIGHT_MISSING]");
-                PoAConsensusErrors.CollateralCommitmentHeightMissing.Throw();
-            }
-
             return Task.CompletedTask;
         }
     }
