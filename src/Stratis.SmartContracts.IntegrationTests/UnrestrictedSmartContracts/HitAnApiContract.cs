daff5d8633ffa954ab428f0054c063d4e4573b16
Jordan Andrews <jordandrews@hotmail.com>
2020-04-01 20:44:08 +1100

    Unrestricted final test

--- src/Stratis.SmartContracts.IntegrationTests/UnrestrictedSmartContracts/HitAnApiContract.cs
+++ src/Stratis.SmartContracts.IntegrationTests/UnrestrictedSmartContracts/HitAnApiContract.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http;
+using Newtonsoft.Json;
 using Stratis.SmartContracts;
 
 public class HitAnApiContract : SmartContract
@@ -18,6 +19,32 @@ public class HitAnApiContract : SmartContract
         this.PersistentState.SetInt32("ImportantNumber", (int) result.StatusCode);
 
         return httpResult.Length;
+    }
+
+    public string CallJsonApi()
+    {
+        return StructGetter.GetTitle(1);
+    }
+}
+
+public static class StructGetter
+{
+    public static string GetTitle(int id)
+    {
+        var client = new HttpClient();
+
+        HttpResponseMessage result = client.GetAsync("https://jsonplaceholder.typicode.com/todos/1").Result;
+        string httpResult = result.Content.ReadAsStringAsync().Result;
+
+        Result resultDeserialized = JsonConvert.DeserializeObject<Result>(httpResult);
+
+        return resultDeserialized.Title;
+    }
+
+    public class Result
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
     }
 }
 
