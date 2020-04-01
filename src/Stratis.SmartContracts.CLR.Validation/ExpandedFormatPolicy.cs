0847c6b9dfbd4494faf9678e6848e80d20017c28
Rowan de Haas <rowandh@users.noreply.github.com>
2020-04-01 16:23:03 +1100

    [Unrestricted] Remove whitelist validation (#4161)
    * Remove whitelist validation
    
    * Add expanded format policy
    
    * Use expanded format policy validator

---
+++ src/Stratis.SmartContracts.CLR.Validation/ExpandedFormatPolicy.cs
@@ -0,0 +1,32 @@
+﻿using Stratis.SmartContracts.CLR.Validation.Validators.Method;
+using Stratis.SmartContracts.CLR.Validation.Validators.Module;
+using Stratis.SmartContracts.CLR.Validation.Validators.Type;
+
+namespace Stratis.SmartContracts.CLR.Validation
+{
+    /// <summary>
+    /// Expanded format policy that removes assembly reference validation.
+    /// </summary>
+    public static class ExpandedFormatPolicy
+    {
+        public static ValidationPolicy Default = new ValidationPolicy()
+            .ModuleDefValidator(new ModuleReferenceValidator())
+            .ModuleDefValidator(new ContractToDeployValidator())
+            .TypeDefValidator(new NamespaceValidator())
+            .TypeDefValidator(new StaticConstructorValidator(), t => t.IsContractType())
+            .TypeDefValidator(new GenericTypeValidator(), t => t.IsContractType())
+            .TypeDefValidator(new SingleConstructorValidator(), t => t.IsContractType())
+            .TypeDefValidator(new ConstructorParamValidator(), t => t.IsContractType())
+            .TypeDefValidator(new InheritsSmartContractValidator(), t => t.IsContractType())
+            .TypeDefValidator(new FieldDefinitionValidator(), t => t.IsContractType())
+            .NestedTypeDefValidator(new TypeHasMethodsValidator())
+            .NestedTypeDefValidator(new TypeHasNestedTypesValidator())
+            .NestedTypeDefValidator(new NestedTypeIsValueTypeValidator())
+            .MethodDefValidator(new TryCatchValidator())
+            .MethodDefValidator(new MethodParamValidator())
+            .MethodDefValidator(new GenericMethodValidator())
+            .MethodDefValidator(new PInvokeValidator())
+            .InstructionValidator(new MultiDimensionalArrayValidator())
+            .InstructionValidator(new NewObjValidator());
+    }
+}
---
+++ src/Stratis.SmartContracts.CLR.Validation/ExpandedFormatPolicyValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mono.Cecil;
+
+namespace Stratis.SmartContracts.CLR.Validation
+{
+    /// <summary>
+    /// Validates the format of a smart contract class using the expanded format policy.
+    /// </summary>
+    public class ExpandedFormatPolicyValidator : ISmartContractValidator
+    {
+        public SmartContractValidationResult Validate(ModuleDefinition moduleDefinition)
+        {
+            ValidationPolicy policy = ExpandedFormatPolicy.Default;
+
+            var validator = new ModulePolicyValidator(policy);
+
+            List<ValidationResult> results = validator.Validate(moduleDefinition).ToList();
+
+            return new SmartContractValidationResult(results);
+        }
+    }
+}
