01568f319e80e776ed9ddf5925b388fb8e01a9af
Rowan de Haas <rowandh@users.noreply.github.com>
2020-04-02 22:33:22 +1100

    Hackathon history fixes (#4169)
    * Add SC wallet history endpoint (#4044)
    
    * Fix sc history endpoint query (#4060)

---
+++ src/Stratis.Bitcoin.Features.SmartContracts/Wallet/ChangePayment.cs
@@ -0,0 +1,22 @@
+﻿using Stratis.Bitcoin.Features.Wallet;
+
+namespace Stratis.Bitcoin.Features.SmartContracts.Wallet
+{
+    public class ChangePayment
+    {
+        private readonly SpendingDetails spendingDetails;
+        private readonly PaymentDetails paymentDetails;
+
+        public ChangePayment(SpendingDetails spendingDetails, PaymentDetails paymentDetails)
+        {
+            this.spendingDetails = spendingDetails;
+            this.paymentDetails = paymentDetails;
+        }
+
+        public bool IsChange(TransactionData output)
+        {
+            return this.spendingDetails.TransactionId == output.Id &&
+                   this.paymentDetails.OutputIndex == output.Index;
+        }
+    }
+}
