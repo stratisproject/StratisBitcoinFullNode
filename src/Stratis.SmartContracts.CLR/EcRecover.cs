0882a4873db07e7736c0131e240f5e6912b79792
Jordan Andrews <jordandrews@hotmail.com>
2020-06-22 14:09:52 +1000

    Test demonstrating ECRECOVER code

---
+++ src/Stratis.SmartContracts.CLR/EcRecover.cs
@@ -0,0 +1,38 @@
+﻿using NBitcoin;
+using NBitcoin.Crypto;
+using Stratis.SmartContracts.Core.Hashing;
+
+namespace Stratis.SmartContracts.CLR
+{
+    /// <summary>
+    /// Holds logic for the equivalent of the ECRECOVER opcode.
+    ///
+    /// This is static for now but when we know more about how we are going to use it we will adjust as necessary.
+    /// </summary>
+    public static class EcRecover
+    {
+        // TODO: Not sure what this is yet.
+        private const int RecId = 0;
+
+        public static ECDSASignature SignMessage(Key privateKey, byte[] message)
+        {
+            uint256 hashedUint256 = GetUint256FromMessage(message);
+            return privateKey.Sign(hashedUint256);
+        }
+
+        public static Address GetAddressFromSignatureAndMessage(byte[] signature, byte[] message, Network network)
+        {
+            // TODO: Error handling for incorrect signature format etc.
+
+            uint256 hashedUint256 = GetUint256FromMessage(message);
+            ECDSASignature loadedSignature = new ECDSASignature(signature);
+            PubKey pubKey = ECKeyUtils.RecoverFromSignature(RecId, loadedSignature, hashedUint256, true);
+            return pubKey.GetAddress(network).ToString().ToAddress(network);
+        }
+
+        private static uint256 GetUint256FromMessage(byte[] message)
+        {
+            return new uint256(HashHelper.Keccak256(message));
+        }
+    }
+}
---
+++ src/Stratis.SmartContracts.IntegrationTests/ECRecoverTests.cs
@@ -0,0 +1,43 @@
+﻿using NBitcoin;
+using NBitcoin.Crypto;
+using Stratis.SmartContracts.CLR;
+using Stratis.SmartContracts.Networks;
+using Xunit;
+using Key = NBitcoin.Key;
+
+namespace Stratis.SmartContracts.IntegrationTests
+{
+    public class ECRecoverTests
+    {
+        private readonly Network network;
+
+        public ECRecoverTests()
+        {
+            this.network = new SmartContractsPoARegTest();
+        }
+
+        // 2 things to test: 
+
+        // 1) That we have the ECDSA code and can make it available.
+
+        [Fact]
+        public void CanSignAndRetrieveSender()
+        {
+            Key privateKey = new Key();
+            Address address = privateKey.PubKey.GetAddress(this.network).ToString().ToAddress(this.network);
+
+            byte[] message = new byte[] { 0x69, 0x76, 0xAA };
+
+            ECDSASignature offChainSignature = EcRecover.SignMessage(privateKey, message);
+
+            Address recoveredAddress = EcRecover.GetAddressFromSignatureAndMessage(offChainSignature.ToDER(), message, this.network);
+
+            Assert.Equal(address, recoveredAddress);
+        }
+
+        // 2) That we can enable the method in new contracts without affecting the older contracts
+
+        // TODO
+
+    }
+}
