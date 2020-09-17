e1df237f067efa85bcb50465a611bae21262d778
Kevin Loubser <zeptin@gmail.com>
2020-09-17 12:19:29 +0200

    Allow transaction building with only an OP_RETURN

--- src/Stratis.Bitcoin.Features.Wallet/Models/RequestModels.cs
+++ src/Stratis.Bitcoin.Features.Wallet/Models/RequestModels.cs
@@ -344,8 +344,6 @@ namespace Stratis.Bitcoin.Features.Wallet.Models
         /// recipient will receive in STRAT (or a sidechain coin). If the transaction was realized,
         /// both the values would be used to create the UTXOs for the transaction recipients.
         /// </summary> 
-        [Required(ErrorMessage = "A list of recipients is required.")]
-        [MinLength(1)]
         public List<RecipientModel> Recipients { get; set; }
 
         /// <summary>
